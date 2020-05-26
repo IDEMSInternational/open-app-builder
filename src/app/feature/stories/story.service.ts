59745020037c75c18a39e0e06e3e31ca796cbf8f
maxwell fundi <maxwellfundi@live.com>
2020-05-26 12:31:26 +0300

    Added mama is sick story

--- src/app/feature/stories/story.service.ts
+++ src/app/feature/stories/story.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, from } from 'rxjs';
 import { medicationRoutineStory } from './story-data/medication-routine-story';
 import { puttingOutFireStory } from './story-data/putting-out-fire-story';
 import { Story } from './story.model';
 import { breadAndBullyStory } from './story-data/bread-and-bully-story';
 import { bibiSaraStory } from './story-data/bibi-sara-story';
 import { whatsappNegative } from './story-data/whatsapp-negative';
+import { mamaissick } from './story-data/mama-is-sick';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,6 @@ export class StoryService {
   constructor() { }
 
   getStoryList(): Observable<Story[]> {
-    return of([medicationRoutineStory, puttingOutFireStory, breadAndBullyStory, bibiSaraStory, whatsappNegative]);
+    return of([medicationRoutineStory, puttingOutFireStory, breadAndBullyStory, bibiSaraStory, whatsappNegative, mamaissick]);
   }
 }
