393a777b8e809d27cd8a89885bcb8ea77b94c519
Michael Clapham <michaeljclapham@gmail.com>
2020-06-03 13:21:04 +0100

    Add about page

---
+++ src/app/feature/about/about.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'plh-about',
+  templateUrl: './about.page.html',
+  styleUrls: ['./about.page.scss'],
+})
+export class AboutPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
