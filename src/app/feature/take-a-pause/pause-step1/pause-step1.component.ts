2768feeaa61ae074702e20a4918d1ab1af3edc09
Shiundumichael <shimichael9@gmail.com>
2020-06-04 14:37:58 +0300

    have started working on take a pause step one

---
+++ src/app/feature/take-a-pause/pause-step1/pause-step1.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'plh-pause-step1',
+  templateUrl: './pause-step1.component.html',
+  styleUrls: ['./pause-step1.component.scss'],
+})
+export class PauseStep1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/feature/take-a-pause/take-a-pause.module.ts
+++ src/app/feature/take-a-pause/take-a-pause.module.ts
@@ -8,6 +8,7 @@ import { TakeAPausePageRoutingModule } from './take-a-pause-routing.module';
 
 import { TakeAPausePage } from './take-a-pause.page';
 import { PauseStep0Component } from './pause-step0/pause-step0.component';
+import { PauseStep1Component } from './pause-step1/pause-step1.component';
 
 @NgModule({
   imports: [
@@ -18,7 +19,8 @@ import { PauseStep0Component } from './pause-step0/pause-step0.component';
   ],
   declarations: [
     TakeAPausePage,
-    PauseStep0Component
+    PauseStep0Component, 
+    PauseStep1Component
   ]
 })
 export class TakeAPausePageModule {}
