4bb9aa25aff43a436138ce73a969b9ab6d5f76b3
Barbra2020 <bmootian@gmail.com>
2020-06-05 10:33:02 +0300

    generating component pause step two component

--- src/app/feature/take-a-pause/take-a-pause.module.ts
+++ src/app/feature/take-a-pause/take-a-pause.module.ts
@@ -8,6 +8,7 @@ import { TakeAPausePageRoutingModule } from './take-a-pause-routing.module';
 
 import { TakeAPausePage } from './take-a-pause.page';
 import { PauseStep0Component } from './pause-step0/pause-step0.component';
+import { PauseStep2Component } from '../pause-step2/pause-step2.component';
 
 @NgModule({
   imports: [
@@ -18,7 +19,8 @@ import { PauseStep0Component } from './pause-step0/pause-step0.component';
   ],
   declarations: [
     TakeAPausePage,
-    PauseStep0Component
+    PauseStep0Component,
+    PauseStep2Component
   ]
 })
 export class TakeAPausePageModule {}
