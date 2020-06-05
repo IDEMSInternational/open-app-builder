27823c541f454ec3a073d09e182d9896285fa030
luyali020 <praiseebenezer035@gmail.com>
2020-06-05 10:53:29 +0300

    I have created the link component

---
+++ src/app/feature/pause-remote/pause-remote.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'plh-pause-remote',
+  templateUrl: './pause-remote.component.html',
+  styleUrls: ['./pause-remote.component.scss'],
+})
+export class PauseRemoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
