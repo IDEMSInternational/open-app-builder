637f2e2f4da083fc487118d78df5cee6c3c43f86
luyali020 <praiseebenezer035@gmail.com>
2020-06-08 10:03:47 +0300

    I have created the link component

---
+++ src/app/feature/take-a-pause/pause-think/pause-think.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'plh-pause-think',
+  templateUrl: './pause-think.component.html',
+  styleUrls: ['./pause-think.component.scss'],
+})
+export class PauseThinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
