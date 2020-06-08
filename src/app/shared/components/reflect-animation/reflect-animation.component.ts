66239fc0298c7de27dd32897007996239483415c
Michael Clapham <michaeljclapham@gmail.com>
2020-06-08 01:41:03 +0100

    TG-260 Added start of animation for reflecting blob.

---
+++ src/app/shared/components/reflect-animation/reflect-animation.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'plh-reflect-animation',
+  templateUrl: './reflect-animation.component.html',
+  styleUrls: ['./reflect-animation.component.scss'],
+})
+export class ReflectAnimationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
