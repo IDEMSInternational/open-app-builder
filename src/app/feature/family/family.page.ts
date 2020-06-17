1b76cf96d039a29ee6078ef4a67846452586af3a
Michael Clapham <michaeljclapham@gmail.com>
2020-06-17 13:55:55 +0100

    Added view family members screen

---
+++ src/app/feature/family/family.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FamilyService } from 'src/app/shared/services/family/family.service';
+import { FamilyMember } from 'src/app/shared/model/family.model';
+
+@Component({
+  selector: 'plh-family',
+  templateUrl: './family.page.html',
+  styleUrls: ['./family.page.scss'],
+})
+export class FamilyPage implements OnInit {
+
+  familyMembers: FamilyMember[];
+
+  constructor(private familyService: FamilyService) {
+    this.familyService.addFamilyMember({
+      name: "Michael",
+      ageRange: "adult",
+      age: 26
+    });
+  }
+
+  ngOnInit() {
+    this.familyMembers = this.familyService.getFamilyMembers();
+  }
+
+}
