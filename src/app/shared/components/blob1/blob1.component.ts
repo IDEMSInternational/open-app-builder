fe4cd8f81fbe7f04ecb7e781ea3054328b8395a3
Michael Clapham <michaeljclapham@gmail.com>
2020-06-30 15:26:46 +0100

    Customising blob color using Input.

--- src/app/shared/components/blob1/blob1.component.ts
+++ src/app/shared/components/blob1/blob1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'plh-blob1',
@@ -7,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Blob1Component implements OnInit {
 
+  @Input()
+  bodyColor = "#00A1CD";
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
 }
