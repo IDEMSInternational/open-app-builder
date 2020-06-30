a1f3972d23569e3961d4198347652dfee07bf5c4
Shiundumichael <shimichael9@gmail.com>
2020-06-30 15:35:07 +0300

    Moved blob into component.

--- src/app/feature/family/family.module.ts
+++ src/app/feature/family/family.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { FamilyPageRoutingModule } from './family-routing.module';
 
 import { FamilyPage } from './family.page';
+import { Blob1Component } from 'src/app/shared/components/blob1/blob1.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { FamilyPage } from './family.page';
     IonicModule,
     FamilyPageRoutingModule
   ],
-  declarations: [FamilyPage]
+  declarations: [FamilyPage, Blob1Component]
 })
 export class FamilyPageModule {}
---
+++ src/app/shared/components/blob1/blob1.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'plh-blob1',
+  templateUrl: './blob1.component.html',
+  styleUrls: ['./blob1.component.scss'],
+})
+export class Blob1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
