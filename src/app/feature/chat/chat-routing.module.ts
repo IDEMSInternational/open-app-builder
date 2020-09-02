63c8dcd2654e542702cc137f8de775fdb71f5bd3
Michael Clapham <michaeljclapham@gmail.com>
2020-09-03 00:39:06 +0100

    Added stressed multi hand blob to chat screen.

--- src/app/feature/chat/chat-routing.module.ts
+++ src/app/feature/chat/chat-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ChatPage } from './chat.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 const routes: Routes = [
   {
