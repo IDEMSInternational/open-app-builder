86a527c8673998c77bc8485d9ec0a2d114e4e064
Michael Clapham <michaeljclapham@gmail.com>
2020-09-18 16:22:23 +0100

    Fix prod receive url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   rapidPro: {
-    receiveURL: "https://rapidpro.idems.international/c/fcm/00a59f43-7da6-4497-837d-52d111ca4560/receive",
+    receiveUrl: "https://rapidpro.idems.international/c/fcm/00a59f43-7da6-4497-837d-52d111ca4560/receive",
     contactRegisterUrl: "https://rapidpro.idems.international/c/fcm/00a59f43-7da6-4497-837d-52d111ca4560/register"
   }
 };
