f72dead074c85f3c13e14382464232df23b73cce
chrismclarke <chris.m.clarke@live.co.uk>
2020-09-23 15:28:47 -0700

    build out calendar module

--- src/app/feature/calendar/calendar.page.ts
+++ src/app/feature/calendar/calendar.page.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { DbService, IDBEvent } from "src/app/shared/services/db/db.service";
+import { EventService } from "src/app/shared/services/event/event.service";
+import { LocalNotificationService } from "src/app/shared/services/notification/local-notification.service";
 
 @Component({
-  selector: 'plh-calendar',
-  templateUrl: './calendar.page.html',
-  styleUrls: ['./calendar.page.scss'],
+  selector: "plh-calendar",
+  templateUrl: "./calendar.page.html",
+  styleUrls: ["./calendar.page.scss"],
 })
-export class CalendarPage implements OnInit {
-
-  constructor() { }
+export class CalendarPage implements OnInit, OnDestroy {
+  listeners$: Subscription;
+  constructor(
+    private localNotificationService: LocalNotificationService,
+    private eventService: EventService,
+    private dbService: DbService
+  ) {}
 
   ngOnInit() {
+    this._addEventListeners();
+    this.loadCalendar();
+  }
+  ngOnDestroy() {
+    this._removeEventListeners();
+  }
+  async loadCalendar() {
+    console.log("loading calendar");
+    const rows = await this.dbService.db.table("calendar").toArray();
+    console.log("rows", rows);
   }
 
+  private _addEventListeners() {}
+
+  private _removeEventListeners() {
+    this.listeners$.unsubscribe();
+  }
+
+  /**
+   * Add scheduled notification and update database records
+   */
+  async scheduleNotification() {
+    await this.localNotificationService.scheduleNotification();
+    const event: IDBEvent = {
+      topic: "DB",
+      subtopic: "calendar",
+      payload: { data: { name: "test_event" }, operation: "CREATE" },
+    };
+    this.eventService.publish(event).subscribe(() => this.loadCalendar());
+  }
 }
