9faaf585ce9b6428855241249af41cc60b656b95
chrismclarke <chris.m.clarke@live.co.uk>
2020-09-24 12:50:39 -0700

    add intro tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 
-import { Platform, MenuController } from "@ionic/angular";
+import { Platform, MenuController, ModalController } from "@ionic/angular";
 import { Router } from "@angular/router";
 import { Plugins, StatusBarStyle, Capacitor } from "@capacitor/core";
 const { SplashScreen } = Plugins;
 import { NotificationService } from "./shared/services/notification/notification.service";
 import { DbService } from "./shared/services/db/db.service";
+import { IntroTutorialPage } from "./feature/intro-tutorial/intro-tutorial.page";
 
 @Component({
   selector: "app-root",
@@ -13,18 +14,21 @@ import { DbService } from "./shared/services/db/db.service";
   styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
+  skipTutorial: boolean;
   constructor(
     private platform: Platform,
     private menuController: MenuController,
     private router: Router,
     private notifications: NotificationService,
-    private dbService: DbService
+    private dbService: DbService,
+    private modalCtrl: ModalController
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      this.showTutorial();
       this.dbService.init();
       this.menuController.enable(true, "main-side-menu");
 
@@ -33,6 +37,25 @@ export class AppComponent {
         this.notifications.init();
       }
     });
+  }
+
+  async showTutorial() {
+    // skip tutorial if already seen, or viewing policy pages direct (e.g. google bot)
+    this.skipTutorial =
+      ["/privacy", "/app-terms"].includes(location.pathname) ||
+      localStorage.getItem("tutorialComplete")
+        ? true
+        : false;
+    if (!this.skipTutorial) {
+      const modal = await this.modalCtrl.create({
+        component: IntroTutorialPage,
+        backdropDismiss: false,
+        componentProps: { isModal: true },
+      });
+      await modal.present();
+      await modal.onDidDismiss();
+      this.skipTutorial = true;
+    }
   }
 
   clickOnMenuItem(id: string) {
