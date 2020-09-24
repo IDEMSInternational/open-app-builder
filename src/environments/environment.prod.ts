0abb98305c268606ef07e931cd8e2b81bed0220c
chrismclarke <chris.m.clarke@live.co.uk>
2020-09-24 12:47:48 -0700

    add firebase and auth libs, bindings, service, env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,12 @@
+import FIREBASE_CONFIG from "./firebaseConfig";
+
 export const environment = {
   production: true,
   rapidPro: {
-    receiveUrl: "https://rapidpro.idems.international/c/fcm/af89b1b8-e8b7-4e4b-b478-817849dd7982/receive",
-    contactRegisterUrl: "https://rapidpro.idems.international/c/fcm/af89b1b8-e8b7-4e4b-b478-817849dd7982/register"
-  }
+    receiveUrl:
+      "https://rapidpro.idems.international/c/fcm/af89b1b8-e8b7-4e4b-b478-817849dd7982/receive",
+    contactRegisterUrl:
+      "https://rapidpro.idems.international/c/fcm/af89b1b8-e8b7-4e4b-b478-817849dd7982/register",
+  },
+  FIREBASE_CONFIG,
 };
