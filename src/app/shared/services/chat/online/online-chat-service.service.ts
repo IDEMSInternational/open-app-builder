b6683143739dd6672926f1f291c87ff2376ccf40
Michael Clapham <michaeljclapham@gmail.com>
2020-10-05 21:21:06 +0100

    Wrap notification service

--- src/app/shared/services/chat/online/online-chat-service.service.ts
+++ src/app/shared/services/chat/online/online-chat-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, from, throwError } from 'rxjs';
+import { IRapidProMessage, NotificationService } from '../../notification/notification.service';
 import { ChatMessage } from '../chat-msg.model';
 import { ChatService } from '../chat.service';
 
@@ -8,11 +9,39 @@ import { ChatService } from '../chat.service';
 })
 export class OnlineChatServiceService implements ChatService {
 
-  constructor() { }
+  public messages$: BehaviorSubject<ChatMessage[]> = new BehaviorSubject([]);
+
+  constructor(private notificationService: NotificationService) {
+    this.notificationService.messages$.subscribe((messages) => {
+      let chatMessages = messages.map(this.convertFromRapidProMsg);
+      this.messages$.next(chatMessages);
+    },
+    (err) => {
+      this.messages$.error(err);
+    });
+  }
 
   public sendMessage(message: ChatMessage): Observable<any> {
-    return throwError("Not implemented yet");
+    return from(this.notificationService.sendRapidproMessage(message.text));
   }
-  
-  public messages$: Subject<ChatMessage>;
+
+  private convertFromRapidProMsg(rpMsg: IRapidProMessage): ChatMessage {
+    let quickReplies: string[] = [];
+    if (Array.isArray(rpMsg.quick_replies)) {
+      quickReplies = rpMsg.quick_replies
+    } else {
+      try {
+        quickReplies = JSON.parse(rpMsg.quick_replies);
+      } catch (ex) {
+        quickReplies = [];
+      }
+    }
+    return {
+      text: rpMsg.message,
+      sender: "bot",
+      dateReceived: new Date(),
+      responseOptions: quickReplies.map((choice) => ({ text: choice }))
+    };
+  }
+
 }
