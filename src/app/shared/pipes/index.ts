e97324253c8a37cfe0101501b24c88189d4f36dd
Chris <chris.m.clarke@live.co.uk>
2020-11-09 15:03:04 +0000

    reminder due date picker and bindigns

--- src/app/shared/pipes/index.ts
+++ src/app/shared/pipes/index.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 
 import { ObjectValuesPipe } from "./objectValues.pipe";
+import { IsoDateToDateInputPipe } from "./isoDateToDateInput.pipe";
+
+const Pipes = [ObjectValuesPipe, IsoDateToDateInputPipe];
 
 @NgModule({
+  declarations: Pipes,
   imports: [],
-  exports: [ObjectValuesPipe],
-  declarations: [ObjectValuesPipe],
-  providers: [],
+  exports: Pipes,
 })
 export class SharedPipesModule {}
