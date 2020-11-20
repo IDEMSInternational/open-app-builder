d00c33eecaf6477edd3dccd61beee1b428f6622f
chrismclarke <chris.m.clarke@live.co.uk>
2020-11-19 20:33:23 -0800

    tidying

--- src/app/feature/goals/services/task-actions.service.ts
+++ src/app/feature/goals/services/task-actions.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { AlertController, ModalController } from "@ionic/angular";
 import { DbService } from "src/app/shared/services/db/db.service";
-import { ChatPage } from "../../chat/chat.page";
+import { ChatPage } from "../../chat/components/chat.page";
 import { ITaskAction, ITaskActionStatus } from "../models/goals.model";
 
 @Injectable({
