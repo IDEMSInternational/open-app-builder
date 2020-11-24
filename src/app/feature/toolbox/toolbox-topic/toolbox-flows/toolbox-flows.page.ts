266d92f1dff1435ac420d3a70836942b33342ff5
Mike Nsubuga <nsubugamike021@gmail.com>
2020-11-24 21:23:50 +0300

    collapsing using details

--- src/app/feature/toolbox/toolbox-topic/toolbox-flows/toolbox-flows.page.ts
+++ src/app/feature/toolbox/toolbox-topic/toolbox-flows/toolbox-flows.page.ts
@@ -3,18 +3,19 @@ import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
 import { LocalNotificationService } from 'src/app/shared/services/notification/local-notification.service';
 import { ToolboxTopic, ToolboxTopicType, ToolboxSection } from 'src/app/shared/services/toolbox/toolbox.model';
 import { ToolboxService } from 'src/app/shared/services/toolbox/toolbox.service';
+import {OpenClose} from 'src/app/feature/goals/animations'
 
 @Component({
   selector: 'plh-toolbox-flows',
   templateUrl: './toolbox-flows.page.html',
   styleUrls: ['./toolbox-flows.page.scss'],
+  animations: [OpenClose]
 })
 export class ToolboxFlowsPage implements OnInit {
 
   type: ToolboxTopicType = "ONE_ON_ONE_TIME";
   topic: ToolboxTopic
   flow
-
 
   constructor(private activatedRoute: ActivatedRoute, private toolboxService: ToolboxService, private router: Router, 
     private localNotificationService: LocalNotificationService) {
