21d5f28049fd90f8e217d13a84f386cee69dcc43
chrismclarke <chris.m.clarke@live.co.uk>
2020-11-25 14:06:51 -0800

    minor updates

--- src/app/feature/toolbox/components/toolbox-topic/toolbox-flows/toolbox-flows.page.ts
+++ src/app/feature/toolbox/components/toolbox-topic/toolbox-flows/toolbox-flows.page.ts
@@ -1,36 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { LocalNotificationService } from 'src/app/shared/services/notification/local-notification.service';
-import { ToolboxTip } from 'src/app/feature/toolbox/models/toolbox.model';
-import { ToolboxService } from 'src/app/feature/toolbox/services/toolbox.service';
-import {OpenClose} from 'src/app/feature/goals/animations'
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { ToolboxTip } from "src/app/feature/toolbox/models/toolbox.model";
+import { ToolboxService } from "src/app/feature/toolbox/services/toolbox.service";
+import { OpenClose } from "src/app/feature/goals/animations";
 
 @Component({
-  selector: 'plh-toolbox-flows',
-  templateUrl: './toolbox-flows.page.html',
-  styleUrls: ['../toolbox-topic.page.scss'],
-  animations: [OpenClose]
+  selector: "plh-toolbox-flows",
+  templateUrl: "./toolbox-flows.page.html",
+  styleUrls: ["../toolbox-topic.page.scss"],
+  animations: [OpenClose],
 })
 export class ToolboxFlowsPage implements OnInit {
+  Flow_Name: string;
+  flow: ToolboxTip;
 
-  Flow_Name: string
-  flow: ToolboxTip
-
-  constructor(private activatedRoute: ActivatedRoute, private toolboxService: ToolboxService, private router: Router, 
-    private localNotificationService: LocalNotificationService) {
-      this.activatedRoute.params.subscribe((params) => {
-        this.Flow_Name = params.flow_name
-        console.log("Flow name", this.Flow_Name)
-        this.toolboxService.getFlows(this.Flow_Name).subscribe((module)=>{
-          this.flow = module
-          console.log("Modules", this.flow)
-
-        })
+  constructor(private activatedRoute: ActivatedRoute, private toolboxService: ToolboxService) {
+    this.activatedRoute.params.subscribe((params) => {
+      this.Flow_Name = params.flow_name;
+      this.toolboxService.getFlows(this.Flow_Name).subscribe((module) => {
+        this.flow = module;
+        console.log("flow", this.flow);
       });
-    }
-
-  ngOnInit() {
+    });
   }
 
-  
+  ngOnInit() {}
 }
--- src/app/feature/toolbox/components/toolbox-topic/toolbox-topic.page.ts
+++ src/app/feature/toolbox/components/toolbox-topic/toolbox-topic.page.ts
@@ -1,42 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
-import { title } from 'process';
-import { LocalNotificationService } from 'src/app/shared/services/notification/local-notification.service';
-import { ToolboxTopic, ToolboxTopicType, ToolboxSection, ToolboxTip } from 'src/app/feature/toolbox/models/toolbox.model';
-import { ToolboxService } from 'src/app/feature/toolbox/services/toolbox.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ToolboxTopicType, ToolboxTip } from "src/app/feature/toolbox/models/toolbox.model";
+import { ToolboxService } from "src/app/feature/toolbox/services/toolbox.service";
 
 @Component({
-  selector: 'plh-toolbox-topic',
-  templateUrl: './toolbox-topic.page.html',
-  styleUrls: ['../toolbox.page.scss'],
+  selector: "plh-toolbox-topic",
+  templateUrl: "./toolbox-topic.page.html",
+  styleUrls: ["../toolbox.page.scss"],
 })
 export class ToolboxTopicPage implements OnInit {
-
   type: ToolboxTopicType;
   modules: ToolboxTip[];
-  title: string = null
+  title: string = null;
 
-  constructor(private activatedRoute: ActivatedRoute, private toolboxService: ToolboxService, private router: Router, 
-    private localNotificationService: LocalNotificationService) {
-     
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private toolboxService: ToolboxService,
+    private router: Router
+  ) {
     this.activatedRoute.params.subscribe((params) => {
       this.type = params["type"] as ToolboxTopicType;
-      console.log("check params", params)
-      this.toolboxService.getModules(this.type).subscribe((module)=>{
-        this.modules = module
-        this.title = this.replaceTitle(module[0].Module)
-        console.log("Modules", this.modules)
-      })
+      this.toolboxService.getModules(this.type).subscribe((module) => {
+        this.modules = module;
+        this.title = this.replaceTitle(module[0].Module);
+      });
     });
   }
 
-  ngOnInit() {
-  }
-  onClickFlow(module: ToolboxTip){
-    this.router.navigate([module.Flow_Name],{relativeTo: this.activatedRoute})
+  ngOnInit() {}
+  onClickFlow(module: ToolboxTip) {
+    this.router.navigate([module.Flow_Name], { relativeTo: this.activatedRoute });
   }
 
-  replaceTitle(title: string){
+  replaceTitle(title: string) {
     return title.replace(/_/g, " ");
   }
 }
