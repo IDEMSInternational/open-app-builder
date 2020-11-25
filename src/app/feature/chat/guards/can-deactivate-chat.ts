89f8e282d0da23619e97180dfd10bb654f203e57
Michael Clapham <michaeljclapham@gmail.com>
2020-11-25 14:43:48 +0000

    Don't show alert modal once flow is finished

--- src/app/feature/chat/guards/can-deactivate-chat.ts
+++ src/app/feature/chat/guards/can-deactivate-chat.ts
@@ -4,10 +4,11 @@ import { AlertController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { ChatPage } from '../components/chat.page';
 import { ChatActionType } from '../models';
+import { OfflineChatService } from '../services/offline/offline-chat.service';
 
 @Injectable()
 export class CanDeactivateChat implements CanDeactivate<ChatPage> {
-  constructor(private alertController: AlertController) {}
+  constructor(private alertController: AlertController, private chatService: OfflineChatService) {}
 
   async canDeactivate(
     component: ChatPage,
@@ -15,13 +16,21 @@ export class CanDeactivateChat implements CanDeactivate<ChatPage> {
     currentState: RouterStateSnapshot,
     nextState: RouterStateSnapshot
   ): Promise<boolean> {
-    /* If navigating away because of a chat action don't show popup */
+    /* If navigating away because of a chat action don't show alert */
     if (component.lastReceivedMsg && component.lastReceivedMsg.actions) {
       const navigationAction = component.lastReceivedMsg.actions.find((action) => action.type === ChatActionType.NAVIGATE);
       if (navigationAction) {
         return true;
       }
     }
+
+    /* If flow has finished then don't show alert */
+    const flowStatusEvents = this.chatService.flowStatus$.getValue();
+    const latestFlowEvent = flowStatusEvents[flowStatusEvents.length - 1];
+    if (latestFlowEvent.status === "completed") {
+      return true;
+    }
+
     const alert = await this.alertController.create({
         header: "Are you sure you want to leave?",
         message: "If you leave part way through you will loose your progress",
