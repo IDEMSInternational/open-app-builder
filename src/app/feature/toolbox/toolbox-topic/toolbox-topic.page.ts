20b73ba0221557ed97bcb388d72294caa56c1613
Mike Nsubuga <nsubugamike021@gmail.com>
2020-11-25 10:59:32 +0300

    switched data structure for toolbox-page

--- src/app/feature/toolbox/toolbox-topic/toolbox-topic.page.ts
+++ src/app/feature/toolbox/toolbox-topic/toolbox-topic.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
 import { title } from 'process';
 import { LocalNotificationService } from 'src/app/shared/services/notification/local-notification.service';
-import { ToolboxTopic, ToolboxTopicType, ToolboxSection } from 'src/app/shared/services/toolbox/toolbox.model';
+import { ToolboxTopic, ToolboxTopicType, ToolboxSection, ToolboxTip } from 'src/app/shared/services/toolbox/toolbox.model';
 import { ToolboxService } from 'src/app/shared/services/toolbox/toolbox.service';
 
 @Component({
@@ -14,14 +14,19 @@ export class ToolboxTopicPage implements OnInit {
 
   type: ToolboxTopicType = "ONE_ON_ONE_TIME";
   topic: ToolboxTopic;
+  modules: ToolboxTip[];
 
   constructor(private activatedRoute: ActivatedRoute, private toolboxService: ToolboxService, private router: Router, 
     private localNotificationService: LocalNotificationService) {
-      this.toolboxService.getTips().subscribe((tips)=>{
-        console.log("new tips", tips)
-      })
+     
     this.activatedRoute.params.subscribe((params) => {
       this.type = params["type"] as ToolboxTopicType;
+
+      this.toolboxService.getModules(this.type).subscribe((module)=>{
+        this.modules = module
+        console.log("Modules", this.modules)
+      })
+
       this.toolboxService.getTopic(this.type).subscribe((topic) => {
         this.topic = topic;
         console.log("topics ", this.topic)
