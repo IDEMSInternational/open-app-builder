07dc32c6f9d58a32dff4a7adba0c0f7e6913cf2f
Michael Clapham <michaeljclapham@gmail.com>
2020-12-09 17:17:39 +0000

    Show module steps

--- src/app/feature/module_page/flow-components/components/step_item.ts
+++ src/app/feature/module_page/flow-components/components/step_item.ts
@@ -1,10 +1,18 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { FlowTypes } from 'src/app/shared/model/flowTypes';
 
 @Component({
   selector: "module-list-flow-step-item",
-  template: `<div>Step Item</div>`,
+  template: `<div class="step-item-container">
+      <div class="step-item-checkbox"></div>
+      <div class="step-item-button">{{row.text}}</div>
+    </div>`,
+  styleUrls: ["./flow-components-common.scss", "./step_item.scss"]
 })
 export class StepItemFlowComponent implements OnInit {
+  
+  @Input() row: FlowTypes.Module_pageRow;
+  @Input() flow: FlowTypes.Module_page;
   constructor() {}
 
   ngOnInit() {}
--- src/app/feature/module_page/flow-components/components/title.ts
+++ src/app/feature/module_page/flow-components/components/title.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { FlowTypes } from 'src/app/shared/model/flowTypes';
 
 @Component({
   selector: "module-list-flow-title",
-  template: `<div>Title</div>`,
+  template: `<h1>{{row.text}}</h1>`,
+  styleUrls: ["./flow-components-common.scss"]
 })
 export class TitleFlowComponent implements OnInit {
+
+  @Input() row: FlowTypes.Module_pageRow;
+  @Input() flow: FlowTypes.Module_page;
+  
   constructor() {}
 
   ngOnInit() {}
