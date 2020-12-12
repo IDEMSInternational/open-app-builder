0e7b68f5dc175811d78421d2b683585fa443e56e
Michael Clapham <michaeljclapham@gmail.com>
2020-12-12 12:53:22 +0000

    Open initial flow if first time on home screen

--- src/app/feature/module_list/module-list.ts
+++ src/app/feature/module_list/module-list.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Params } from "@angular/router";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 import { FlowTypes } from "scripts/types";
 import { IAppSkin } from "src/app/shared/model";
 import { MODULE_LIST } from "src/app/shared/services/data/data.service";
+import { LocalStorageService } from 'src/app/shared/services/local-storage/local-storage.service';
 
 @Component({
   selector: "plh-module-list",
@@ -13,14 +14,24 @@ export class ModuleListComponent implements OnInit {
   dataLoaded = false;
   appSkin: IAppSkin = "MODULE_FOCUS_SKIN";
   moduleListRow: FlowTypes.Module_listRow;
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private localStorageService: LocalStorageService) {}
 
   ngOnInit() {
+    this.openInitialFlowIfFirstTime();
     this.setDefaultModule();
   }
   async setDefaultModule() {
     // TODO
     this.moduleListRow = MODULE_LIST[0].rows[0];
     this.dataLoaded = true;
+  }
+
+  private openInitialFlowIfFirstTime() {
+    if (!this.localStorageService.getBoolean(LocalStorageService.OPENED_APP_BEFORE)) {
+      this.localStorageService.setBoolean(LocalStorageService.OPENED_APP_BEFORE, true);
+      this.router.navigateByUrl("/chat/flow/first_app_opening");
+    }
   }
 }
