ce8cb767c96fec2c8bacbf2ad9e7adc39507244b
chrismclarke <chris.m.clarke@live.co.uk>
2020-12-21 15:03:12 -0800

    code tidying

--- src/app/feature/chat/components/chat-responses/chat-responses.component.ts
+++ src/app/feature/chat/components/chat-responses/chat-responses.component.ts
@@ -1,17 +1,15 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { SettingsService } from 'src/app/pages/settings/settings.service';
-import { LocalStorageService } from 'src/app/shared/services/local-storage/local-storage.service';
-import { ChatMessage, ChatResponseOption } from '../../models';
-import { ResponsesModalComponent } from './responses-modal/responses-modal.component';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { SettingsService } from "src/app/pages/settings/settings.service";
+import { ChatMessage, ChatResponseOption } from "../../models";
+import { ResponsesModalComponent } from "./responses-modal/responses-modal.component";
 
 @Component({
-  selector: 'plh-chat-responses',
-  templateUrl: './chat-responses.component.html',
-  styleUrls: ['./chat-responses.component.scss'],
+  selector: "plh-chat-responses",
+  templateUrl: "./chat-responses.component.html",
+  styleUrls: ["./chat-responses.component.scss"],
 })
 export class ChatResponsesComponent implements OnChanges {
-
   @Input()
   responseOptions: ChatResponseOption[];
 
@@ -27,24 +25,30 @@ export class ChatResponsesComponent implements OnChanges {
 
   constructor(private modalController: ModalController, private settingsService: SettingsService) {
     this.settingsService.getUserSetting("USE_MODAL_FOR_CHAT_RESPONSES").subscribe((useModal) => {
-      this.useModal = useModal === "true"
+      this.useModal = useModal === "true";
     });
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (this.useModal && changes.responseOptions && changes.responseOptions.currentValue
-      && changes.responseOptions.currentValue.length > 0) {
-      this.modalController.create({
-        component: ResponsesModalComponent,
-        componentProps: {
-          responseOptions: this.responseOptions,
-          message: this.message,
-          onOptionSelect: this.onOptionSelect
-        },
-        cssClass: "slide-up-modal",
-        backdropDismiss: false,
-        swipeToClose: false
-      }).then((modal) => modal.present());
+    if (
+      this.useModal &&
+      changes.responseOptions &&
+      changes.responseOptions.currentValue &&
+      changes.responseOptions.currentValue.length > 0
+    ) {
+      this.modalController
+        .create({
+          component: ResponsesModalComponent,
+          componentProps: {
+            responseOptions: this.responseOptions,
+            message: this.message,
+            onOptionSelect: this.onOptionSelect,
+          },
+          cssClass: "slide-up-modal",
+          backdropDismiss: false,
+          swipeToClose: false,
+        })
+        .then((modal) => modal.present());
     }
     if (changes.message.currentValue !== changes.message.previousValue) {
       if (!this.message.tickedByDefault) {
@@ -70,5 +74,4 @@ export class ChatResponsesComponent implements OnChanges {
       return this.message.responseOptions;
     }
   }
-
 }
