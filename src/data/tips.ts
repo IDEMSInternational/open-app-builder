3f66f3706b1b87ee6546275d5f8a171007b87544
Michael Clapham <michaeljclapham@gmail.com>
2021-01-21 00:50:17 +0000

    Content update

--- src/data/tips.ts
+++ src/data/tips.ts
@@ -281,21 +281,72 @@
   },
   {
     "flow_type": "tips",
-    "module": "ONE_ON_ONE_TIME",
-    "flow_name": "mod_1on1_tips",
-    "title": "One-on-One Time Tips",
+    "module": "welcome",
+    "flow_name": "mod_welcome_tips",
+    "title": "Self-Care Tools",
     "status": "released",
     "rows": [
       {
         "type": "title",
-        "message_text": "Essential Tools One-on-One Time"
+        "message_text": "Essential Tools for Self-Care"
+      },
+      {
+        "type": "list_group",
+        "rows": [
+          {
+            "type": "list_item",
+            "message_text": "RELAX"
+          },
+          {
+            "type": "text",
+            "message_text": "Do a 30 second quick relaxation activity. Here's one:"
+          },
+          {
+            "type": "list_item",
+            "message_text": "1. Close your eyes. \n2. Listen to your breath as it goes in and out five times. \n3. Notice how you feel. \n4. When you are ready open your eyes again. You are in control!"
+          },
+          {
+            "type": "list_item",
+            "message_text": "RECOGNISE"
+          },
+          {
+            "type": "text",
+            "message_text": "Take a moment and think of one thing YOU have done recently that you have done well! Say it aloud if you can ‘Well done for …\""
+          },
+          {
+            "type": "list_item",
+            "message_text": "REWARD"
+          },
+          {
+            "type": "text",
+            "message_text": "Remember you deserve your quality time too! What makes you happy?"
+          }
+        ]
+      },
+      {
+        "type": "text",
+        "message_text": "Tap your ParentPoints <icon>, <icon>, <icon>: see your success!",
+        "comment": "Requires icons"
+      }
+    ]
+  },
+  {
+    "flow_type": "tips",
+    "module": "1on1",
+    "flow_name": "mod_1on1_tips",
+    "title": "One-on-One Time Tools",
+    "status": "released",
+    "rows": [
+      {
+        "type": "title",
+        "message_text": "Essential Tools for One-on-One Time"
       },
       {
         "type": "list_group",
         "rows": [
           {
             "type": "list_intro",
-            "message_text": "3 tools to make your One-on-One Time great! You can always find these one-on-one tools in ‘’My toolkit’’. "
+            "message_text": "3 tools to make your One-on-One Time great!"
           },
           {
             "type": "list_item",
@@ -329,29 +380,26 @@
       },
       {
         "type": "text",
-        "message_text": "Every time you do one-on-one time, mark your Parent Point <icon> to track your success"
+        "message_text": "Tap your ParentPoint <icon>: see your success!",
+        "comment": "plh_images/habits/habit_spend_time_icon.svg"
       }
     ]
   },
   {
     "flow_type": "tips",
-    "module": "PRAISE_AND_POSITIVE_REINFORCEMENT",
+    "module": "praise",
     "flow_name": "mod_praise_tips",
-    "title": "Praise Tips",
+    "title": "Praise Tools",
     "status": "released",
     "rows": [
       {
         "type": "title",
-        "message_text": "Essential Tools Praise",
+        "message_text": "Essential Tools for Praise",
         "comment": "Image of teen sweeping, parent encouraging them  \n(hand on shoulder/high five)"
       },
       {
         "type": "list_group",
         "rows": [
-          {
-            "type": "list_intro",
-            "message_text": "These will be in your toolbox for quick access anytime."
-          },
           {
             "type": "list_item",
             "message_text": "1. SEE IT"
@@ -384,28 +432,25 @@
       },
       {
         "type": "text",
-        "message_text": "Every time you praise your teen, mark your Parent Point <icon> to track your success"
+        "message_text": "Tap your ParentPoint <icon>: see your success!",
+        "comment": "plh_images/habits/habit_praise_teen_icon.svg"
       }
     ]
   },
   {
     "flow_type": "tips",
-    "module": "POSITIVE_INSTRUCTIONS",
+    "module": "instruct",
     "flow_name": "mod_instruct_tips",
-    "title": "Positive Instructions Tips",
+    "title": "Positive Instructions Tools",
     "status": "released",
     "rows": [
       {
         "type": "title",
-        "message_text": "Essential Tools Positive Instructions"
+        "message_text": "Essential Tools for Positive Instructions"
       },
       {
         "type": "list_group",
         "rows": [
-          {
-            "type": "list_intro",
-            "message_text": "Find these in your toolbox for easy access anytime"
-          },
           {
             "type": "list_item",
             "message_text": "1. GET REAL"
