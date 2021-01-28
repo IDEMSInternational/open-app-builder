a81e67ea96e232ed44fe47ee3475fa82abdc853e
chrismclarke <chris.m.clarke@live.co.uk>
2021-01-28 12:02:33 -0800

    tidy android build deps

--- android/app/src/main/java/international/idems/plh_teens/MainActivity.java
+++ android/app/src/main/java/international/idems/plh_teens/MainActivity.java
@@ -6,7 +6,6 @@ import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
-import com.baumblatt.capacitor.firebase.auth.CapacitorFirebaseAuth;
 
 public class MainActivity extends BridgeActivity {
   @Override
@@ -17,7 +16,6 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
-      add(CapacitorFirebaseAuth.class);
     }});
   }
 }
