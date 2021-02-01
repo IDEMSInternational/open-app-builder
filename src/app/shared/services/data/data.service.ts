56992e8e5dbeab56ec4108f77d276a9b78a46179
Michael Clapham <michaeljclapham@gmail.com>
2021-02-01 13:19:52 +0000

    Pull tour steps from gsheets

--- src/app/shared/services/data/data.service.ts
+++ src/app/shared/services/data/data.service.ts
@@ -11,6 +11,7 @@ import { module_list } from "src/data/module_list";
 import { module_page } from "src/data/module_page";
 import { care_package_list } from "src/data/care_package_list";
 import { habit_ideas } from "src/data/habit_ideas";
+import { tour } from "src/data/tour";
 
 export const COMPLETION_LIST = completion_list;
 export const CONVERSATION = conversation;
@@ -22,6 +23,7 @@ export const TASK_LIST = task_list;
 export const TIPS = tips;
 export const CARE_PACKAGE_LIST = care_package_list;
 export const HABIT_IDEAS = habit_ideas;
+export const TOUR = tour;
 
 /** A simple variable just to type-check/ensure all data types have been exported in this file */
 const mapping: { [key in FlowTypes.FlowType] } = {
@@ -34,7 +36,8 @@ const mapping: { [key in FlowTypes.FlowType] } = {
   task_list: TASK_LIST,
   tips: TIPS,
   care_package_list: CARE_PACKAGE_LIST,
-  habit_ideas: HABIT_IDEAS
+  habit_ideas: HABIT_IDEAS,
+  tour: TOUR
 };
 
 /**
--- src/app/shared/services/tour/tour.service.ts
+++ src/app/shared/services/tour/tour.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
 import introJs from 'intro.js';
+import { TOUR } from '../data/data.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,28 +12,26 @@ export class TourService {
   introJS = introJs();
 
   constructor(private router: Router) {
-    this.introJS.setOptions({
-      steps: [
-        {
-          intro: "Intro text",
-          title: "Title"
-        },
-        {
-          intro: "Text",
-          element: "#main-content > plh-module-list > div > div > plh-module-focus-skin > div > div > div > ion-button"
-        },
-        {
-          intro: "More text"
-        }
-      ] as any
-    });
+
   }
 
-  async startTour() {
+  async startTour(tourName = "test_tour") {
+    let matchingTour = TOUR.find((t) => t.flow_name === tourName);
 
-    await this.router.navigateByUrl("/module_list");
-    setTimeout(() => {
+    if (matchingTour) {
+      this.introJS.setOptions({
+        steps: matchingTour.rows.map((row) => ({
+          intro: row.message_text,
+          title: row.title,
+          element: row.element,
+          onbeforechange: () => {
+            this.router.navigateByUrl(row.route);
+          }
+        }))
+      });
+      this.introJS.onbeforechange
+      await this.router.navigateByUrl("/module_list");
       this.introJS.start();
-    }, 500);
+    }
   }
 }
