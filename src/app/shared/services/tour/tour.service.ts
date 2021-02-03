3c15f7be42556a069f519d481dcb2bafa07db1db
Michael Clapham <michaeljclapham@gmail.com>
2021-02-03 17:11:57 +0000

    Correct navigating to a different route

--- src/app/shared/services/tour/tour.service.ts
+++ src/app/shared/services/tour/tour.service.ts
@@ -18,19 +18,28 @@ export class TourService {
   async startTour(tourName = "test_tour") {
     let matchingTour = TOUR.find((t) => t.flow_name === tourName);
 
-    if (matchingTour) {
+    if (matchingTour && matchingTour.rows && matchingTour.rows.length > 0) {
       this.introJS.setOptions({
         steps: matchingTour.rows.map((row) => ({
           intro: row.message_text,
           title: row.title,
           element: row.element,
-          onbeforechange: () => {
-            this.router.navigateByUrl(row.route);
-          }
-        }))
+        })),
+        hidePrev: true
       });
-      this.introJS.onbeforechange
-      await this.router.navigateByUrl("/module_list");
+      this.introJS.onafterchange(() => {
+        setTimeout(() => this.introJS.refresh(), 500);
+      });
+      this.introJS.onbeforechange((elem) => {
+        this.introJS.currentStep();
+        const stepNumber = this.introJS.currentStep()
+        const currentRow = matchingTour.rows[stepNumber];
+        if (currentRow.route) {
+          this.router.navigateByUrl(currentRow.route);
+        }
+      });
+      const startRoute = matchingTour.rows[0].route ? matchingTour.rows[0].route : "module_list";
+      await this.router.navigateByUrl(startRoute);
       this.introJS.start();
     }
   }
