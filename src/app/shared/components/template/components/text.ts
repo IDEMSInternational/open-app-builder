b21f9eb3378724e89903d67fef18bec85bd3a64f
Michael Clapham <michaeljclapham@gmail.com>
2021-02-12 17:23:52 +0000

    at local var pipe

--- src/app/shared/components/template/components/text.ts
+++ src/app/shared/components/template/components/text.ts
@@ -3,7 +3,7 @@ import { FlowTypes } from 'src/app/shared/model/flowTypes';
 
 @Component({
   selector: "plh-tmpl-text",
-  template: `<p>{{row.value}}</p>`,
+  template: `<p>{{row.value | localVarsReplace: localVariables}}</p>`,
   styleUrls: ["./tmpl-components-common.scss"]
 })
 export class TmplTextComponent implements OnInit {
