a65cd12168ccbd456853d1ae9b0639786b97d310
Michael Clapham <michaeljclapham@gmail.com>
2021-02-15 19:22:25 +0000

    Remove _group from template type names

--- src/app/shared/components/template/components/animated_section.ts
+++ src/app/shared/components/template/components/animated_section.ts
@@ -2,9 +2,9 @@ import { Component, Input } from "@angular/core";
 import { FlowTypes } from "src/app/shared/model/flowTypes";
 
 @Component({
-    selector: "plh-tmpl-animated-section-group",
-    template: `<div class="animated-section-group">
-        <h2>Animated Section Group</h2>
+    selector: "plh-tmpl-animated-section",
+    template: `<div class="animated-section">
+        <h2>Animated Section</h2>
         <plh-tmpl-comp-host
             *ngFor="let childRow of row.rows"
             [row]="childRow"
@@ -13,7 +13,7 @@ import { FlowTypes } from "src/app/shared/model/flowTypes";
         ></plh-tmpl-comp-host>
     </div>`
 })
-export class AnimatedSectionGroupComponent {
+export class AnimatedSectionComponent {
     @Input() row: FlowTypes.TemplateRow;
     @Input() template: FlowTypes.Template;
     @Input() localVariables: { [name: string]: string };
