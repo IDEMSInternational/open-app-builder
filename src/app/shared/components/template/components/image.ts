a183b962e25b5f799b8905bdc95398da6ea30438
Michael Clapham <michaeljclapham@gmail.com>
2021-02-15 19:41:04 +0000

    Add /assets/plh_assets to src if 404 on img

--- src/app/shared/components/template/components/image.ts
+++ src/app/shared/components/template/components/image.ts
@@ -1,16 +1,34 @@
+import { HttpClient } from "@angular/common/http";
 import { Component, Input, OnInit } from "@angular/core";
+import { tr } from "date-fns/locale";
 import { FlowTypes } from 'src/app/shared/model/flowTypes';
 
 @Component({
   selector: "plh-tmpl-image",
   template: `
     <div class="tmpl-image-container">
-      <img [src]="row.value">
+      <img *ngIf="imageSrc" [src]="imageSrc">
     </div>
   `,
   styleUrls: ["./tmpl-components-common.scss"]
 })
 export class TmplImageComponent {
-  @Input() row: FlowTypes.TemplateRow;
+
+  assetsPrefix = "/assets/plh_assets/";
+
+  constructor(private http: HttpClient) {
+  }
+
+  imageSrc: string;
+  @Input() set row (value: FlowTypes.TemplateRow) {
+    this.http.get(value.value, { responseType: "arraybuffer" })
+      .toPromise()
+      .then(() => {
+        this.imageSrc = value.value;
+      })
+      .catch(() => {
+        this.imageSrc = (this.assetsPrefix + value.value).replace("//", "/");
+      });
+  }
   @Input() template: FlowTypes.Template;
 }
