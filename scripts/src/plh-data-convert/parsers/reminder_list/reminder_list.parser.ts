f38796a14f9c8ef0bda5b3e1940eecf630b1e597
chrismclarke <chris.m.clarke@live.co.uk>
2021-02-18 19:42:15 -0800

    reminder parser improvements

--- scripts/src/plh-data-convert/parsers/reminder_list/reminder_list.parser.ts
+++ scripts/src/plh-data-convert/parsers/reminder_list/reminder_list.parser.ts
@@ -23,24 +23,56 @@ export class ReminderListParser extends DefaultParser {
  * event_completed | first_app_launch | delay_1_day
  * ```
  */
-function extractConditionList(c: string) {
-  // some common authoring scenarios have been reduced to single keywords for ease-of-authoring
-  // replace these with full specifications
-  const shorthandReplacements = {
-    sent: "reminder_action | sent",
-    first_app_launch: "app_event | first_app_launch",
-  };
-  Object.entries(shorthandReplacements).forEach(
-    ([original, replacement]) => (c = c.replace(original, replacement))
-  );
-
-  let [actionStr, value, timingStr] = c.split("|").map((s: string) => s.trim());
-
+function extractConditionList(conditionText: string) {
+  conditionText = _replaceShorthandText(conditionText);
+  let [actionStr, value, timingStr] = conditionText.split("|").map((s: string) => s.trim());
   const action = actionStr as FlowTypes.Reminder_conditionList["action"];
   let timing: FlowTypes.Reminder_conditionList["timing"] = null;
   if (timingStr) {
-    const [comparison, quantity, unit] = timingStr.split("_").map((s: string) => s.trim());
-    timing = { comparison, quantity: quantity ? Number(quantity) : null, unit } as any;
+    const [comparatorText, quantity, unit] = timingStr.split("_").map((s: string) => s.trim());
+    const comparator = _extractComparator(comparatorText);
+    timing = { comparator, quantity: quantity ? Number(quantity) : null, unit } as any;
   }
   return { action, value, timing };
+}
+
+/**
+ * some common authoring scenarios have been reduced to single keywords for ease-of-authoring
+ * replace these with full specifications
+ */
+function _replaceShorthandText(text: string) {
+  const shorthandReplacements = {
+    sent: "reminder_action | sent",
+    first_app_launch: "app_event | first_app_launch",
+    app_launch: "app_event | app_launch",
+  };
+  Object.entries(shorthandReplacements).forEach(([original, replacement]) => {
+    // use a regular expression to prevent matching words that have additional content before
+    // e.g. app_launch should not match on first_app_launch (start of string regex)
+    const regex = new RegExp(`^${original}`);
+    text = text.replace(regex, replacement);
+  });
+  return text;
+}
+
+/**
+ * When timing conditions are specified related to a reminder, the condition should be specified
+ * with leading text such as before_3_day or after_2_day.
+ * Convert the text into a logical comparator that will be used during evaluation
+ */
+function _extractComparator(
+  text: string
+): FlowTypes.Reminder_conditionList["timing"]["comparator"] {
+  // when comparing, the target evaluation value will be compared relative to todays date / app day
+  // first the difference between the target event and today is calculated, called diffInDays (app or calendar)
+  // for an event to happen n days before (or more), diffInDays > n
+  // for an event to happen within n days, diffInDays <= n
+  switch (text) {
+    case "before":
+      return ">";
+    case "within":
+      return "<=";
+    default:
+      throw new Error(`Reminder timing comparison not defined: ${text}`);
+  }
 }
