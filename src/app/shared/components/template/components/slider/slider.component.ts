c35210301b4fe8d41a3445cee212426b54d10953
Yehor <egor.miroshnichenko@wtt-solutions.com>
2021-03-03 11:14:54 +0200

    fixed: slider range value list

--- src/app/shared/components/template/components/slider/slider.component.ts
+++ src/app/shared/components/template/components/slider/slider.component.ts
@@ -54,31 +54,31 @@ export class SliderComponent implements ITemplateComponent, OnInit {
       case (arr.length <= 11):
         this.listNumbers = arr;
         return this.listNumbers;
-      case (arr.length > 11 && arr.length <= 20):
-        this.listNumbers = arr.filter(item => item % 2 === 0);
+      case (arr.length > 11 && arr.length <= 21):
+        this.listNumbers = [0, 2, 6, 8, 10, 15, 20]
         return this.listNumbers;
-      case (arr.length > 20 && arr.length <= 30):
+      case (arr.length > 21 && arr.length <= 31):
         this.listNumbers = [0, 5, 10, 15, 20 , 25, arr[arr.length - 1]]
         return this.listNumbers;
-      case (arr.length > 30 && arr.length < 40):
+      case (arr.length > 31 && arr.length <= 41):
         this.listNumbers = [0, 5, 10, 15, 20, 25, 30, 35, arr[arr.length - 1]]
         return this.listNumbers;
-      case (arr.length > 40 && arr.length <= 50):
-          this.listNumbers = [0, 5, 10, 15, 20, 25, 30, 35, arr[arr.length - 1]]
+      case (arr.length > 41 && arr.length <= 51):
+          this.listNumbers = [0, 10, 15, 20, 25, 30, 35, 40, arr[arr.length - 1]]
           return this.listNumbers;
-      case (arr.length > 50 && arr.length <= 60):
-        this.listNumbers = [0, 10, 20 , 30, 40, arr[arr.length - 1]]
-        return this.listNumbers;
-      case (arr.length > 60 && arr.length <= 70):
+      case (arr.length > 51 && arr.length <= 61):
         this.listNumbers = [0, 10, 20 , 30, 40, 50, arr[arr.length - 1]]
         return this.listNumbers;
-      case (arr.length > 70 && arr.length <= 80):
+      case (arr.length > 61 && arr.length <= 71):
         this.listNumbers = [0, 10, 20 , 30, 40, 50, 60, arr[arr.length - 1]]
         return this.listNumbers;
-      case (arr.length > 80 && arr.length <= 90):
+      case (arr.length > 71 && arr.length <= 81):
         this.listNumbers = [0, 10, 20 , 30, 40, 50, 60, 70, arr[arr.length - 1]]
         return this.listNumbers;
-      case (arr.length > 90):
+      case (arr.length > 81 && arr.length <= 91):
+        this.listNumbers = [0, 10, 20 , 30, 40, 50, 60, 70, 80, arr[arr.length - 1]]
+        return this.listNumbers;
+      case (arr.length > 91):
         this.listNumbers = [0, 20, 40, 60, 80, arr[arr.length - 1]]
         return this.listNumbers;
     }
