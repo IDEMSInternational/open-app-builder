bfc95f057e603e723e9a0742c44b10bb82323142
chrismclarke <chris.m.clarke@live.co.uk>
2021-03-03 22:04:59 -0800

    WiP - templating system reworking

--- src/app/shared/components/template/components/template_group.ts
+++ src/app/shared/components/template/components/template_group.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from "@angular/core";
-import { FlowTypes } from 'src/app/shared/model/flowTypes';
+import { FlowTypes } from "src/app/shared/model/flowTypes";
 import { TEMPLATE } from "src/app/shared/services/data/data.service";
 import { LocalVarsReplacePipe } from "../local-vars-replace.pipe";
 import { ITemplateComponent } from "./tmpl.component";
@@ -7,16 +7,16 @@ import { ITemplateComponent } from "./tmpl.component";
 @Component({
   selector: "plh-tmpl-template-group",
   template: `<div class="template">
-    <plh-tmpl-comp *ngFor="let innerRow of populatedRows"
+    <plh-tmpl-comp
+      *ngFor="let innerRow of populatedRows"
       [row]="innerRow"
       [template]="template"
       [localVariables]="localVariables"
     ></plh-tmpl-comp>
   </div>`,
-  styleUrls: ["./tmpl-components-common.scss"]
+  styleUrls: ["./tmpl-components-common.scss"],
 })
 export class TmplTemplateGroupComponent implements ITemplateComponent {
-
   @Input() set row(value: FlowTypes.TemplateRow) {
     this.populateRowsFromParent(value);
   }
@@ -24,8 +24,7 @@ export class TmplTemplateGroupComponent implements ITemplateComponent {
   @Input() localVariables: { [name: string]: any };
   populatedRows: FlowTypes.TemplateRow[];
 
-  constructor() {
-  }
+  constructor() {}
 
   stringify(obj) {
     return JSON.stringify(obj);
@@ -33,7 +32,10 @@ export class TmplTemplateGroupComponent implements ITemplateComponent {
 
   private populateRowsFromParent(ourRow: FlowTypes.TemplateRow) {
     /* The value field is used to select a template. This field can have a @local.template_name var  */
-    const parsedRowValue = LocalVarsReplacePipe.parseMessageTemplate(ourRow.value, this.localVariables);
+    const parsedRowValue = LocalVarsReplacePipe.parseMessageTemplate(
+      ourRow.value,
+      this.localVariables
+    );
     const superTemplate = TEMPLATE.find((t) => t.flow_name === parsedRowValue);
     if (superTemplate) {
       const overrideTree = this.createOverrideTree(ourRow.rows);
@@ -69,18 +71,9 @@ export class TmplTemplateGroupComponent implements ITemplateComponent {
       .map((prop) => overrideTree[prop])
       .find((r) => r.name === parentRow.name);
     if (matchingOurRow) {
-      const result = { ...parentRow, ...matchingOurRow, type: parentRow.type }
+      const result = { ...parentRow, ...matchingOurRow, type: parentRow.type };
       return result;
     }
     return parentRow;
-  }
-
-  private mergeNameArrays(base: { name?: string }[], override: { name?: string }[]): { name: string }[] {
-    const baseMap = {};
-    base.forEach((obj) => baseMap[obj.name] = obj);
-    const overrideMap = {};
-    override.forEach((obj) => overrideMap[obj.name] = obj);
-    const allNames = Object.keys(baseMap).concat(Object.keys(overrideMap));
-    return allNames.map((name) => overrideMap[name] ? overrideMap[name] : baseMap[name]);
   }
 }
