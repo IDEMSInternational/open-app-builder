d589903ec260399ab195053bd952e39b764879f0
Michael Clapham <michaeljclapham@gmail.com>
2021-03-05 21:09:42 +0000

    test commit

--- src/app/shared/components/animations/index.ts
+++ src/app/shared/components/animations/index.ts
@@ -15,7 +15,7 @@ export interface AnimationMetadata {
 export const ANIMATION_METADATA_BY_ID: Record<string, AnimationMetadata> = {
     "relax1": {
         id: "relax1",
-        name: "Relaxing blob with rings 1",
+        name: "Relaxing blob with rings 1 ",
         description: "Blue blub that's smiling with eyes closed whilst lighter blue rings of light expand in and out around it :)",
         component: RelaxAnim1Component
     }
