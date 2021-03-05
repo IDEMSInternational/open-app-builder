e9647fb7475eeb32da82bb6a605a0b9acb4d33fb
Michael Clapham <michaeljclapham@gmail.com>
2021-03-05 13:43:27 +0000

    Relax with rings animation

---
+++ src/app/shared/components/animations/index.ts
@@ -0,0 +1,20 @@
+import { Component, Type } from "@angular/core";
+import { RelaxAnim1Component } from "./relax1/relax-anim1";
+
+export const ANIMATION_COMPONENTS = [
+    RelaxAnim1Component
+];
+
+export interface AnimationMetadata {
+    id: string;
+    name: string;
+    component: Type<Component>
+}
+
+export const ANIMATION_METADATA_BY_ID: Record<string, AnimationMetadata> = {
+    "relax1": {
+        id: "relax1",
+        name: "Relaxing blob with rings 1",
+        component: RelaxAnim1Component
+    }
+}
