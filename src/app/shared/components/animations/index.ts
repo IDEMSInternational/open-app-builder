e8e4d252f0e2c0b34a970f15913c53e696314187
Michael Clapham <michaeljclapham@gmail.com>
2021-03-05 13:58:07 +0000

    Added animation description

--- src/app/shared/components/animations/index.ts
+++ src/app/shared/components/animations/index.ts
@@ -8,6 +8,7 @@ export const ANIMATION_COMPONENTS = [
 export interface AnimationMetadata {
     id: string;
     name: string;
+    description?: string;
     component: Type<Component>
 }
 
@@ -15,6 +16,7 @@ export const ANIMATION_METADATA_BY_ID: Record<string, AnimationMetadata> = {
     "relax1": {
         id: "relax1",
         name: "Relaxing blob with rings 1",
+        description: "Blue blub that's smiling with eyes closed whilst lighter blue rings of light expand in and out around it.",
         component: RelaxAnim1Component
     }
 }
