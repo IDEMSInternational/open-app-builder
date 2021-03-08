5be6cdbb66763af2d1228fbb6b7d8545e2863f1f
Michael Clapham <michaeljclapham@gmail.com>
2021-03-08 19:29:27 +0000

    Animation browser

--- src/app/shared/components/animations/index.ts
+++ src/app/shared/components/animations/index.ts
@@ -9,10 +9,15 @@ export interface AnimationMetadata {
     id: AnimationId;
     name: string;
     description?: string;
-    component: Type<Component>
+    component?: Type<Component>
+    svgAssetPath?: string;
 }
 
-export type AnimationId = "relax1";
+export type AnimationId = 
+    "relax1"
+    | "waving-blob1"
+    | "elder-waving1"
+    | "neighbour-jumping1";
 
 export const ANIMATION_METADATA_BY_ID: Record<AnimationId, AnimationMetadata> = {
     "relax1": {
@@ -20,5 +25,20 @@ export const ANIMATION_METADATA_BY_ID: Record<AnimationId, AnimationMetadata> = 
         name: "Relaxing blob with rings 1 ",
         description: "Blue blub that's smiling with eyes closed whilst lighter blue rings of light expand in and out around it :)",
         component: RelaxAnim1Component
+    },
+    "waving-blob1": {
+        id: "waving-blob1",
+        name: "Waving blob 1",
+        svgAssetPath: "assets/images/chat-characters/egg_character1.svg"
+    },
+    "elder-waving1": {
+        id: "elder-waving1",
+        name: "Elder blob waving 1",
+        svgAssetPath: "assets/images/Elder_Large.svg"
+    },
+    "neighbour-jumping1": {
+        id: "neighbour-jumping1",
+        name: "Neighbour Jumping 1",
+        svgAssetPath: "assets/images/anim-blobs/neighbour_large.svg"
     }
 };
