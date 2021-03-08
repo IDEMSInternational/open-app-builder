eaf78f5fcd1e10b8cfb00b7d02ea8ff4f4f193c5
Michael Clapham <michaeljclapham@gmail.com>
2021-03-08 19:29:27 +0000

    Animation browser

--- src/app/feature/animations/animations.page.ts
+++ src/app/feature/animations/animations.page.ts
@@ -12,12 +12,14 @@ import { AnimationMetadata, ANIMATION_METADATA_BY_ID } from 'src/app/shared/comp
 
     <ion-content>
       <ion-input class="input" placeholder="Search for animations" (ionChange)="searchInputChanged($event.target.value)"></ion-input>
-      <ion-card class="animation-card" *ngFor="let anim of animations">
-        <h2>{{anim.name}}</h2>
-        <span class="id-text">{{anim.id}}</span>
-        <p>{{anim.description}}</p>
-        <plh-css-animation [animationId]="anim.id"></plh-css-animation>
-      </ion-card>
+      <section class="animation-cards">
+        <ion-card class="animation-card" *ngFor="let anim of animations">
+          <h2>{{anim.name}}</h2>
+          <span class="id-text">{{anim.id}}</span>
+          <p>{{anim.description}}</p>
+          <plh-css-animation [animationId]="anim.id"></plh-css-animation>
+        </ion-card>
+      </section>
     </ion-content>
   `,
   styleUrls: ['./animations.page.scss'],
@@ -36,7 +38,7 @@ export class AnimationsPage implements OnInit {
       .filter((anim) => {
         const asString = anim.id + " " + anim.name + " " + anim.description;
         return asString.toLowerCase().indexOf(term.toLowerCase()) > -1;
-      });
+      }); 
   }
 
 }
