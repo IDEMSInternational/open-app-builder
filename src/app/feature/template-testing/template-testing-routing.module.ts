937214a079da06cab8a076b7a36fa7f4d8faf4e4
chrismclarke <chris.m.clarke@live.co.uk>
2021-03-10 11:34:12 -0800

    add template list page

--- src/app/feature/template-testing/template-testing-routing.module.ts
+++ src/app/feature/template-testing/template-testing-routing.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { TemplateTestingPage } from './template-testing.page';
+import { TemplateTestingPage } from "./template-testing.page";
 
 const routes: Routes = [
   {
-    path: ':templateName',
-    component: TemplateTestingPage
-  }
+    path: ":templateName",
+    component: TemplateTestingPage,
+  },
+  {
+    path: "",
+    component: TemplateTestingPage,
+  },
 ];
 
 @NgModule({
