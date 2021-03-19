1164c9a090b623828a4a3d998715f4f2a3460265
Michael Clapham <michaeljclapham@gmail.com>
2021-03-19 16:35:00 +0000

    Added global data file

---
+++ src/data/global.ts
@@ -0,0 +1,469 @@
+/* tslint:disable */
+  import { FlowTypes } from "src/app/shared/model/flowTypes";
+  export const global: FlowTypes.Global[] = [
+  {
+    "flow_type": "global",
+    "flow_name": "debug_set_global_1",
+    "status": "released",
+    "rows": [
+      {
+        "type": "set_global",
+        "name": "debug_variable_1",
+        "value": "Value of the first debug variable"
+      }
+    ],
+    "_xlsxPath": "plh_sheets_beta\\plh_templating\\quality_assurance\\debug_templates\\debug_small_issues.xlsx"
+  },
+  {
+    "flow_type": "global",
+    "flow_name": "home_screen_variables",
+    "status": "released",
+    "rows": [
+      {
+        "type": "set_global",
+        "name": "weekly_workshops",
+        "value": "Weekly Workshops"
+      },
+      {
+        "type": "set_global",
+        "name": "weekly_workshop",
+        "value": "Weekly Workshop"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_points",
+        "value": "Parent Points"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point",
+        "value": "Parent Point"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_centre",
+        "value": "Parent Centre"
+      }
+    ],
+    "_xlsxPath": "plh_sheets_beta\\plh_templating\\top_templates\\navigation_templates\\global_navigation.xlsx"
+  },
+  {
+    "flow_type": "global",
+    "flow_name": "parent_centre_variables",
+    "status": "released",
+    "rows": [
+      {
+        "type": "set_global",
+        "name": "help",
+        "value": "Help"
+      },
+      {
+        "type": "set_global",
+        "name": "help_icon",
+        "value": "plh_images/icons/heart.svg",
+        "comments": "placeholder"
+      },
+      {
+        "type": "set_global",
+        "name": "essential_tools",
+        "value": "Essential Tools"
+      },
+      {
+        "type": "set_global",
+        "name": "essential_tools_icon",
+        "value": "plh_images/icons/heart.svg",
+        "comments": "placeholder"
+      },
+      {
+        "type": "set_global",
+        "name": "covid",
+        "value": "COVID"
+      },
+      {
+        "type": "set_global",
+        "name": "covid_icon",
+        "value": "plh_images/icons/heart.svg",
+        "comments": "placeholder"
+      },
+      {
+        "type": "set_global",
+        "name": "relax_and_activities",
+        "value": "Relax & Activities"
+      },
+      {
+        "type": "set_global",
+        "name": "relax_and_activities_icon",
+        "value": "plh_images/icons/heart.svg",
+        "comments": "placeholder"
+      },
+      {
+        "type": "set_global",
+        "name": "extra",
+        "value": "Extra"
+      },
+      {
+        "type": "set_global",
+        "name": "extra_icon",
+        "value": "plh_images/icons/heart.svg",
+        "comments": "placeholder"
+      },
+      {
+        "type": "set_global",
+        "name": "support_contacts",
+        "value": "Support Contacts"
+      },
+      {
+        "type": "set_global",
+        "name": "support_contacts_icon",
+        "value": "plh_images/icons/heart.svg",
+        "comments": "placeholder"
+      },
+      {
+        "type": "set_global",
+        "name": "evidence_base",
+        "value": "Evidence Base"
+      },
+      {
+        "type": "set_global",
+        "name": "evidence_base_icon",
+        "value": "plh_images/icons/heart.svg",
+        "comments": "placeholder"
+      },
+      {
+        "type": "set_global",
+        "name": "technical_support",
+        "value": "Technical Support"
+      },
+      {
+        "type": "set_global",
+        "name": "technical_support_icon",
+        "value": "plh_images/icons/heart.svg",
+        "comments": "placeholder"
+      }
+    ],
+    "_xlsxPath": "plh_sheets_beta\\plh_templating\\top_templates\\navigation_templates\\global_navigation.xlsx"
+  },
+  {
+    "flow_type": "global",
+    "flow_name": "parent_point_variables",
+    "status": "released",
+    "rows": [
+      {
+        "type": "set_global",
+        "name": "parent_point_relax",
+        "value": "Relax"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_relax_image",
+        "value": "plh_images/habits/habit_relax_image.svg"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_treat_yourself",
+        "value": "Treat yourself well"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_treat_yourself_image",
+        "value": "plh_images/habits/habit_treat_yourself_image.svg"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_praise_yourself",
+        "value": "Praise yourself"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_praise_yourself_image",
+        "value": "plh_images/habits/habit_praise_yourself_image.svg"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_spend_time",
+        "value": "One on one time"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_spend_time_image",
+        "value": "plh_images/habits/habit_spend_time_image.svg"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_praise_teen",
+        "value": "Praise your teen"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_praise_teen_image",
+        "value": "plh_images/habits/habit_praise_teen_image.svg"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_breathe",
+        "value": "Breathe not yell"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_breathe_image",
+        "value": "plh_images/habits/habit_breathe_image.svg"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_money",
+        "value": "Good money choice"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_money_image",
+        "value": "plh_images/habits/habit_money_image.svg"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_consequence",
+        "value": "Calm consequence"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_consequence_image",
+        "value": "plh_images/habits/habit_consequence_image.svg"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_safe",
+        "value": "Safe"
+      },
+      {
+        "type": "set_global",
+        "name": "parent_point_safe_image",
+        "value": "plh_images/habits/habit_safe_image.svg"
+      }
+    ],
+    "_xlsxPath": "plh_sheets_beta\\plh_templating\\top_templates\\parent_point_templates\\global_parent_points.xlsx"
+  },
+  {
+    "flow_type": "global",
+    "flow_name": "workshop_titles",
+    "status": "released",
+    "rows": [
+      {
+        "type": "set_global",
+        "name": "w_self_care",
+        "value": "Self-Care",
+        "comments": "self-care"
+      },
+      {
+        "type": "set_global",
+        "name": "w_self_care_short",
+        "value": "Self-Care",
+        "comments": "ETW 1 -- done"
+      },
+      {
+        "type": "set_global",
+        "name": "w_1on1",
+        "value": "One-on-One Time",
+        "comments": "1on1"
+      },
+      {
+        "type": "set_global",
+        "name": "w_1on1_short",
+        "value": "1 on 1",
+        "comments": "ETW 3 -- done"
+      },
+      {
+        "type": "set_global",
+        "name": "w_praise",
+        "value": "Praise & Positive Reinforcement",
+        "comments": "praise"
+      },
+      {
+        "type": "set_global",
+        "name": "w_praise_short",
+        "value": "Praise",
+        "comments": "ETW 4 -- done"
+      },
+      {
+        "type": "set_global",
+        "name": "w_instruct",
+        "value": "Positive Instructions",
+        "comments": "instruct"
+      },
+      {
+        "type": "set_global",
+        "name": "w_instruct_short",
+        "value": "Instructions",
+        "comments": "ETW 2 -- done"
+      },
+      {
+        "type": "set_global",
+        "name": "w_stress",
+        "value": "Managing Anger & Stress",
+        "comments": "stress"
+      },
+      {
+        "type": "set_global",
+        "name": "w_stress_short",
+        "value": "Stress",
+        "comments": "ETW 5"
+      },
+      {
+        "type": "set_global",
+        "name": "w_money",
+        "value": "Family Budgeting",
+        "comments": "money"
+      },
+      {
+        "type": "set_global",
+        "name": "w_money_short",
+        "value": "Money"
+      },
+      {
+        "type": "set_global",
+        "name": "w_rules",
+        "value": "Rules",
+        "comments": "rules"
+      },
+      {
+        "type": "set_global",
+        "name": "w_rules_short",
+        "value": "Rules"
+      },
+      {
+        "type": "set_global",
+        "name": "w_responsibility",
+        "value": "Accepting Responsibility",
+        "comments": "responsibility"
+      },
+      {
+        "type": "set_global",
+        "name": "w_responsibility_short",
+        "value": "Consequences"
+      },
+      {
+        "type": "set_global",
+        "name": "w_solve",
+        "value": "Problem Solving",
+        "comments": "solve"
+      },
+      {
+        "type": "set_global",
+        "name": "w_solve_short",
+        "value": "Problem Solve"
+      },
+      {
+        "type": "set_global",
+        "name": "w_safe",
+        "value": "Keeping Your Teen Safe",
+        "comments": "safe"
+      },
+      {
+        "type": "set_global",
+        "name": "w_safe_short",
+        "value": "Safety"
+      },
+      {
+        "type": "set_global",
+        "name": "w_crisis",
+        "value": "Dealing with Crisis",
+        "comments": "crisis"
+      },
+      {
+        "type": "set_global",
+        "name": "w_crisis_short",
+        "value": "Crisis"
+      }
+    ],
+    "_xlsxPath": "plh_sheets_beta\\plh_templating\\top_templates\\workshop_templates\\global_variables_workshops.xlsx"
+  },
+  {
+    "flow_type": "global",
+    "flow_name": "workshop_completion_levels",
+    "status": "released",
+    "rows": [
+      {
+        "type": "set_field",
+        "name": "w_self-care_completion_level",
+        "value": 0
+      },
+      {
+        "type": "set_field",
+        "name": "w_1on1_completion_level",
+        "value": 0
+      },
+      {
+        "type": "set_field",
+        "name": "w_praise_completion_level",
+        "value": 0
+      },
+      {
+        "type": "set_field",
+        "name": "w_instruct_completion_level",
+        "value": 0
+      },
+      {
+        "type": "set_field",
+        "name": "w_stress_completion_level",
+        "value": 0
+      },
+      {
+        "type": "set_field",
+        "name": "w_money_completion_level",
+        "value": 0
+      },
+      {
+        "type": "set_field",
+        "name": "w_rules_completion_level",
+        "value": 0
+      },
+      {
+        "type": "set_field",
+        "name": "w_responsibility_completion_level",
+        "value": 0
+      },
+      {
+        "type": "set_field",
+        "name": "w_solve_completion_level",
+        "value": 0
+      },
+      {
+        "type": "set_field",
+        "name": "w_safe_completion_level",
+        "value": 0
+      },
+      {
+        "type": "set_field",
+        "name": "w_crisis_completion_level",
+        "value": 0
+      }
+    ],
+    "_xlsxPath": "plh_sheets_beta\\plh_templating\\top_templates\\workshop_templates\\global_variables_workshops.xlsx"
+  },
+  {
+    "flow_type": "global",
+    "flow_name": "workshop_options",
+    "status": "released",
+    "rows": [
+      {
+        "type": "set_field",
+        "name": "do_workshops_together",
+        "value": true
+      },
+      {
+        "type": "set_field",
+        "name": "guide_number",
+        "value": "guide_2"
+      },
+      {
+        "type": "set_field",
+        "name": "group_name",
+        "value": "Friends"
+      },
+      {
+        "type": "set_field",
+        "name": "user_name",
+        "value": "Lucie"
+      }
+    ],
+    "_xlsxPath": "plh_sheets_beta\\plh_templating\\top_templates\\workshop_templates\\global_variables_workshops.xlsx"
+  }
+]
