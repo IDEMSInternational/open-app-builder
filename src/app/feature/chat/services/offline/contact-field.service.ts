4c7d564aa8683df4abebc6fbe7168eade31118f3
Michael Clapham <michaeljclapham@gmail.com>
2021-03-19 17:06:55 +0000

    Add set_field action

--- src/app/feature/chat/services/offline/contact-field.service.ts
+++ src/app/feature/chat/services/offline/contact-field.service.ts
@@ -41,4 +41,8 @@ export class ContactFieldService {
   async setContactField(key: string, value: string): Promise<any> {
     return this.localStorageService.setString("rp-contact-field." + key, value);
   }
+
+  setContactFieldSync(key: string, value: string) {
+    this.localStorageService.setString("rp-contact-field." + key, value);
+  }
 }
