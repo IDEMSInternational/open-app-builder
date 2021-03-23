ab6aed23d28ad9cb3dc4d8bd6ce671722ee7e332
Yehor <egor.miroshnichenko@wtt-solutions.com>
2021-03-23 14:17:09 +0200

    fix: change to one width parameter

--- src/app/shared/components/template/components/button/button.component.ts
+++ src/app/shared/components/template/components/button/button.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 
-import { getStringParamFromTemplateRow, getBooleanParamFromTemplateRow, getNumberParamFromTemplateRow } from "../../../../utils";
+import { getStringParamFromTemplateRow, getBooleanParamFromTemplateRow } from "../../../../utils";
 import { TemplateBaseComponent } from "../base";
 
 @Component({
@@ -13,8 +13,7 @@ export class TmplButtonComponent extends TemplateBaseComponent implements OnInit
     hexBgColor: string | null;
     disabled: boolean = false;
     height: number;
-    widthPx: number;
-    widthPercent: number;
+    width: string | null;
     constructor() {
         super();
     }
@@ -26,8 +25,7 @@ export class TmplButtonComponent extends TemplateBaseComponent implements OnInit
     getParams() {
             this.color = getStringParamFromTemplateRow(this._row, 'color',  'primary');
             this.hexBgColor = getStringParamFromTemplateRow(this._row, 'hexBgColor', null);
-            this.widthPx = getNumberParamFromTemplateRow(this._row, 'widthPx', null);
-            this.widthPercent = getNumberParamFromTemplateRow(this._row, 'widthPercent', 100);
+            this.width = getStringParamFromTemplateRow(this._row, 'width', '100%');
             this.disabled = getBooleanParamFromTemplateRow(this._row, 'disabled', false);
     }
 }
