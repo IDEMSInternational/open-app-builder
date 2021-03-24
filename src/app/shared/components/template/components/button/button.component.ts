f54a26ea7ebe7627924b6ba6ed7410c9812d3c8d
Yehor <egor.miroshnichenko@wtt-solutions.com>
2021-03-24 11:02:57 +0200

    feat: update documentation, small changes name of variables

--- src/app/shared/components/template/components/button/button.component.ts
+++ src/app/shared/components/template/components/button/button.component.ts
@@ -1,31 +1,32 @@
-import {Component, OnInit} from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
 import { getStringParamFromTemplateRow, getBooleanParamFromTemplateRow } from "../../../../utils";
 import { TemplateBaseComponent } from "../base";
 
 @Component({
-    selector: 'plh-button',
-    templateUrl: './button.component.html',
-    styleUrls: ['./button.component.scss'],
+  selector: "plh-button",
+  templateUrl: "./button.component.html",
+  styleUrls: ["./button.component.scss"],
 })
 export class TmplButtonComponent extends TemplateBaseComponent implements OnInit {
-    color: string = 'primary';
-    hexBgColor: string | null;
-    disabled: boolean = false;
-    height: number;
-    width: string | null;
-    constructor() {
-        super();
-    }
+  style: string = "primary";
+  color: string | null;
+  disabled: boolean = false;
+  height: string | null;
+  width: string | null;
 
-    ngOnInit() {
-        this.getParams();
-    }
+  constructor() {
+    super();
+  }
 
-    getParams() {
-            this.color = getStringParamFromTemplateRow(this._row, 'color',  'primary');
-            this.hexBgColor = getStringParamFromTemplateRow(this._row, 'hexBgColor', null);
-            this.width = getStringParamFromTemplateRow(this._row, 'width', '100%');
-            this.disabled = getBooleanParamFromTemplateRow(this._row, 'disabled', false);
-    }
+  ngOnInit() {
+    this.getParams();
+  }
+
+  getParams() {
+    this.style = getStringParamFromTemplateRow(this._row, "style", "primary");
+    this.color = getStringParamFromTemplateRow(this._row, "color", null);
+    this.width = getStringParamFromTemplateRow(this._row, "width", "100%");
+    this.height = getStringParamFromTemplateRow(this._row, "height", "62px");
+    this.disabled = getBooleanParamFromTemplateRow(this._row, "disabled", false);
+  }
 }
