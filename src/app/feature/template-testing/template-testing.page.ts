0ff717b3c5b6962c122ff28abf72e58012af096e
Yehor <egor.miroshnichenko@wtt-solutions.com>
2021-03-24 13:20:10 +0200

    feat: filter search

--- src/app/feature/template-testing/template-testing.page.ts
+++ src/app/feature/template-testing/template-testing.page.ts
@@ -9,11 +9,23 @@ import { TEMPLATE } from "src/app/shared/services/data/data.service";
 })
 export class TemplateTestingPage implements OnInit {
   templateName: string;
+  filterTerm: string;
   allTemplates = TEMPLATE.sort((a, b) => (a.flow_name > b.flow_name ? 1 : -1));
-
+  data = this.allTemplates;
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.templateName = this.route.snapshot.params.templateName;
+  }
+
+  search() {
+    this.allTemplates = this.data;
+    this.allTemplates = this.allTemplates.filter(
+      (i) => i.flow_name.toLocaleLowerCase().indexOf(this.filterTerm.toLowerCase()) > -1
+    );
+  }
+
+  trackByFn(index) {
+    return index;
   }
 }
