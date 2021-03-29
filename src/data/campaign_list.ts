be3782e3bee54815f4c29fe2b889ac460dca3a58
Yehor <egor.miroshnichenko@wtt-solutions.com>
2021-03-29 11:05:04 +0300

    feat: start

--- src/data/campaign_list.ts
+++ src/data/campaign_list.ts
@@ -35,6 +35,6 @@
         "every": "7_app:day"
       }
     ],
-    "_xlsxPath": "plh_sheets_beta\\ver_7_design\\periodic_content\\reminders.xlsx"
+    "_xlsxPath": "plh_sheets_beta/ver_7_design/periodic_content/reminders.xlsx"
   }
 ]
