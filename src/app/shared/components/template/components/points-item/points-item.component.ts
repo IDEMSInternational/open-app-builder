e108e5550a09fe8e6e8b56a051f9c3a2ff6ef887
Yehor <egor.miroshnichenko@wtt-solutions.com>
2021-04-02 12:19:23 +0300

    feat: add component functionality on click

--- src/app/shared/components/template/components/points-item/points-item.component.ts
+++ src/app/shared/components/template/components/points-item/points-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
 import { TemplateBaseComponent } from "../base";
 import { FlowTypes, ITemplateRowProps } from "../../models";
 import { getStringParamFromTemplateRow } from "../../../../utils";
@@ -13,11 +13,14 @@ export class TmplPointBoxComponent
   implements ITemplateRowProps, OnInit {
   @Input() template: FlowTypes.Template;
   @Input() localVariables: { [name: string]: any };
-
+  @ViewChild("star", { static: false }) star: ElementRef;
+  @ViewChild("item", { static: false }) item: ElementRef;
   icon_src: string | null;
   text: string | null;
   assetsPrefix = "/assets/plh_assets/";
   icon_result: string;
+  wasClicked: boolean = false;
+
   constructor() {
     super();
   }
@@ -30,10 +33,24 @@ export class TmplPointBoxComponent
     this.icon_src = getStringParamFromTemplateRow(this._row, "icon_src", null);
     this.text = getStringParamFromTemplateRow(this._row, "text", null);
     this.icon_result = this.getPathImg();
+    if (!this._row.value) this._row.value = 0;
   }
 
   getPathImg(): string {
     const src = this.assetsPrefix + this.icon_src;
     return src.replace("//", "/");
+  }
+
+  clickPointItem() {
+    this.triggerActions("click");
+    this._row.value += 1;
+    this.star.nativeElement.classList.add("on-add");
+    setTimeout((_) => {
+      this.star.nativeElement.classList.remove("on-add");
+    }, 1000);
+    if (!this.wasClicked) {
+      this.item.nativeElement.classList.add("complete");
+    }
+    this.wasClicked = true;
   }
 }
