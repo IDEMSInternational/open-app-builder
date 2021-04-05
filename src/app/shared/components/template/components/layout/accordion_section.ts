62da1320786b08afad90d01cd79cb4a226bd9e32
Andrii Hubariev <andrii.hubariev@wtt-solutions.com>
2021-04-05 18:48:55 +0800

    accordion changes

--- src/app/shared/components/template/components/layout/accordion_section.ts
+++ src/app/shared/components/template/components/layout/accordion_section.ts
@@ -1,43 +1,63 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  Output,
+  ViewChild,
+  EventEmitter,
+  Input,
+} from "@angular/core";
+import { BehaviorSubject } from "scripts/node_modules/rxjs";
 import { getStringParamFromTemplateRow } from "src/app/shared/utils";
 import { TemplateBaseComponent } from "../base";
 
 @Component({
   selector: "plh-tmpl-accordion-section",
-  template: `<div class="accordion-wrapper" [class.opened]="isOpened">
+  template: `<div class="accordion-wrapper">
     <div
       [ngClass]="{
         completed: status === 'completed',
-        uncopleted: status === 'uncompleted',
-        disabled: status === 'disabled'
+        disabled: status === 'disabled',
+        inProgress: status === 'inProgress'
       }"
       class="accordion-status"
       [ngSwitch]="status"
     >
-      <img *ngSwitchCase="'completed'" src="/assets/plh_assets/plh_images/icons/tick.svg" />
+      <img *ngSwitchCase="'completed'" src="/assets/plh_assets/plh_images/icons/tick_white.svg" />
       <img *ngSwitchCase="'disabled'" src="/assets/plh_assets/plh_images/icons/lock.svg" />
     </div>
-    <div class="accordion-section">
-      <div #progress class="progress">
-        <plh-template-component
-          (click)="toggleOpen($event)"
-          *ngFor="let childRow of _row.rows"
-          [row]="childRow"
-          [parent]="parent"
-        >
-        </plh-template-component>
-      </div>
+    <div
+      class="accordion-section"
+      [ngClass]="{
+        openSection: _row.parameter_list.state === 'open',
+        completed: status === 'completed'
+      }"
+      [class.open-section]="_row.parameter_list.state === 'open'"
+    >
+      <div #progress class="progress"></div>
+      <h2 (click)="toggleOpen()">{{ title }}</h2>
+      <plh-template-component
+        style="z-index: 2"
+        *ngFor="let childRow of _row.rows"
+        [row]="childRow"
+        [parent]="parent"
+      >
+      </plh-template-component>
     </div>
   </div>`,
-  styleUrls: ["./accordion.scss"],
+  styleUrls: ["./accordion.scss", "../tmpl-components-common.scss"],
 })
 export class AccordionSectionComponent
   extends TemplateBaseComponent
   implements OnInit, AfterViewInit {
-  public status: string;
-  public isOpened: boolean;
   private progressBar: HTMLElement;
-  private progressValue: number;
+  public status: string;
+  public title: string;
+  public progressValue$: BehaviorSubject<number> = new BehaviorSubject(0);
+
+  @Input() id: string;
+  @Output() toggleState = new EventEmitter<string>();
 
   @ViewChild("progress") progress: ElementRef;
 
@@ -47,18 +67,33 @@ export class AccordionSectionComponent
 
   ngAfterViewInit() {
     this.progressBar = this.progress.nativeElement;
-    this.progressBar.style.background = `linear-gradient(to right, var(--ion-color-tertiary) ${this.progressValue}%, #ffff 0%)`;
+    this.progressValue$.subscribe((value) => {
+      this.updateStatus(value);
+      this.updateProgressBar(value);
+    });
   }
 
-  toggleOpen(e) {
-    if (e.target.localName === "p" && this.status !== "disabled") {
-      this.isOpened = !this.isOpened;
+  public toggleOpen() {
+    if (this.status !== "disabled") {
+      this.toggleState.emit(this.id);
     }
   }
 
-  getParams() {
-    this.isOpened = getStringParamFromTemplateRow(this._row, "state", null) === "open";
-    this.status = getStringParamFromTemplateRow(this._row, "status", "disabled");
-    this.progressValue = this._row.value;
+  private getParams() {
+    this.status = getStringParamFromTemplateRow(this._row, "status", null);
+    this.title = getStringParamFromTemplateRow(this._row, "title", null);
+    this.progressValue$.next(this._row.value);
+  }
+
+  private updateProgressBar(value: number) {
+    this.progressBar.style.width = `${value}%`;
+  }
+
+  private updateStatus(value: number) {
+    if (value === 100) {
+      this.status = "completed";
+    } else if (value > 0) {
+      this.status = "inProgress";
+    }
   }
 }
