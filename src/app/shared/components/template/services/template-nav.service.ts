1e3a12e4554758d266f484d7da33097a4d559c6d
Michael Clapham <michaeljclapham@gmail.com>
2021-04-07 13:36:21 +0100

    Fix nested template goto

--- src/app/shared/components/template/services/template-nav.service.ts
+++ src/app/shared/components/template/services/template-nav.service.ts
@@ -49,11 +49,11 @@ export class TemplateNavService {
     action: FlowTypes.TemplateRowAction,
     container: TemplateContainerComponent
   ) {
-    const { router, name } = container;
+    const parentName = container.row ? container.row.value : container.name;
     const [templatename] = action.args;
     const nav_parent_triggered_by = action._triggeredBy;
-    const queryParams: INavQueryParams = { nav_parent: name, nav_parent_triggered_by };
-    return router.navigate(["template", templatename], {
+    const queryParams: INavQueryParams = { nav_parent: parentName, nav_parent_triggered_by };
+    return container.router.navigate(["template", templatename], {
       queryParams,
       queryParamsHandling: "merge",
     });
