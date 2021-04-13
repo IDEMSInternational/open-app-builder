39e641f8c797c3c44cea66dbcbf32e3e2de5768f
smborio <smborio@idems.international>
2021-04-13 10:55:15 +0100

    content update 2021/04/13 for content review

--- src/data/data_list.ts
+++ src/data/data_list.ts
@@ -3,6 +3,86 @@
   export const data_list: FlowTypes.Data_list[] = [
   {
     "flow_type": "data_list",
+    "flow_name": "relax_list",
+    "data_list_name": "relax_list",
+    "status": "released",
+    "rows": [
+      {
+        "id": "relax_1",
+        "title": "Breathe",
+        "task_id": "task_relax",
+        "text_template": "relax_text_1",
+        "audio_asset": "assets/plh_assets/plh_audio/sample.mp3",
+        "animation_asset": "assets/plh_assets/plh_images/habits/habit_relax_image.svg"
+      },
+      {
+        "id": "relax_2",
+        "title": "Senses",
+        "task_id": "task_relax",
+        "text_template": "relax_text_2",
+        "audio_asset": "assets/plh_assets/plh_audio/sample.mp3",
+        "animation_asset": "assets/plh_assets/plh_images/habits/habit_relax_image.svg"
+      },
+      {
+        "id": "relax_3",
+        "title": "Think about the day",
+        "task_id": "task_relax",
+        "text_template": "relax_text_3",
+        "audio_asset": "assets/plh_assets/plh_audio/sample.mp3",
+        "animation_asset": "assets/plh_assets/plh_images/habits/habit_relax_image.svg"
+      },
+      {
+        "id": "relax_4",
+        "title": "Breathe",
+        "task_id": "task_relax",
+        "text_template": "relax_text_4",
+        "audio_asset": "assets/plh_assets/plh_audio/sample.mp3",
+        "animation_asset": "assets/plh_assets/plh_images/habits/habit_relax_image.svg"
+      },
+      {
+        "id": "relax_5",
+        "title": "Breathe",
+        "task_id": "task_relax",
+        "text_template": "relax_text_5",
+        "audio_asset": "assets/plh_assets/plh_audio/sample.mp3",
+        "animation_asset": "assets/plh_assets/plh_images/habits/habit_relax_image.svg"
+      },
+      {
+        "id": "relax_6",
+        "title": "Think about the day",
+        "task_id": "task_relax",
+        "text_template": "relax_text_6",
+        "audio_asset": "assets/plh_assets/plh_audio/sample.mp3",
+        "animation_asset": "assets/plh_assets/plh_images/habits/habit_relax_image.svg"
+      },
+      {
+        "id": "relax_7",
+        "title": "Breathe",
+        "task_id": "task_relax",
+        "text_template": "relax_text_7",
+        "audio_asset": "assets/plh_assets/plh_audio/sample.mp3",
+        "animation_asset": "assets/plh_assets/plh_images/habits/habit_relax_image.svg"
+      },
+      {
+        "id": "relax_8",
+        "title": "Take a moment",
+        "task_id": "task_relax",
+        "text_template": "relax_text_8",
+        "audio_asset": "assets/plh_assets/plh_audio/sample.mp3",
+        "animation_asset": "assets/plh_assets/plh_images/habits/habit_relax_image.svg"
+      },
+      {
+        "id": "relax_9",
+        "task_id": "task_relax",
+        "text_template": "relax_text_9",
+        "audio_asset": "assets/plh_assets/plh_audio/sample.mp3",
+        "animation_asset": "assets/plh_assets/plh_images/habits/habit_relax_image.svg"
+      }
+    ],
+    "_xlsxPath": "plh_sheets_beta/plh_templating/core_templates/core_templates_relax.xlsx"
+  },
+  {
+    "flow_type": "data_list",
     "flow_name": "example_data_list",
     "status": "released",
     "data_list_name": "example",
