941680120c128c61f4d48791b194884744a0e1e3
Michael Clapham <michaeljclapham@gmail.com>
2021-04-14 17:51:49 +0100

    Workaround for intro.js to support ng routes

--- src/app/shared/services/tour/tour.service.ts
+++ src/app/shared/services/tour/tour.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 
 import introJs from "intro.js";
+import { NAME_CLASS_PREFIX } from "../../components/template/template-component";
 import { TOUR } from "../data/data.service";
 
 @Injectable({
@@ -23,23 +24,45 @@ export class TourService {
 
     if (matchingTour && matchingTour.rows && matchingTour.rows.length > 0) {
       this.introJS.setOptions({
-        steps: matchingTour.rows.map((row) => ({
-          intro: row.message_text,
-          title: row.title,
-          element: row.element,
-        })),
+        steps: matchingTour.rows.map((row) => {
+          let elementSelector = row.element;
+          return {
+            intro: row.message_text,
+            title: row.title,
+            element: elementSelector,
+            elementSelector: elementSelector,
+          } as any;
+        }),
         hidePrev: true,
-      });
-      this.introJS.onafterchange(() => {
-        setTimeout(() => this.introJS.refresh(), this.waitForRoutingDelay);
       });
       this.introJS.onbeforechange((elem) => {
         this.introJS.currentStep();
         const stepNumber = this.introJS.currentStep();
         const currentRow = matchingTour.rows[stepNumber];
+
+        /* If route changes then navigate, then after delay, actually change intro.js step */
         if (currentRow.route) {
-          this.router.navigateByUrl(currentRow.route);
+          const currentPath = this.router.url.replace(/^\//g, "");
+          const stepPath = currentRow.route.replace(/^\//g, "");
+          if (currentPath !== stepPath) {
+            this.router.navigateByUrl(currentRow.route).then(() => {
+              setTimeout(() => {
+                // Workaround to make sure element highlights work on new route
+                this.revaluateIntroElements();
+                setTimeout(() => {
+                  this.introJS.goToStepNumber(stepNumber + 1);
+                }, this.waitForRoutingDelay / 2);
+              }, this.waitForRoutingDelay / 2);
+            });
+            return false;
+          }
         }
+      });
+      this.introJS.onafterchange((elem) => {
+        this.introJS.refresh();
+        setTimeout(() => {
+          this.introJS.refresh();
+        }, this.waitForRoutingDelay);
       });
       const startRoute = matchingTour.rows[0].route ? matchingTour.rows[0].route : "module_list";
       await this.router.navigateByUrl(startRoute);
@@ -47,5 +70,19 @@ export class TourService {
         this.introJS.start();
       }, this.waitForRoutingDelay);
     }
+  }
+
+  revaluateIntroElements() {
+    // Workaround from here:
+    // https://github.com/usablica/intro.js/issues/717#issuecomment-646445169
+    this.introJS["_options"].steps.forEach((step, key) => {
+      if (step.elementSelector) {
+        const el = document.querySelector(step.elementSelector);
+        if (el !== null) {
+          this.introJS["_introItems"][key].element = el;
+        }
+        this.introJS["_introItems"][key].position = step.position ? step.position : "bottom";
+      }
+    });
   }
 }
