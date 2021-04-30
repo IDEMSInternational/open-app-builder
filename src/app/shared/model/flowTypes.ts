e7e2c8f9ce698b01eb7eb550e0c37d8910925de4
Chris <chris.m.clarke@live.co.uk>
2021-04-30 08:25:38 +0100

    fix type restrictions

--- src/app/shared/model/flowTypes.ts
+++ src/app/shared/model/flowTypes.ts
@@ -37,6 +37,8 @@ export namespace FlowTypes {
   export interface FlowTypeBase {
     flow_type: FlowType;
     flow_name: string;
+    /** allows further level of grouping within flows */
+    flow_subtype?: string;
     /** Used to hide unfinished content from the app */
     status: "draft" | "released";
     /** Events triggered from the flow that would ordinarily write to the db (e.g. emit completed) will be ignored */
@@ -46,8 +48,9 @@ export namespace FlowTypes {
     module?: string;
     /** if specified, row data will be made accessible via the `@data` accessor within the provided namespace */
     data_list_name?: string;
-    // debug info
-    _xlsxPath?: string;
+
+    _xlsxPath?: string; // debug info
+    __EMPTY?: string; // empty cells (can be removed after pr 679 merged)
   }
 
   /**
