6f5382cd7c3fa387577e86ddf6be29faa6e5f481
Andrii Hubariev <andrii.hubariev@wtt-solutions.com>
2021-04-30 20:55:45 +0800

    added page scroll to the top

--- src/app/feature/template-testing/template-testing.page.ts
+++ src/app/feature/template-testing/template-testing.page.ts
@@ -1,21 +1,35 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit, ViewChild } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { IonContent } from "@ionic/angular";
+import { fromEvent, Subscription } from "rxjs";
+import { delay } from "rxjs/operators";
 import { TEMPLATE } from "src/app/shared/services/data/data.service";
-
 @Component({
   selector: "plh-template-testing",
   templateUrl: "./template-testing.page.html",
   styleUrls: ["./template-testing.page.scss"],
 })
-export class TemplateTestingPage implements OnInit {
+export class TemplateTestingPage implements OnInit, OnDestroy {
+  @ViewChild("pageTop") pageTop: IonContent;
   templateName: string;
   filterTerm: string;
   allTemplates = TEMPLATE.sort((a, b) => (a.flow_name > b.flow_name ? 1 : -1));
   data = this.allTemplates;
+  subscription: Subscription;
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.templateName = this.route.snapshot.params.templateName;
+    this.subscription = fromEvent(document, "click")
+      .pipe(delay(SCROLL_TOP_DELEY))
+      .subscribe((e: { target }) => {
+        if (
+          e.target.classList.contains("navigation") ||
+          e.target.parentElement.classList.contains("navigation")
+        ) {
+          this.pageScroller();
+        }
+      });
   }
 
   search() {
@@ -25,7 +39,18 @@ export class TemplateTestingPage implements OnInit {
     );
   }
 
+  public pageScroller() {
+    this.pageTop.scrollToTop(SCROLL_TOP_DURATION);
+  }
+
   trackByFn(index) {
     return index;
   }
+
+  ngOnDestroy() {
+    if (this.subscription) this.subscription.unsubscribe();
+  }
 }
+
+const SCROLL_TOP_DELEY = 100;
+const SCROLL_TOP_DURATION = 500;
