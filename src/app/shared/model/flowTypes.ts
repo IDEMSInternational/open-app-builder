e6271042881e00c7ec93d38d5f9288d58e0f06ab
Chris <chris.m.clarke@live.co.uk>
2021-05-18 15:50:08 +0100

    type tidying

--- src/app/shared/model/flowTypes.ts
+++ src/app/shared/model/flowTypes.ts
@@ -452,7 +452,7 @@ export namespace FlowTypes {
       | "set_theme"
       | "start_tour"
       | "trigger_actions";
-    args: any[]; // likely string or boolean
+    args: (string | boolean)[];
     /** field populated for tracking the component that triggered the action */
     _triggeredBy?: TemplateRow;
     // debug info
