de5c4b863013b864262e1a2f85eb31fdc7509abc
Chris <chris.m.clarke@live.co.uk>
2021-05-18 21:33:41 +0100

    remove reminders page

--- src/app/feature/reminders/reminders.module.ts
+++ src/app/feature/reminders/reminders.module.ts
@@ -3,11 +3,8 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { IonicModule } from "@ionic/angular";
 
-import { RemindersPageRoutingModule } from "./reminders-page-routing.module";
-import { RemindersPageComponent } from "./pages/reminders-page.component";
-
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, RemindersPageRoutingModule],
-  declarations: [RemindersPageComponent],
+  imports: [CommonModule, FormsModule, IonicModule],
+  declarations: [],
 })
 export class RemindersModule {}
