e669d0c4310151cc507d3a16e6ca0913f9fc66a2
Chris <chris.m.clarke@live.co.uk>
2021-05-21 11:53:55 +0100

    add notifications debug page

---
+++ src/app/feature/campaign/pages/notifications-debug/notifications-debug.page.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  templateUrl: "./notifications-debug.page.html",
+  styleUrls: ["./notifications-debug.page.scss"],
+})
+export class NotificationsDebugPage implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
