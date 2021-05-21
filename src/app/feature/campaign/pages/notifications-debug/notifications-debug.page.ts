aba0e5ce442776dff0ccce3e571d56860b14d21e
Chris <chris.m.clarke@live.co.uk>
2021-05-21 13:19:20 +0100

    notifications debug page

--- src/app/feature/campaign/pages/notifications-debug/notifications-debug.page.ts
+++ src/app/feature/campaign/pages/notifications-debug/notifications-debug.page.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from "@angular/core";
+import { LocalNotification } from "@capacitor/core";
+import { LocalNotificationService } from "src/app/shared/services/notification/local-notification.service";
 
 @Component({
   templateUrl: "./notifications-debug.page.html",
   styleUrls: ["./notifications-debug.page.scss"],
 })
 export class NotificationsDebugPage implements OnInit {
-  constructor() {}
+  constructor(public localNotificationService: LocalNotificationService) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.localNotificationService.loadNotifications();
+  }
+
+  public async removeNotification(notification: LocalNotification) {
+    await this.localNotificationService.removeNotification(notification);
+  }
+
+  public logDebugInfo(notification: LocalNotification) {
+    console.group(notification.extra.id);
+    console.log(notification);
+    console.groupEnd();
+  }
 }
