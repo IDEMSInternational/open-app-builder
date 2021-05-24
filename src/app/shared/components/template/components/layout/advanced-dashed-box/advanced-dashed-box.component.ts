8a449d1a86662b0080fbcdd5d8b5ad654e4ccffb
Andrii Hubariev <andrii.hubariev@wtt-solutions.com>
2021-05-24 19:00:53 +0800

    dashed_box type for display group

--- src/app/shared/components/template/components/layout/advanced-dashed-box/advanced-dashed-box.component.ts
+++ src/app/shared/components/template/components/layout/advanced-dashed-box/advanced-dashed-box.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from "@angular/core";
-import { SafeHtml, DomSanitizer } from "@angular/platform-browser";
+import { Component, Input, OnInit } from "@angular/core";
+import { FlowTypes } from "scripts/types";
 import { getStringParamFromTemplateRow } from "src/app/shared/utils";
 import { TemplateBaseComponent } from "../../base";
 
@@ -9,27 +9,30 @@ import { TemplateBaseComponent } from "../../base";
   styleUrls: ["./advanced-dashed-box.component.scss"],
 })
 export class TmplAdvancedDashedBoxComponent extends TemplateBaseComponent implements OnInit {
+  @Input() templateRow: FlowTypes.TemplateRow;
   style: string;
   icon_src: string | null;
   assetsPrefix = "/assets/plh_assets/";
   icon_result: string;
   icon_position: string;
-  innerHTML: SafeHtml;
 
-  constructor(private domSanitizer: DomSanitizer) {
+  constructor() {
     super();
   }
 
   ngOnInit() {
     this.getParams();
-    this.innerHTML = this.domSanitizer.bypassSecurityTrustHtml(this._row.value);
   }
 
   getParams() {
-    this.icon_src = getStringParamFromTemplateRow(this._row, "icon_src", "");
-    this.style = getStringParamFromTemplateRow(this._row, "style", "default");
+    this.icon_src = getStringParamFromTemplateRow(this.templateRow, "icon_src", "");
+    this.style = getStringParamFromTemplateRow(this.templateRow, "style", "default");
     this.icon_result = this.getPathImg();
-    this.icon_position = getStringParamFromTemplateRow(this._row, "icon_position", "top-left");
+    this.icon_position = getStringParamFromTemplateRow(
+      this.templateRow,
+      "icon_position",
+      "top-left"
+    );
   }
 
   getPathImg(): string {
