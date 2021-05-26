24080991a9a65e9033c49fdc361e44785a469176
Andrii Hubariev <andrii.hubariev@wtt-solutions.com>
2021-05-26 16:54:30 +0800

    numbering pipe checking for first line text

--- src/app/shared/components/template/pipes/numbering.pipe.ts
+++ src/app/shared/components/template/pipes/numbering.pipe.ts
@@ -6,12 +6,14 @@ import { Pipe, PipeTransform } from "@angular/core";
 export class NumberingPipe implements PipeTransform {
   transform(value: any, args?: any[]): any {
     let result: string;
-    result = value
+    const firstLineText = value.match(/(.*?\:)/, "");
+    const list = value
       .split("-")
       .map((r, idx) => {
         if (idx > 0) return (r = `<li>${r}</li>`);
       })
       .join(" ");
-    return `<ol>${result}<ol>`;
+    result = firstLineText ? `<p>${firstLineText[0]}</p><ol>${list}<ol>` : `<ol>${list}<ol>`;
+    return result;
   }
 }
