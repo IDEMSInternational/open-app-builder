e2c671dace9be0773018dbd045d2400dbf689091
chrismclarke <chris.m.clarke@live.co.uk>
2021-07-01 13:39:23 -0700

    add yarn workspaces

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,18 +1,18 @@
-const logger = require('./src/helpers/logger');
-const db = require('./src/database/database.service');
-const SyncServer = require('./src/sync-server/sync-server.service');
+const logger = require("./src/helpers/logger");
+const db = require("./src/database/database.service");
+const SyncServer = require("./src/sync-server/sync-server.service");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const webSocketPort = process.env.WEBSOCKET_PORT;
 
 try {
   db.client.connect().then(() => {
     logger.info(`Connected to database.`);
-    
+
     SyncServer(webSocketPort);
     logger.info(`Websocket server started at ${webSocketPort} port.`);
   });
-} catch(err) {
+} catch (err) {
   logger.error(err);
 }
