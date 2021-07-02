cf0afed3112eeabaf575ba7162ed4ed115d514e2
chrismclarke <chris.m.clarke@live.co.uk>
2021-07-02 13:48:33 -0700

    integrate migrations

---
+++ packages/server/docker/config/db/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Example config (not currently required)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER docker;
+    CREATE DATABASE docker;
+    GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+EOSQL
