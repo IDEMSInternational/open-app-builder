5fb1a8735d7a6fe069d9b5ff73f046667315828c
chrismclarke <chris.m.clarke@live.co.uk>
2021-07-04 13:54:39 -0700

    building out backend systems and services

--- packages/server/docker/config/db/init.sh
+++ packages/server/docker/config/db/init.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -e
 
-# Create Metabase table
-# TODO - use env vars for pw
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER metabase WITH PASSWORD 'metabase';
-    CREATE DATABASE metabase;
-    GRANT ALL PRIVILEGES ON DATABASE metabase TO metabase;
+# Create App user/password/table
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER $DB_USER WITH PASSWORD '${DB_PASSWORD}';
+    CREATE DATABASE ${DB_NAME};
+    GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_USER};
 EOSQL
+
+# Create Metabase user/password/table
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER ${MB_DB_USER} WITH PASSWORD '${MB_DB_PASS}';
+    CREATE DATABASE ${MB_DB_DBNAME};
+    GRANT ALL PRIVILEGES ON DATABASE ${MB_DB_DBNAME} TO ${MB_DB_USER};
+EOSQL
+
+
