08a91f85ba5959dde07c401f51c7d8ed8f624682
chrismclarke <chris.m.clarke@live.co.uk>
2021-07-09 00:39:13 -0700

    fix cors and routes

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -9,18 +9,20 @@ async function bootstrap() {
   await setupDB();
   // API Boostrap (auto connects to DB)
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   // Make available on reverse proxy path (e.g. /api)
-  app.setGlobalPrefix(environment.API_BASE_PATH || "");
+  // app.setGlobalPrefix(environment.API_BASE_PATH || "");
   // OpenAPI/Swagger documentation bootstrap
   const config = new DocumentBuilder()
     .setTitle("IDEMS Apps API")
     .setDescription("App-Server Communication")
     .setVersion("1.0")
     .addTag("api")
+    // .setBasePath("/api")
     // Fix swagger redirection issue
     // https://github.com/nestjs/swagger/issues/448
     // https://stackoverflow.com/questions/63954037/nestjs-swagger-missing-base-path
-    .addServer(environment.API_BASE_PATH || "")
+    .addServer(environment.API_BASE_PATH ? `/${environment.API_BASE_PATH}` : "")
     .build();
   const document = SwaggerModule.createDocument(app, config, { ignoreGlobalPrefix: true });
   SwaggerModule.setup("", app, document);
