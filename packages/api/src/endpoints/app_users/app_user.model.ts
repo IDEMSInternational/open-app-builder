33ce290065a33af86bf8b5614412e75aac2f8731
chrismclarke <chris.m.clarke@live.co.uk>
2021-07-09 11:45:37 -0700

    add timestamps to app_users

--- packages/api/src/endpoints/app_users/app_user.model.ts
+++ packages/api/src/endpoints/app_users/app_user.model.ts
@@ -1,7 +1,7 @@
 import { Column, DataType, Model, Table } from "sequelize-typescript";
 
 // NOTE - the table will not be created from this model, but instead via db migration scripts
-@Table({ tableName: "app_users" })
+@Table({ tableName: "app_users", timestamps: true })
 export class AppUser extends Model<AppUser> {
   @Column({ allowNull: false, unique: true })
   app_user_id: string;
