78834e3bd6213e19ba91b583745c29b4ff03177d
chrismclarke <chris.m.clarke@live.co.uk>
2021-07-11 00:44:06 -0700

    add workspace data

---
+++ packages/plh-data/data/template/index.ts
@@ -0,0 +1,4 @@
+import { FlowTypes } from "../../model/flowTypes";
+import template_debug from "./template.debug";
+import template_1 from "./template";
+export const template: FlowTypes.Template[] = [].concat(template_debug, template_1);
