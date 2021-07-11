ca77bb5dba9baf7e85f9f9c66d75ee548216c1f7
chrismclarke <chris.m.clarke@live.co.uk>
2021-07-11 00:44:06 -0700

    add workspace data

---
+++ packages/plh-data/data/module_page/index.ts
@@ -0,0 +1,3 @@
+import { FlowTypes } from "../../model/flowTypes";
+import module_page_0 from "./module_page";
+export const module_page: FlowTypes.Module_page[] = [].concat(module_page_0);
---
+++ packages/plh-data/data/tour/index.ts
@@ -0,0 +1,3 @@
+import { FlowTypes } from "../../model/flowTypes";
+import tour_0 from "./tour";
+export const tour: FlowTypes.Tour[] = [].concat(tour_0);
