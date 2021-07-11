c7f01e6032f48d51997a615a25fbcd21a26bbfb5
chrismclarke <chris.m.clarke@live.co.uk>
2021-07-11 00:41:58 -0700

    create data workspace

---
+++ packages/plh-data/index.ts
@@ -0,0 +1,18 @@
+export { care_package_list } from "./data/care_package_list";
+export { completion_list } from "./data/completion_list";
+export { component_defaults } from "./data/component_defaults";
+export { conversation } from "./data/conversation";
+export { data_list } from "./data/data_list";
+export { global } from "./data/global";
+export { goal_list } from "./data/goal_list";
+export { habit_ideas } from "./data/habit_ideas";
+export { habit_list } from "./data/habit_list";
+export { home_page } from "./data/home_page";
+export { module_list } from "./data/module_list";
+export { module_page } from "./data/module_page";
+export { task_list } from "./data/task_list";
+export { template } from "./data/template";
+export { tips } from "./data/tips";
+export { tour } from "./data/tour";
+
+export * from "./model";
