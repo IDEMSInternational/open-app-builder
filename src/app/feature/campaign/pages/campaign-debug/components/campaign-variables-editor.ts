9718e3797d0e0fb6d58c0d76b2a9c3cf67c48c88
chrismclarke <chris.m.clarke@live.co.uk>
2021-07-21 23:06:34 -0700

    add editor support for field first setter

--- src/app/feature/campaign/pages/campaign-debug/components/campaign-variables-editor.ts
+++ src/app/feature/campaign/pages/campaign-debug/components/campaign-variables-editor.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, OnInit } from "@angular/core";
+import { AfterViewInit, Component, Input } from "@angular/core";
 import { addDays } from "@fullcalendar/common";
 import { ModalController } from "@ionic/angular";
 import { TemplateService } from "src/app/shared/components/template/services/template.service";
@@ -8,6 +8,7 @@ import {
   DataEvaluationService,
   IDataEvaluationCache,
 } from "src/app/shared/services/data/data-evaluation.service";
+import { DbService } from "src/app/shared/services/db/db.service";
 import { generateTimestamp } from "src/app/shared/utils";
 
 @Component({
@@ -23,7 +24,7 @@ import { generateTimestamp } from "src/app/shared/utils";
       </ion-toolbar>
     </ion-header>
     <ion-content class="ion-padding background-primary-lighter" *ngIf="dbData">
-      <h4>App Settings</h4>
+      <h4>DB Variables</h4>
       <ion-item>
         <ion-label>
           <span>First Launch</span>
@@ -50,13 +51,23 @@ import { generateTimestamp } from "src/app/shared/utils";
       <h4>Field Variables</h4>
       <ion-item *ngFor="let row of editorRows">
         <ion-label>{{ row.field }}</ion-label>
+        <ion-datetime
+          *ngIf="row.condition_type === 'db_lookup'"
+          #fieldFirst
+          placeholder="Set First"
+          [value]="row.current_value_first"
+          (ionChange)="setFieldFirst(row.field, row.current_value, fieldFirst.value)"
+        ></ion-datetime>
         <ion-select
           #select
-          [value]="row.value"
+          [value]="row.current_value"
           (ionChange)="setFieldVariable(row.field, select.value)"
           [compareWith]="compareFieldValue"
+          placeholder="Set Value"
         >
-          <ion-select-option *ngFor="let value of row.values_list">{{ value }}</ion-select-option>
+          <ion-select-option *ngFor="let value of row.condition_values">{{
+            value
+          }}</ion-select-option>
           <ion-select-option value="">(null)</ion-select-option>
         </ion-select>
       </ion-item>
@@ -77,14 +88,19 @@ export class CampaignDebugVariablesEditorComponent implements AfterViewInit {
     public modalCtrl: ModalController,
     private templateService: TemplateService,
     private dataEvaluationService: DataEvaluationService,
-    private appEvents: AppEventService
+    private appEvents: AppEventService,
+    private dbService: DbService
   ) {}
 
-  editorRows: IEditorRow = [];
+  editorRows: IEditorRow[] = [];
 
   async ngAfterViewInit() {
-    this.dbData = await this.dataEvaluationService.refreshDBCache();
+    await this.loadDBData();
     this.editorRows = this.generateVariablesList(this.campaignRows);
+  }
+
+  private async loadDBData() {
+    this.dbData = await this.dataEvaluationService.refreshDBCache();
   }
 
   public setFieldVariable(field: string, value: string) {
@@ -92,6 +108,13 @@ export class CampaignDebugVariablesEditorComponent implements AfterViewInit {
       value = null;
     }
     this.templateService.setField(field, value);
+  }
+
+  public async setFieldFirst(field: string, fieldValue: string, firstDate: string) {
+    // modify all db records so that first will have fixed timestamp
+    const dbRef = this.dbService.table("data_events").where("name").equals(field);
+    await dbRef.modify({ _created: generateTimestamp(firstDate) });
+    await this.loadDBData();
   }
 
   /**
@@ -115,46 +138,72 @@ export class CampaignDebugVariablesEditorComponent implements AfterViewInit {
             _created: launchTimestamp,
           });
         }
-        this.dbData = await this.dataEvaluationService.refreshDBCache();
+        await this.loadDBData();
       }
     }
   }
 
   /**
+   * Iterate over campaign list rows and extract all field evaluation and db_lookup conditions
+   * For case of field evaluation or get_field:first (db lookup) populate a list of variables
+   * used, current value, and values list to satisfy conditions
    *
-   * @returns
+   * NOTE - the method is a bit hacky as originally only designed to accomodate localstorage
+   * variables but now also used for db get_first condition (and should be merged with other hardcoded db methods above)
    */
   private generateVariablesList(rows: FlowTypes.Campaign_listRow[]) {
-    const currentValues = this.loadFieldVariables();
-    const fieldValues = {};
+    const contactFieldValues = this.loadFieldVariables();
+    const fieldValues: { [field: string]: IEditorRow } = {};
     rows.forEach((row) => {
       const conditions = [...row.activation_condition_list, ...row.deactivation_condition_list];
       conditions.forEach((condition) => {
         const { condition_type, condition_args } = condition;
+        let field: string;
+        let condition_value: string;
+        let current_value: any;
+        let current_value_first: string;
         switch (condition_type) {
+          // extract data from localStorage contact variables
           case "field_evaluation":
-            const { field, value } = condition_args.field_evaluation;
-            if (!fieldValues[field]) {
-              fieldValues[field] = {};
-            }
-            fieldValues[field][value] = value;
+            field = condition_args.field_evaluation.field;
+            condition_value = condition_args.field_evaluation.value;
+            current_value = contactFieldValues[`rp-contact-field.${field}`];
             break;
+          // extract data from db tables (currently data_events only)
           case "db_lookup":
+            if (condition_args.db_lookup.table_id === "data_events") {
+              field = condition_args.db_lookup.filter.field;
+              condition_value = condition_args.db_lookup.filter.value;
+              let allResults = this.dataEvaluationService.data.dbCache.data_events[field] || [];
+              if (condition_args.db_lookup.order === "desc") {
+                allResults = allResults.reverse();
+              }
+              current_value = allResults[0]?.value;
+              current_value_first = allResults[0]?._created;
+            }
             break;
-          default:
-            break;
+        }
+        if (field) {
+          if (!fieldValues[field]) {
+            fieldValues[field] = {
+              condition_values: [],
+              condition_type,
+              current_value,
+              field,
+              current_value_first,
+            };
+          }
+          if (!fieldValues[field].condition_values.includes(condition_value)) {
+            fieldValues[field].condition_values.push(condition_value);
+          }
         }
       });
     });
-    const variablesWithRecommendedValues: IEditorRow = [];
+    const variablesWithRecommendedValues: IEditorRow[] = [];
     Object.keys(fieldValues)
       .sort()
       .forEach((field) => {
-        variablesWithRecommendedValues.push({
-          field,
-          values_list: Object.values(fieldValues[field]),
-          value: currentValues[`rp-contact-field.${field}`],
-        });
+        variablesWithRecommendedValues.push(fieldValues[field]);
       });
     return variablesWithRecommendedValues;
   }
@@ -178,6 +227,8 @@ export class CampaignDebugVariablesEditorComponent implements AfterViewInit {
 }
 type IEditorRow = {
   field: string;
-  value: string;
-  values_list: string[];
-}[];
+  current_value: string;
+  current_value_first: string;
+  condition_values: string[];
+  condition_type: FlowTypes.DataEvaluationCondition["condition_type"];
+};
