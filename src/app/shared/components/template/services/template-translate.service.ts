17178b337b3f91ac8696250b9c0ea99f02a0817e
chrismclarke <chris.m.clarke@live.co.uk>
2021-07-29 16:43:52 -0700

    add app imports for translation strings

--- src/app/shared/components/template/services/template-translate.service.ts
+++ src/app/shared/components/template/services/template-translate.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { TRANSLATION_STRINGS } from "plh-data";
 import { LocalStorageService } from "src/app/shared/services/local-storage/local-storage.service";
 import { FlowTypes } from "../models";
 
@@ -15,18 +16,31 @@ const DEFAULT_LANGUAGE = "eng";
  */
 export class TemplateTranslateService {
   app_language: string;
+  translation_strings = {};
 
   constructor(private localStorageService: LocalStorageService) {
-    this.app_language = localStorageService.getString(APP_LANGUAGE_FIELD) || DEFAULT_LANGUAGE;
+    const currentLanguage = localStorageService.getString(APP_LANGUAGE_FIELD);
+    if (currentLanguage) {
+      this.setLanguage(currentLanguage, false);
+    } else {
+      this.setLanguage(DEFAULT_LANGUAGE, true);
+    }
   }
 
   /** Set the local storage variable that tracks the app language */
-  setLanguage(code: string) {
-    this.localStorageService.setString(APP_LANGUAGE_FIELD, code);
-    this.app_language = code;
+  setLanguage(code: string, updateDB = true) {
+    if (code) {
+      if (updateDB) {
+        this.localStorageService.setString(APP_LANGUAGE_FIELD, code);
+      }
+      this.app_language = code;
+      this.translation_strings = TRANSLATION_STRINGS[code] || {};
+      console.log("[Language Set]", code);
+    }
   }
 
   /**
+   * TODO - CC 2021-07-09 Update description following system updates
    * Take a template and return with value assigned to correct language translation
    * @param row template row where translatable value field formatted as:
    * ```
@@ -45,27 +59,32 @@ export class TemplateTranslateService {
   translateRow(row: FlowTypes.TemplateRow = {} as any) {
     const translated = { ...row };
     // Case 1 - row with translate fields identified (e.g. template row)
-    if (row._translatedFields) {
-      Object.keys(row._translatedFields).forEach((field) => {
-        translated[field] = this.translateValue(row._translatedFields[field]);
+    if (row._translations) {
+      Object.keys(row._translations).forEach((field) => {
+        translated[field] = this.translateValue(row[field]);
       });
     }
     // Case 2 - row value assigned from data list with translate fields
+    // TODO - confirm if working for new implementation
     const { value } = row;
     if (value && value._translatedFields) {
-      Object.keys(value._translatedFields).forEach((field) => {
-        translated.value[field] = this.translateValue(row.value._translatedFields[field]);
-      });
+      console.warn("[Translations] - requires implementation", value);
+      // Object.keys(value._translatedFields).forEach((field) => {
+      //   translated.value[field] = this.translateValue(row.value._translatedFields[field]);
+      // });
     }
     // Note - there is a third case when row value assigned from calculation (e.g. data list child field)
     // but this is currently manually handled in the template-variables service as required
     return translated;
   }
 
-  private translateValue(fieldTranslations: { [language_code: string]: any } = {}) {
-    if (!fieldTranslations.hasOwnProperty(this.app_language)) {
-      console.warn("[Translation missing]", `[${this.app_language}] ${fieldTranslations.eng}`);
+  public translateValue(value: any) {
+    let translated = value;
+    if (typeof value === "string" && this.translation_strings.hasOwnProperty(value)) {
+      translated = this.translation_strings[value];
+    } else {
+      // console.warn("[Translation missing]", `[${this.app_language}] ${fieldTranslations.eng}`);
     }
-    return fieldTranslations[this.app_language];
+    return translated;
   }
 }
