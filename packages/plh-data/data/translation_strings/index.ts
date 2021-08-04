9822c66ebe5655958488afcda2ce995de3f1e74c
EsmeeIDEMS <esmee@idems.international>
2021-08-04 12:22:56 +0100

    Replace translation_strings by TRANSLATION_STRINGS

--- packages/plh-data/data/translation_strings/index.ts
+++ packages/plh-data/data/translation_strings/index.ts
@@ -1,5 +1,5 @@
 import { FlowTypes } from "../../model/flowTypes";
 import translation_strings_0 from "./translation_strings";
-export const translation_strings: FlowTypes.Translation_strings[] = [].concat(
+export const TRANSLATION_STRINGS: FlowTypes.Translation_strings[] = [].concat(
   translation_strings_0
 );
