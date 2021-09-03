b1d3286eec8885523d374cf9e231c4ad5b0c2466
Chris <chris.m.clarke@live.co.uk>
2021-09-03 23:38:12 +0100

    add app field constants

--- packages/data-models/index.ts
+++ packages/data-models/index.ts
@@ -1,4 +1,4 @@
-export * from "./constants";
+export { APP_FIELDS, FIELD_PREFIX, DYNAMIC_PREFIXES } from "./constants";
 export * from "./flowTypes";
 export * from "./rapidpro.model";
 export * from "./tips.model";
