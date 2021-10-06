b83d0597ab371a6fe955b9b6e2ba129b30872048
chrismclarke <chris.m.clarke@live.co.uk>
2021-10-06 13:37:33 -0700

    improve table controller service bindings

--- packages/api/src/endpoints/tables/tables.service.ts
+++ packages/api/src/endpoints/tables/tables.service.ts
@@ -1,23 +1,43 @@
 import { Injectable } from "@nestjs/common";
-import { ColumnMappingType, FieldDataType } from "src/types";
-import { addTableColumn, arrayToHashmap, dropTableColumn, listTableColumns } from "src/utils";
+import { EventEmitter2 } from "@nestjs/event-emitter";
+import { InjectConnection } from "@nestjs/sequelize";
+import { Sequelize } from "sequelize";
+import { ColumnMappingType, CUSTOM_EVENTS, FieldDataTypeMapping } from "src/types";
+import { arrayToHashmap, dropTableColumn, listTableColumns } from "src/utils";
 
 @Injectable()
 export class TableService {
-  // TODO - add auth requirement
+  constructor(
+    @InjectConnection()
+    private sequelize: Sequelize,
+    private eventEmitter: EventEmitter2
+  ) {}
 
-  /**
-   *
-   */
+  /** Dynamically create or drop table columns */
   async updateTableColumns(table_name: string, op: "add" | "drop", columns: ColumnMappingType[]) {
     const tableColumns = await listTableColumns(table_name);
     const tableColumnsHashmap = arrayToHashmap(tableColumns, "column_name");
     switch (op) {
       case "add":
         const newColumns = columns.filter((c) => !tableColumnsHashmap.hasOwnProperty(c.field_name));
+        // TODO - handle remapping of existing column
+        // TODO - could process together as transaction
         for (const column of newColumns) {
-          const field_type = FieldDataType[column.field_type || "text"] || FieldDataType.text;
-          await addTableColumn(table_name, column.field_name, field_type);
+          const queryInterface = this.sequelize.getQueryInterface();
+          const fieldTypeKey = column.field_type || "text";
+          if (!FieldDataTypeMapping.hasOwnProperty(fieldTypeKey)) {
+            console.error(`[${fieldTypeKey}] not supported`);
+            throw new Error(
+              `[${fieldTypeKey}] not supported, available types: [${Object.keys(
+                FieldDataTypeMapping
+              )}]`
+            );
+          }
+          await queryInterface.addColumn(
+            table_name,
+            column.field_name,
+            FieldDataTypeMapping[fieldTypeKey]
+          );
         }
         break;
       case "drop":
@@ -30,34 +50,8 @@ export class TableService {
         break;
     }
     const updatedColumns = await listTableColumns(table_name);
-    // await this.repopulateRawRows();
+    // notify listeners via event in case reprocessing required
+    this.eventEmitter.emit(`${CUSTOM_EVENTS.COLUMNS_UPDATED}.${table_name}`, updatedColumns);
     return { columns: updatedColumns };
-  }
-
-  /**
-   * Take data that has been populated into a JSONB column and attempt to flatten into
-   * individual columns from json keys (if columns exist)
-   */
-  async flattenTableJSONRows(table_name: string, json_column_name: string) {
-    let tableColumns = await listTableColumns(table_name);
-    // TODO
-  }
-
-  /**
-   * TODO - could possibly move to other service (or map to above function)
-   */
-  private async repopulateRawRows() {
-    // await loadColumns();
-    // const users = await model.findAll();
-    // // TODO - better handled with bulk method but not sure if they support raw
-    // for (const user of users) {
-    //   const mappedData = getMappedFieldsFromData(user.raw);
-    //   // apply a raw update to allow population of dynamic fields
-    //   await user.update({ ...mappedData }, { raw: true });
-    // }
-    // model.bulkCreate(update, {
-    //   updateOnDuplicate: ["app_user_id"],
-    //   fields: Object.keys(columnsHashmap) as any,
-    // });
   }
 }
