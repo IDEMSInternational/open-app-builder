cdf54d203bc840301e71ab531a90121a640fb64c
chrismclarke <chris.m.clarke@live.co.uk>
2021-10-06 13:41:40 -0700

    add data type mappings

--- packages/api/src/endpoints/tables/dto/column-mapping.dto.ts
+++ packages/api/src/endpoints/tables/dto/column-mapping.dto.ts
@@ -1,8 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { ColumnMappingType, FieldDataType } from "src/types";
+import { ColumnMappingType } from "src/types";
 
 const ColumnMappingExample: ColumnMappingType[] = [
-  { field_name: "example_text", field_type: FieldDataType.text },
+  { field_name: "test_text", field_type: "text" },
+  { field_name: "test_integer", field_type: "integer" },
+  { field_name: "test_number", field_type: "number" },
+  { field_name: "test_date", field_type: "date" },
+  { field_name: "test_boolean", field_type: "boolean" },
 ];
 
 export class TableColumnMappingDto {
