228d8074c4285b97c0fc6e42d02c0c764b831e81
chrismclarke <chris.m.clarke@live.co.uk>
2021-10-06 13:41:40 -0700

    add data type mappings

--- packages/api/src/types/index.ts
+++ packages/api/src/types/index.ts
@@ -1,9 +1,21 @@
+import Sequelize from "sequelize";
+
 // TODO - could be optimised for more specific types
-export enum FieldDataType {
-  text = "text",
-  integer = "integer",
-  number = "numeric",
-}
-export type ColumnMappingType = { field_name: string; field_type?: FieldDataType };
+// https://sequelize.org/v5/manual/data-types.html
+export const FieldDataTypeMapping = {
+  text: Sequelize.TEXT,
+  integer: Sequelize.INTEGER,
+  number: Sequelize.REAL, // postgres only
+  date: Sequelize.DATE,
+  boolean: Sequelize.BOOLEAN,
+} as const;
+
+type FieldDataMappingKey = keyof typeof FieldDataTypeMapping;
+
+export type ColumnMappingType = { field_name: string; field_type?: FieldDataMappingKey };
 
 export type ITableColumn = { column_name: string; data_type: string };
+
+export const CUSTOM_EVENTS = {
+  COLUMNS_UPDATED: "update_columns",
+};
