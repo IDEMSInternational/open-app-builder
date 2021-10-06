7589c9f86433fedf63917c7c9c0b0cb5923e97a8
chrismclarke <chris.m.clarke@live.co.uk>
2021-10-06 13:31:02 -0700

    add tables middleware to limit writeable tables

--- packages/api/src/endpoints/tables/index.ts
+++ packages/api/src/endpoints/tables/index.ts
@@ -1,12 +1,17 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { TableService } from "./tables.service";
 import { TablesController } from "./tables.controller";
 import { TablesModel } from "./tables.model";
+import { TablesMiddleware } from "./tables.middleware";
 
 @Module({
   imports: [SequelizeModule.forFeature([TablesModel])],
   providers: [TableService],
   controllers: [TablesController],
 })
-export class TablesModule {}
+export class TablesModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TablesMiddleware).forRoutes("/tables/*");
+  }
+}
