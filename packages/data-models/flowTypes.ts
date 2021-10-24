1a678b70e18268530a8d425d4da479c0876c0508
chrismclarke <chris.m.clarke@live.co.uk>
2021-10-24 16:48:44 -0700

    refactor data models

--- packages/data-models/flowTypes.ts
+++ packages/data-models/flowTypes.ts
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { DYNAMIC_PREFIXES } from "./constants";
-import { RapidProFlowExport } from "./rapidpro.model";
+import { RapidProFlowExport } from "@IDEMSInternational/rapidpro-excel/types";
 import { TipRow } from "./tips.model";
-// import { IDBTable } from "../../../src/app/shared/services/db/db.service";
 
 /*********************************************************************************************
  *  Base flow types
@@ -99,7 +98,6 @@ export namespace FlowTypes {
   }
 
   export interface Conversation extends RapidProFlowExport.RootObject {}
-
   export interface Translation_strings {
     [sourceText: string]: string;
   }
@@ -184,71 +182,6 @@ export namespace FlowTypes {
   }
   /** As not all tasks will launch flows, use actions to specify different ways to handle a task  */
   export type Start_action = "start_new_flow" | "give_award" | "open_app";
-
-  // To Sort - possibly these typings affect the input and not the output???
-
-  /** Format of conversation rows prior to processing */
-  export interface ConversationSheet extends FlowTypeWithData {
-    flow_type: "conversation";
-    rows: ConversationRow[];
-  }
-  /** Format of conversation rows post processing */
-  export interface ConversationRow {
-    row_id?: string | number;
-    type:
-      | "start_new_flow"
-      | "send_message"
-      | "story_slide"
-      | "go_to"
-      | "save_value"
-      | "exit"
-      | "mark_as_completed"
-      | "split_random";
-    from?: string | number;
-    condition?: string | number;
-    condition_var?: string;
-    character?: string;
-    message_text: string;
-    media?: string;
-    icon?: string;
-    choose_multi?: boolean;
-    display_as_tick?: boolean;
-    ticked_by_default?: boolean;
-    default_choice?: string;
-    save_name?: string;
-    choice_media_display?: "both" | "media" | "text";
-    choice_1?: string;
-    choice_1_Media?: string;
-    choice_2?: string;
-    choice_2_Media?: string;
-    choice_3?: string;
-    choice_3_Media?: string;
-    choice_4?: string;
-    choice_4_Media?: string;
-    choice_5?: string;
-    choice_5_Media?: string;
-    choice_6?: string;
-    choice_6_Media?: string;
-    choice_7?: string;
-    choice_7_Media?: string;
-    choice_8?: string;
-    choice_8_Media?: string;
-    choice_9?: string;
-    choice_9_Media?: string;
-    choice_10?: string;
-    choice_10_Media?: string;
-    choice_11?: string;
-    choice_11_Media?: string;
-    choice_12?: string;
-    choice_12_Media?: string;
-    condition_type?: RapidProFlowExport.RouterCaseType;
-    // This is the UUID of the Node first created for this row, which is used to set how nodes go into this node.
-    // This is set once.
-    nodeUUIDForExit?: string;
-    // This is the node to refer to when this row is mentioned as a from in another row.
-    // This is updated e.g. when looping through from nodes.
-    _rapidProNode?: RapidProFlowExport.Node;
-  }
 
   export interface CarePackage {
     id: string;
--- packages/data-models/index.ts
+++ packages/data-models/index.ts
@@ -1,4 +1,3 @@
 export { APP_FIELDS, FIELD_PREFIX, DYNAMIC_PREFIXES } from "./constants";
 export * from "./flowTypes";
-export * from "./rapidpro.model";
 export * from "./tips.model";
