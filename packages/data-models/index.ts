04d6832d797f927d84ff5750a6953a49e5213476
chrismclarke <chris.m.clarke@live.co.uk>
2021-10-25 18:12:16 -0700

    update exports

--- packages/data-models/index.ts
+++ packages/data-models/index.ts
@@ -1,3 +1,5 @@
 export { APP_FIELDS, FIELD_PREFIX, DYNAMIC_PREFIXES } from "./constants";
 export * from "./flowTypes";
 export * from "./tips.model";
+// Re-export rapidpro excel types
+export { RapidProFlowExport, RapidproExcelModel } from "@IDEMSInternational/rapidpro-excel/types";
