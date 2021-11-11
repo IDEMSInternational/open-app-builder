c95495d5165d471faa8ad317011b177dce278c8e
chrismclarke <chris.m.clarke@live.co.uk>
2021-11-11 11:32:11 -0800

    add async asset pipe, deprecate existing

--- src/app/shared/components/template/pipes/index.ts
+++ src/app/shared/components/template/pipes/index.ts
@@ -1,6 +1,13 @@
 import { MarkdownPipe } from "./markdown.pipe";
 import { NumberingPipe } from "./numbering.pipe";
 import { PLHAssetPipe } from "./plh-asset.pipe";
+import { PLHAssetTranslatedPipe } from "./plh-asset-translated.pipe";
 import { TranslatePipe } from "./translate.pipe";
 
-export const TEMPLATE_PIPES = [MarkdownPipe, PLHAssetPipe, NumberingPipe, TranslatePipe];
+export const TEMPLATE_PIPES = [
+  MarkdownPipe,
+  NumberingPipe,
+  PLHAssetPipe,
+  PLHAssetTranslatedPipe,
+  TranslatePipe,
+];
