ad30b5118fcbc32293d745b026e134778db9a87f
chrismclarke <chris.m.clarke@live.co.uk>
2021-11-12 12:27:06 -0800

    remove deprecated asset pipe

--- src/app/shared/components/template/pipes/index.ts
+++ src/app/shared/components/template/pipes/index.ts
@@ -1,13 +1,6 @@
 import { MarkdownPipe } from "./markdown.pipe";
 import { NumberingPipe } from "./numbering.pipe";
-import { PLHAssetPipe } from "./plh-asset.pipe";
 import { PLHAssetTranslatedPipe } from "./plh-asset-translated.pipe";
 import { TranslatePipe } from "./translate.pipe";
 
-export const TEMPLATE_PIPES = [
-  MarkdownPipe,
-  NumberingPipe,
-  PLHAssetPipe,
-  PLHAssetTranslatedPipe,
-  TranslatePipe,
-];
+export const TEMPLATE_PIPES = [MarkdownPipe, NumberingPipe, PLHAssetTranslatedPipe, TranslatePipe];
