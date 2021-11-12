04862ab6c13f90624b094b8859b41d7753baa8d1
chrismclarke <chris.m.clarke@live.co.uk>
2021-11-12 09:30:53 -0800

    move asset folder name to shared data-models

--- packages/data-models/index.ts
+++ packages/data-models/index.ts
@@ -1,4 +1,10 @@
-export { APP_FIELDS, FIELD_PREFIX, DYNAMIC_PREFIXES } from "./constants";
+export {
+  APP_FIELDS,
+  FIELD_PREFIX,
+  DYNAMIC_PREFIXES,
+  APP_ASSETS_SUBFOLDER,
+  APP_STRINGS,
+} from "./constants";
 export * from "./flowTypes";
 export * from "./rapidpro.model";
 export * from "./tips.model";
