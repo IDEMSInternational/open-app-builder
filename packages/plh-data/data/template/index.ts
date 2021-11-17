42200279ea1008cbc422ec878c6d7f0b34d50c81
chrismclarke <chris.m.clarke@live.co.uk>
2021-11-17 13:40:27 -0800

    move language list template

--- packages/plh-data/data/template/index.ts
+++ packages/plh-data/data/template/index.ts
@@ -1,4 +1,9 @@
 import { FlowTypes } from "data-models";
+import template_component_demo from "./template.component_demo";
 import template_debug from "./template.debug";
-import template_1 from "./template";
-export const template: FlowTypes.Template[] = [].concat(template_debug, template_1);
+import template_2 from "./template";
+export const template: FlowTypes.Template[] = [].concat(
+  template_component_demo,
+  template_debug,
+  template_2
+);
