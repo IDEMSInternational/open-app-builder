c2bf2a30e82e37a8400e5703fe783aff1d19acdc
chrismclarke <chris.m.clarke@live.co.uk>
2021-11-21 18:25:21 -0800

    fix video component

--- src/app/shared/components/template/components/video.ts
+++ src/app/shared/components/template/components/video.ts
@@ -1,27 +1,18 @@
-import { HttpClient } from "@angular/common/http";
-import { Component, Input, OnInit } from "@angular/core";
-import { FlowTypes } from "data-models";
+import { Component } from "@angular/core";
 import { getStringParamFromTemplateRow } from "src/app/shared/utils";
 import { TemplateBaseComponent } from "./base";
 
 @Component({
   selector: "plh-tmpl-video",
   template: `<div class="tmpl-video-container margin-t-large" [class]="style">
-    <video [src]="row.value | plhAsset" controls></video>
+    <video [src]="_row.value | plhAsset" controls></video>
   </div>`,
   styleUrls: ["./tmpl-components-common.scss"],
 })
-export class TmplVideoComponent extends TemplateBaseComponent implements OnInit {
+export class TmplVideoComponent extends TemplateBaseComponent {
   style: string;
-
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-  }
 
   getParams() {
     this.style = getStringParamFromTemplateRow(this._row, "style", null);
   }
-
-  @Input() template: FlowTypes.Template;
 }
