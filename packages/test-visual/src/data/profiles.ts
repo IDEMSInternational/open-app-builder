e24b8c6ce48c1b7769b6e4cc9747eeccb91bb10a
Chris\Laptop <chris.m.clarke@live.co.uk>
2021-11-23 09:24:48 -0800

    set app language in profile

--- packages/test-visual/src/data/profiles.ts
+++ packages/test-visual/src/data/profiles.ts
@@ -1,6 +1,6 @@
 // TODO - merge with cypress profiles
 export const USER_PROFILE_DEFAULT = {
-  fields: { _app_language: "eng", name: "test default user" },
+  fields: { _app_language: "za_en", name: "test default user" },
   tables: {
     user_meta: [{ key: "first_app_open", value: "2021-09-02" }],
   },
