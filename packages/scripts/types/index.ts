195eb4231d080c8d3b639e7b4931908b1f27ba0b
chrismclarke <chris.m.clarke@live.co.uk>
2021-11-26 11:36:23 -0800

    refactor gdrive scripts to work with idems config

--- packages/scripts/types/index.ts
+++ packages/scripts/types/index.ts
@@ -1,2 +1,5 @@
 // Provide access to FlowTypes used in the app
 export { FlowTypes } from "data-models";
+
+// TODO - find cleaner way to share types
+export { IDeploymentConfig } from "../../../.idems_app/types/deployment.type";
