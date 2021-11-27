cff1f46e5e91089f33d8d931c8ed32f884973eda
chrismclarke <chris.m.clarke@live.co.uk>
2021-11-27 14:17:30 -0800

    add gdrive scripts cli

---
+++ packages/@idemsInternational/gdrive-tools/src/utils/logging.utils.ts
@@ -0,0 +1,31 @@
+import boxen from "boxen";
+import chalk from "chalk";
+
+/** Record a 2-line error message in a box with additional optional logging and exit */
+export function logError(opts: Partial<ILogErrorOptions> = {}) {
+  const { msg1, msg2, error, logOnly } = { ...logErrorDefaultOptions, ...opts };
+  console.log(
+    boxen(
+      `
+          ${chalk.red(msg1)}
+          
+          ${chalk.yellow(msg2)}
+          
+          `,
+      { padding: 1, borderColor: "red" }
+    )
+  );
+  if (error) {
+    console.error(error);
+  }
+  if (!logOnly) {
+    process.exit(1);
+  }
+}
+interface ILogErrorOptions {
+  msg1: string;
+  msg2: string;
+  error?: Error; // Optional error object to log
+  logOnly?: boolean; // specify to log and continue processing instead of exiting
+}
+const logErrorDefaultOptions: ILogErrorOptions = { msg1: "", msg2: "", logOnly: false };
