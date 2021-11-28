f8e7ac9235cd91feda10a940fe10f9f928b39c46
chrismclarke <chris.m.clarke@live.co.uk>
2021-11-27 17:06:25 -0800

    update gitignore

---
+++ .idems_app/deployments/example/config.ts
@@ -0,0 +1,11 @@
+import { IDeploymentConfig } from "../../types/deployment.type";
+
+const config: IDeploymentConfig = {
+  name: "Example Config",
+  github_repo: "",
+  google_drive: {
+    sheets_folder_id: "",
+    assets_folder_id: "",
+  },
+};
+export default config;
---
+++ .idems_app/deployments/plh/config.ts
@@ -0,0 +1,11 @@
+import { IDeploymentConfig } from "../../types/deployment.type";
+
+const config: IDeploymentConfig = {
+  name: "PLH",
+  github_repo: "",
+  google_drive: {
+    sheets_folder_id: "1u3n4dI5pWvEIbuO7MXctku5ZYmKtgqPi",
+    assets_folder_id: "1dp9QAQ84m8pm0IBQTSPXe1ramyynKPNn",
+  },
+};
+export default config;
