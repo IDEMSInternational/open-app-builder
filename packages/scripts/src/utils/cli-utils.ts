8e2022cc9236f1260b3232ab1239caf5e66df7b1
chrismclarke <chris.m.clarke@live.co.uk>
2021-11-28 20:49:55 -0800

    code tidying

--- packages/scripts/src/utils/cli-utils.ts
+++ packages/scripts/src/utils/cli-utils.ts
@@ -1,5 +1,3 @@
-import boxen from "boxen";
-import chalk from "chalk";
 import * as inquirer from "inquirer";
 
 /**
@@ -21,48 +19,3 @@ export async function promptInput(message: string) {
   const res = await inquirer.prompt([{ type: "input", message, name }]);
   return res[name];
 }
-
-/** Record a 2-line error message in a box with additional optional logging and exit */
-export function logError(opts: Partial<ILogOptions> = {}) {
-  const { msg1, msg2, error, logOnly } = { ...defaultLog, ...opts };
-  console.log(
-    boxen(
-      `
-  ${chalk.red(msg1)}
-        
-  ${chalk.yellow(msg2)}
-        
-        `,
-      { padding: 1, borderColor: "red" }
-    )
-  );
-  if (error) {
-    console.error(error);
-  }
-  if (!logOnly) {
-    process.exit(1);
-  }
-}
-
-/** Display an output message in a blue box with 2 lines of text */
-export function logOutput(opts: Partial<ILogOptions> = {}) {
-  const { msg1, msg2 } = { ...defaultLog, ...opts };
-  console.log(
-    boxen(
-      `
-  ${chalk.blueBright(msg1)}
-          
-  ${chalk.cyanBright(msg2)}
-          `,
-      { padding: 1, borderColor: "blueBright" }
-    )
-  );
-}
-
-interface ILogOptions {
-  msg1: string;
-  msg2: string;
-  error?: Error; // Optional error object to log
-  logOnly?: boolean; // specify to log and continue processing instead of exiting
-}
-const defaultLog: ILogOptions = { msg1: "", msg2: "", logOnly: false };
---
+++ packages/scripts/src/utils/logging.utils.ts
@@ -0,0 +1,62 @@
+import boxen from "boxen";
+import chalk from "chalk";
+
+/** Record a 2-line error message in a box with additional optional logging and exit */
+export function logError(opts: Partial<ILogOptions> = {}) {
+  const { msg1, msg2, error, logOnly } = { ...defaultLog, ...opts };
+  console.log(
+    boxen(
+      `
+    ${chalk.red(msg1)}
+          
+    ${chalk.yellow(msg2)}
+          
+          `,
+      { padding: 1, borderColor: "red" }
+    )
+  );
+  if (error) {
+    console.error(error);
+  }
+  if (!logOnly) {
+    process.exit(1);
+  }
+}
+
+/** Display an output message in a blue box with 2 lines of text */
+export function logOutput(opts: Partial<ILogOptions> = {}) {
+  const { msg1, msg2 } = { ...defaultLog, ...opts };
+  console.log(
+    boxen(
+      `
+    ${chalk.blueBright(msg1)}
+            
+    ${chalk.cyanBright(msg2)}
+            `,
+      { padding: 1, borderColor: "blueBright" }
+    )
+  );
+}
+
+/** Display an output message in a blue box with 2 lines of text */
+export function logWarning(opts: Partial<ILogOptions> = {}) {
+  const { msg1, msg2 } = { ...defaultLog, ...opts };
+  console.log(
+    boxen(
+      `
+      ${chalk.yellowBright(msg1)}
+              
+      ${chalk.yellow(msg2)}
+              `,
+      { padding: 1, borderColor: "yellowBright" }
+    )
+  );
+}
+
+interface ILogOptions {
+  msg1: string;
+  msg2: string;
+  error?: Error; // Optional error object to log
+  logOnly?: boolean; // specify to log and continue processing instead of exiting
+}
+const defaultLog: ILogOptions = { msg1: "", msg2: "", logOnly: false };
