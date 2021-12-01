465d50d51a14b37ee95e9bd5045717ce64fb67b5
chrismclarke <chris.m.clarke@live.co.uk>
2021-12-01 10:46:24 -0800

    temp update type annotation

--- packages/app-data/data/translation_strings/index.ts
+++ packages/app-data/data/translation_strings/index.ts
@@ -5,4 +5,4 @@ import za_st from "./za_st";
 import za_tn from "./za_tn";
 import za_xh from "./za_xh";
 import za_zu from "./za_zu";
-export const TRANSLATION_STRINGS = { es_sp, tz_sw, za_af, za_st, za_tn, za_xh, za_zu };
+export const TRANSLATION_STRINGS: any = { es_sp, tz_sw, za_af, za_st, za_tn, za_xh, za_zu };
