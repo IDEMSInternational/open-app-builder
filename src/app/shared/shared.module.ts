df2bee8f169490b6230d7630aa76af37a040820d
chrismclarke <chris.m.clarke@live.co.uk>
2021-12-07 09:55:39 -0800

    code tidying

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,32 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { Blob1Component } from "./components/blobs/blob1/blob1.component";
-import { BlobComponent } from "./components/blobs/blob/blob.component";
-import { ReflectAnimationComponent } from "./components/reflect-animation/reflect-animation.component";
 import { IonicModule } from "@ionic/angular";
-import { StressedMultiHandAnimComponent } from "./components/stressed-multi-hand-anim/stressed-multi-hand-anim.component";
 import { PLHMainHeaderComponent } from "./components/plh-main-header";
 import { PLHMainTabsComponent } from "./components/plh-main-tabs";
 import { RouterModule } from "@angular/router";
 import { TemplateComponentsModule } from "./components/template/template.module";
-import { ANIMATION_COMPONENTS } from "./components/animations";
-import { AnimationComponent } from "./components/animations/animation.component";
-import { ComboBoxModalComponent } from "./components/common/components/combo-box-modal/combo-box-modal.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { PLHDebugToggleComponent } from "./components/debug-toggle";
 
-const Components = [
-  BlobComponent,
-  Blob1Component,
-  ReflectAnimationComponent,
-  StressedMultiHandAnimComponent,
-  PLHMainHeaderComponent,
-  PLHMainTabsComponent,
-  PLHDebugToggleComponent,
-  AnimationComponent,
-  ComboBoxModalComponent,
-  ...ANIMATION_COMPONENTS,
-];
+const Components = [PLHMainHeaderComponent, PLHMainTabsComponent, PLHDebugToggleComponent];
 
 @NgModule({
   declarations: [Components],
