cd6862d8b6595bca80a68c1bb01f1c6be627b79b
chrismclarke <chris.m.clarke@live.co.uk>
2021-12-07 09:55:56 -0800

    remove legacy routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,50 +6,13 @@ import { TourComponent } from "./feature/tour/tour.component";
 const FeatureRoutes: Routes = [
   {
     path: "",
-    redirectTo: "home",
+    redirectTo: "/template/home_page",
     pathMatch: "full",
   },
   {
     path: "home",
-    loadChildren: () => import("./feature/home/home.module").then((m) => m.HomePageModule),
-  },
-  {
-    path: "module_list",
-    loadChildren: () =>
-      import("./feature/module_list/module-list.module").then((m) => m.ModuleListModule),
-  },
-  {
-    path: "module_page",
-    loadChildren: () =>
-      import("./feature/module_page/module-page.module").then((m) => m.ModulePageModule),
-  },
-  {
-    path: "take-a-pause",
-    loadChildren: () =>
-      import("./feature/take-a-pause/take-a-pause.module").then((m) => m.TakeAPausePageModule),
-  },
-  {
-    path: "conversation",
-    loadChildren: () => import("./feature/chat/chat.module").then((m) => m.ChatPageModule),
-  },
-
-  {
-    path: "tips",
-    loadChildren: () => import("./feature/tips/tips.module").then((m) => m.TipsModule),
-  },
-  {
-    path: "goals",
-    loadChildren: () => import("./feature/goals/goals.module").then((m) => m.GoalsPageModule),
-  },
-  {
-    path: "care-packages",
-    loadChildren: () =>
-      import("./feature/care-packages/care-packages.module").then((m) => m.CarePackagesPageModule),
-  },
-  {
-    path: "habit_ideas",
-    loadChildren: () =>
-      import("./feature/habit-ideas/habit-ideas.module").then((m) => m.HabitIdeasPageModule),
+    redirectTo: "/template/home_page",
+    pathMatch: "full",
   },
   {
     path: "campaigns",
@@ -59,6 +22,11 @@ const FeatureRoutes: Routes = [
     path: "notifications",
     loadChildren: () =>
       import("./feature/notification/notification.module").then((m) => m.NotificationsModule),
+  },
+  {
+    path: "template",
+    loadChildren: () =>
+      import("./feature/template/template.module").then((m) => m.TemplatePageModule),
   },
   {
     path: "theme-editor",
@@ -71,35 +39,6 @@ const FeatureRoutes: Routes = [
   {
     path: "tour/:tourName",
     component: TourComponent,
-  },
-  {
-    path: "parent_points",
-    loadChildren: () =>
-      import("./feature/parent-points/parent-points.module").then((m) => m.ParentPointsPageModule),
-  },
-  {
-    path: "animations",
-    loadChildren: () =>
-      import("./feature/animations/animations.module").then((m) => m.AnimationsPageModule),
-  },
-  /*****************************************************************************************
-   * Legacy paths - these should be removed in the future once modules refactored
-   * (duplicated above via 'conversation' and 'tips' flow type handlers)
-   ****************************************************************************************/
-  {
-    path: "chat",
-    loadChildren: () => import("./feature/chat/chat.module").then((m) => m.ChatPageModule),
-  },
-  {
-    path: "toolbox/topic/:topicId",
-    loadChildren: () => import("./feature/tips/tips.module").then((m) => m.TipsModule),
-  },
-  {
-    path: "template",
-    loadChildren: () =>
-      import("./feature/template-testing/template-testing.module").then(
-        (m) => m.TemplateTestingPageModule
-      ),
   },
 ];
 
@@ -117,11 +56,6 @@ const StaticPages: Routes = [
     path: "app-terms",
     loadChildren: () =>
       import("./pages/app-terms/app-terms.module").then((m) => m.AppTermsPageModule),
-  },
-  {
-    path: "settings",
-    loadChildren: () =>
-      import("./pages/settings/settings.module").then((m) => m.SettingsPageModule),
   },
 ];
 
