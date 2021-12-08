50c16c6a3395d65da540df72917b1cfa899acc14
chrismclarke <chris.m.clarke@live.co.uk>
2021-12-08 15:17:47 -0800

    remove legacy tabs

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { IonicModule } from "@ionic/angular";
 import { PLHMainHeaderComponent } from "./components/plh-main-header";
-import { PLHMainTabsComponent } from "./components/plh-main-tabs";
 import { RouterModule } from "@angular/router";
 import { TemplateComponentsModule } from "./components/template/template.module";
 import { ReactiveFormsModule } from "@angular/forms";
 import { PLHDebugToggleComponent } from "./components/debug-toggle";
 
-const Components = [PLHMainHeaderComponent, PLHMainTabsComponent, PLHDebugToggleComponent];
+const Components = [PLHMainHeaderComponent, PLHDebugToggleComponent];
 
 @NgModule({
   declarations: [Components],
