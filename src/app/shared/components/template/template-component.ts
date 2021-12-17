69cdce6f6ced34a49cb8bd9e690d9ae0de1b8af1
Chris <chris.m.clarke@live.co.uk>
2021-12-17 12:14:48 +0000

    remove template js styles and plh-tmpl-comp

--- src/app/shared/components/template/template-component.ts
+++ src/app/shared/components/template/template-component.ts
@@ -48,24 +48,16 @@ export class TmplCompHostDirective {
 @Component({
   selector: "plh-template-component",
   template: `
-    <!-- <div
-      class="plh-tmpl-comp"
-      [attr.data-hidden]="_row.hidden"
-      [attr.data-debug]="parent.debugMode"
-      [ngClass]="{ disabled: _row.disabled }"
-      [attr.data-rowname]="_row.name"
-    > -->
     <!-- Template Debugger -->
     <plh-template-debugger
       *ngIf="parent.debugMode"
       [row]="_row"
       [parent]="parent"
     ></plh-template-debugger>
+
     <!-- Injected template component -->
     <ng-template plhTemplateComponentHost></ng-template>
-    <!-- </div> -->
   `,
-  // styleUrls: ["./components/tmpl-components-common.scss", "./template-container.component.scss"],
   encapsulation: ViewEncapsulation.None,
   styles: [
     `
@@ -83,7 +75,7 @@ export class TmplCompHostDirective {
     `,
   ],
 })
-export class TemplateComponent implements OnInit, AfterContentInit, ITemplateRowProps {
+export class TemplateComponent implements OnInit, ITemplateRowProps {
   /**
    * Specific data used in component rendering
    * when updated from parent changes will automatically propogate to child
@@ -106,10 +98,10 @@ export class TemplateComponent implements OnInit, AfterContentInit, ITemplateRow
   // Add bindings to track key data attributes on the component itself, e.g.
   // <plh-template-component data-debug-hidden="false" data-display-component="TmplNumberComponent" data-name="number_selector_6" data-type="number_selector">
   @HostBinding("attr.data-hidden") get getAttrHidden() {
-    return this._row && this._row.hidden;
+    return this._row && this._row.hidden ? true : null;
   }
   @HostBinding("attr.data-debug-hidden") get getAttrDat() {
-    return this.parent && this.parent.debugMode;
+    return this.parent && this.parent.debugMode ? true : null;
   }
   @HostBinding("attr.data-display-component") get getComponentDisplayType() {
     return TEMPLATE_COMPONENT_MAPPING[this._row?.type]?.name || "none";
@@ -130,17 +122,10 @@ export class TemplateComponent implements OnInit, AfterContentInit, ITemplateRow
 
   @ViewChild(TmplCompHostDirective, { static: true }) tmplComponentHost: TmplCompHostDirective;
 
-  constructor(
-    private componentFactoryResolver: ComponentFactoryResolver,
-    private elRef: ElementRef
-  ) {}
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
 
   ngOnInit() {
     this.renderRow(this._row);
-  }
-
-  ngAfterContentInit() {
-    this.setStyleList();
   }
 
   private renderRow(row: FlowTypes.TemplateRow) {
@@ -160,63 +145,6 @@ export class TemplateComponent implements OnInit, AfterContentInit, ITemplateRow
           }
         }
     }
-  }
-
-  /**
-   * CC  2021-04-18
-   * TODO - this code looks pretty long and specific, so likely best reviewed
-   * and moved into own service
-   * Also assume more could be handle with stylesheets instead of specific style
-   */
-  private setStyleList() {
-    // const styles = {};
-    // const row = this._row;
-    // if (row.style_list) {
-    //   for (let i = 0; i < row.style_list.length; i++) {
-    //     let splited = row.style_list[i].split(":");
-    //     styles[splited[0]] = splited[1];
-    //     this.elRef.nativeElement.style.setProperty(splited[0], splited[1]);
-    //   }
-    // }
-    // if (
-    //   row.parameter_list &&
-    //   row.parameter_list["style"] === "navigation" &&
-    //   row.type === "display_group"
-    // ) {
-    //   // this.elRef.nativeElement.style.setProperty("display", "flex");
-    //   // this.elRef.nativeElement.style.setProperty("height", "100%");
-    //   // this.elRef.nativeElement.style.setProperty("min-height", "75px");
-    //   // this.elRef.nativeElement.style.setProperty("align-items", "flex-end");
-    //   let el_component = this.elRef.nativeElement.parentElement.closest("plh-template-component");
-    //   let el_container = this.elRef.nativeElement.parentElement.closest("plh-template-container");
-    //   while (el_component && el_container) {
-    //     try {
-    //       if (el_component) {
-    //         el_component.style.setProperty("height", "100%");
-    //         el_component.style.setProperty("display", "flex");
-    //         el_component.style.setProperty("align-items", "flex-end");
-    //       }
-    //       if (el_container) {
-    //         el_container.style.setProperty("height", "100%");
-    //         el_container.style.setProperty("display", "flex");
-    //         el_container.style.setProperty("align-items", "flex-end");
-    //         el_component = el_container.parentElement.closest("plh-template-component");
-    //       }
-    //       if (el_component) {
-    //         el_container = el_component.parentElement.closest("plh-template-container");
-    //       }
-    //     } catch (ex) {
-    //       console.error("navigation style settings exception");
-    //       console.error(ex);
-    //     }
-    //   }
-    // }
-    // if (row.type === "button") {
-    //   this.elRef.nativeElement.style.setProperty("align-self", "normal");
-    // }
-    // if (row.type === "help_icon") {
-    //   this.elRef.nativeElement.style.setProperty("width", "40px");
-    // }
   }
 
   /** Create and render a nested template component */
