05fa9a5f8d74c84c93a2ef434893c3751e0ed8e9
Chris <chris.m.clarke@live.co.uk>
2021-12-19 17:49:37 +0000

    refactor and simplify button component

--- src/app/shared/components/template/components/button/button.component.ts
+++ src/app/shared/components/template/components/button/button.component.ts
@@ -1,14 +1,9 @@
-import {
-  AfterContentInit,
-  Component,
-  ElementRef,
-  HostBinding,
-  HostListener,
-  OnInit,
-  ViewChild,
-} from "@angular/core";
+import { Component, ElementRef, HostBinding, HostListener, OnInit, ViewChild } from "@angular/core";
 import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
-import { getStringParamFromTemplateRow, getBooleanParamFromTemplateRow } from "../../../../utils";
+import {
+  getStringParamFromTemplateRow,
+  getBooleanParamFromTemplateRow,
+} from "src/app/shared/utils";
 import { TemplateBaseComponent } from "../base";
 
 @Component({
@@ -16,15 +11,12 @@ import { TemplateBaseComponent } from "../base";
   templateUrl: "./button.component.html",
   styleUrls: ["./button.component.scss"],
 })
-export class TmplButtonComponent extends TemplateBaseComponent implements OnInit, AfterContentInit {
-  style: string = "primary";
+export class TmplButtonComponent extends TemplateBaseComponent implements OnInit {
+  style: "primary" | "nested_color" = "primary";
   color: string | null;
   disabled: boolean = false;
   textAlign: string;
   buttonAlign: string;
-  nestedStyle: boolean = false;
-  nestedProperty: string;
-  nestedStyleName = "nested_color";
   innerHTML: SafeHtml;
   scaleFactor: number = 1;
   windowWidth: number;
@@ -48,50 +40,15 @@ export class TmplButtonComponent extends TemplateBaseComponent implements OnInit
   getParams() {
     this.style = `${getStringParamFromTemplateRow(this._row, "style", "information")} ${
       this.isTwoColumns() ? "two_columns" : ""
-    }`;
+    }` as any;
     this.disabled = getBooleanParamFromTemplateRow(this._row, "disabled", false);
     if (this._row.disabled) {
       this.disabled = true;
     }
     this.textAlign = getStringParamFromTemplateRow(this._row, "text_align", "left");
     this.buttonAlign = getStringParamFromTemplateRow(this._row, "button_align", "center");
-    this.nestedStyle = this.style.includes(this.nestedStyleName);
   }
 
-  ngAfterContentInit() {
-    if (this.nestedStyle) {
-      this.setNestedStyle();
-    }
-  }
-
-  setNestedStyle() {
-    const color =
-      this.elRef.nativeElement.parentElement.parentElement.parentElement.parentElement.parentElement.attributes[
-        "color"
-      ].value.trim();
-    this.nestedProperty = `linear-gradient(${this.hexToRGB(color, "down", 2)} 0%, ${this.hexToRGB(
-      color,
-      "down",
-      3
-    )} 100%)`;
-  }
-  hexToRGB(hex, change: "up" | "down", tone: number) {
-    const adjust = (change === "up" ? 16 : -16) * tone;
-    const r = parseInt(hex.slice(1, 3), 16),
-      g = parseInt(hex.slice(3, 5), 16),
-      b = parseInt(hex.slice(5, 7), 16);
-    return (
-      "rgb(" +
-      (r + adjust > 255 ? 255 : r + adjust <= 0 ? 0 : r + adjust) +
-      "," +
-      " " +
-      (g + adjust > 255 ? 255 : g + adjust <= 0 ? 0 : g + adjust) +
-      ", " +
-      "" +
-      (b + adjust > 255 ? 255 : b + adjust <= 0 ? 0 : b + adjust) +
-      ")"
-    );
-  }
   private isTwoColumns(): boolean {
     const displayGroupElement = this.elRef.nativeElement.closest(".display-group-wrapper");
     if (displayGroupElement) {
