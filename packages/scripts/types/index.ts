92e4a1e568b14da6300b5ca46a42a518bedebf3d
Chris <chris.m.clarke@live.co.uk>
2021-12-21 10:04:44 +0000

    refactor deployment models

--- packages/scripts/types/index.ts
+++ packages/scripts/types/index.ts
@@ -1,8 +1,2 @@
 // Provide access to FlowTypes used in the app
-export { FlowTypes } from "data-models";
-
-// TODO - find cleaner way to share types
-export {
-  IDeploymentConfig,
-  DEPLOYMENT_CONFIG_EXAMPLE_DEFAULTS,
-} from "../../../.idems_app/core/models";
+export { FlowTypes, IDeploymentConfig, DEPLOYMENT_CONFIG_EXAMPLE_DEFAULTS } from "data-models";
