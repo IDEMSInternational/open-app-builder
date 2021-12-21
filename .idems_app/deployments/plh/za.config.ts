6975e3f1916509e8cbf4715e6fabd29fb8c09fc3
Chris <chris.m.clarke@live.co.uk>
2021-12-21 12:47:25 +0000

    code tidying

--- .idems_app/deployments/plh/za.config.ts
+++ .idems_app/deployments/plh/za.config.ts
@@ -5,6 +5,6 @@ import DEFAULT_CONFIG from "./default.config";
 const config = DEFAULT_CONFIG;
 config.name = "PLH ZA";
 config.app_data.sheets_filter_function = (flow) => !["debug"].includes(flow.flow_subtype);
-config.translations.supported_language_codes = ["za_zu", "za_xh", "za_tn", "za_st", "za_af"];
+config.translations.filter_language_codes = ["za_zu", "za_xh", "za_tn", "za_st", "za_af"];
 
 export default config;
