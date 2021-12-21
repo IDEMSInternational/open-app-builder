08f883f712d0bd90920837b083db32dd56919655
Chris <chris.m.clarke@live.co.uk>
2021-12-21 10:10:55 +0000

    remove legacy filter

--- .idems_app/deployments/plh/default.config.ts
+++ .idems_app/deployments/plh/default.config.ts
@@ -9,12 +9,6 @@ const config: IDeploymentConfig = {
   google_drive: {
     sheets_folder_id: "1n221Zv9LYuwxmjhiboq8vhQg67_K9L5f",
     assets_folder_id: "1dp9QAQ84m8pm0IBQTSPXe1ramyynKPNn",
-    // exclude anything in an 'old' folder, e.g. sheets/core/old/navigation
-    sheets_filter_function: (gdriveEntry) =>
-      !gdriveEntry.folderPath
-        .split("/")
-        .map((folder) => folder.toLowerCase())
-        .includes("old"),
   },
   // Legacy mapping to output to existing workspace
   app_data: {
--- .idems_app/deployments/plh/za.config.ts
+++ .idems_app/deployments/plh/za.config.ts
@@ -3,6 +3,7 @@ import DEFAULT_CONFIG from "./default.config";
 /** ZA config extends the default config **/
 
 const config = DEFAULT_CONFIG;
+config.name = "PLH ZA";
 config.app_data.sheets_filter_function = (flow) => !["debug"].includes(flow.flow_subtype);
 
 export default config;
