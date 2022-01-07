4f5feef2eec43c98a07626025b41e346eb5850aa
chrismclarke <chris.m.clarke@live.co.uk>
2022-01-06 21:54:34 -0800

    add context-menu module

---
+++ src/app/shared/modules/context-menu/context-menu.types.ts
@@ -0,0 +1,9 @@
+export interface IContextMenuAction {
+  id: string;
+  menuButtonText: string;
+  actionHandler: (ev: PointerEvent) => void;
+}
+export interface IContextMenuActionHashmap {
+  [id: string]: IContextMenuAction;
+}
+export type IContextMenuType = "rightClick" | "textSelect";
