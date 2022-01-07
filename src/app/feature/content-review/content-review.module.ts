27dc8ebbd323cf4736be8e22fc7e8ac20caee738
chrismclarke <chris.m.clarke@live.co.uk>
2022-01-07 14:28:53 -0800

    code tidying

--- src/app/feature/content-review/content-review.module.ts
+++ src/app/feature/content-review/content-review.module.ts
@@ -2,20 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ExistingCommentsComponent } from "./components/existing-comments/existing-comments.component";
 import { IonicModule } from "@ionic/angular";
-import { ContextMenuComponent } from "./components/context-menu/context-menu.component";
-import { ReviewingComponent } from "./components/reviewing.component";
-import { SuggestFormComponent } from "./components/suggest-form/suggest-form.component";
 import { FormsModule } from "@angular/forms";
 import { ContentReviewRoutingModule } from "./content-review-routing.module";
 
 @NgModule({
   imports: [CommonModule, IonicModule, FormsModule, ContentReviewRoutingModule],
-  declarations: [
-    ExistingCommentsComponent,
-    SuggestFormComponent,
-    ContextMenuComponent,
-    ReviewingComponent,
-  ],
-  exports: [ReviewingComponent],
+  declarations: [ExistingCommentsComponent],
+  exports: [],
 })
 export class ContentReviewModule {}
