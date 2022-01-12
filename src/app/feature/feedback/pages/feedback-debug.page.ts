a1116f9cd8652e1ce11f09525d64f8fe8d947e7a
chrismclarke <chris.m.clarke@live.co.uk>
2022-01-12 11:46:13 -0800

    add db entries to debug page

--- src/app/feature/feedback/pages/feedback-debug.page.ts
+++ src/app/feature/feedback/pages/feedback-debug.page.ts
@@ -1,12 +1,54 @@
 import { Component, OnInit } from "@angular/core";
+import { Subject } from "packages/api/node_modules/rxjs";
+import { interval } from "rxjs";
+import { debounce, takeUntil } from "rxjs/operators";
+import { DbService } from "src/app/shared/services/db/db.service";
 import { FeedbackService } from "../feedback.service";
+import { IFeedbackEntryDB } from "../feedback.types";
 
 @Component({
   templateUrl: "./feedback-debug.page.html",
-  styles: [``],
+  styleUrls: ["./feedback-debug.page.scss"],
 })
 export class FeedbackDebugPage implements OnInit {
-  constructor(public feedbackService: FeedbackService) {}
+  private destroyed$ = new Subject<boolean>();
+  public feedbackPending: IFeedbackEntryDB[] = [];
+  public feedbackSent: IFeedbackEntryDB[] = [];
+  constructor(public feedbackService: FeedbackService, private dbService: DbService) {}
 
-  ngOnInit() {}
+  ngOnDestroy() {
+    this.destroyed$.next(true);
+    this.destroyed$.unsubscribe();
+  }
+
+  async ngOnInit() {
+    await this.loadDBFeedback();
+    this.dbService
+      .table("feedback")
+      .changes$.pipe(
+        takeUntil(this.destroyed$),
+        debounce(() => interval(500))
+      )
+      .subscribe(() => this.loadDBFeedback());
+  }
+
+  public logFeedback(feedback: IFeedbackEntryDB) {
+    console.log(feedback);
+  }
+  public async syncFeedback() {
+    await this.dbService.syncToServer();
+    await this.loadDBFeedback();
+  }
+
+  private async loadDBFeedback() {
+    this.feedbackPending = await this.dbService
+      .table("feedback")
+      .where({ _sync_status: "pending" })
+      .toArray();
+    this.feedbackSent = await this.dbService
+      .table("feedback")
+      .where({ _sync_status: "sent" })
+      .toArray();
+    console.log("feedback", { pending: this.feedbackPending, sent: this.feedbackSent });
+  }
 }
