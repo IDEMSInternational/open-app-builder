e289726653d80729d959aaefc67a6801734bb402
chrismclarke <chris.m.clarke@live.co.uk>
2022-01-12 20:08:21 -0800

    prevent duplicate feedback items

--- packages/api/src/endpoints/app_feedback/app_feedback.service.ts
+++ packages/api/src/endpoints/app_feedback/app_feedback.service.ts
@@ -14,15 +14,14 @@ export class AppFeedbackService {
     return this.model.findAll();
   }
 
-  async createOrUpdate(raw: UserFeedbackDto) {
-    const { app_user_id, user_feedback_id } = raw;
-    const foundItem = await this.model.findOne({ where: { app_user_id, user_feedback_id } });
+  async createOrUpdate(body: UserFeedbackDto) {
+    const { app_user_id, app_user_record_id, data } = body;
+    const foundItem = await this.model.findOne({ where: { app_user_id, app_user_record_id } });
     if (!foundItem) {
       // Item not found, create a new one
       const feedback = new AppFeedback();
-      return feedback.update({ raw, app_user_id, user_feedback_id });
+      return feedback.update({ data, app_user_id, app_user_record_id });
     }
-    foundItem.raw = raw;
-    return foundItem.update({ raw });
+    return foundItem.update({ data });
   }
 }
