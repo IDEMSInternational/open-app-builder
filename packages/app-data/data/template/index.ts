49ac47d485a8e5bf40090b262c51080c9723328b
EsmeeIDEMS <esmee@idems.international>
2022-01-21 16:02:17 +0100

    Replace parenting content by example maths content

--- packages/app-data/data/template/index.ts
+++ packages/app-data/data/template/index.ts
@@ -1,9 +1,3 @@
 import { FlowTypes } from "data-models";
-import template_component_demo from "./template.component_demo";
-import template_debug from "./template.debug";
-import template_2 from "./template";
-export const template: FlowTypes.Template[] = [].concat(
-  template_component_demo,
-  template_debug,
-  template_2
-);
+import template_0 from "./template";
+export const template: FlowTypes.Template[] = [].concat(template_0);
