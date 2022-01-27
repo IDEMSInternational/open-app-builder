4f04ac48c8431074814abaadda0320dd468942e4
EsmeeIDEMS <esmee@idems.international>
2022-01-27 18:02:06 +0000

    Update TZ app id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,8 +4,8 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "international.idems.plh_teens",
-  appName: "PLH Teens",
+  appId: "international.idems.plh_teens_tz",
+  appName: "PLH Teens TZ",
   bundledWebRuntime: false,
   webDir: "www",
   plugins: {
