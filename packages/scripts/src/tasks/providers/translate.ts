6915331e149bef9cfe09d3ece585b484eae38727
chrismclarke <chris.m.clarke@live.co.uk>
2022-01-29 13:37:57 -0800

    chore: add provider placeholders for android, file, translate

---
+++ packages/scripts/src/tasks/providers/translate.ts
@@ -0,0 +1,7 @@
+const download = (options: { src: string; target: string }) => null;
+const filter = (options: { src: string; target: string }) => null;
+const apply = (options: { src: string; target: string }) => null;
+
+export default {
+  // download, filter, apply
+};
