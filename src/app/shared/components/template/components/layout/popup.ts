c6dec54ac9c49fe6c0336ed33abd02e9ae91926d
chrismclarke <chris.m.clarke@live.co.uk>
2022-02-03 13:17:51 -0800

    fix: fullscreen popup scroll

--- src/app/shared/components/template/components/layout/popup.ts
+++ src/app/shared/components/template/components/layout/popup.ts
@@ -52,7 +52,7 @@ import { TemplateContainerComponent } from "../../template-container.component";
       .popup-content[data-fullscreen] {
         width: 100%;
         height: 100%;
-        max-height: 100%;
+        max-height: 100vh;
         border-radius: 0;
         margin: 0;
       }
