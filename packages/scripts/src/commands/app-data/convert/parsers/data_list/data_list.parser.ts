b72e689a1869d65ef36c796aae36b9d40af1257e
chrismclarke <chris.m.clarke@live.co.uk>
2022-02-04 12:42:09 -0800

    feat: add dynamic field extraction to data list parser

--- packages/scripts/src/commands/app-data/convert/parsers/data_list/data_list.parser.ts
+++ packages/scripts/src/commands/app-data/convert/parsers/data_list/data_list.parser.ts
@@ -1,5 +1,10 @@
-import { FlowTypes } from "data-models";
-import { extractConditionList, parseAppDataCollectionString, setNestedProperty } from "../../utils";
+import { extractDynamicFields, FlowTypes } from "data-models";
+import {
+  extractConditionList,
+  extractDynamicDependencies,
+  parseAppDataCollectionString,
+  setNestedProperty,
+} from "../../utils";
 import { DefaultParser } from "../default/default.parser";
 
 export class DataListParser extends DefaultParser {
@@ -21,6 +26,12 @@ export class DataListParser extends DefaultParser {
         delete row[field];
       }
     });
+    // extract dynamic fields for runtime evaluation
+    const dynamicFields = extractDynamicFields(row);
+    if (dynamicFields) {
+      row._dynamicFields = dynamicFields;
+      row._dynamicDependencies = extractDynamicDependencies(dynamicFields);
+    }
     return row;
   }
 }
