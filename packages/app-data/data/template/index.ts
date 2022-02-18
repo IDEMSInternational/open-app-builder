0fbc74e7ac92e2933da116bbf081bc2b7d9979e9
chrismclarke <chris.m.clarke@live.co.uk>
2022-02-18 14:08:43 -0800

    feat: add hardcoded variables example template

--- packages/app-data/data/template/index.ts
+++ packages/app-data/data/template/index.ts
@@ -2,8 +2,10 @@ import { FlowTypes } from "data-models";
 import template_component_demo from "./template.component_demo";
 import template_debug from "./template.debug";
 import template_2 from "./template";
+import template_example_hardcoded from "./template.example_hardcoded";
 export const template: FlowTypes.Template[] = [].concat(
   template_component_demo,
   template_debug,
-  template_2
+  template_2,
+  template_example_hardcoded
 );
