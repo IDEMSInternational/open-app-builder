32d03ab45d22a01c7480b36eac62c13b82cf99d4
chrismclarke <chris.m.clarke@live.co.uk>
2022-02-18 16:34:23 -0800

    chore: update api models to support app_deployment_name

--- packages/api/src/endpoints/app_feedback/app_feedback.model.ts
+++ packages/api/src/endpoints/app_feedback/app_feedback.model.ts
@@ -9,6 +9,9 @@ export class AppFeedback extends Model<AppFeedback> {
   @Column({ allowNull: false })
   app_user_record_id: string;
 
+  @Column({ allowNull: false })
+  app_deployment_name: string;
+
   @Column({ allowNull: false, type: DataType.JSONB })
   data: any;
 }
---
+++ packages/api/src/endpoints/common/app.dto.ts
@@ -0,0 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+/**
+ * Common app DTO - fields below should be automatically populated by app
+ * when calling api
+ **/
+export class AppCommonDto {
+  @ApiProperty({ description: "App user ID" })
+  app_user_id: string;
+
+  @ApiProperty({ description: "Name of app deployment", default: "" })
+  app_deployment_name: string;
+
+  @ApiProperty({ description: "Current app version", default: "0.0.0" })
+  app_version: string;
+}
