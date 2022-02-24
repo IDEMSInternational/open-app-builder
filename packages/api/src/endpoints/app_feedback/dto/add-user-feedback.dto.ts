02542ec3d391205f2b7f1fadb74755df5f04077f
chrismclarke <chris.m.clarke@live.co.uk>
2022-02-23 18:09:57 -0800

    feat: add server api for local notification interactions

--- packages/api/src/endpoints/app_feedback/dto/add-user-feedback.dto.ts
+++ packages/api/src/endpoints/app_feedback/dto/add-user-feedback.dto.ts
@@ -4,7 +4,4 @@ import { UserCommonDto } from "src/endpoints/common";
 export class UserFeedbackDto extends UserCommonDto {
   @ApiProperty({ description: "Feedback entry" })
   data: any;
-
-  @ApiProperty({ description: "Name of app deployment", default: "" })
-  app_deployment_name: string;
 }
--- packages/api/src/endpoints/common/app.dto.ts
+++ packages/api/src/endpoints/common/app.dto.ts
@@ -14,3 +14,15 @@ export class AppCommonDto {
   @ApiProperty({ description: "Current app version", default: "0.0.0" })
   app_version: string;
 }
+
+import { Column, Model } from "sequelize-typescript";
+export class AppCommonModel extends Model<AppCommonModel> {
+  @Column({ allowNull: false })
+  app_user_id: string;
+
+  @Column({ allowNull: false })
+  app_deployment_name: string;
+
+  @Column({ allowNull: false })
+  app_user_record_id: string;
+}
