203194574d09d330b0740e73014fc11b0024a655
EsmeeIDEMS <esmee@idems.international>
2022-03-01 12:16:27 +0300

    Correct languages

--- .idems_app/deployments/plh/za.config.ts
+++ .idems_app/deployments/plh/za.config.ts
@@ -3,8 +3,8 @@ import DEFAULT_CONFIG from "./default.config";
 /** ZA config extends the default config **/
 
 const config = DEFAULT_CONFIG;
-config.name = "plh_tz";
+config.name = "plh_za";
 config.app_data.sheets_filter_function = (flow) => !["debug"].includes(flow.flow_subtype);
-config.translations.filter_language_codes = ["za_zu", "za_xh", "za_tn", "za_st", "za_af"];
+config.translations.filter_language_codes = ["za_en, za_zu", "za_xh", "za_tn", "za_st", "za_af"];
 
 export default config;
