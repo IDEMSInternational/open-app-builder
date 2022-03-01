ecb35d86f1445a48406d7660c98657c3bb89de8c
EsmeeIDEMS <esmee@idems.international>
2022-03-01 12:12:49 +0300

    Rename plh deployments

--- .idems_app/deployments/plh/za.config.ts
+++ .idems_app/deployments/plh/za.config.ts
@@ -3,7 +3,7 @@ import DEFAULT_CONFIG from "./default.config";
 /** ZA config extends the default config **/
 
 const config = DEFAULT_CONFIG;
-config.name = "PLH ZA";
+config.name = "plh_tz";
 config.app_data.sheets_filter_function = (flow) => !["debug"].includes(flow.flow_subtype);
 config.translations.filter_language_codes = ["za_zu", "za_xh", "za_tn", "za_st", "za_af"];
 
