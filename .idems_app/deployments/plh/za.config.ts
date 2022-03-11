22a8870bf2ac85dfc9d58f01fe6d8c1c8dbc803f
chrismclarke <chris.m.clarke@live.co.uk>
2022-03-10 19:58:50 -0800

    feat: add tz and za constant overrides

--- .idems_app/deployments/plh/za.config.ts
+++ .idems_app/deployments/plh/za.config.ts
@@ -5,7 +5,10 @@ import DEFAULT_CONFIG from "./default.config";
 const config = DEFAULT_CONFIG;
 config.name = "plh_za";
 config.app_data.sheets_filter_function = (flow) => !["debug"].includes(flow.flow_subtype);
-config.app_constants = { APP_LANGUAGES: { default: "za_en" } };
 config.translations.filter_language_codes = ["za_en, za_zu", "za_xh", "za_tn", "za_st", "za_af"];
+
+// Override constants
+config.app_constants.APP_LANGUAGES.default = "za_en";
+config.app_constants.APP_SIDEMENU_DEFAULTS.title = "ParentApp (ZA)";
 
 export default config;
