6713c3741daf68cb77b56e94802ebe6b34110e25
chrismclarke <chris.m.clarke@live.co.uk>
2022-03-10 20:20:21 -0800

    fix: language code typo

--- .idems_app/deployments/plh/za.config.ts
+++ .idems_app/deployments/plh/za.config.ts
@@ -5,7 +5,7 @@ import DEFAULT_CONFIG from "./default.config";
 const config = DEFAULT_CONFIG;
 config.name = "plh_za";
 config.app_data.sheets_filter_function = (flow) => !["debug"].includes(flow.flow_subtype);
-config.translations.filter_language_codes = ["za_en, za_zu", "za_xh", "za_tn", "za_st", "za_af"];
+config.translations.filter_language_codes = ["za_en", "za_zu", "za_xh", "za_tn", "za_st", "za_af"];
 
 // Override constants
 config.app_constants.APP_LANGUAGES.default = "za_en";
