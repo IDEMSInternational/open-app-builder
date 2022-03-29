b5b4ba610df480f7e71980eca643055826e1697d
EsmeeIDEMS <esmee@idems.international>
2022-03-29 13:36:56 +0100

    Content sync 29/03/2022

--- packages/app-data/assets/index.ts
+++ packages/app-data/assets/index.ts
@@ -53,7 +53,7 @@ export const ASSETS_CONTENTS_LIST = {
     size_kb: 33.7,
     md5Checksum: "11afe0fec439d67ceb9b6e6c819d2204",
   },
-  "EFM110InvisibleAdd ub.svg": {
+  "EFM110InvisibleAddSub.svg": {
     size_kb: 16,
     md5Checksum: "86472c32ba00ac2209471028aadf8363",
   },
@@ -193,13 +193,13 @@ export const ASSETS_CONTENTS_LIST = {
     size_kb: 31.3,
     md5Checksum: "fc95af2efe49b07cb62b73ee36fff6a8",
   },
-  "EFM16015Puzzle3by3.svg": {
-    size_kb: 24.6,
-    md5Checksum: "35f9bedf1a8c83405a974627ab768f22",
-  },
   "EFM160Codebreaker.svg": {
     size_kb: 11.2,
     md5Checksum: "34cd7690d9a17838f2e7dea151c16920",
+  },
+  "EFM160FifteenPuzzle3by3.svg": {
+    size_kb: 24.6,
+    md5Checksum: "35f9bedf1a8c83405a974627ab768f22",
   },
   "EFM170Fingers4Plus2.svg": {
     size_kb: 6.3,
@@ -221,6 +221,34 @@ export const ASSETS_CONTENTS_LIST = {
     size_kb: 21.8,
     md5Checksum: "14b2df861806e215abc2c00fd86d4ba4",
   },
+  "EFM180ComplexShapeSums.svg": {
+    size_kb: 14.8,
+    md5Checksum: "cb3f4a9ebc89cd16ea157b16042314be",
+  },
+  "EFM180SimpleShapeSums.svg": {
+    size_kb: 10.3,
+    md5Checksum: "67ae7db90dcec1eda9c9c48b7774627d",
+  },
+  "EFM190NumberCardsto20.svg": {
+    size_kb: 96.4,
+    md5Checksum: "69f42c26c7d064f271baa02d06ac3b02",
+  },
+  "EFM200NumberShapes.svg": {
+    size_kb: 6.7,
+    md5Checksum: "32593005fbb1c8e134137061d7ed3a64",
+  },
+  "EFM200SimTriangle.svg": {
+    size_kb: 5,
+    md5Checksum: "8f748e744836eb3127c55a3c64b7a460",
+  },
+  "EFM200SquareOfOddNumbers.svg": {
+    size_kb: 9.7,
+    md5Checksum: "99cf8aac3bed1ac145208e0e9445e08f",
+  },
+  "EFM200SymmetricArt.svg": {
+    size_kb: 12.3,
+    md5Checksum: "478f6161ca5b6f7db9c24731afafcd94",
+  },
   "EFM20Balloons.svg": {
     size_kb: 27,
     md5Checksum: "a9ef17e2a7f45ba9873720111a5d860a",
@@ -237,6 +265,58 @@ export const ASSETS_CONTENTS_LIST = {
     size_kb: 14.7,
     md5Checksum: "26e0b01c34e42d875bba1643b531dc78",
   },
+  "EFM220NumberBlobs.svg": {
+    size_kb: 39.3,
+    md5Checksum: "a0b4e947981614a21cd79f2cc4bf7d83",
+  },
+  "EFM230OrderedSteps.svg": {
+    size_kb: 4.1,
+    md5Checksum: "d3778624481a5644ce941c252d2ddea8",
+  },
+  "EFM230OrderingCards.svg": {
+    size_kb: 21.7,
+    md5Checksum: "35f8d29ed5c959be47890de992a41ac6",
+  },
+  "EFM240EvenOddAndKropkiSudoku4by4.svg": {
+    size_kb: 19.9,
+    md5Checksum: "4ee94a947af4509900ac7775af7bc102",
+  },
+  "EFM240SumdokuDiffdoku.svg": {
+    size_kb: 25.6,
+    md5Checksum: "d32059ce3837d12d570ee94df4c55677",
+  },
+  "EFM270PyramidSolitaire.svg": {
+    size_kb: 32.5,
+    md5Checksum: "5343801697ccafc1a3642f8f9d1f1765",
+  },
+  "EFM280DotsAndBoxes.svg": {
+    size_kb: 6.8,
+    md5Checksum: "4dd24fa087a3628a44194c76e7840216",
+  },
+  "EFM280Tapatan.svg": {
+    size_kb: 7.1,
+    md5Checksum: "09af0fac0fc9e16f12438fd46947b1c6",
+  },
+  "EFM290Fingers11-3.svg": {
+    size_kb: 21.6,
+    md5Checksum: "621981665dddbf6c23ce2b8209f287e9",
+  },
+  "EFM290FingersFor8-3.svg": {
+    size_kb: 11.8,
+    md5Checksum: "dc94089b70ab7b272decca5ae74d6f89",
+  },
+  "EFM290FingersFor8Plus3.svg": {
+    size_kb: 11.8,
+    md5Checksum: "6abcea34ea7848eb202e63eaa6c49192",
+  },
+  "EFM300IslandHoppingCompensation.svg": {
+    size_kb: 24.7,
+    md5Checksum: "a9cce4409268b9e84e3dbb138085e3c8",
+  },
+  "EFM300RegionSums.svg": {
+    size_kb: 10.9,
+    md5Checksum: "fe01ebdb0030678072fe72fd12f8ca7a",
+  },
   "EFM30MugOnShelf.svg": {
     size_kb: 4.9,
     md5Checksum: "a9a3442408519d13b68b3b217d4c81f9",
@@ -248,6 +328,66 @@ export const ASSETS_CONTENTS_LIST = {
   "EFM30Socks.svg": {
     size_kb: 18,
     md5Checksum: "e259999070a52db6ce7803a5e3c78382",
+  },
+  "EFM310DiffTriangles.svg": {
+    size_kb: 14.9,
+    md5Checksum: "497bcfc64f3d0c9b0bee0d09b1780e15",
+  },
+  "EFM310SumTriangles.svg": {
+    size_kb: 15.5,
+    md5Checksum: "e49ec917e13e0e187d826b2437e3d231",
+  },
+  "EFM330IslandHoppingSkipCounting.svg": {
+    size_kb: 19.9,
+    md5Checksum: "d135334551aa4cf2e9cf352555833d66",
+  },
+  "EFM340FixIt.svg": {
+    size_kb: 17.3,
+    md5Checksum: "ebe0d3f57b804c0d6b94fa19d2896388",
+  },
+  "EFM340PairingDown.svg": {
+    size_kb: 29.6,
+    md5Checksum: "f5571ef08b856edef43066d1938e1246",
+  },
+  "EFM350ExpandedFormFoldingPaper.svg": {
+    size_kb: 11.4,
+    md5Checksum: "a97fc802c169e40219a8f248da9b2b28",
+  },
+  "EFM350IslandHoppingby1sAnd10s.svg": {
+    size_kb: 15.6,
+    md5Checksum: "32c18b32568938b9644624e491075276",
+  },
+  "EFM370CircleGraphs1To12StarOfDavid.svg": {
+    size_kb: 13.5,
+    md5Checksum: "bb3d624de74615dbbea30a4309901927",
+  },
+  "EFM370CircleGraphs1to6MagicCircle.svg": {
+    size_kb: 19.2,
+    md5Checksum: "d50b5c1de7eb9914b7ef1a663c3e8f0d",
+  },
+  "EFM370CircleGraphs1To7H.svg": {
+    size_kb: 8.1,
+    md5Checksum: "a92954e71b6a8a94d61c2c09107bdf3d",
+  },
+  "EFM370CircleGraphs1To9Star.svg": {
+    size_kb: 7.3,
+    md5Checksum: "0023f35a7b29420a1184bcd6981aed9f",
+  },
+  "EFM380MultiplyingCommutes.svg": {
+    size_kb: 5.4,
+    md5Checksum: "d1999d720133eda63422285e42fa6322",
+  },
+  "EFM390AdditionPyramid.svg": {
+    size_kb: 18,
+    md5Checksum: "c020a023b6db3e3664a6302c90c9d3e4",
+  },
+  "EFM390SumSquare.svg": {
+    size_kb: 8,
+    md5Checksum: "2bd46664774fa2890c8adea948ddd7aa",
+  },
+  "EFM400FibTableTo9.svg": {
+    size_kb: 14.5,
+    md5Checksum: "94bcdddfe90d09dfe15ce879f27f6f49",
   },
   "EFM40CheerioPyramid.svg": {
     size_kb: 40.5,
@@ -261,6 +401,66 @@ export const ASSETS_CONTENTS_LIST = {
     size_kb: 18.4,
     md5Checksum: "3cf3a5001cfc3791f2f5fb300c0a6810",
   },
+  "EFM440BondedGroups.svg": {
+    size_kb: 28.3,
+    md5Checksum: "fb977e2b7296ffcc05c85ab6e2037dd7",
+  },
+  "EFM440MissingNumbers.svg": {
+    size_kb: 15.7,
+    md5Checksum: "bb8e6fc844020e96c811f1d6ef0373ac",
+  },
+  "EFM450MultiplicationCards1.svg": {
+    size_kb: 13.3,
+    md5Checksum: "296bf8959490fce08c950b16691a207e",
+  },
+  "EFM450MultiplicationCards2.svg": {
+    size_kb: 7.6,
+    md5Checksum: "f8ef2a360e1dde5652a42742ff692b07",
+  },
+  "EFM450MultiplicationCards3.svg": {
+    size_kb: 8,
+    md5Checksum: "01bb557b9eee21593f7a04ef30a39169",
+  },
+  "EFM450RevealingProducts.jpg": {
+    size_kb: 52.1,
+    md5Checksum: "e2b73dd22f88dd1df652c9373eee06b2",
+  },
+  "EFM450RevealingProducts.odg": {
+    size_kb: 12.1,
+    md5Checksum: "32894c21aba13a95322fd15ce47143b4",
+  },
+  "EFM450RevealingProducts.svg": {
+    size_kb: 71.8,
+    md5Checksum: "8bd8b3e9ab5a877884c45c8c04409918",
+  },
+  "EFM460SieveOfEratosthenes.svg": {
+    size_kb: 47,
+    md5Checksum: "abcfcb40d3eaad5417830d0231edb0f2",
+  },
+  "EFM470LeverBalance.svg": {
+    size_kb: 11.3,
+    md5Checksum: "9200d1b91b8006a55a55d6731817389f",
+  },
+  "EFM480TurningTheTables.svg": {
+    size_kb: 17.4,
+    md5Checksum: "6fa7a2f3e0d0464a51efbd811e26d3b1",
+  },
+  "EFM490DivideUpTheBox.svg": {
+    size_kb: 7.3,
+    md5Checksum: "71ebf83808a6000744f1827206e1f85e",
+  },
+  "EFM490RectangleArea.svg": {
+    size_kb: 3.3,
+    md5Checksum: "70c9b5709daea97d1b545e2be90bfddb",
+  },
+  "EFM500SimpleShapeProducts.svg": {
+    size_kb: 10.3,
+    md5Checksum: "8122b7fdc12bf58c0e979c5af5ce5425",
+  },
+  "EFM500SimpleShapeSums.svg": {
+    size_kb: 10.3,
+    md5Checksum: "67ae7db90dcec1eda9c9c48b7774627d",
+  },
   "EFM50NumberCards.svg": {
     size_kb: 133.6,
     md5Checksum: "656a99c681b1a460dc1e5de6f0c6edb6",
@@ -272,6 +472,54 @@ export const ASSETS_CONTENTS_LIST = {
   "EFM50ShapeDeck.svg": {
     size_kb: 42.9,
     md5Checksum: "1bbff7046b5159fc7f239992038cdaea",
+  },
+  "EFM510FactorTrees.svg": {
+    size_kb: 17.2,
+    md5Checksum: "abff417470e3e33b2ba1d376633faeae",
+  },
+  "EFM510ProductPyramid.svg": {
+    size_kb: 17.6,
+    md5Checksum: "438916864bd3d27d5c0846ee13d9dde5",
+  },
+  "EFM520LetterSubstitution1.svg": {
+    size_kb: 12.4,
+    md5Checksum: "0bf2fd06a53637bdeb7138d850c5bae5",
+  },
+  "EFM520LetterSubstitution2.svg": {
+    size_kb: 24.8,
+    md5Checksum: "fb985bcfde96ed859b5e7d7945631716",
+  },
+  "EFM530BilliardBallBouncing.svg": {
+    size_kb: 5.9,
+    md5Checksum: "66ba6208de534a50b2a1f9a45026389d",
+  },
+  "EFM530SquaresInSquares.svg": {
+    size_kb: 3.1,
+    md5Checksum: "1778e540ff7959bf42506ffe1e9140ba",
+  },
+  "EFM540IslandHoppingWithProducts.svg": {
+    size_kb: 21.7,
+    md5Checksum: "93db1766f59cf0b591d78fed7dab1446",
+  },
+  "EFM540ProductGame.svg": {
+    size_kb: 26.5,
+    md5Checksum: "0645ffa477ce4cbadef31ab158bd9f75",
+  },
+  "EFM550Mobile1.svg": {
+    size_kb: 6.9,
+    md5Checksum: "20c927df7e1a6a63b1a3c55cec0f0320",
+  },
+  "EFM550Mobile2.svg": {
+    size_kb: 10,
+    md5Checksum: "2a00f4817f411c8df34d60ae5ce1592e",
+  },
+  "EFM570CrossProducts3By3.svg": {
+    size_kb: 12.9,
+    md5Checksum: "b5abb9478f5ee6cc18d6a49a6833c2d7",
+  },
+  "EFM570CrossProducts4By4.svg": {
+    size_kb: 17.7,
+    md5Checksum: "e4b71b43189451a8699c740883948a1f",
   },
   "EFM60HomeStoreItems.svg": {
     size_kb: 35.3,
@@ -313,10 +561,6 @@ export const ASSETS_CONTENTS_LIST = {
     size_kb: 4.9,
     md5Checksum: "69975069334656ddb56bd16963e30cb2",
   },
-  "EFM80StringNumberLine.svg": {
-    size_kb: 22,
-    md5Checksum: "9e9fe741ed186fb9770ea823814c1314",
-  },
   "EFM90HouseConnecttheDots.svg": {
     size_kb: 9.2,
     md5Checksum: "703ff9d596068fb1c49739f7d09d2c16",
@@ -329,6 +573,10 @@ export const ASSETS_CONTENTS_LIST = {
     size_kb: 20.8,
     md5Checksum: "d11a654dd4eecfa5c50fe1b893535041",
   },
+  "EFM90StringNumberLine.svg": {
+    size_kb: 22,
+    md5Checksum: "9e9fe741ed186fb9770ea823814c1314",
+  },
   "EFM95ColoredShapesonFloor.svg": {
     size_kb: 10.2,
     md5Checksum: "5f50e0c639e5c917cca9713642a3bf6a",
@@ -337,8 +585,60 @@ export const ASSETS_CONTENTS_LIST = {
     size_kb: 12.2,
     md5Checksum: "99580b2701744820518584dcfbee485b",
   },
-  "network.mp4": {
-    size_kb: 53849,
-    md5Checksum: "ab1d06984cc23e829e517aa6a18ebc8b",
+  "EFMStorybooks/EFM-SB-0-PageDivider.svg": {
+    size_kb: 3.2,
+    md5Checksum: "ea064735a285b957803db53a4b0e197f",
+  },
+  "EFMStorybooks/EFM-SB-AVTM-1.png": {
+    size_kb: 227.4,
+    md5Checksum: "7a1ecf8702751d9234087bff343c174e",
+  },
+  "EFMStorybooks/EFM-SB-AVTM-10.png": {
+    size_kb: 238.3,
+    md5Checksum: "7d07005bac8dee5736d7fffc541fced3",
+  },
+  "EFMStorybooks/EFM-SB-AVTM-11.png": {
+    size_kb: 401.2,
+    md5Checksum: "4926c8ac14565300438d05591b8bf74f",
+  },
+  "EFMStorybooks/EFM-SB-AVTM-2.png": {
+    size_kb: 255.7,
+    md5Checksum: "727db625179d40101095e353bd76ee0c",
+  },
+  "EFMStorybooks/EFM-SB-AVTM-3.png": {
+    size_kb: 219.1,
+    md5Checksum: "f26906d64c247810cce2252c099761d6",
+  },
+  "EFMStorybooks/EFM-SB-AVTM-4.png": {
+    size_kb: 285.3,
+    md5Checksum: "50f2e256264adebb38757132e9e093c2",
+  },
+  "EFMStorybooks/EFM-SB-AVTM-5.png": {
+    size_kb: 122.3,
+    md5Checksum: "dbd8ee226b22722830d331d657cfef04",
+  },
+  "EFMStorybooks/EFM-SB-AVTM-6.png": {
+    size_kb: 250.4,
+    md5Checksum: "83a40f410cc90f0e3976862891971f19",
+  },
+  "EFMStorybooks/EFM-SB-AVTM-7.png": {
+    size_kb: 226.4,
+    md5Checksum: "9862e2506a78b11a4893c692385e07bb",
+  },
+  "EFMStorybooks/EFM-SB-AVTM-8.png": {
+    size_kb: 177,
+    md5Checksum: "e26c155469f5f2c2b797b01bb456213e",
+  },
+  "EFMStorybooks/EFM-SB-AVTM-9.png": {
+    size_kb: 269.6,
+    md5Checksum: "6edbd19f71d1c80356da9d9c724c7590",
+  },
+  "EFMStorybooks/EFM-SB-AVTM-Back.png": {
+    size_kb: 110,
+    md5Checksum: "e0eb292539ae5755187e9853b71c4649",
+  },
+  "EFMStorybooks/EFM-SB-AVTM-Cover.png": {
+    size_kb: 227.4,
+    md5Checksum: "7a1ecf8702751d9234087bff343c174e",
   },
 };
