3aab5c144e506e7237b1b8e4d08ae2aa360352eb
Chris <chris.m.clarke@live.co.uk>
2022-04-16 13:08:05 +0100

    chore: update docs

--- packages/scripts/src/commands/app-data/sync.ts
+++ packages/scripts/src/commands/app-data/sync.ts
@@ -2,6 +2,7 @@
 import { spawnSync } from "child_process";
 import { Command } from "commander";
 import fs from "fs-extra";
+import { logWarning } from "../../utils";
 import { getActiveDeployment } from "../deployment/get";
 
 const program = new Command("sync");
@@ -22,7 +23,11 @@ export default program
   )
   .option("-c --clean", "clear all cached data for a clean sync")
   .action(async (options: IProgramOptions) => {
-    await syncAppData(options);
+    // await syncAppData(options);
+    logWarning({
+      msg1: "NOTE - Script has been renamed and will be deprecated",
+      msg2: `"yarn scripts app-data sync" -> "yarn workflow sync"`,
+    });
   });
 
 /***************************************************************************************
