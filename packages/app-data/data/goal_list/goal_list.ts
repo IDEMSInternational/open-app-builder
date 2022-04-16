17589816b4d065c5988fc4ec2636a03782b4a6df
Chris <chris.m.clarke@live.co.uk>
2022-04-16 23:27:20 +0100

    chore: remove deprecated types

--- packages/app-data/data/goal_list/goal_list.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable */
-import { FlowTypes } from "data-models";
-const goal_list: FlowTypes.Goal_list[] = [];
-export default goal_list;
--- packages/app-data/data/module_page/module_page.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable */
-import { FlowTypes } from "data-models";
-const module_page: FlowTypes.Module_page[] = [];
-export default module_page;
