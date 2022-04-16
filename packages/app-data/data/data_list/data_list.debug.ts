34b50ac32e522338132996dfc66b953797cc6849
Chris <chris.m.clarke@live.co.uk>
2022-04-16 11:41:05 +0100

    chore: sync latest content

--- packages/app-data/data/data_list/data_list.debug.ts
+++ packages/app-data/data/data_list/data_list.debug.ts
@@ -30,6 +30,71 @@ const data_list: FlowTypes.Data_list[] = [
   },
   {
     flow_type: "data_list",
+    flow_name: "debug_data_list",
+    status: "released",
+    data_list_name: "debug",
+    flow_subtype: "debug",
+    rows: [
+      {
+        id: "item_1",
+        text: "Item 1 text",
+        image_asset: "plh_images/habits/habit_relax.svg",
+      },
+      {
+        id: "item_2",
+        text: "Item 2 text",
+        image_asset: "plh_images/icons/play_white.svg",
+      },
+      {
+        id: "item_3",
+        text: "Item 3 text",
+      },
+    ],
+    _xlsxPath: "quality_assurance/debug_templates/debug_data_lists.xlsx",
+  },
+  {
+    flow_type: "data_list",
+    flow_name: "debug_variable_data",
+    status: "released",
+    data_list_name: "debug_vars",
+    flow_subtype: "debug",
+    rows: [
+      {
+        id: "number_1",
+        value: 1,
+      },
+      {
+        id: "bool_true",
+        value: true,
+      },
+      {
+        id: "bool_false",
+        value: false,
+      },
+      {
+        id: "number_0",
+        value: 0,
+      },
+      {
+        id: "list_1",
+        value_list: ["1", "2", "3"],
+      },
+      {
+        id: "text_1",
+        value: "text1",
+      },
+      {
+        id: "collection_1",
+        value_collection: {
+          key1: "val1",
+          key2: "val2",
+        },
+      },
+    ],
+    _xlsxPath: "quality_assurance/debug_templates/debug_nesting_variables.xlsx",
+  },
+  {
+    flow_type: "data_list",
     flow_name: "example_lang_data_list",
     status: "released",
     data_list_name: "example_lang",
@@ -194,71 +259,6 @@ const data_list: FlowTypes.Data_list[] = [
       },
     ],
     _xlsxPath: "quality_assurance/example_templates/example_items.xlsx",
-  },
-  {
-    flow_type: "data_list",
-    flow_name: "debug_data_list",
-    status: "released",
-    data_list_name: "debug",
-    flow_subtype: "debug",
-    rows: [
-      {
-        id: "item_1",
-        text: "Item 1 text",
-        image_asset: "plh_images/habits/habit_relax.svg",
-      },
-      {
-        id: "item_2",
-        text: "Item 2 text",
-        image_asset: "plh_images/icons/play_white.svg",
-      },
-      {
-        id: "item_3",
-        text: "Item 3 text",
-      },
-    ],
-    _xlsxPath: "quality_assurance/debug_templates/debug_data_lists.xlsx",
-  },
-  {
-    flow_type: "data_list",
-    flow_name: "debug_variable_data",
-    status: "released",
-    data_list_name: "debug_vars",
-    flow_subtype: "debug",
-    rows: [
-      {
-        id: "number_1",
-        value: 1,
-      },
-      {
-        id: "bool_true",
-        value: true,
-      },
-      {
-        id: "bool_false",
-        value: false,
-      },
-      {
-        id: "number_0",
-        value: 0,
-      },
-      {
-        id: "list_1",
-        value_list: ["1", "2", "3"],
-      },
-      {
-        id: "text_1",
-        value: "text1",
-      },
-      {
-        id: "collection_1",
-        value_collection: {
-          key1: "val1",
-          key2: "val2",
-        },
-      },
-    ],
-    _xlsxPath: "quality_assurance/debug_templates/debug_nesting_variables.xlsx",
   },
 ];
 export default data_list;
