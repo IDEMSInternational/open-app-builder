fbca195917850a1b93861650ff13fcc9e17ae62c
Chris <chris.m.clarke@live.co.uk>
2022-04-17 01:30:36 +0100

    chore: code tidying

--- packages/scripts/src/commands/app-data/sync.ts
+++ packages/scripts/src/commands/app-data/sync.ts
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
-import { spawnSync } from "child_process";
 import { Command } from "commander";
-import fs from "fs-extra";
 import { logWarning } from "../../utils";
-import { getActiveDeployment } from "../deployment/get";
 
 const program = new Command("sync");
 
@@ -29,53 +26,3 @@ export default program
       msg2: `"yarn scripts app-data sync" -> "yarn workflow sync"`,
     });
   });
-
-/***************************************************************************************
- * Main Methods
- *************************************************************************************/
-/**
- * Read the default deployment json and retrieve parsed ts for the named active deployment
- */
-async function syncAppData(options: IProgramOptions) {
-  const scriptsExec = `yarn workspace scripts start`;
-  // Download
-  let downloadCmd = "app-data download";
-  if (options.sheetname) {
-    downloadCmd += ` --sheetname ${options.sheetname}`;
-  }
-  if (options.clean) {
-    cleanFolders();
-  }
-  const { status } = spawnSync(`${scriptsExec} ${downloadCmd}`, {
-    stdio: "inherit",
-    shell: true,
-  });
-  if (status === 1) {
-    process.exit(1);
-  }
-
-  // Convert
-  let convertCmd = "app-data convert";
-  spawnSync(`${scriptsExec} ${convertCmd}`, { stdio: "inherit", shell: true });
-  // Translate
-  let translateCmd = "app-data translate";
-  spawnSync(`${scriptsExec} ${translateCmd}`, { stdio: "inherit", shell: true });
-  // Copy
-  let copyCmd = "app-data copy";
-
-  spawnSync(`${scriptsExec} ${copyCmd}`, { stdio: "inherit", shell: true });
-}
-
-function cleanFolders() {
-  const { google_drive, app_data, translations } = getActiveDeployment();
-  const targetFolders: string[] = [
-    google_drive.cache_path,
-    app_data.converter_cache_path,
-    translations.output_cache_path,
-  ];
-  for (const targetFolder of targetFolders) {
-    if (fs.existsSync(targetFolder)) {
-      fs.emptyDirSync(targetFolder);
-    }
-  }
-}
