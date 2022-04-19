ffd6d5913bcbe28097ee238d8e49f550376b44ef
Chris <chris.m.clarke@live.co.uk>
2022-04-19 13:30:39 +0100

    fix: refactor default workflows

---
+++ packages/data-models/workflows.ts
@@ -0,0 +1,68 @@
+import { IDeploymentWorkflows } from "./workflow.model";
+/** Default workflows made available to all deployments */
+export const WORKFLOW_DEFAULTS: IDeploymentWorkflows = {
+  sync: {
+    label: "Sync All Content",
+    steps: [
+      {
+        name: "sync_sheets",
+        function: async ({ tasks }) => tasks.workflow.runWorkflow({ name: "sync_templates" }),
+      },
+      {
+        name: "sync_assets",
+        function: async ({ tasks }) => tasks.workflow.runWorkflow({ name: "sync_assets" }),
+      },
+    ],
+  },
+  sync_templates: {
+    label: "Sync Latest Templates",
+    steps: [
+      {
+        name: "sheets_dl",
+        function: async ({ tasks, config }) =>
+          tasks.gdrive.download({ folderId: config.google_drive.sheets_folder_id }),
+      },
+      {
+        name: "sheets_process",
+        function: async ({ tasks, workflow }) =>
+          tasks.template.process({ inputFolder: workflow.sheets_dl.output }),
+      },
+      {
+        name: "translations_apply",
+        function: async ({ tasks, workflow }) =>
+          tasks.translate.apply({ inputFolder: workflow.sheets_process.output }),
+      },
+      {
+        name: "translations_copy_for_export",
+        function: async ({ tasks, workflow }) =>
+          tasks.translate.copyContentForTranslators({
+            inputFolder: workflow.sheets_process.output,
+          }),
+      },
+      {
+        name: "app_copy_sheets",
+        function: async ({ tasks, workflow }) =>
+          tasks.appData.copy({ localSheetsFolder: workflow.translations_apply.output }),
+      },
+    ],
+  },
+  sync_assets: {
+    label: "Sync Latest Assets",
+    steps: [
+      {
+        name: "assets_dl",
+        function: async ({ tasks, config }) =>
+          tasks.gdrive.download({ folderId: config.google_drive.assets_folder_id }),
+      },
+      {
+        name: "app_copy_assets",
+        function: async ({ tasks, workflow }) =>
+          tasks.appData.copy({ localAssetsFolder: workflow.assets_dl.output }),
+      },
+    ],
+  },
+  setup: {
+    label: "(TODO) - Setup Deployment",
+    steps: [],
+  },
+};
