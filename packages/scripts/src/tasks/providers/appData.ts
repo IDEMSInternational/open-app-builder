297715c07b5475ddffa4cbb060cf509f0b1fade8
Chris <chris.m.clarke@live.co.uk>
2022-04-21 09:39:37 +0100

    feat: add src asset replication methods and binds

--- packages/scripts/src/tasks/providers/appData.ts
+++ packages/scripts/src/tasks/providers/appData.ts
@@ -1,4 +1,7 @@
 import { spawnSync } from "child_process";
+import path from "path";
+import { ROOT_DIR } from "../../paths";
+import { replicateDir } from "../../utils";
 
 // const qualityCheckAssets = () => {};
 
@@ -37,6 +40,32 @@ const copy = (options: {
   let copyCmd = `app-data copy ${args}`;
 
   spawnSync(`${scriptsExec} ${copyCmd}`, { stdio: "inherit", shell: true });
+  populateSrcAssets({ appSheetsFolder, appAssetsFolder, appTranslationsFolder });
 };
 
-export default { copy };
+/**
+ * HACK - make additional copy direct to app assets to force angular to detect changes if running
+ * Note - ideally this will be removed when transitioning away from app-data (to standalone repos)
+ * and direct copy can be included instead
+ */
+const populateSrcAssets = (options: {
+  appSheetsFolder?: string;
+  appAssetsFolder?: string;
+  appTranslationsFolder?: string;
+}) => {
+  const { appSheetsFolder, appAssetsFolder, appTranslationsFolder } = options;
+  if (appSheetsFolder) {
+    const targetFolder = path.resolve(ROOT_DIR, "src", "assets", "app_data", "sheets");
+    replicateDir(appSheetsFolder, targetFolder);
+  }
+  if (appAssetsFolder) {
+    const targetFolder = path.resolve(ROOT_DIR, "src", "assets", "app_data", "assets");
+    replicateDir(appAssetsFolder, targetFolder);
+  }
+  if (appTranslationsFolder) {
+    const targetFolder = path.resolve(ROOT_DIR, "src", "assets", "app_data", "translations");
+    replicateDir(appTranslationsFolder, targetFolder);
+  }
+};
+
+export default { copy, populateSrcAssets };
