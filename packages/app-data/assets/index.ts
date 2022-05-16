173599e7835648032a9bf676a7d31207755abc32
EsmeeIDEMS <esmee@idems.international>
2022-05-16 18:36:52 +0200

    Content sync 16/05/2022

--- packages/app-data/assets/index.ts
+++ packages/app-data/assets/index.ts
@@ -2630,6 +2630,10 @@ export const ASSETS_CONTENTS_LIST = {
     size_kb: 13482.2,
     md5Checksum: "62f06faa95a979d128b4356db9bb396e",
   },
+  "plh_video/lets_slow_down.srt": {
+    size_kb: 1.7,
+    md5Checksum: "77e6d4265f602b20745a738b9809bb15",
+  },
   "quality_assurance/test_audio.mp3": {
     size_kb: 43.4,
     md5Checksum: "5ddddf934d2187d084c75b7e27797fae",
