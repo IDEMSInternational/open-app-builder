ab124d9abb199168e56b86b01db6141374e85a65
Johnny McQuade <jfmcquade@hotmail.com>
2022-05-16 16:51:02 +0100

    WIP: migrated to '@capacitor-firebase/authentication' plugin

--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -1,38 +1,50 @@
-import { Injectable } from "@angular/core";
-import { AngularFireAuth } from "@angular/fire/auth";
-import firebase from "firebase";
-// import { FirebaseAuthentication } from "@capacitor-firebase/authentication";
+import { Injectable, NgZone } from "@angular/core";
+import { Observable, Subject } from "rxjs";
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { getAuth, signInWithCredential, GoogleAuthProvider } from "firebase/auth";
+import { AuthStateChange, FirebaseAuthentication } from "@capacitor-firebase/authentication";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
-  constructor(public afAuth: AngularFireAuth) {}
+  private readonly authStateSubj = new Subject<AuthStateChange>();
 
-  async signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    return this.afAuth.signInWithRedirect(provider);
+  constructor(public afAuth: AngularFireAuth, private readonly ngZone: NgZone) {
+    FirebaseAuthentication.removeAllListeners().then(() => {
+      FirebaseAuthentication.addListener("authStateChange", (change) => {
+        this.ngZone.run(() => {
+          this.authStateSubj.next(change);
+        });
+      });
+    });
   }
 
+  public get authState$(): Observable<AuthStateChange> {
+    return this.authStateSubj.asObservable();
+  }
+
+  // with native SDK:
+  async signInWithGoogle() {
+    // 1. Create credentials on the native layer
+    const result = await FirebaseAuthentication.signInWithGoogle();
+    // 2. Sign in on the web layer using the id token
+    const credential = GoogleAuthProvider.credential(result.credential?.idToken);
+    const auth = getAuth();
+    await signInWithCredential(auth, credential);
+  }
+  // without native SDK:
+  // async signInWithGoogle() {
+  //   const result = await FirebaseAuthentication.signInWithGoogle();
+  //   await result.user
+  // }
+
   async signOut() {
-    return this.afAuth.signOut();
+    await FirebaseAuthentication.signOut();
   }
 
   async getCurrentUser() {
-    return firebase.auth().currentUser;
+    const result = await FirebaseAuthentication.getCurrentUser();
+    return result.user;
   }
-
-  // async signInWithGoogle() {
-  //   const result = await FirebaseAuthentication.signInWithGoogle();
-  //   return result.user;
-  // }
-
-  // async signOut() {
-  //   await FirebaseAuthentication.signOut();
-  // }
-
-  // async getCurrentUser() {
-  //   const result = await FirebaseAuthentication.getCurrentUser();
-  //   return result.user;
-  // }
 }
