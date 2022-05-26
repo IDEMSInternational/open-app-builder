0febf4b8b283aef3af3c41325818ffa9ae722458
Chris <chris.m.clarke@live.co.uk>
2022-05-26 11:43:04 +0100

    feat: add WiP workflow methods

--- packages/scripts/src/tasks/providers/gdrive.ts
+++ packages/scripts/src/tasks/providers/gdrive.ts
@@ -3,6 +3,8 @@ import path from "path";
 import { WorkflowRunner } from "../../commands/workflow/run";
 import { AUTH_TOKEN_PATH, CREDENTIALS_PATH } from "../../paths";
 
+const gdriveToolsExec = `yarn workspace @idemsInternational/gdrive-tools start`;
+
 /**
  *
  * @param options
@@ -24,12 +26,50 @@ const download = (options: { folderId: string }) => {
 
   const dlArgs = `--folder-id ${folderId} --output-path "${outputPath}" --log-name "${folderId}.log"`;
 
-  const gdriveToolsExec = `yarn workspace @idemsInternational/gdrive-tools start`;
-
   const cmd = `${gdriveToolsExec} download ${commonArgs} ${dlArgs}`;
 
   spawnSync(cmd, { stdio: "inherit", shell: true });
   return outputPath;
 };
 
-export default { download };
+/**
+ * Present options for configuring google drive liveReload
+ * This can automatically watch for changes to folders and pass back to listener
+ * @param options.folderId Google Drive folder id to watch for changes
+ * @param options.onUpdate Handler to respond to file updates
+ * @param optons.customCommands Additional commands to make available to CLI
+ */
+const liveReload = async (options: {
+  folderId: string;
+  onUpdate?: () => void;
+  customCommands?: IWatchCommand[];
+}) => {
+  const cmd = `${gdriveToolsExec} watch --folder-id ${options.folderId} `;
+  spawnSync(cmd, { stdio: "inherit", shell: true });
+  // WiP - allow manual intervention to force actions
+
+  // const watchCommands: IWatchCommand[] = [
+  //   ...(options.customCommands || []),
+  //   {
+  //     keybinding: "w",
+  //     name: "Toggle Sheets Watch",
+  //     command: async () => null,
+  //   },
+  //   {
+  //     keybinding: "q",
+  //     name: "Quit",
+  //     command: async () => process.exit(0),
+  //   },
+  // ];
+  // const prompts = watchCommands.map((v) => ({ name: v.name, value: v }));
+  // const selected = await promptOptions<IWatchCommand>(prompts, "Sync Options");
+  // await selected.command();
+};
+
+interface IWatchCommand {
+  keybinding: string;
+  name: string;
+  command: () => Promise<void>;
+}
+
+export default { download, liveReload };
