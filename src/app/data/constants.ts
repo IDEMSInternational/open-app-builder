158d0df1b98325957c908c1a127fd4799b6208b0
Chris <chris.m.clarke@live.co.uk>
2022-05-27 10:58:39 +0100

    chore: tidy app constant imports

--- src/app/data/constants.ts
+++ src/app/data/constants.ts
@@ -1,11 +1,11 @@
-import { DEFAULT_CONSTANTS } from "data-models";
+import { DEFAULT_APP_CONSTANTS, IAppConstants } from "data-models";
 import { environment } from "src/environments/environment";
 import { deepMergeObjects } from "../shared/utils";
 
 const app_constant_overrides = (environment.deploymentConfig as any).app_constants || {};
 
 /** List of constants provided by data-models combined with deployment-specific overrides */
-export const APP_CONSTANTS: typeof DEFAULT_CONSTANTS = deepMergeObjects(
-  DEFAULT_CONSTANTS,
+export const APP_CONSTANTS: IAppConstants = deepMergeObjects(
+  DEFAULT_APP_CONSTANTS,
   app_constant_overrides
 );
