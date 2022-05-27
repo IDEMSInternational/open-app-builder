afcd2e46c6582901f055b2275a5d3371e0181a73
Chris <chris.m.clarke@live.co.uk>
2022-05-27 11:05:25 +0100

    fix: add config clone methods

--- packages/data-models/index.ts
+++ packages/data-models/index.ts
@@ -3,5 +3,5 @@ export * from "./functions";
 export { IDeploymentWorkflows, IWorkflow, IWorkflowContext, WORKFLOW_DEFAULTS } from "./workflows";
 export { IDeploymentConfig, DEPLOYMENT_CONFIG_EXAMPLE_DEFAULTS } from "./deployment.model";
 
-export { DEFAULT_APP_CONSTANTS } from "./constants";
+export { getDefaultAppConstants } from "./constants";
 export type { IAppConstants } from "./constants";
