bf96e4507df77091342a454b3579b7f415e0e8d6
chrismclarke <chris.m.clarke@live.co.uk>
2022-05-31 06:56:07 -0700

    feat: git deployment tagging, ts write updates

--- packages/scripts/src/tasks/providers/git.ts
+++ packages/scripts/src/tasks/providers/git.ts
@@ -1,58 +1,101 @@
-import simpleGit, { SimpleGit } from "simple-git";
+import fs from "fs-extra";
+import path from "path";
+import semver from "semver";
+import simpleGit, { ResetMode } from "simple-git";
+import type { SimpleGit } from "simple-git";
+import { Project, PropertyAssignment, SyntaxKind } from "ts-morph";
 import { getActiveDeployment } from "../../commands/deployment/get";
-import { logWarning, promptInput } from "../../utils";
+import { logError, logOutput, logWarning, promptInput, promptOptions } from "../../utils";
+import { IDeploymentConfigJson } from "../../commands/deployment/set";
 
 class GitProvider {
   private git: SimpleGit;
-  constructor() {
-    // Ensure any calls to git are made from configured subrepo git client
-    const { _workspace_path } = getActiveDeployment();
-    if (_workspace_path) {
-      this.git = simpleGit(_workspace_path);
+  private deployment: IDeploymentConfigJson;
+
+  /**
+   * NOTE - as constructor not used all public api endpoints should call initialiseGitProvider()
+   **/
+  constructor() {}
+
+  public async createContentRelease() {
+    await this.initialiseGitProvider();
+    await this.git.add(".");
+    const status = await this.git.status();
+    console.log(status);
+    const tagName = await this.promptReleaseTag();
+    const branchName = `content/${tagName}`;
+    // sync latest from master
+    // TODO - probably want a hard reset or stash to avoid conflicts
+    await this.git.reset([ResetMode.SOFT]);
+    await this.git.checkoutLocalBranch(branchName);
+    // apply changes
+    await this.updateGitConfigTs({ content_tag_latest: tagName });
+    console.log("committing changes");
+    const changes = await this.git.status();
+    for (const file of changes.files) {
+      console.log();
     }
+    await this.git.add([this.deployment._config_ts_path]);
+    await this.git.commit(`chore: update config`);
+    await this.git.add(["app_data"]);
+    await this.git.commit(`content: ${tagName}`);
+    await this.git.addTag(tagName);
+    console.log("pushing changes");
+    await this.git.push("origin", branchName);
+    logOutput({
+      msg1: "Content uploaded successfully",
+      msg2: `${this.deployment.git.content_repo}/compare/main...${branchName}`,
+    });
+    // open PR
   }
 
-  public async clone(options: { repoPath: string }) {
-    console.log("cloning", this.git);
-    // not required - instead check .git and if not add remote
-    // if (this.git) {
-    //   return logWarning({ msg1: "Git repo already exists, skip clone", msg2: this.subrepoPath });
-    // }
-    // try {
-    //   await simpleGit().clone(options.repoPath, "app-data", { "--depth": 1 });
-    //   logOutput({ msg1: "Repo cloned successfully", msg2: this.subrepoPath });
-    // } catch (error) {
-    //   logError({ msg1: "Could not clone repo", msg2: error.message });
-    // }
+  private async promptReleaseTag() {
+    const { git } = this.deployment;
+    const currentContentTag = git.content_tag_latest || "1.0.0";
+    const releaseTypes: semver.ReleaseType[] = ["patch", "minor", "major"];
+    const options = releaseTypes.map((releaseType) => {
+      const version = semver.inc(currentContentTag, releaseType);
+      return { name: `${releaseType} (${version})`, value: version };
+    });
+    const nextTag = await promptOptions<string>(options, "Specify a version tag");
+    return nextTag;
   }
 
-  public async getRemotes() {
-    return this.git.getRemotes(true);
-  }
-
-  public async stashChanges() {
-    return this.git.stash();
-  }
-
-  public pullFromRemote() {
-    return this.git.pull("origin", "main");
-  }
-
-  public async createLocalPullRequest(suffix: number = 1) {
-    const branchName = `content/${formatTimestamp()}-${suffix}`;
-
-    // TODO - PromptInput - branch name (accept or change)
-    await promptInput("Specify a name for the PR branch", branchName);
-
-    const branchExists = await this.checkBranchExists(branchName);
-    if (branchExists) {
-      logWarning({
-        msg1: "A branch with that name already exists, retry with another branch",
-        msg2: branchName,
-      });
-      return this.createLocalPullRequest(suffix + 1);
+  /**
+   * Update the workflow config.ts git child property directly by parsing and updating ts nodes
+   * https://stackoverflow.com/questions/61213501/read-and-update-object-with-typescript-compiler-api
+   */
+  private async updateGitConfigTs(update: Partial<IDeploymentConfigJson["git"]>) {
+    // Setup ts reader
+    const project = new Project();
+    const { _config_ts_path } = this.deployment;
+    const sourceFile = project.addSourceFileAtPath(_config_ts_path);
+    // extract config.git property
+    const configNode = sourceFile.getVariableDeclarationOrThrow("config");
+    const gitConfigNode = configNode
+      .getInitializerIfKindOrThrow(SyntaxKind.ObjectLiteralExpression)
+      .getPropertyOrThrow("git") as PropertyAssignment;
+    const editableNode = gitConfigNode.getInitializerIfKindOrThrow(
+      SyntaxKind.ObjectLiteralExpression
+    );
+    const indentSize = editableNode.getChildIndentationLevel();
+    // Update individual values
+    // TODO - only supports string literal values
+    for (const [name, value] of Object.entries(update)) {
+      const childNode = editableNode.getProperty(name);
+      if (childNode) {
+        const stringLiteralNode = childNode.getFirstChildByKindOrThrow(SyntaxKind.StringLiteral);
+        stringLiteralNode.replaceWithText((writer) => writer.quote(value));
+      } else {
+        editableNode
+          .addPropertyAssignment({
+            name,
+            initializer: (writer) => writer.quote(value),
+          })
+          .formatText({ indentSize });
+      }
     }
-    this.git.branch([branchName]);
+    await project.save();
   }
 
   private async checkBranchExists(name: string) {
@@ -60,6 +103,45 @@ class GitProvider {
     console.log("branches", branches);
     const remote = await this.git.listRemote(["--heads", "origin", `${name}`]);
     return remote ? true : false;
+  }
+
+  /**
+   * Ensure git configured for workspace and return configured git provider
+   *
+   * NOTE - as async cannot be called in constructor and so should be called first
+   * from any public api methods
+   * */
+  private async initialiseGitProvider() {
+    if (this.git) {
+      return;
+    }
+    this.deployment = getActiveDeployment();
+    const { _workspace_path, git, _config_ts_path } = this.deployment;
+    if (!git?.content_repo) {
+      logError({
+        msg1: "No git content repo configured, specify in config",
+        msg2: _config_ts_path,
+      });
+    }
+    if (!_workspace_path) {
+      logError({
+        msg1: "No active deployment specified",
+        msg2: "yarn scripts deployment set",
+      });
+    }
+    // Ensure git initialised with correct remote
+    this.git = simpleGit(_workspace_path);
+    if (!fs.existsSync(path.join(_workspace_path, ".git"))) {
+      await this.git.init();
+    }
+    const remotes = await this.git.getRemotes(true);
+    if (!remotes.find((remote) => remote.refs.fetch === git.content_repo)) {
+      for (const remote of remotes) {
+        await this.git.removeRemote(remote.name);
+      }
+      await this.git.addRemote("origin", git.content_repo);
+    }
+    await this.git.checkout("origin/main");
   }
 }
 // Export class without initialisation to avoid constructor call until required
