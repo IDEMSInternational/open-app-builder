b9b85b0ab0c9eb4c833d22b27062de876461627b
chrismclarke <chris.m.clarke@live.co.uk>
2022-05-31 11:12:11 -0700

    chore: code tidying

--- packages/data-models/workflows/content.workflows.ts
+++ packages/data-models/workflows/content.workflows.ts
@@ -1,16 +1,13 @@
 import type { IDeploymentWorkflows } from "./workflow.model";
 /** Default workflows made available to all deployments */
 const workflows: IDeploymentWorkflows = {
-  content_repo_release: {
-    label: "Create a new content release",
+  content_release: {
+    label: "Tag a new content version for release",
     steps: [
       {
-        name: "TODO",
+        name: "Create github release",
         function: async ({ tasks }) => {
           await tasks.git().createContentRelease();
-          // await tasks.git.removeChanges();
-          // await tasks.git.pullRemote();
-          // await tasks.file.replicateDir();
           // await tasks.git.createPr('content/2022-05-26')
         },
       },
