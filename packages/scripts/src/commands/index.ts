e3a6f66b223795df50243d243dcb8e4e85e06f80
chrismclarke <chris.m.clarke@live.co.uk>
2022-05-31 11:40:47 -0700

    chore: remove legacy scripts

--- packages/scripts/src/commands/index.ts
+++ packages/scripts/src/commands/index.ts
@@ -17,15 +17,7 @@ program.version("1.0.0").description("IDEMS App Scripts");
 
 // Handle legacy command renames so can still run `yarn scripts gdrive-download`
 const legacyCommandMappings = {
-  "gdrive-download": ["app-data", "download"],
-  "gdrive-auth": ["app-data", "download", "--authorize"],
-  "decrypt-config": ["config", "decrypt"],
-  "encrypt-config": ["config", "encrypt"],
-  "sync-plh-content": ["app-data", "sync"],
-  "sync-single": ["app-data", "sync", "--sheetname"],
-  "app-data-copy": ["app-data", "copy"],
-  "app-data-sync": ["app-data", "sync"],
-  "app-data-convert": ["app-data", "convert"],
+  "legacy-command": ["new-command", "arg"],
 };
 const cmdName = process.argv[2] || "";
 const mapping = legacyCommandMappings[cmdName];
