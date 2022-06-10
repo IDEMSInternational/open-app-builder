331dee68dd308b24594aac08bd85ab744208a26b
chrismclarke <chris.m.clarke@live.co.uk>
2022-06-10 07:41:32 -0700

    chore: wip individual file processing

--- packages/scripts/src/tasks/providers/template.ts
+++ packages/scripts/src/tasks/providers/template.ts
@@ -2,6 +2,7 @@ import { spawnSync } from "child_process";
 import path from "path";
 import { WorkflowRunner } from "../../commands/workflow/run";
 
+/** Process all templates within a folder */
 const process = (options: { inputFolder: string }) => {
   const { _workspace_path } = WorkflowRunner.config;
 
@@ -19,4 +20,24 @@ const process = (options: { inputFolder: string }) => {
   return outputFolder;
 };
 
-export default { process };
+/** WiP - allow processing of just a single template file */
+const processIndividualFile = (options: { inputFile: string }) => {
+  const { _workspace_path } = WorkflowRunner.config;
+
+  const { inputFile } = options;
+  console.log("TODO - process downloaded file");
+
+  // const outputFolder = path.resolve(_workspace_path, "tasks", "template", "outputs");
+  // const cacheFolder = path.resolve(_workspace_path, "tasks", "template", "cache");
+
+  // const processExec = `yarn start app-data convert`;
+
+  // const args = `--input-folder ${inputFolder} --output-folder ${outputFolder} --cache-folder ${cacheFolder}`;
+
+  // const cmd = `${processExec} ${args}`;
+
+  // spawnSync(cmd, { stdio: "inherit", shell: true });
+  // return outputFolder;
+};
+
+export default { process, processIndividualFile };
