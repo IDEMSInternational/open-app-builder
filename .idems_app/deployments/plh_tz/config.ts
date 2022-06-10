fd5a7373de64b333085c3aff3bfbfeca7e147ef2
chrismclarke <chris.m.clarke@live.co.uk>
2022-06-10 13:45:23 -0700

    chore: update child deployment configs

--- .idems_app/deployments/plh_tz/config.ts
+++ .idems_app/deployments/plh_tz/config.ts
@@ -3,6 +3,10 @@ import PARENT_CONFIG from "../plh/config";
 
 const config = extendConfig(PARENT_CONFIG);
 config.name = "plh_tz";
+config.google_drive.sheets_folder_id="10Vj5UJbKRK33_82U7BQ8gj7DQzP7f_NA"
+// TODO - this is currently the same as the parent, needs consideration
+config.google_drive.assets_folder_id="1dp9QAQ84m8pm0IBQTSPXe1ramyynKPNn"
+config.google_drive.assets_filter_function = (entry)=>entry.folderPath.startsWith('za_')
 config.app_data.sheets_filter_function = (flow) =>
   !["debug", "component_demo", "example_hardcoded", "campaign_rows_debug"].includes(
     flow.flow_subtype
