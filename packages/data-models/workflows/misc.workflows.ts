ca755c596c26a03c2db3659a9d9bbc5b5034e4fc
Johnny McQuade <jfmcquade@hotmail.com>
2022-06-15 16:15:29 +0100

    feat: added workflows to populate android assets

--- packages/data-models/workflows/misc.workflows.ts
+++ packages/data-models/workflows/misc.workflows.ts
@@ -27,6 +27,47 @@ const workflows: IDeploymentWorkflows = {
       },
     ],
   },
+  populate_android_assets: {
+    label: "Populate Android assets (app icon and splash screen)",
+    steps: [
+      {
+        name: "Clear",
+        function: async ({ tasks, config }) =>
+          tasks.file.remove({ src: config.workflows.task_cache_path }),
+      },
+      {
+        name: "set_splash_image",
+        function: async ({ tasks, workflow }) =>
+          tasks.workflow.runWorkflow({ name: "set_splash_image", parent: workflow }),
+      },
+      {
+        name: "set_launcher_icon",
+        function: async ({ tasks, workflow }) =>
+          tasks.workflow.runWorkflow({ name: "set_launcher_icon", parent: workflow }),
+      },
+    ],
+  },
+  set_splash_image: {
+    label: "Generate splash screen image from splash.png asset and copy to relevant folders",
+    steps: [
+      {
+        name: "set_splash_image",
+        function: async ({ tasks, config }) =>
+          tasks.android.set_splash_image(config.app_data.assets_output_path, "global/android"),
+      },
+    ],
+  },
+  set_launcher_icon: {
+    label:
+      "Generate app launcher icon from icon.png asset and, if provided, generate adaptive icon from icon-foreground.png and icon-background.png. Copy generated files to relevant folders",
+    steps: [
+      {
+        name: "set_launcher_icon",
+        function: async ({ tasks, config }) =>
+          tasks.android.set_launcher_icon(config.app_data.assets_output_path, "global/android"),
+      },
+    ],
+  },
 };
 
 export default workflows;
--- packages/scripts/src/tasks/providers/android/android.ts
+++ packages/scripts/src/tasks/providers/android/android.ts
@@ -1,22 +1,101 @@
-import { spawnSync } from "child_process";
+import * as path from "path";
+import { Options, run } from 'cordova-res';
+import fs from "fs"
+import { ROOT_DIR } from "../../../paths";
+import { logError } from "../../../utils";
 
 const add_assets = () => null;
-const set_launcher_icon = () => null;
-const set_splash_image = () => null;
 const set_app_meta = () => null;
 
-const generate_splash_image = () => {
-  const cmd = `cordova-res android --type splash --skip-config --copy`;
-  spawnSync(cmd, { stdio: "inherit", shell: true });
+const set_splash_image = async (assetsFolderPath: string, subPath: string) => {
+  const fullAssetsFolderPath = path.resolve(assetsFolderPath)
+  const androidAssetsPath = path.join(fullAssetsFolderPath, subPath)
+
+  const availableAssets = fs.readdirSync(androidAssetsPath)
+
+  if (!availableAssets.includes('splash.png')) {
+    return logError({
+      msg1: "splash.png not found",
+      msg2: `An splash.png file is required to generate a splash screen.`,
+    })
+  }
+
+  const options: Options = {
+    directory: ROOT_DIR,
+    resourcesDirectory: path.join(ROOT_DIR, 'resources'),
+    logstream: process.stdout,
+    platforms: {
+      android: {
+        splash: {
+          sources: [path.join(androidAssetsPath, 'splash.png')]
+        }
+      }
+    },
+    skipConfig: true,
+    copy: true,
+    projectConfig: {
+      android: {
+        directory: path.join(ROOT_DIR, 'android')
+      }
+    }
+  };
+
+  return await run(options)
 };
-const generate_launcher_icon = () => {
-  const cmd = `cordova-res android --type splash --skip-config --copy`;
-  spawnSync(cmd, { stdio: "inherit", shell: true });
+
+const set_launcher_icon = async (assetsFolderPath: string, subPath: string) => {
+  const fullAssetsFolderPath = path.resolve(assetsFolderPath)
+  const androidAssetsPath = path.join(fullAssetsFolderPath, subPath)
+
+  const availableAssets = fs.readdirSync(androidAssetsPath)
+
+  const iconSources = []
+  if (availableAssets.includes('icon.png')) {
+    iconSources.push(
+      path.join(androidAssetsPath, 'icon.png')
+    )
+  } else {
+    return logError({
+      msg1: "icon.png not found",
+      msg2: `An icon.png file is required to be used as a fall back for when the device's android version does not support adaptive icons.`,
+    })
+  }
+
+  let includeAdaptiveIcons = false
+  if (availableAssets.includes('icon-background.png') && availableAssets.includes('icon-foreground.png')) {
+    includeAdaptiveIcons = true
+  }
+
+  const options: Options = {
+    directory: ROOT_DIR,
+    resourcesDirectory: path.join(ROOT_DIR, 'resources'),
+    logstream: process.stdout,
+    platforms: {
+      android: includeAdaptiveIcons
+      ? {
+        "adaptive-icon": {
+          icon: { sources: iconSources },
+          foreground: { sources: [path.join(androidAssetsPath, 'icon-foreground.png')] },
+          background: { sources: [path.join(androidAssetsPath, 'icon-background.png')] }
+        }
+      }
+      : { icon: { sources: iconSources } }
+    },
+    skipConfig: true,
+    copy: true,
+    projectConfig: {
+      android: {
+        directory: path.join(ROOT_DIR, 'android')
+      }
+    }
+  };
+
+  return await run(options)
 };
 
 export default {
   // add_assets,
-  // set_launcher_icon,
-  // set_splash_image,
+  set_launcher_icon,
+  set_splash_image,
   // set_app_meta,
 };
