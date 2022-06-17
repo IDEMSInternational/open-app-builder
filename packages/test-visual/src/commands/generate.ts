84e634a9fd4a59cf796e2c3d068c02d59691d3a1
chrismclarke <chris.m.clarke@live.co.uk>
2022-06-17 12:02:47 -0700

    chore: code tidying

--- packages/test-visual/src/commands/generate.ts
+++ packages/test-visual/src/commands/generate.ts
@@ -135,7 +135,7 @@ export class ScreenshotGenerate {
   private stopServer() {
     if (this.server) {
       return new Promise((resolve, reject) => {
-        this.server?.close((err) => {
+        this.server!.close((err) => {
           if (err) {
             reject(err);
           }
