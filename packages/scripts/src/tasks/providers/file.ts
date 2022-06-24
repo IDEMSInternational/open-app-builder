def681280dfe07fe1a8d2e4795b097f9380053e8
chrismclarke <chris.m.clarke@live.co.uk>
2022-06-24 13:01:28 -0700

    feat: add support for single file conversion

--- packages/scripts/src/tasks/providers/file.ts
+++ packages/scripts/src/tasks/providers/file.ts
@@ -2,9 +2,11 @@ import fs from "fs-extra";
 
 const remove = (options: { src: string }) => fs.removeSync(options.src);
 const filter = (options: { src: string; target?: string }) => null;
-const copy = (options: { src: string; target: string }) => null;
+const copy = (options: { src: string; target: string; copyOptions?: fs.CopyOptionsSync }) =>
+  fs.copySync(options.src, options.target, options.copyOptions || {});
 
 export default {
   remove,
+  copy,
   // filter, copy
 };
