bd4b388adcd34d5cdbd8c2be66fa95578ced9dc7
Johnny McQuade <jfmcquade@hotmail.com>
2022-07-01 16:33:35 +0100

    Updated config structure for android asset paths. More tidying up from PR review

--- packages/data-models/workflows/misc.workflows.ts
+++ packages/data-models/workflows/misc.workflows.ts
@@ -27,51 +27,6 @@ const workflows: IDeploymentWorkflows = {
       },
     ],
   },
-  // Generate Android assets from source images (splash.png, icon.png and, optionally, icon-foreground.png and icon-background.png)
-  // Icon images must be at least 1024×1024px, splash image must be at least 2732×2732px
-  // Further specifications provided here: https://www.npmjs.com/package/cordova-res
-  populate_android_assets: {
-    label: "Populate Android assets (app icon and splash screen)",
-    steps: [
-      {
-        name: "set_splash_image",
-        function: async ({ tasks, workflow }) =>
-          tasks.workflow.runWorkflow({ name: "set_splash_image", parent: workflow }),
-      },
-      {
-        name: "set_launcher_icon",
-        function: async ({ tasks, workflow }) =>
-          tasks.workflow.runWorkflow({ name: "set_launcher_icon", parent: workflow }),
-      },
-    ],
-  },
-  set_splash_image: {
-    label: "Generate splash screen image from splash.png asset and copy to relevant folders",
-    steps: [
-      {
-        name: "set_splash_image",
-        function: async ({ tasks, config }) =>
-          tasks.android.set_splash_image(
-            config.app_data.assets_output_path,
-            config.app_data.android_assets_subpath
-          ),
-      },
-    ],
-  },
-  set_launcher_icon: {
-    label:
-      "Generate app launcher icon from icon.png asset and, if provided, generate adaptive icon from icon-foreground.png and icon-background.png. Copy generated files to relevant folders",
-    steps: [
-      {
-        name: "set_launcher_icon",
-        function: async ({ tasks, config }) =>
-          tasks.android.set_launcher_icon(
-            config.app_data.assets_output_path,
-            config.app_data.android_assets_subpath
-          ),
-      },
-    ],
-  },
 };
 
 export default workflows;
