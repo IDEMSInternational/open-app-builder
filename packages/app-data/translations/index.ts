547e2923914668044e67524e1615f055b161e607
Johnny McQuade <jfmcquade@hotmail.com>
2022-08-02 15:56:27 +0100

    testing #1460

--- packages/app-data/translations/index.ts
+++ packages/app-data/translations/index.ts
@@ -1,11 +1,5 @@
 type ITranslationContents = { [language_code: string]: { filename: string } };
 export const TRANSLATIONS_CONTENT_LIST: ITranslationContents = {
-  es_sp: {
-    filename: "es_sp/strings.json",
-  },
-  tz_sw: {
-    filename: "tz_sw/strings.json",
-  },
   za_af: {
     filename: "za_af/strings.json",
   },
