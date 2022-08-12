0d7a50b3073ea0087fcd7664430383012539b71d
chrismclarke <chris.m.clarke@live.co.uk>
2022-08-11 22:40:49 -0700

    chore: bust parser cache

--- packages/scripts/src/commands/app-data/convert/index.ts
+++ packages/scripts/src/commands/app-data/convert/index.ts
@@ -44,7 +44,7 @@ export default program
 
 class AppDataConverter {
   /** Change version to invalidate any cached conversions */
-  public converterVersion = 20220730.4;
+  public converterVersion = 20220811.1;
 
   private activeDeployment = getActiveDeployment();
   private paths = {
