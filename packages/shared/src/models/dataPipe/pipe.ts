5f56218e618b797b728485f3a3226af737b4cd68
Chris <chris.m.clarke@live.co.uk>
2022-08-18 12:39:35 +0100

    feat: add support for named outputs

--- packages/shared/src/models/dataPipe/pipe.ts
+++ packages/shared/src/models/dataPipe/pipe.ts
@@ -1,4 +1,4 @@
-import { DataFrame } from "danfojs";
+import { DataFrame, toJSON } from "danfojs";
 import { OPERATORS } from "./operators";
 
 export interface IDataPipeOperation {
@@ -9,16 +9,40 @@ export interface IDataPipeOperation {
 }
 
 export class DataPipe {
-  df = new DataFrame([]);
+  private df = new DataFrame([]);
+  public outputTargets: { [key: string]: any } = {};
 
   constructor(private steps: IDataPipeOperation[], private inputSources = {}) {}
 
   run() {
     for (const step of this.steps) {
+      // assign input if specified (default to previous output)
+      if (step.input_source) {
+        this.setInputSource(step.input_source);
+      }
+      // validate operator
       const operator = OPERATORS[step.operation];
       if (!operator) {
         throw new Error(`No pipeline operator exists: ${step.operation}`);
       }
+      // apply operation
+      const instance = new operator(this.df, step.args);
+      console.log(step.operation, instance.args);
+      const output = instance.apply();
+      // Assign output as next input. Populate as named input/output if specified
+      this.df = output;
+      if (step.output_target) {
+        this.setOutputTarget(step.output_target);
+      }
+    }
+  }
+
+  setOutputTarget(target: string) {
+    const [name, localOnly] = target.split(":").map((v) => v.trim());
+    const jsonOutput = toJSON(this.df);
+    this.inputSources[name] = jsonOutput;
+    if (!localOnly) {
+      this.outputTargets[name] = jsonOutput;
     }
   }
 
