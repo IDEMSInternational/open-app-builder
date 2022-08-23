3da32876303fba25572058de54dd28f671c57eb2
Chris <chris.m.clarke@live.co.uk>
2022-08-23 11:13:24 +0100

    feat: add json cache processor and specs

---
+++ packages/scripts/src/commands/app-data/convert/processors/jsonCache.spec.ts
@@ -0,0 +1,71 @@
+import { emptyDirSync, existsSync, readdirSync, readJsonSync, rmdirSync } from "fs-extra";
+import path from "path";
+import { JSONCacheProcessor } from "./jsonCache";
+
+import { SCRIPTS_WORKSPACE_PATH } from "../../../../paths";
+const testCacheDir = path.resolve(SCRIPTS_WORKSPACE_PATH, "test", "data", "cache");
+
+// Clear previous folders
+if (existsSync(testCacheDir)) {
+  emptyDirSync(testCacheDir);
+  rmdirSync(testCacheDir);
+}
+
+const testData = {
+  jsonEntry: {
+    input: { nested: { json: true } },
+    expectedName: "8c7f67b9692991973e0b6a5ff8a8cf07",
+    customName: "jsonEntry",
+  },
+};
+
+const cache = new JSONCacheProcessor(testCacheDir, 1);
+
+describe("Cache", () => {
+  it("Creates cache folder", () => {
+    expect(existsSync(cache.folderPath)).toEqual(true);
+  });
+  it("Populates cache contents", () => {
+    cache.save();
+    const { contentsPath } = cache;
+    expect(existsSync(contentsPath)).toEqual(true);
+  });
+  it("Versions cache contents", () => {
+    const { contentsPath } = cache;
+    expect(readJsonSync(contentsPath)._version).toEqual(cache.version);
+  });
+  it("Generates md5 cache entry name from json", () => {
+    const name = cache.generateCacheEntryName(testData.jsonEntry.input);
+    expect(name).toEqual(testData.jsonEntry.expectedName);
+  });
+  it("Generates md5 cache entry name from string", () => {
+    const name = cache.generateCacheEntryName("hello");
+    expect(name).toEqual("5d41402abc4b2a76b9719d911017c592");
+  });
+  it("Throws error on invalid cache name", () => {
+    expect(() => cache.generateCacheEntryName(undefined)).toThrowError(
+      "Invalid cache entry name: undefined"
+    );
+  });
+  it("Creates unnamed entry", () => {
+    const { input, expectedName } = testData.jsonEntry;
+    cache.add(input);
+    expect(cache.contents[expectedName]).toEqual(input);
+  });
+  it("Creates named entry", () => {
+    const { input, customName } = testData.jsonEntry;
+    cache.add(input, customName);
+    expect(cache.contents[customName]).toEqual(input);
+  });
+  it("Writes cache files to disk", () => {
+    cache.save();
+    const cachedFile = path.resolve(cache.folderPath, testData.jsonEntry.expectedName);
+    expect(existsSync(cachedFile)).toEqual(true);
+  });
+
+  it("Invalidates cache on version update", () => {
+    expect(readdirSync(cache.folderPath).length).toBeGreaterThan(0);
+    const newCache = new JSONCacheProcessor(testCacheDir, 2);
+    expect(readdirSync(cache.folderPath).length).toEqual(0);
+  });
+});
---
+++ packages/scripts/src/commands/app-data/convert/processors/jsonCache.ts
@@ -0,0 +1,101 @@
+import { createHash } from "crypto";
+import { emptyDirSync, existsSync, mkdirSync, readJSONSync, writeJSONSync } from "fs-extra";
+import path from "path";
+import { generateFolderFlatMap } from "../utils";
+
+/**
+ * The cache processor creates a file cache that can store json outputs from
+ * previous processes
+ *
+ * A main contents list keeps track of all cached inputs and their corresponding outputs,
+ * named via an md5 hash of the input contents (with optional override)
+ *
+ * A versioning system handles clearing cache as required
+ */
+export class JSONCacheProcessor {
+  public folderPath: string;
+  public contentsPath: string;
+  public contents: {};
+  public version: number;
+
+  constructor(folderPath: string, version: number) {
+    this.version = version;
+    this.setup(folderPath);
+  }
+
+  add(data: any, entryName?: string) {
+    if (!entryName) {
+      entryName = this.generateCacheEntryName(data);
+    }
+    this.contents[entryName] = data;
+  }
+  get(entryName: string) {
+    return this.contents[entryName];
+  }
+
+  /**
+   * Write cache contents to file
+   * TODO - provide incremental write instead of clearing everything
+   */
+  public save() {
+    // Write files
+    emptyDirSync(this.folderPath);
+    for (const [name, value] of Object.entries<any>(this.contents)) {
+      if (value && typeof value === "object") {
+        const outputPath = path.resolve(this.folderPath, name);
+        writeJSONSync(outputPath, value);
+      }
+    }
+
+    // Write contents
+    const { contentsPath, folderPath } = this;
+    const contents = generateFolderFlatMap(folderPath, true, (p) => p !== "_contents.json");
+    contents._version = this.version as any;
+    this.contents = contents as any;
+    writeJSONSync(contentsPath, contents);
+  }
+
+  /**  */
+  public generateCacheEntryName(data: any) {
+    if (data) {
+      if (typeof data === "object") {
+        data = JSON.stringify(data);
+      } else {
+        data = `${data}`;
+      }
+      const hash = createHash("md5");
+      hash.update(data);
+      const checksum = hash.digest("hex");
+      return checksum;
+    } else {
+      throw new Error(`Invalid cache entry name: ${data}`);
+    }
+  }
+
+  private setup(folderPath: string) {
+    this.folderPath = folderPath;
+    this.contentsPath = path.resolve(folderPath, "_contents.json");
+    this.contentsPath = path.resolve(this.folderPath, "_contents.json");
+    if (!existsSync(this.folderPath)) {
+      mkdirSync(this.folderPath);
+    }
+    this.load();
+  }
+
+  /**
+   * Load cache from contents file
+   * Invalidate and clear if cache contents version does not match current cache version
+   */
+  private load() {
+    this.contents = {};
+    if (existsSync(this.contentsPath)) {
+      const cacheContents = readJSONSync(this.contentsPath);
+      const { _version } = cacheContents;
+      if (_version === this.version) {
+        this.contents = cacheContents;
+      } else {
+        emptyDirSync(this.folderPath);
+      }
+    }
+  }
+}
