373d1806523f65b1b6313086ca7c0435b9e32992
Chris <chris.m.clarke@live.co.uk>
2022-08-28 20:38:01 +0100

    feat: add data pipe processed extraction to flowparser

--- packages/scripts/src/commands/app-data/convert/processors/flowParser/flowParser.ts
+++ packages/scripts/src/commands/app-data/convert/processors/flowParser/flowParser.ts
@@ -15,7 +15,10 @@ export class FlowParserProcessor extends BaseProcessor<FlowTypes.FlowTypeWithDat
     data_pipe: new Parsers.DataPipeParser(this),
   };
 
-  public processedFlows: IParsedWorkbookData = {};
+  /** Keep a track of all processed flows by type and name (used in data_pipes)*/
+  public processedFlowHashmap: {
+    [flowType in FlowTypes.FlowType]?: { [flow_name: string]: FlowTypes.FlowTypeWithData["rows"] };
+  } = {};
 
   constructor(paths: IConverterPaths) {
     super({ paths, namespace: "flowParser" });
@@ -31,6 +34,7 @@ export class FlowParserProcessor extends BaseProcessor<FlowTypes.FlowTypeWithDat
     }
     try {
       const parsed = parser.run(flow);
+      this.updateProcessedFlowHashmap(parsed);
       return parsed;
     } catch (error) {
       this.logger.error({ message: "Template parse error", details: flow });
@@ -38,23 +42,66 @@ export class FlowParserProcessor extends BaseProcessor<FlowTypes.FlowTypeWithDat
     }
   }
 
+  public updateProcessedFlowHashmap(flow: FlowTypes.FlowTypeWithData) {
+    const { flow_name, flow_type } = flow;
+    if (!this.processedFlowHashmap[flow_type]) {
+      this.processedFlowHashmap[flow_type] = {};
+    }
+    // Key should be unique as duplicates checked in main convert method
+    this.processedFlowHashmap[flow_type][flow_name] = flow.rows;
+  }
+
   /**
    * Apply combined parser postProcess methods
    * @returns hashmap of flowtypes with postprocessed flows
    */
   public async postProcess(flows: FlowTypes.FlowTypeWithData[]): Promise<IParsedWorkbookData> {
-    const flowsByType = groupJsonByKey(flows, "flow_type");
-    return flowsByType;
+    const flowsByType: IParsedWorkbookData = groupJsonByKey(flows, "flow_type");
+    const flowTypesWithGenerated: IParsedWorkbookData =
+      this.populateDataPipeGeneratedFlows(flowsByType);
+    return flowTypesWithGenerated;
   }
 
   /**
-   * TODO - ignore cache if data_pipe
-   * TODO - refine to bypass cache iff datalists no in cache
+   * Data Pipes can return multiple generated flows from a single pipeline
+   * Extract these generated flows and populate as datalists
    */
+  private populateDataPipeGeneratedFlows(flowsByType: IParsedWorkbookData) {
+    if (flowsByType.data_pipe) {
+      flowsByType.data_list ?? {};
+      for (const flow of flowsByType.data_pipe) {
+        const dataPipeFlow = flow as FlowTypes.DataPipeFlow;
+        if (dataPipeFlow._processed) {
+          for (const [flow_name, rows] of Object.entries(dataPipeFlow._processed)) {
+            const datalist: FlowTypes.Data_list = {
+              flow_name,
+              flow_subtype: "generated-" + flow.flow_name,
+              flow_type: "data_list",
+              rows,
+            };
+            if (flowsByType.data_list[flow_name]) {
+              this.logger.error({
+                message: "Generated datalist will override existing datalist",
+                details: [flow_name],
+              });
+            }
+            flowsByType.data_list[flow_name] = datalist;
+          }
+        }
+      }
+    }
+    return flowsByType;
+  }
+
   public shouldUseCachedEntry(
     input: FlowTypes.FlowTypeWithData,
     cachedEntry: IContentsEntry
   ): Boolean {
+    // Ignore cache if data_pipe (in case dependencies have changed)
+    // TODO refine to bypass cache iff datalists not in cache
+    if (input.flow_type === "data_pipe") {
+      return false;
+    }
     return true;
   }
 }
