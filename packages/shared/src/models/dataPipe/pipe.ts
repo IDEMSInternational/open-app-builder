38563ecc2a4c6bea519db2a5d5a499324aae798a
Chris <chris.m.clarke@live.co.uk>
2022-08-28 20:29:18 +0100

    test: add data pipe missing input tests

--- packages/shared/src/models/dataPipe/pipe.ts
+++ packages/shared/src/models/dataPipe/pipe.ts
@@ -18,6 +18,10 @@ export class DataPipe {
   }
 
   run() {
+    const missingInputs = this.checkMissingInputs();
+    if (missingInputs) {
+      throw new Error("Input sources missing for data pipe: " + missingInputs);
+    }
     for (const step of this.steps) {
       // assign input if specified (default to previous output)
       if (step.input_source) {
@@ -30,7 +34,6 @@ export class DataPipe {
       }
       // apply operation
       const instance = new operator(this.df, step.args_list, this);
-      console.log(step.operation, instance.args_list);
       const output = instance.apply();
       // Assign output as next input. Populate as named input/output if specified
       this.df = output;
@@ -38,6 +41,7 @@ export class DataPipe {
         this.setOutputTarget(step.output_target);
       }
     }
+    return this.outputTargets;
   }
 
   setOutputTarget(target: string) {
@@ -54,5 +58,28 @@ export class DataPipe {
       throw new Error(`Data source not found: ${name}`);
     }
     this.df = new DataFrame(this.inputSources[name]);
+  }
+
+  /** Extract a list of all required input sources and check to see if they already exist or will be generated */
+  public checkMissingInputs() {
+    const availableInputs = {};
+    const missingInputs = {};
+    // list existing inputs
+    for (const inputSource of Object.keys(this.inputSources)) {
+      availableInputs[inputSource] = true;
+    }
+    // compare required inputs with available inputs (including generated outputs)
+    for (const step of this.steps) {
+      const { input_source, output_target } = step;
+      if (input_source && !availableInputs[input_source]) {
+        missingInputs[input_source] = true;
+      }
+      if (output_target) {
+        const [outputName] = step.output_target.split(":").map((v) => v.trim());
+        availableInputs[outputName] = true;
+      }
+    }
+    const missingList = Object.keys(missingInputs);
+    return missingList.length === 0 ? null : missingList;
   }
 }
