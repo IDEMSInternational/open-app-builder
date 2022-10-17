1da21d8b3df54a96935ab07132eed850f206320f
Johnny McQuade <jfmcquade@hotmail.com>
2022-10-17 13:22:08 +0100

    updated modular skin and added clarifying comments

--- .idems_app/deployments/plh/global.config.ts
+++ .idems_app/deployments/plh/global.config.ts
@@ -1,6 +1,6 @@
 import { IDeploymentConfig } from "data-models";
 import { getDefaultAppConfig } from "data-models/constants";
-import { weekly_workshop, modular } from "./skins";
+import * as SKINS from "./skins";
 
 const app_config = getDefaultAppConfig();
 
@@ -34,7 +34,7 @@ config.app_config.APP_SIDEMENU_DEFAULTS.title = "ParentApp";
 config.app_config.APP_HEADER_DEFAULTS.title = "ParentApp";
 config.app_config.NOTIFICATION_DEFAULTS.title = "New message from PLH";
 config.app_config.NOTIFICATION_DEFAULTS.text = "You have a new message from ParentApp";
-config.app_config.APP_SKINS.default = weekly_workshop
-config.app_config.APP_SKINS.available = [modular]
+config.app_config.APP_SKINS.default = SKINS.weekly_workshop
+config.app_config.APP_SKINS.available = [SKINS.modular]
 
 export default config;
--- packages/data-models/skin.model.ts
+++ packages/data-models/skin.model.ts
@@ -1,5 +1,7 @@
 import { IAppConfigOverride } from ".";
 
+/** Skins can be applied to an app to override values within the core APP_CONFIG.
+ * They can also be referenced by name at the template level to apply skin-specific overrides. */
 export interface IAppSkin {
   name: string;
   appConfig?: IAppConfigOverride;
