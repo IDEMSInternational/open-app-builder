484a8b58e874a30deab27e10884b5fd66d6b8649
Johnny McQuade <jfmcquade@hotmail.com>
2022-10-20 12:34:58 +0100

    debug logs for functional testing

--- .idems_app/deployments/plh/skins/index.ts
+++ .idems_app/deployments/plh/skins/index.ts
@@ -3,7 +3,12 @@ import { IAppSkin } from "data-models";
 // PLH-specific skins
 
 export const weekly_workshop: IAppSkin = {
-  name: "weekly_workshop"
+  name: "weekly_workshop",
+  appConfig: {
+    APP_HEADER_DEFAULTS: {
+      title: "test"
+    }
+  }
 }
 
 export const modular: IAppSkin = {
