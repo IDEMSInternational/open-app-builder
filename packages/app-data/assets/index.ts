08b74f3118f9f003ed77e7e0c178ea6d9279f0c4
EsmeeIDEMS <esmee@idems.international>
2022-11-01 22:47:57 +0000

    plh_tz content sync

--- packages/app-data/assets/index.ts
+++ packages/app-data/assets/index.ts
@@ -1,7 +1,4 @@
-export const UNTRACKED_ASSETS = [
-  "es_sp/quality_assurance/test_image_untracked.png",
-  "theme_professional/plh_images/workshops/w_instruct/read_2/slide_1_a.svg",
-];
+export const UNTRACKED_ASSETS = [];
 export const ASSETS_CONTENTS_LIST = {
   "android/icon-background.png": {
     size_kb: 6.4,
@@ -702,12 +699,6 @@ export const ASSETS_CONTENTS_LIST = {
   "plh_images/family.svg": {
     size_kb: 864.1,
     md5Checksum: "4a88fa5e3141147eaac754f65d17342d",
-    themeVariations: {
-      theme_professional: {
-        size_kb: 45.3,
-        md5Checksum: "6b480cdb1b435f40059c705a3a9e5b38",
-      },
-    },
   },
   "plh_images/first_app_launch/frustrated.svg": {
     size_kb: 11.9,
