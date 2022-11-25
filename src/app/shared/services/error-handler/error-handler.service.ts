1db0ee532b60084b3bbcd4f71e11eb04c9433a6a
chrismclarke <chris.m.clarke@live.co.uk>
2022-11-24 22:48:22 -0800

    feat: add support for sentry error logging

--- src/app/shared/services/error-handler/error-handler.service.ts
+++ src/app/shared/services/error-handler/error-handler.service.ts
@@ -1,5 +1,7 @@
 import { ErrorHandler, Injectable, Injector } from "@angular/core";
-import { Capacitor } from "@capacitor/core";
+import { Device } from "@capacitor/device";
+import * as Sentry from "@sentry/angular";
+import { environment } from "src/environments/environment";
 import { fromError as getStacktraceFromError } from "stacktrace-js";
 import { CrashlyticsService } from "../crashlytics/crashlytics.service";
 
@@ -7,23 +9,60 @@ import { CrashlyticsService } from "../crashlytics/crashlytics.service";
   providedIn: "root",
 })
 export class ErrorHandlerService extends ErrorHandler {
+  private initialised = false;
+
+  private sentryEnabled = false;
+
   // Error handling is important and needs to be loaded first.
   // Because of this we should manually inject the services with Injector.
   constructor(private injector: Injector) {
     super();
   }
 
-  /**
-   * Custom error handling. On web this uses the default error handling
-   * (console logs and modal in dev mode, ignored in production), on android
-   * this logs to firebase crashlytics
-   */
-  handleError(error: Error) {
-    if (Capacitor.isNativePlatform()) {
-      return this.logToCrashlytics(error);
-    } else {
-      super.handleError(error);
+  private async initialise() {
+    const { production, deploymentConfig } = environment;
+    if (production && deploymentConfig?.error_logging?.dsn) {
+      await this.initialiseSentry();
     }
+  }
+
+  /**
+   * Custom error handling. Will always call default handler, as well
+   * as any 3rd party integrations
+   */
+  async handleError(error: Error) {
+    if (!this.initialised) {
+      await this.initialise();
+    }
+    if (this.sentryEnabled) {
+      Sentry.captureException(error);
+    }
+    super.handleError(error);
+  }
+
+  /**
+   * Initialise sentry to record remote logs. Note, the same code can be used
+   * to initialise either sentry or glitchtip endpoints.
+   * By default logs will be associated with both deployment name and app version
+   *
+   * TODO - can also be integrated with Capacitor (and crashlytics possibly removed)
+   * https://docs.sentry.io/platforms/javascript/guides/capacitor/
+   */
+  private async initialiseSentry() {
+    const { deploymentConfig, version, production } = environment;
+    const { error_logging, name } = deploymentConfig;
+    Sentry.init({
+      dsn: error_logging.dsn,
+      environment: production ? "production" : "development",
+      release: `${name}-${version}`,
+      autoSessionTracking: false,
+      attachStacktrace: true,
+      enabled: true,
+      tracesSampleRate: 0, // ensure performance traces not sent (not installed)
+    });
+    const { uuid } = await Device.getId();
+    Sentry.setUser({ id: uuid });
+    this.sentryEnabled = true;
   }
 
   private async logToCrashlytics(error: Error) {
