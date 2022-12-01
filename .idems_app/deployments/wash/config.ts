ef4b7341fe29b3c853b34e658f549a718c4fab83
Johnny McQuade <jfmcquade@hotmail.com>
2022-12-01 10:37:16 +0000

    updated wash deployment config

--- .idems_app/deployments/wash/config.ts
+++ .idems_app/deployments/wash/config.ts
@@ -1,20 +1,20 @@
 import { IDeploymentConfig } from "data-models";
-import { getDefaultAppConstants } from "data-models/constants";
+import { getDefaultAppConfig } from "data-models/appConfig";
 
-const app_constants = getDefaultAppConstants();
+const app_config = getDefaultAppConfig();
 
 const config: IDeploymentConfig = {
   name: "WASH App",
-  app_constants,
+  app_config,
   google_drive: {
     sheets_folder_id: "1AQqtRLoNuhbHP3KPzzQqzZcjdttT0rr8",
     assets_folder_id: "1hebyXSR-rkpYLH_No2QTvCQiYfbh1RE9",
   },
 };
 
-config.app_constants.APP_SIDEMENU_DEFAULTS.title = "WASH App";
-config.app_constants.APP_HEADER_DEFAULTS.title = "WASH App";
-config.app_constants.NOTIFICATION_DEFAULTS.title = "New message from WASH App";
-config.app_constants.NOTIFICATION_DEFAULTS.text = "You have a new message from WASH App";
+config.app_config!.APP_SIDEMENU_DEFAULTS!.title = "WASH App";
+config.app_config!.APP_HEADER_DEFAULTS!.title = "WASH App";
+config.app_config!.NOTIFICATION_DEFAULTS!.title = "New message from WASH App";
+config.app_config!.NOTIFICATION_DEFAULTS!.text = "You have a new message from WASH App";
 
 export default config;
