628d4020fc721f2ccfae48e3c62702809e551300
EsmeeIDEMS <esmee@idems.international>
2022-12-01 11:56:44 +0000

    Content sync 2022-12-01 (2)

--- packages/app-data/assets/index.ts
+++ packages/app-data/assets/index.ts
@@ -12,4 +12,16 @@ export const ASSETS_CONTENTS_LIST = {
     size_kb: 46.2,
     md5Checksum: "65664bfb46d5c1448bbaf3adc6c1c6a6",
   },
+  "icons/in_progress_white.svg": {
+    size_kb: 1.1,
+    md5Checksum: "f7bdd991a067c30d0cb140d5b77745bb",
+  },
+  "icons/tick_white.svg": {
+    size_kb: 0.3,
+    md5Checksum: "81136e04436759812dc3f6e90c61de11",
+  },
+  "modules/clean_water_1.png": {
+    size_kb: 23.4,
+    md5Checksum: "b534e8f65359e14333e1fe5cda545f27",
+  },
 };
