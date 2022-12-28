3a209b6d3dcea33d7f1938995c2899feca32a7b9
chrismclarke <chris.m.clarke@live.co.uk>
2022-12-27 20:41:04 -0800

    refactor: persisted and reactive adapters

--- src/app/shared/services/dynamic-data/adapters/persistedMemory.ts
+++ src/app/shared/services/dynamic-data/adapters/persistedMemory.ts
@@ -1,4 +1,11 @@
-import { addRxPlugin, createRxDatabase, RxCollection, RxDatabase, RxJsonSchema } from "rxdb";
+import {
+  addRxPlugin,
+  createRxDatabase,
+  RxCollection,
+  RxDatabase,
+  RxDocument,
+  RxJsonSchema,
+} from "rxdb";
 import { getRxStorageDexie } from "rxdb/plugins/dexie";
 import { RxDBJsonDumpPlugin } from "rxdb/plugins/json-dump";
 addRxPlugin(RxDBJsonDumpPlugin);
@@ -9,7 +16,7 @@ addRxPlugin(RxDBUpdatePlugin);
 
 import { FlowTypes } from "packages/data-models";
 import { environment } from "src/environments/environment";
-import { deepMergeObjects } from "../../../utils";
+import { deepMergeObjects, generateTimestamp } from "../../../utils";
 
 const SCHEMA_BASE: RxJsonSchema<any> = {
   title: "base schema for id-primary key data",
@@ -21,13 +28,14 @@ const SCHEMA_BASE: RxJsonSchema<any> = {
   properties: {
     id: {
       type: "string",
-      maxLength: 128, // <- the primary key must have set maxLength
+      maxLength: 196, // <- the primary key must have set maxLength
     },
     flow_name: { type: "string", maxLength: 64 },
     flow_type: { type: "string", maxLength: 64 },
+    row_id: { type: "string", maxLength: 64 },
   },
-  required: ["id", "flow_name", "flow_type"],
-  indexes: ["flow_name", "flow_type"],
+  required: ["id", "flow_type", "flow_name", "row_id"],
+  indexes: ["flow_type", "flow_name", "row_id"],
 };
 
 interface IDataUpdate {
@@ -37,13 +45,17 @@ interface IDataUpdate {
   data?: Record<string, any>;
 }
 
-export class DynamicDataPersistedDB {
+export class PersistedMemoryAdapter {
   private db: RxDatabase<{
     [key: string]: RxCollection<any, {}, {}, {}>;
   }>;
 
   private state: {
-    [flow_type in FlowTypes.FlowType]?: { [flow_name: string]: { [row_id: string]: any } };
+    [flow_type in FlowTypes.FlowType]?: {
+      [flow_name: string]: {
+        [row_id: string]: { [key: string]: any };
+      };
+    };
   } = {};
 
   public async create() {
@@ -51,19 +63,23 @@ export class DynamicDataPersistedDB {
       name: `${environment.deploymentName}_user`,
       storage: getRxStorageDexie({ autoOpen: true }),
     });
-    await this.populateInitialState();
-
+    if (!this.collection) {
+      await this.db.addCollections({ user: { schema: SCHEMA_BASE } });
+    }
+    await this.mapDBToState();
     return this;
   }
-  private async populateInitialState() {
-    const initialWriteState = await this.db.exportJSON();
-    console.log({ initialWriteState });
+  private get collection() {
+    return this.db.collections["user"];
   }
 
   public get(flow_type: FlowTypes.FlowType, flow_name: string) {
     return this.state[flow_type]?.[flow_name];
   }
 
+  /**
+   * Provide a partial data update. Will be merged with existing data
+   */
   public update(update: IDataUpdate) {
     const { flow_name, flow_type, id, data } = update;
     if (!this.state[flow_type]) this.state[flow_type] = {};
@@ -72,28 +88,36 @@ export class DynamicDataPersistedDB {
     const merged = deepMergeObjects(existingData, data);
     this.state[flow_type][flow_name][id] = merged;
     // TODO - debounce write to disk
-    this.persistState();
+    this.persistStateToDB();
   }
 
   /**
    *
-   * TODO - could perform more incremental updates
+   * TODO - could perform more incremental updates and avoid overwrite with same data
    */
-  private async persistState() {
+  private async persistStateToDB() {
+    console.log("persisting state to db", this.state);
+
+    const updates: { id: string; row_id: string; flow_type: string; flow_name: string }[] = [];
     for (const [flow_type, flowHash] of Object.entries(this.state)) {
-      const collectionName = flow_type;
-      if (!this.db[collectionName]) {
-        console.log("adding collection", flow_type);
-        await this.db.addCollections({ [flow_type]: { schema: SCHEMA_BASE } });
-      }
-      const updates: { id: string; flow_name: string }[] = [];
       for (const [flow_name, rowHash] of Object.entries(flowHash)) {
-        for (const [id, update] of Object.entries(rowHash)) {
-          updates.push({ ...update, id, flow_name });
+        for (const [row_id, update] of Object.entries(rowHash)) {
+          const id = `${flow_type}__${flow_name}__${row_id}`;
+          updates.push({ ...update, id, row_id, flow_type, flow_name });
         }
       }
-      console.log({ flow_type, updates });
-      await this.db[collectionName].bulkUpsert(updates);
     }
+    await this.collection.bulkUpsert(updates);
+  }
+  private async mapDBToState() {
+    console.log("mapping state to db");
+    const { docs } = await this.collection.exportJSON();
+    for (const doc of docs) {
+      const { flow_type, flow_name, row_id, value } = doc;
+      if (!this.state[flow_type]) this.state[flow_type] = {};
+      if (!this.state[flow_type][flow_name]) this.state[flow_type][flow_name] = {};
+      this.state[flow_type][flow_name][row_id] = value;
+    }
+    console.log("state mapped", JSON.parse(JSON.stringify(this.state)));
   }
 }
