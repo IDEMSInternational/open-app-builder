dd1b6e99f7df01e85aeeb20834570b8729101238
Johnny McQuade <jfmcquade@hotmail.com>
2023-01-17 10:42:30 +0000

    simplified config file

--- .idems_app/deployments/perinatal/config.ts
+++ .idems_app/deployments/perinatal/config.ts
@@ -7,16 +7,12 @@ const config: IDeploymentConfig = {
     sheets_folder_id: "",
     assets_folder_id: "",
   },
-  app_data: {},
-  translations: {
-    translated_strings_path: "packages/app-data/translations_source/from_translators",
-    source_strings_path: "packages/app-data/translations_source/to_translate",
-  },
   app_config: getDefaultAppConfig(),
 };
 // Override constants
 config.app_config!.APP_HEADER_DEFAULTS!.title = "Perinatal";
 config.app_config!.APP_SIDEMENU_DEFAULTS!.title = "Perinatal";
+config.app_config!.NOTIFICATION_DEFAULTS!.title = "New message from Perinatal";
 config.app_config!.NOTIFICATION_DEFAULTS!.text = "You have a new message from Perinatal";
 
 export default config;
