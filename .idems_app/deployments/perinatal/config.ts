2f6d2c4e2785a504e3d6fa0cfb91e851a98ba2e5
EsmeeIDEMS <esmee@idems.international>
2023-01-17 15:27:37 +0000

    Update config and content sync

--- .idems_app/deployments/perinatal/config.ts
+++ .idems_app/deployments/perinatal/config.ts
@@ -4,15 +4,15 @@ import { getDefaultAppConfig } from "data-models";
 const config: IDeploymentConfig = {
   name: "perinatal",
   google_drive: {
-    sheets_folder_id: "",
-    assets_folder_id: "",
+    sheets_folder_id: "1LyMSXxi0FN5iAZQL5ySLBW56jmDqVabU",
+    assets_folder_id: "1sqW2TdYXALIlvBnWlU-XT90BpllkLrGs",
   },
   app_config: getDefaultAppConfig(),
 };
 // Override constants
-config.app_config!.APP_HEADER_DEFAULTS!.title = "Perinatal";
-config.app_config!.APP_SIDEMENU_DEFAULTS!.title = "Perinatal";
-config.app_config!.NOTIFICATION_DEFAULTS!.title = "New message from Perinatal";
-config.app_config!.NOTIFICATION_DEFAULTS!.text = "You have a new message from Perinatal";
+config.app_config!.APP_HEADER_DEFAULTS!.title = "Perinatal App";
+config.app_config!.APP_SIDEMENU_DEFAULTS!.title = "Perinatal App";
+config.app_config!.NOTIFICATION_DEFAULTS!.title = "New message from Perinatal App";
+config.app_config!.NOTIFICATION_DEFAULTS!.text = "You have a new message from Perinatal App";
 
 export default config;
