d3e4960cb467ebeb93a019d9b3b1b9f7946ebed4
chrismclarke <chris.m.clarke@live.co.uk>
2023-01-19 09:31:25 -0800

    chore: add error dsns

--- .idems_app/deployments/plh/debug.config.ts
+++ .idems_app/deployments/plh/debug.config.ts
@@ -12,4 +12,6 @@ config.app_config!.APP_SKINS!.defaultSkinName = SKINS.debug.name;
 // Limit available skins to only include debug skin, to force this skin to be applied on init
 config.app_config!.APP_SKINS!.available = [SKINS.debug];
 
+config.error_logging!.dsn='https://69ccfba168f04b848cdd96ac066d01b5@app.glitchtip.com/2438'
+
 export default config;
--- .idems_app/deployments/plh/tz.config.ts
+++ .idems_app/deployments/plh/tz.config.ts
@@ -15,6 +15,6 @@ config.translations!.filter_language_codes = ["tz_en", "tz_sw"];
 config.app_config!.APP_LANGUAGES!.default = "tz_sw";
 config.app_config!.APP_SIDEMENU_DEFAULTS!.title = "ParentApp (TZ)";
 config.app_config!.APP_AUTHENTICATION_DEFAULTS!.enforceLogin = true;
-config.error_logging!.dsn=''
+config.error_logging!.dsn='https://1bfed3b77f4e4a0f9b55b510915c7f1c@app.glitchtip.com/2233'
 
 export default config;
