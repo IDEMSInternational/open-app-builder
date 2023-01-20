17f7c3342ed8034bb11582b1ed67b59f9b867a64
chrismclarke <chris.m.clarke@live.co.uk>
2023-01-20 11:34:08 -0800

    chore: update config

--- .idems_app/deployments/perinatal/config.ts
+++ .idems_app/deployments/perinatal/config.ts
@@ -8,12 +8,14 @@ const config: IDeploymentConfig = {
     assets_folder_id: "1sqW2TdYXALIlvBnWlU-XT90BpllkLrGs",
   },
   app_config: getDefaultAppConfig(),
+  error_logging:{
+    dsn:"https://d2a84a6e88b24a7199736d4d153c0a7a@app.glitchtip.com/2443"
+  }
 };
 // Override constants
 config.app_config!.APP_HEADER_DEFAULTS!.title = "Perinatal App";
 config.app_config!.APP_SIDEMENU_DEFAULTS!.title = "Perinatal App";
 config.app_config!.NOTIFICATION_DEFAULTS!.title = "New message from Perinatal App";
 config.app_config!.NOTIFICATION_DEFAULTS!.text = "You have a new message from Perinatal App";
-config.error_logging!.dsn="https://d2a84a6e88b24a7199736d4d153c0a7a@app.glitchtip.com/2443"
 
 export default config;
