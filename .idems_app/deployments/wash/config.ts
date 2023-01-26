310a4dd0aa9e7379c59ad043ca7dacf38d68b7c1
Johnny McQuade <jfmcquade@hotmail.com>
2023-01-26 11:32:18 +0000

    chore: added wach deployment config and firebase hosting setup

---
+++ .idems_app/deployments/wash/config.ts
@@ -0,0 +1,23 @@
+import { IDeploymentConfig } from "data-models";
+import { getDefaultAppConfig } from "data-models/appConfig";
+
+const app_config = getDefaultAppConfig();
+
+const config: IDeploymentConfig = {
+  name: "wash",
+  app_config,
+  google_drive: {
+    sheets_folder_id: "1AQqtRLoNuhbHP3KPzzQqzZcjdttT0rr8",
+    assets_folder_id: "1hebyXSR-rkpYLH_No2QTvCQiYfbh1RE9",
+  },
+  error_logging:{
+    dsn:"https://976f6a201e1049fdb65d3f642724f0f9@app.glitchtip.com/2444"
+  }
+};
+
+config.app_config!.APP_SIDEMENU_DEFAULTS!.title = "WASH App";
+config.app_config!.APP_HEADER_DEFAULTS!.title = "WASH App";
+config.app_config!.NOTIFICATION_DEFAULTS!.title = "New message from WASH App";
+config.app_config!.NOTIFICATION_DEFAULTS!.text = "You have a new message from WASH App";
+
+export default config;
