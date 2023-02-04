1a830e95e3207ce6567f3f28b77cc15b1fe70c2c
chrismclarke <chris.m.clarke@live.co.uk>
2023-02-03 23:12:44 -0800

    refactor: sheets sync

--- packages/app-data/index.ts
+++ packages/app-data/index.ts
@@ -1,5 +1,5 @@
 import ASSETS_CONTENTS_LIST from './assets/contents.json'
-export { SHEETS_CONTENT_LIST } from "./sheets";
-export { TRANSLATIONS_CONTENT_LIST } from "./translations";
+import  SHEETS_CONTENT_LIST  from "./sheets/contents.json";
+import TRANSLATIONS_CONTENT_LIST from "./translations/contents.json";
 
-export { ASSETS_CONTENTS_LIST } 
+export { ASSETS_CONTENTS_LIST,SHEETS_CONTENT_LIST, TRANSLATIONS_CONTENT_LIST } 
