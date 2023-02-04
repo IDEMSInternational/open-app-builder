24ba57602b319cbe1b316eed4b9e703a9d4855bd
chrismclarke <chris.m.clarke@live.co.uk>
2023-02-03 23:46:59 -0800

    chore: code tidying

--- packages/app-data/index.ts
+++
@@ -1,5 +0,0 @@
-import ASSETS_CONTENTS_LIST from './assets/contents.json'
-import  SHEETS_CONTENT_LIST  from "./sheets/contents.json";
-import TRANSLATIONS_CONTENT_LIST from "./translations/contents.json";
-
-export { ASSETS_CONTENTS_LIST,SHEETS_CONTENT_LIST, TRANSLATIONS_CONTENT_LIST } 
