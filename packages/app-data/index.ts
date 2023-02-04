a0fa062b1a2385c7f9b12aabef5749ddfc159862
chrismclarke <chris.m.clarke@live.co.uk>
2023-02-03 22:50:08 -0800

    refactor: asset contents

--- packages/app-data/index.ts
+++ packages/app-data/index.ts
@@ -1,3 +1,5 @@
-export { ASSETS_CONTENTS_LIST } from "./assets";
+import ASSETS_CONTENTS_LIST from './assets/contents.json'
 export { SHEETS_CONTENT_LIST } from "./sheets";
 export { TRANSLATIONS_CONTENT_LIST } from "./translations";
+
+export { ASSETS_CONTENTS_LIST } 
