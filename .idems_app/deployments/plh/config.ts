7d9816965fc03c1e74cd98c0c45a531630f2e082
EsmeeIDEMS <esmee@idems.international>
2023-02-23 14:54:51 +0000

    plh config: update default skin

--- .idems_app/deployments/plh/config.ts
+++ .idems_app/deployments/plh/config.ts
@@ -30,7 +30,7 @@ config.app_config.APP_SIDEMENU_DEFAULTS.title = "ParentApp";
 config.app_config.APP_HEADER_DEFAULTS.title = "ParentApp";
 config.app_config.NOTIFICATION_DEFAULTS.title = "New message from ParentApp";
 config.app_config.NOTIFICATION_DEFAULTS.text = "You have a new message from ParentApp";
-config.app_config.APP_SKINS.defaultSkinName = SKINS.weekly_workshop.name;
+config.app_config.APP_SKINS.defaultSkinName = SKINS.modular.name;
 config.app_config.APP_SKINS.available = [SKINS.modular, SKINS.weekly_workshop];
 config.app_config.APP_THEMES.available = ["default", "professional"];
 
