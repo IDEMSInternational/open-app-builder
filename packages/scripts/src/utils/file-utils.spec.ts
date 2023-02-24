88e6f71470b3d468d0c45199c3167fb975efe6c5
chrismclarke <chris.m.clarke@live.co.uk>
2023-02-23 17:39:47 -0800

    chore: code tidying

--- packages/scripts/src/utils/file-utils.spec.ts
+++ packages/scripts/src/utils/file-utils.spec.ts
@@ -1,24 +1,18 @@
-import * as FileUtils from "./file-utils";
+import { setNestedProperty } from "./file-utils";
 
 describe("setNestedProperty", () => {
   it("Sets object deep property", () => {
-    expect(FileUtils.setNestedProperty("a.b.c", 1, undefined)).toEqual({
-      a: { b: { c: 1 } },
-    });
+    const res = setNestedProperty("a.b.c", 1);
+    expect(res).toEqual({ a: { b: { c: 1 } } });
   });
   it("Preserves deep properties", () => {
-    const res = FileUtils.setNestedProperty<any>("a.b.c", 1, {
-      a: { b: { d: 2 } },
-    });
-    expect(res).toEqual({
-      a: { b: { c: 1, d: 2 } },
-    });
+    const obj = { a: { b: { d: 2 } } };
+    const res = setNestedProperty<any>("a.b.c", 1, obj);
+    expect(res).toEqual({ a: { b: { c: 1, d: 2 } } });
   });
   it("Supports variable properties", () => {
     const varProperty = "c";
-    const res = FileUtils.setNestedProperty<any>(`a.b.${varProperty}`, 1);
-    expect(res).toEqual({
-      a: { b: { c: 1 } },
-    });
+    const res = setNestedProperty<any>(`a.b.${varProperty}`, 1);
+    expect(res).toEqual({ a: { b: { c: 1 } } });
   });
 });
