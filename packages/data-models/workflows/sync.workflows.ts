0f589566aa58a8e7f9ce37cba90fb28e61c47d29
Chris <chris.m.clarke@live.co.uk>
2023-05-03 18:24:10 +0100

    chore: remove parent sync

--- packages/data-models/workflows/sync.workflows.ts
+++ packages/data-models/workflows/sync.workflows.ts
@@ -12,23 +12,6 @@ const workflows: IDeploymentWorkflows = {
     ],
     steps: [
       {
-        name: "sync_parent",
-        function: async ({ tasks, workflow, config }) => {
-          if (config._parent_config) {
-            const shouldSyncParent = await tasks.userInput.promptOptions(
-              [
-                { name: "No", value: false },
-                { name: "Yes", value: true },
-              ],
-              `Would you like to sync parent [${config._parent_config.name}] content also`
-            );
-            if (shouldSyncParent) {
-              await tasks.workflow.runWorkflow({ name: "sync_parent", parent: workflow });
-            }
-          }
-        },
-      },
-      {
         name: "sync_assets",
         function: async ({ tasks, workflow }) =>
           tasks.workflow.runWorkflow({ name: "sync_assets", parent: workflow }),
@@ -51,19 +34,6 @@ const workflows: IDeploymentWorkflows = {
           } else {
             console.log('Use "--content-watch" or "-cw" to enable live sync\n');
           }
-        },
-      },
-    ],
-  },
-  sync_parent: {
-    steps: [
-      {
-        name: "sync_parent",
-        function: async ({ tasks, workflow, config }) => {
-          await tasks.deployment.set(config._parent_config.name);
-          await tasks.workflow.runWorkflow({ name: "sync_sheets", parent: workflow });
-          await tasks.workflow.runWorkflow({ name: "sync_assets", parent: workflow });
-          await tasks.deployment.set(config.name);
         },
       },
     ],
