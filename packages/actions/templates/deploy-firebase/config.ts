874b9be81e15e6c35a87813d148f3d194e7f275c
Chris <chris.m.clarke@live.co.uk>
2023-06-26 10:48:49 +0100

    refactor: deploy firebase action template

---
+++ packages/actions/templates/deploy-firebase/config.ts
@@ -0,0 +1,35 @@
+import { ITemplateConfig } from "../../models";
+
+export const DeployFirebase: ITemplateConfig = {
+  name: "Deploy to Firebase",
+  description:
+    "Build and deploy to firebase hosting\nSupports both live and preview deployments, and per-branch triggers",
+  templatePath: "deploy-firebase/template.yml",
+  requires: [{ name: "App Build" }],
+  inputs: () => [
+    { name: "FIREBASE_PROJECT_ID", description: "ID of firebase project used", required: true },
+    {
+      name: "FIREBASE_HOSTING_CHANNEL",
+      description:
+        "Name of channel to deploy to (default 'live' is main site, any other word, e.g. 'pr' will create random temp preview site)",
+      defaultValue: "live",
+    },
+    {
+      name: "FIREBASE_HOSTING_TARGET",
+      description: "Optional override target if using multiple hosting sites for firebase project",
+      defaultValue: "",
+    },
+    {
+      name: "PUSH_TARGET_BRANCH",
+      description: "Name of branch used to trigger action on push",
+      defaultValue: "main",
+    },
+  ],
+  secrets: [
+    {
+      name: "FIREBASE_SERVICE_ACCOUNT",
+      description: "JSON export of firebase service account (from console)",
+      required: true,
+    },
+  ],
+};
