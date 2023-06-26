d272c928a0403ff753d5fb00c3ed73cca9eb9e59
Chris <chris.m.clarke@live.co.uk>
2023-06-26 11:01:03 +0100

    feat: app build action template

---
+++ packages/actions/templates/app-build/config.ts
@@ -0,0 +1,22 @@
+import { ITemplateConfig } from "../../models";
+
+export const AppBuild: ITemplateConfig = {
+  name: "App Build",
+  description: "Create main app build for use in deployments",
+  templatePath: "app-build/template.yml",
+  inputs: (deployment) => [
+    {
+      name: "DEPLOYMENT_NAME",
+      description: "name of deployment used",
+      required: true,
+      defaultValue: deployment.name,
+    },
+  ],
+  secrets: [
+    {
+      name: "FIREBASE_SERVICE_ACCOUNT",
+      description: "JSON export of service account (exported from firebase console)",
+      required: true,
+    },
+  ],
+};
