18b58300264eadc9f55cf67c59d2a700c2f24cbe
Chris <chris.m.clarke@live.co.uk>
2023-06-26 12:40:41 +0100

    feat: add pr create content action template

--- packages/actions/templates/index.ts
+++ packages/actions/templates/index.ts
@@ -1,4 +1,5 @@
 import { AppBuild } from "./app-build/config";
 import { DeployFirebase } from "./deploy-firebase/config";
+import { PRCreateContent } from "./pr-create-content/config";
 
-export const ACTION_TEMPLATES = { AppBuild, DeployFirebase };
+export const ACTION_TEMPLATES = { AppBuild, DeployFirebase, PRCreateContent };
