f548e98c19d17dcc9b1bb457f2802516467b6326
Johnny McQuade <jfmcquade@hotmail.com>
2023-07-17 17:40:09 +0100

    feat: new 'card-portrait' variant for button component; new 'icon' variant for toggle-bar component

--- src/app/shared/components/template/components/button/button.component.ts
+++ src/app/shared/components/template/components/button/button.component.ts
@@ -5,6 +5,34 @@ import {
 } from "src/app/shared/utils";
 import { TemplateBaseComponent } from "../base";
 
+interface IButtonParams {
+  /** TEMPLATE PARAMETER: "variant" */
+  variant:
+    | "alternative"
+    | "card"
+    | "card-portrait"
+    | "flexible"
+    | "full"
+    | "information"
+    | "medium"
+    | "navigation"
+    | "short"
+    | "standard"
+    | "tall";
+  /** TEMPLATE PARAMETER: "style". Legacy, use "variant" instead. */
+  style: string;
+  /** TEMPLATE PARAMETER: "disabled". If true, button is disabled and greyed out */
+  disabled: boolean;
+  /** TEMPLATE PARAMETER: "text_align" */
+  textAlign: "left" | "centre" | "right";
+  /** TEMPLATE PARAMETER: "button_align" */
+  buttonAlign: "left" | "centre" | "right";
+  /** TEMPLATE PARAMETER: "icon". The path to an icon asset */
+  icon: string;
+  /** TEMPLATE PARAMETER: "image". The path to an image asset */
+  image: string;
+}
+
 /**
  * A general-purpose button component
  */
@@ -14,25 +42,22 @@ import { TemplateBaseComponent } from "../base";
   styleUrls: ["./button.component.scss"],
 })
 export class TmplButtonComponent extends TemplateBaseComponent implements OnInit {
-  /** TEMPLATE PARAMETER: "style" */
-  style:
-    | "information"
-    | "navigation"
-    | "full"
-    | "flexible"
-    | "medium"
-    | "short"
-    | "tall"
-    | "standard"
-    | "alternative" = "information";
+  /** TEMPLATE PARAMETER: "variant" */
+  variant: IButtonParams["variant"] = "information";
+  /** TEMPLATE PARAMETER: "style". Legacy, use "variant" instead. */
+  style: IButtonParams["style"];
   /** TEMPLATE PARAMETER: "disabled". If true, button is disabled and greyed out */
-  disabled: boolean = false;
+  disabled: IButtonParams["disabled"] = false;
   /** TEMPLATE PARAMETER: "text_align" */
-  textAlign: "left" | "centre" | "right" = "left";
+  textAlign: IButtonParams["textAlign"] = "left";
   /** TEMPLATE PARAMETER: "button_align" */
-  buttonAlign: "left" | "centre" | "right" = "centre";
+  buttonAlign: IButtonParams["buttonAlign"] = "centre";
   /** TEMPLATE PARAMETER: "icon". The path to an icon asset */
-  icon: string;
+  icon: IButtonParams["icon"];
+  /** TEMPLATE PARAMETER: "image". The path to an image asset */
+  image: IButtonParams["image"];
+  /** @ignore */
+  isCardPortrait: boolean = false;
 
   /** @ignore */
   constructor(private elRef: ElementRef) {
@@ -47,11 +72,16 @@ export class TmplButtonComponent extends TemplateBaseComponent implements OnInit
     this.style = `${getStringParamFromTemplateRow(this._row, "style", "information")} ${
       this.isTwoColumns() ? "two_columns" : ""
     }` as any;
+    this.variant = getStringParamFromTemplateRow(this._row, "variant", "")
+      .split(",")
+      .join(" ") as IButtonParams["variant"];
+    this.isCardPortrait = this.variant.split(" ").includes("card-portrait");
     this.disabled = getBooleanParamFromTemplateRow(this._row, "disabled", false);
+    this.image = getStringParamFromTemplateRow(this._row, "image", null);
     if (this._row.disabled) {
       this.disabled = true;
     }
-    this.textAlign = getStringParamFromTemplateRow(this._row, "text_align", "left") as any;
+    this.textAlign = getStringParamFromTemplateRow(this._row, "text_align", null) as any;
     this.buttonAlign = getStringParamFromTemplateRow(this._row, "button_align", "center") as any;
     this.icon = getStringParamFromTemplateRow(this._row, "icon", null);
   }
--- src/app/shared/components/template/components/toggle-bar/toggle-bar.ts
+++ src/app/shared/components/template/components/toggle-bar/toggle-bar.ts
@@ -6,32 +6,43 @@ import {
   getBooleanParamFromTemplateRow,
 } from "src/app/shared/utils";
 
+interface IToggleParams {
+  /** TEMPLATE PARAMETER: "variant" */
+  variant: "" | "icon" | "in_button";
+  /** TEMPLATE PARAMETER: "style". Legacy, use "variant" instead. */
+  style: string;
+  /** TEMPLATE PARAMETER: "show_tick_and_cross" */
+  showTickAndCross: boolean;
+  /** TEMPLATE PARAMETER: "position". Default "left" */
+  position: "left" | "center" | "right";
+  /** TEMPLATE PARAMETER: "false_text". Label text to display when value is false */
+  falseText: string;
+  /** TEMPLATE PARAMETER: "true_text". Label text to display when value is true */
+  trueText: string;
+  /** TEMPLATE PARAMETER: "icon_true". Clickable icon to display when value is true */
+  iconTrue: string;
+  /** TEMPLATE PARAMETER: "icon_false". Clickable icon to display when value is false */
+  iconFalse: string;
+}
+
 @Component({
   selector: "plh-tmpl-toggle-bar",
-  template: `
-    <div class="container" [class]="position" [attr.data-param-style]="style">
-      <div class="toggle_wrapper" [class.show-tick-cross]="showTickAndCross">
-        <ion-toggle mode="md" [checked]="_row.value" (click)="handleClick($event)"> </ion-toggle>
-      </div>
-      <span
-        class="label"
-        *ngIf="true_text && false_text"
-        [innerHTML]="true_text && false_text | markdown"
-        >{{ _row.value ? true_text : false_text }}</span
-      >
-    </div>
-  `,
+  templateUrl: "./toggle-bar.html",
   styleUrls: ["./toggle-bar.scss"],
 })
 export class TmplToggleBarComponent
   extends TemplateBaseComponent
   implements ITemplateRowProps, OnInit
 {
-  public false_text: string;
-  public true_text: string;
-  public position: string;
-  public showTickAndCross: boolean;
-  style: string;
+  public falseText: IToggleParams["falseText"];
+  public trueText: IToggleParams["trueText"];
+  public position: IToggleParams["position"];
+  public showTickAndCross: IToggleParams["showTickAndCross"];
+  public iconTrue: IToggleParams["iconTrue"];
+  public iconFalse: IToggleParams["iconFalse"];
+  style: IToggleParams["style"];
+  variant: IToggleParams["variant"];
+  isVariantIcon: boolean;
 
   ngOnInit() {
     this.getParams();
@@ -48,10 +59,20 @@ export class TmplToggleBarComponent
   }
 
   getParams() {
-    this.false_text = getStringParamFromTemplateRow(this._row, "false_text", "");
-    this.true_text = getStringParamFromTemplateRow(this._row, "true_text", "");
-    this.position = getStringParamFromTemplateRow(this._row, "position", "left");
+    this.falseText = getStringParamFromTemplateRow(this._row, "false_text", "");
+    this.trueText = getStringParamFromTemplateRow(this._row, "true_text", "");
+    this.position = getStringParamFromTemplateRow(
+      this._row,
+      "position",
+      "left"
+    ) as IToggleParams["position"];
     this.showTickAndCross = getBooleanParamFromTemplateRow(this._row, "show_tick_and_cross", true);
     this.style = getStringParamFromTemplateRow(this._row, "style", "");
+    this.variant = getStringParamFromTemplateRow(this._row, "variant", "")
+      .split(",")
+      .join(" ") as IToggleParams["variant"];
+    this.isVariantIcon = this.variant.split(" ").includes("icon");
+    this.iconTrue = getStringParamFromTemplateRow(this._row, "icon_true", "");
+    this.iconFalse = getStringParamFromTemplateRow(this._row, "icon_false", "");
   }
 }
