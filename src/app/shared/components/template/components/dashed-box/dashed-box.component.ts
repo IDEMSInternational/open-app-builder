d24a7f003db45163b3f785873051bcc411df3847
kwAsant <jodyagyekye@gmail.com>
2023-08-03 16:05:01 +0100

    Have now officially fixed the code and will now make a pull request. I did so using the marked library; which allows markdown to be parsed into HTML. Using the parse() method from the library, it converts the markdown into HTML and then use bypassSecurityTrustHtml() method is used to convert it to a SafeHtml object which is then passed to the innerHTML property of the div. source: https://marked.js.org

--- src/app/shared/components/template/components/dashed-box/dashed-box.component.ts
+++ src/app/shared/components/template/components/dashed-box/dashed-box.component.ts
@@ -3,12 +3,13 @@ import { TemplateBaseComponent } from "../base";
 import { ITemplateRowProps } from "../../models";
 import { getStringParamFromTemplateRow } from "../../../../utils";
 import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
+import * as marked from "marked"; // import the marked library
+// the library allows for markdown to be parsed into html
+// source: https://marked.js.org
+// using parse() method from library it converts the markdown into
+// HTML and then the bypassSecurityTrustHtml() method is used to convert it to
+// a SafeHtml object which is then passed to the innerHTML property of the div
 
-@Component({
-  selector: "plh-dashed-box",
-  templateUrl: "./dashed-box.component.html",
-  styleUrls: ["./dashed-box.component.scss"],
-})
 @Component({
   selector: "plh-dashed-box",
   templateUrl: "./dashed-box.component.html",
@@ -28,7 +29,7 @@ export class TmplDashedBoxComponent
 
   ngOnInit() {
     this.getParams();
-    this.innerHTML = this.domSanitizer.bypassSecurityTrustHtml(this._row.value);
+    this.innerHTML = this.domSanitizer.bypassSecurityTrustHtml(marked.parse(this._row.value));
   }
 
   getParams() {
