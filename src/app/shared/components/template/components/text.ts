10478def11c91da5fcbda80dcbb25e72c707f466
kwAsant <jodyagyekye@gmail.com>
2023-08-04 10:51:01 +0100

    final changes

--- src/app/shared/components/template/components/text.ts
+++ src/app/shared/components/template/components/text.ts
@@ -25,7 +25,6 @@ export class TmplTextComponent extends TemplateBaseComponent implements OnInit {
 
   ngOnInit() {
     this.getParams();
-    // this.textValue = this._row.value ;
   }
 
   getParams() {
