4017ce1e5de1d9a5a0d9c51efc576fdf4dba8c26
chrismclarke <chris.m.clarke@live.co.uk>
2023-08-10 16:24:19 -0700

    chore: fix data-model imports

--- packages/data-models/index.ts
+++ packages/data-models/index.ts
@@ -9,3 +9,5 @@ export {
 } from "./deployment.model";
 export { IAppSkin } from "./skin.model";
 export { IAppConfig, IAppConfigOverride, getDefaultAppConfig } from "./appConfig";
+export * from "./db.model";
+export * from "./deployment.model";
