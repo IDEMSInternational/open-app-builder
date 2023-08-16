f3224568657ed3e5de8fc3a476eac8314112fb13
Johnny McQuade <jfmcquade@hotmail.com>
2023-08-16 13:50:45 +0100

    refactor: audio component params

--- src/app/shared/components/template/components/audio/audio.component.ts
+++ src/app/shared/components/template/components/audio/audio.component.ts
@@ -11,9 +11,37 @@ import { ITemplateRowProps } from "../../models";
 import { TemplateBaseComponent } from "../base";
 import { TemplateAssetService } from "../../services/template-asset.service";
 
+// Names of ion-icons to be used by default in the player.
+// Will be overridden if user provides values for play_icon_asset, pause_icon_asset or forward_icon_asset
 const PLAY_ICON_DEFAULT = "play-outline";
 const PAUSE_ICON_DEFAULT = "pause-outline";
 const FORWARD_ICON_DEFAULT = "play-forward";
+
+interface IAudioParams {
+  /** TEMPLATE PARAMETER: "src". Will be overridden by a value passed as "value" to the component. */
+  src: string;
+  /** TEMPLATE PARAMETER: "play_icon_asset". The path to an svg to override the default play icon. Default icon is ion's "play-outline" */
+  playIcon: string;
+  /** TEMPLATE PARAMETER: "pause_icon_asset". The path to an svg to override the default pause icon. Default icon is ion's "pause-outline" */
+  pauseIcon: string;
+  /** TEMPLATE PARAMETER: "forward_icon_asset". The path to an svg to override the default forward icon.
+   * Will be mirrored to be used as the reqind icon. Default icon is ion's "play-forward"
+   * */
+  forwardIcon: string;
+  /** TEMPLATE PARAMETER: "title" */
+  titleAudio: string;
+  /** TEMPLATE PARAMETER: "help". Text to be displayed as a tooltip when clicking the "help" icon.
+   * Icon and tooltip will not be displayed if value not provided. Default null */
+  help: string;
+  /** TEMPLATE PARAMETER: "range_bar_disabled". If true, the use cannot scrub through the audio using the range bar.
+   * Default false.
+   */
+  rangeBarDisabled: boolean;
+  /** TEMPLATE PARAMETER: "time_to_rewind". The increment of time, in seconds, that will be applied when clicking the forward or backward buttons.
+   * Default 15.
+   */
+  timeToRewind: number;
+}
 
 @Component({
   selector: "plh-audio",
@@ -26,22 +54,25 @@ export class TmplAudioComponent
 {
   @Input() template: FlowTypes.Template;
   @ViewChild("range", { static: false }) range: IonRange;
-  src: string | null;
-  titleAudio: string | null;
-  help: string | null;
+
+  params: Partial<IAudioParams> = {};
+
+  /** @ignore */
   isMute: boolean = false;
+  /** @ignore */
   player: Howl = null;
+  /** @ignore */
   isPlayed: boolean = false;
+  /** @ignore */
   errorTxt: string | null;
+  /** @ignore */
   progress = 0;
-  timeToRewind: number = 15;
+  /** @ignore */
   rangeBarTouched: boolean = false;
+  /** @ignore */
   currentTimeSong: string = "0";
-  rangeBarDisabled: boolean = false;
+  /** @ignore */
   hasStarted: boolean = false;
-  playIcon: string;
-  pauseIcon: string;
-  forwardIcon: string;
 
   constructor(private templateAssetService: TemplateAssetService) {
     super();
@@ -53,19 +84,26 @@ export class TmplAudioComponent
   }
 
   getParams() {
-    this.src = this.templateAssetService.getTranslatedAssetPath(
+    this.params.src = this.templateAssetService.getTranslatedAssetPath(
       this._row.value || this.getAssetParamFromTemplateRow("src", null)
     );
-    this.playIcon = this.getAssetParamFromTemplateRow("play_icon_asset", PLAY_ICON_DEFAULT);
-    this.pauseIcon = this.getAssetParamFromTemplateRow("pause_icon_asset", PAUSE_ICON_DEFAULT);
-    this.forwardIcon = this.getAssetParamFromTemplateRow(
+    this.params.playIcon = this.getAssetParamFromTemplateRow("play_icon_asset", PLAY_ICON_DEFAULT);
+    this.params.pauseIcon = this.getAssetParamFromTemplateRow(
+      "pause_icon_asset",
+      PAUSE_ICON_DEFAULT
+    );
+    this.params.forwardIcon = this.getAssetParamFromTemplateRow(
       "forward_icon_asset",
       FORWARD_ICON_DEFAULT
     );
-    this.titleAudio = getStringParamFromTemplateRow(this._row, "title", "Title");
-    this.help = getStringParamFromTemplateRow(this._row, "help", null);
-    this.rangeBarDisabled = getBooleanParamFromTemplateRow(this._row, "range_bar_disabled", false);
-    this.timeToRewind = getNumberParamFromTemplateRow(this._row, "time_to_rewind", 15);
+    this.params.titleAudio = getStringParamFromTemplateRow(this._row, "title", "");
+    this.params.help = getStringParamFromTemplateRow(this._row, "help", null);
+    this.params.rangeBarDisabled = getBooleanParamFromTemplateRow(
+      this._row,
+      "range_bar_disabled",
+      false
+    );
+    this.params.timeToRewind = getNumberParamFromTemplateRow(this._row, "time_to_rewind", 15);
   }
 
   getAssetParamFromTemplateRow(parameterName: string, _default: string | null) {
@@ -74,9 +112,9 @@ export class TmplAudioComponent
   }
 
   initPlayer() {
-    return this.src
+    return this.params.src
       ? (this.player = new Howl({
-          src: [this.src],
+          src: [this.params.src],
           onplay: () => {
             this.isPlayed = true;
             this.updateProgress();
@@ -108,15 +146,15 @@ export class TmplAudioComponent
   }
 
   rewindNext() {
-    this.player.seek((this.player.seek() as any) + this.timeToRewind);
+    this.player.seek((this.player.seek() as any) + this.params.timeToRewind);
     this.customUpdateWhenRewind();
   }
 
   rewindPrev() {
     this.player.seek(
-      (this.player.seek() as any) < this.timeToRewind
+      (this.player.seek() as any) < this.params.timeToRewind
         ? (this.player.seek(0) as any)
-        : (this.player.seek() as any) - this.timeToRewind
+        : (this.player.seek() as any) - this.params.timeToRewind
     );
     this.customUpdateWhenRewind();
   }
