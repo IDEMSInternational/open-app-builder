1ebb0dc754cf66b7a9fb91490e2c671861a384fc
chrismclarke <chris.m.clarke@live.co.uk>
2023-08-18 14:04:22 -0700

    refactor: angular shared imports

--- packages/data-models/index.ts
+++ packages/data-models/index.ts
@@ -1,13 +1,6 @@
-export * from "./flowTypes";
-export * from "./functions";
-
-export {
-  IDeploymentConfig,
-  IDeploymentConfigJson,
-  DEPLOYMENT_CONFIG_EXAMPLE_DEFAULTS,
-  DEPLOYMENT_CONFIG_VERSION,
-} from "./deployment.model";
-export { IAppSkin } from "./skin.model";
-export { IAppConfig, IAppConfigOverride, getDefaultAppConfig } from "./appConfig";
+export * from "./appConfig";
 export * from "./db.model";
 export * from "./deployment.model";
+export * from "./flowTypes";
+export * from "./functions";
+export * from "./skin.model";
