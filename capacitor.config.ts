efcdf1daa8d68e2f5861d008413e980523b3baec
Johnny McQuade <jfmcquade@hotmail.com>
2023-08-31 17:55:40 +0100

    chore: update core android files to reflect migration to Capacitor 5

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,6 @@ import { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "international.idems.plh_teens_tz",
   appName: "PLH Teens TZ",
-  bundledWebRuntime: false,
   webDir: "www",
   plugins: {
     SplashScreen: {
