1afbdc705a5c0791df8f1a308b6331a539c41fab
chrismclarke <chris.m.clarke@live.co.uk>
2023-10-02 12:55:21 -0700

    feat: multiple gdrive asset_folder_ids

--- packages/workflows/src/sync.workflows.ts
+++ packages/workflows/src/sync.workflows.ts
@@ -52,8 +52,8 @@ const workflows: IDeploymentWorkflows = {
       {
         name: "sheets_dl",
         function: async ({ tasks, config, options }) => {
-          // HACK - ensure drive id provided as array
-          let { sheets_folder_ids, sheets_folder_id } = config.google_drive;
+          // HACK - ensure drive id provided as array (can be removed once deprecation removed)
+          let { sheets_folder_ids, sheets_folder_id, sheets_filter_function } = config.google_drive;
           if (!sheets_folder_ids) {
             logWarning({
               msg1: "[sheets_folder_id] config property is deprecated",
@@ -68,7 +68,10 @@ const workflows: IDeploymentWorkflows = {
             outputs = sheets_folder_ids.map((folder_id) => tasks.gdrive.getOutputFolder(folder_id));
           } else {
             for (const folderId of sheets_folder_ids) {
-              const output = await tasks.gdrive.download({ folderId });
+              const output = await tasks.gdrive.download({
+                folderId,
+                filterFn: sheets_filter_function,
+              });
               outputs.push(output);
             }
           }
@@ -107,21 +110,37 @@ const workflows: IDeploymentWorkflows = {
       {
         name: "assets_dl",
         function: async ({ tasks, config, options }) => {
-          const folderId = config.google_drive.assets_folder_id;
-          const filterFn = config.google_drive.assets_filter_function;
+          // HACK - ensure drive id provided as array (can be removed once deprecation removed)
+          let { assets_folder_ids, assets_folder_id, assets_filter_function } = config.google_drive;
+          if (!assets_folder_ids) {
+            logWarning({
+              msg1: "[assets_folder_id] config property is deprecated",
+              msg2: "use [assets_folder_ids] instead",
+            });
+            assets_folder_ids = [assets_folder_id];
+          }
+          /** Return output of paths to downloaded assets */
+          let outputs: string[] = [];
           // If skipping download still need to return download folder for next step
           if (options.skipDownload) {
-            return tasks.gdrive.getOutputFolder(folderId);
+            outputs = assets_folder_ids.map((folder_id) => tasks.gdrive.getOutputFolder(folder_id));
           } else {
-            return tasks.gdrive.download({ folderId, filterFn });
+            for (const folderId of assets_folder_ids) {
+              const output = await tasks.gdrive.download({
+                folderId,
+                filterFn: assets_filter_function,
+              });
+              outputs.push(output);
+            }
           }
+          return outputs;
         },
       },
       {
         name: "assets_post_process",
         function: async ({ tasks, workflow }) =>
           tasks.appData.postProcessAssets({
-            sourceAssetsFolder: workflow.assets_dl.output,
+            sourceAssetsFolders: workflow.assets_dl.output,
           }),
       },
     ],
