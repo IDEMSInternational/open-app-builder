c148bb584f67582951e9840e3fbf5bffc8028b54
chrismclarke <chris.m.clarke@live.co.uk>
2023-10-02 08:36:55 -0700

    chore: code tidying

--- packages/data-models/deployment.model.ts
+++ packages/data-models/deployment.model.ts
@@ -10,11 +10,11 @@ export interface IDeploymentConfig {
   google_drive: {
     /** @deprecated Use `sheets_folder_ids` array instead */
     sheets_folder_id?: string;
-    /** ID of folder containing app sheets, as seen in end of url */
+    /** IDs of folders containing app sheets, as seen in end of url */
     sheets_folder_ids?: string[];
     /** @deprecated Use `assets_folder_ids` array instead */
     assets_folder_id?: string;
-    /** ID of folder containing app assets, as seen in end of url */
+    /** IDs of folders containing app assets, as seen in end of url */
     assets_folder_ids?: string[];
     /** generated gdrive access token. Default `packages/scripts/config/token.json` */
     auth_token_path?: string;
