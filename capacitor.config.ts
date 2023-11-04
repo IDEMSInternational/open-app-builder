b089cdf5a981ab24e21e06de5c140b41cf426b9c
chrismclarke <chris.m.clarke@live.co.uk>
2023-11-03 22:31:45 -0700

    chore: add capacitor live-reload notes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -20,6 +20,15 @@ const config: CapacitorConfig = {
   },
   server: {
     androidScheme: "http",
+    /**
+     * NOTE - to support live-reload on external device (e.g. emulator)
+     * 1) Uncomment url and replace with local ip to serve live-reload on local device
+     * 2) Sync to capacitor `npx cap sync`
+     * 3) Serve via `yarn ng serve --configuration=external`
+     * 4) Run app from android studio `npx cap open android` and run
+     * Local browser (localhost:4000), device app and device browser ([ip]:4200) should all be able to access served app
+     **/
+    // url: "http://192.168.50.67:4200",
   },
 };
 
