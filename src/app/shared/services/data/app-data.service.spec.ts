5e4e50a2859650e32e309ccb0dc13076a230eef1
Johnny McQuade <jfmcquade@hotmail.com>
2023-11-10 12:26:46 +0000

    test: add tests for app data service

--- src/app/shared/services/data/app-data.service.spec.ts
+++ src/app/shared/services/data/app-data.service.spec.ts
@@ -11,12 +11,73 @@ import { MockErrorHandlerService } from "../error-handler/error-handler.service.
 import { DbService } from "../db/db.service";
 import { MockDbService } from "../db/db.service.spec";
 import { Injectable } from "@angular/core";
+import { ISheetContents } from "src/app/data";
 
-const DATA_MOCK = {
-  test_flow: [
-    { id: "id1", number: 1, string: "hello", boolean: true },
-    { id: "id2", number: 2, string: "goodbye", boolean: false },
-  ],
+const DATA_MOCK: { [flow_name: string]: FlowTypes.FlowTypeWithData } = {
+  flow_a: {
+    flow_name: "flow_a",
+    flow_type: "data_list",
+    rows: [
+      { id: "a_id1", number: 1, string: "a_hello", boolean: true },
+      { id: "a_id2", number: 2, string: "a_goodbye", boolean: false },
+    ],
+  },
+  flow_b: {
+    flow_name: "flow_b",
+    flow_type: "data_list",
+    rows: [
+      { id: "b_id1", number: 1, string: "b_hello", boolean: true },
+      { id: "b_id2", number: 2, string: "b_goodbye", boolean: false },
+    ],
+    _overrides: { flow_c: "true" },
+  },
+  flow_c: {
+    flow_name: "flow_c",
+    flow_type: "data_list",
+    rows: [
+      { id: "c_id1", number: 1, string: "c_hello", boolean: true },
+      { id: "c_id2", number: 2, string: "c_goodbye", boolean: false },
+    ],
+    _overrides: { flow_b: "true" },
+  },
+  flow_d: {
+    flow_name: "flow_d",
+    flow_type: "data_list",
+    rows: [
+      { id: "d_id1", number: 1, string: "d_hello", boolean: true },
+      { id: "d_id2", number: 2, string: "d_goodbye", boolean: false },
+    ],
+    _overrides: { flow_a: "true" },
+  },
+};
+
+const CONTENTS_MOCK: ISheetContents = {
+  data_list: {
+    flow_a: {
+      flow_name: "flow_a",
+      flow_type: "data_list",
+    },
+    flow_b: {
+      flow_name: "flow_b",
+      flow_type: "data_list",
+      _overrides: { flow_c: "true" },
+    },
+    flow_c: {
+      flow_name: "flow_c",
+      flow_type: "data_list",
+      _overrides: { flow_b: "true" },
+    },
+    flow_d: {
+      flow_name: "flow_d",
+      flow_type: "data_list",
+      _overrides: { flow_a: "true" },
+    },
+  },
+  data_pipe: {},
+  generator: {},
+  global: {},
+  template: {},
+  tour: {},
 };
 
 /** Mock calls for sheets from the appData service to return test data */
@@ -25,7 +86,7 @@ export class MockAppDataService implements Partial<AppDataService> {
     flow_type: FlowTypes.FlowType,
     flow_name: string
   ): Promise<T> {
-    const rows = DATA_MOCK[flow_name] || [];
+    const rows = DATA_MOCK[flow_name].rows || [];
     return { flow_name, flow_type, rows } as any;
   }
 }
@@ -33,15 +94,16 @@ export class MockAppDataService implements Partial<AppDataService> {
 /** Use an extended service for testing to allow override of protected variables */
 @Injectable({ providedIn: "root" })
 class AppDataServiceExtended extends AppDataService {
-  protected sheetContents = {
-    data_list: {},
+  protected sheetContents = CONTENTS_MOCK;
+  protected translationContents = {};
+  public appDataCache = {
+    data_list: { ...DATA_MOCK },
     data_pipe: {},
     generator: {},
     global: {},
     template: {},
     tour: {},
   };
-  protected translationContents = {};
 }
 
 /********************************************************************************
@@ -69,6 +131,23 @@ describe("AppDataService", () => {
 
   it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  it("returns data for a flow", async () => {
+    const data = await service.getSheet("data_list", "flow_a");
+    expect(data.rows?.[0].id).toEqual("a_id1");
+  });
+
+  it("handles flow overrides", async () => {
+    const data = await service.getSheet("data_list", "flow_d");
+    expect(data.rows?.[0].id).toEqual("a_id1");
+  });
+
+  // In the case that flow_b is overridden by flow_c, which in turn is overridden by flow_b,
+  // the loop should be broken after the initial override
+  it("handles recursive flow overrides", async () => {
+    const data = await service.getSheet("data_list", "flow_b");
+    expect(data.rows?.[0].id).toEqual("c_id1");
   });
   // TODO - add public method tests
 });
