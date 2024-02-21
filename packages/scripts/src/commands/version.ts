fe7db4bff5d5d168c77d8c5ae82dd362b0165dea
chrismclarke <chris.m.clarke@live.co.uk>
2024-02-20 20:22:01 -0800

    refactor: android-specific version script

--- packages/scripts/src/commands/version.ts
+++ packages/scripts/src/commands/version.ts
@@ -5,38 +5,81 @@ import * as fs from "fs-extra";
 import inquirer from "inquirer";
 import { APP_BUILD_GRADLE_PATH, MAIN_PACKAGE_PATH } from "../paths";
 
+interface IProgramOptions {
+  /** Set package.json version  */
+  package?: boolean;
+  /** Set android version  */
+  android?: boolean;
+}
+
 /***************************************************************************************
  * CLI
- * @example yarn
+ * @example yarn worksapce scripts run version --package --android 1.0.6
  *************************************************************************************/
 const program = new Command("version");
 export default program
   .description("Set app version")
-  .argument("[newVersion]", "newVersion")
-  .action(async (newVersion: string | undefined) => {
-    await version(newVersion);
+  .argument("[version]", "version number to apply")
+  .option("-p, --package", "apply versioning to package.json")
+  .option("-a, --android", "apply versioning to android build")
+  .action(async (version: string | undefined, options: IProgramOptions) => {
+    await setVersion(version, options);
   });
 
 /***************************************************************************************
  * Main Methods
  *************************************************************************************/
 
-/**
- * Set a consistent version number by incrementing the current
- * package.json version and also assigning to android version codes
- */
-async function version(newVersion?: string) {
-  const currentVersion = fs.readJSONSync(MAIN_PACKAGE_PATH).version;
-  if (!newVersion) newVersion = await promptNewVersion(currentVersion);
-  if (!isNextVersionValid(currentVersion, newVersion)) {
-    const heading = chalk.yellow(
-      `Version number must be increased\n${currentVersion} -> ${newVersion}`
-    );
-    console.log(boxen(heading, { padding: 1, borderColor: "red" }));
+async function setVersion(version: string | undefined, options: IProgramOptions) {
+  if (!options.package && !options.android) {
+    console.log(chalk.yellow(`Specify what to version, e.g. --android --package`));
     process.exit(1);
   }
-  updatePackageJson(newVersion);
-  updateGradleBuild(newVersion);
+  if (options.package) await setPackageVersion(version);
+  if (options.android) await setAndroidBuildVersion(version);
+}
+
+/** Update root package.json version, ensuring increment  */
+async function setPackageVersion(version?: string) {
+  const previousVersion = fs.readJSONSync(MAIN_PACKAGE_PATH).version;
+  if (!version) {
+    version = await promptVersion("Package", previousVersion);
+  }
+  // additional increment check in case version supplied directly (non prompt)
+  ensureVersionIncremented(previousVersion, version);
+  updatePackageJson(version);
+}
+
+async function setAndroidBuildVersion(version?: string) {
+  const previousVersion = getGradleBuildVersion();
+  if (!version) {
+    version = await promptVersion("Android", previousVersion);
+  }
+  // additional increment check in case version supplied directly (non prompt)
+  ensureVersionIncremented(previousVersion, version);
+  updateGradleBuild(version);
+}
+
+async function promptVersion(prefix: string, previousVersion: string) {
+  const message = `[${prefix}] Specify a version number (current: ${previousVersion})`;
+  const { input } = await inquirer.prompt([
+    {
+      message,
+      name: "input",
+      validate: (v: string) =>
+        isVersionIncremented(previousVersion, v) ? true : "Version number must be increased",
+    },
+  ]);
+  return input;
+}
+
+function getGradleBuildVersion() {
+  const gradleBuildFile = fs.readFileSync(APP_BUILD_GRADLE_PATH, {
+    encoding: "utf-8",
+  });
+  const match = gradleBuildFile.match(/versionName .*$/gm);
+  const code = match?.[0].replace("versionName", "").replace(/[^0-9.]/g, "");
+  return code || "0.0.0";
 }
 
 function updateGradleBuild(newVersionName: string) {
@@ -54,22 +97,20 @@ async function updatePackageJson(newVersion: string) {
   packageJson.version = newVersion;
   fs.writeJSONSync(MAIN_PACKAGE_PATH, packageJson, { spaces: 2 });
 }
-
-async function promptNewVersion(currentVersion: string) {
-  const { version } = await inquirer.prompt([
-    {
-      message: `Specify a version number (current: ${currentVersion})`,
-      name: "version",
-      validate: (v) =>
-        isNextVersionValid(currentVersion, version) ? true : "Version number must be increased",
-    },
-  ]);
-  return version;
+function ensureVersionIncremented(previousVersion: string, nextVersion: string) {
+  if (previousVersion && nextVersion && isVersionIncremented(previousVersion, nextVersion)) {
+    return true;
+  }
+  const heading = chalk.yellow(
+    `Version number must be increased\n${previousVersion} -> ${nextVersion}`
+  );
+  console.log(boxen(heading, { padding: 1, borderColor: "red" }));
+  process.exit(1);
 }
 
-function isNextVersionValid(currentVersion: string, newVersion: string) {
-  const nextCode = Number(_generateVersionCode(newVersion));
-  const currentCode = Number(_generateVersionCode(currentVersion));
+function isVersionIncremented(previousVersion: string, nextVersion: string) {
+  const nextCode = Number(_generateVersionCode(nextVersion));
+  const currentCode = Number(_generateVersionCode(previousVersion));
   return nextCode > currentCode;
 }
 
