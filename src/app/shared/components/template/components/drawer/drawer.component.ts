3417117ec062202e6608937d1cab702801466f4b
Johnny McQuade <jfmcquade@hotmail.com>
2024-03-04 10:33:01 +0000

    refactor: drawer component no longer depends on header height

--- src/app/shared/components/template/components/drawer/drawer.component.ts
+++ src/app/shared/components/template/components/drawer/drawer.component.ts
@@ -29,7 +29,7 @@ export class TmplDrawerComponent extends TemplateBaseComponent implements OnInit
       this.isOpen = false;
     } else {
       drawer.style.transition = "transform .3s ease-out";
-      drawer.style.transform = `translateY(${-this.getOpenHeight()}px)`;
+      drawer.style.transform = `translateY(-${this.getOpenHeight()}px)`;
       this.isOpen = true;
     }
   }
@@ -37,6 +37,10 @@ export class TmplDrawerComponent extends TemplateBaseComponent implements OnInit
   private getOpenHeight() {
     const drawer = this.drawer.nativeElement;
     const handle = drawer.children[0];
-    return drawer.offsetHeight - handle.offsetHeight - 10;
+    const windowHeight = window.innerHeight;
+    const drawerDomRect = drawer.getBoundingClientRect() as DOMRect;
+    // As translateY is used to set drawer's default position, calculate hieght change relative to this value
+    const defaultTranslateY = windowHeight - drawerDomRect.top;
+    return drawerDomRect.height - handle.offsetHeight - defaultTranslateY - 10;
   }
 }
