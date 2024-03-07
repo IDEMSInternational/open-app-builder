b042a1fb186101ac1084294bfe9ecdc000b9fc17
chrismclarke <chris.m.clarke@live.co.uk>
2024-03-07 11:22:37 -0800

    refactor: item eval context calc

--- src/app/shared/components/template/components/data-items/data-items.component.ts
+++ src/app/shared/components/template/components/data-items/data-items.component.ts
@@ -74,8 +74,10 @@ export class TmplDataItemsComponent extends TemplateBaseComponent implements OnD
     parameterList: any
   ) {
     const parsedItemDataList = await this.parseDataList(itemDataList);
-    const itemRows = new ItemProcessor(parsedItemDataList, parameterList).process(rows);
-    const itemRowsWithMeta = this.setItemMeta(itemRows, parsedItemDataList);
+    const { itemRows, itemData } = new ItemProcessor(parsedItemDataList, parameterList).process(
+      rows
+    );
+    const itemRowsWithMeta = this.setItemMeta(itemRows, itemData);
 
     const parsedItemRows = await this.hackProcessRows(itemRowsWithMeta);
     // TODO - deep diff and only update changed
@@ -86,34 +88,34 @@ export class TmplDataItemsComponent extends TemplateBaseComponent implements OnD
   /**
    * Update item dynamic evaluation context and action lists to include relevant
    * item data
+   * @param templateRows List of template rows generated from itemData by item processor
+   * @param itemData List of original item data used to create item rows (post operations such as filter/sort)
    * */
-  private setItemMeta(
-    rows: FlowTypes.TemplateRow[],
-    dataList: {
-      [index: string]: any;
-    }
-  ) {
-    const rowIds = Object.values(dataList).map((v) => v.id);
-    const lastRowIndex = rowIds.length - 1;
-    return rows.map((r) => {
-      // Reassign metadata fields previously assigned by item as rendered row count may have changed
-      const itemIndex = rowIds.indexOf(r._evalContext.itemContext.id);
+  private setItemMeta(templateRows: FlowTypes.TemplateRow[], itemData: FlowTypes.Data_listRow[]) {
+    const lastItemIndex = itemData.length - 1;
+    const itemDataIDs = itemData.map((item) => item.id);
+    // Reassign metadata fields previously assigned by item as rendered row count may have changed
+    return templateRows.map((r) => {
+      // Map the row item context to the original list of items rendered to know position in item list.
+      const itemIndex = itemDataIDs.indexOf(r._evalContext.itemContext._id);
+      // Update metadata fields as _first, _last and index may have changed based on dynamic updates
       r._evalContext.itemContext = {
         ...r._evalContext.itemContext,
+        _index: itemIndex,
         _first: itemIndex === 0,
-        _last: itemIndex === lastRowIndex,
+        _last: itemIndex === lastItemIndex,
       };
       // Update any action list set_item args to contain name of current data list and item id
       // and set_items action to include all currently displayed rows
       if (r.action_list) {
         r.action_list = r.action_list.map((a) => {
           if (a.action_id === "set_item") {
-            a.args = [this.dataListName, r._evalContext.itemContext.id];
+            a.args = [this.dataListName, r._evalContext.itemContext._id];
           }
           if (a.action_id === "set_items") {
             // TODO - add a check for @item refs and replace parameter list with correct values
             // for each individual item (default will be just to pick the first)
-            a.args = [this.dataListName, Object.values(dataList).map((v) => v.id)];
+            a.args = [this.dataListName, itemDataIDs];
           }
           return a;
         });
