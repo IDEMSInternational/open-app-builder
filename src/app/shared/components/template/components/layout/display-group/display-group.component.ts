303e85bf108bc3a3b142cde8152672e5d08721ea
Johnny McQuade <jfmcquade@hotmail.com>
2024-03-08 11:53:07 +0000

    feat: add 3 new variants for display group: box_gray, box_primary, box_secondary

--- src/app/shared/components/template/components/layout/display-group/display-group.component.ts
+++ src/app/shared/components/template/components/layout/display-group/display-group.component.ts
@@ -2,13 +2,22 @@ import { Component, OnInit } from "@angular/core";
 import { TemplateBaseComponent } from "../../base";
 import { getNumberParamFromTemplateRow, getStringParamFromTemplateRow } from "../../../../../utils";
 
+interface IDisplayGroupParams {
+  /** TEMPLATE PARAMETER: "variant" */
+  variant: "box_gray" | "box_primary" | "box_secondary";
+  /** TEMPLATE PARAMETER: "style". TODO: Various additional legacy styles, review and convert some to variants */
+  style: "form" | "dashed_box" | "default" | string | null;
+  /** TEMPLATE PARAMETER: "offset". Add a custom bottom margin */
+  offset: number;
+}
+
 @Component({
   selector: "plh-tmpl-display-group",
   templateUrl: "./display-group.component.html",
   styleUrls: ["../../tmpl-components-common.scss", "./display-group.component.scss"],
 })
 export class TmplDisplayGroupComponent extends TemplateBaseComponent implements OnInit {
-  params: { style: string | null; offset: number } = {} as any;
+  params: Partial<IDisplayGroupParams> = {};
   bgColor: string;
   type: "form" | "dashed_box" | "default";
 
@@ -23,6 +32,9 @@ export class TmplDisplayGroupComponent extends TemplateBaseComponent implements 
   getParams() {
     this.params.style = getStringParamFromTemplateRow(this._row, "style", "row");
     this.params.offset = getNumberParamFromTemplateRow(this._row, "offset", 0);
+    this.params.variant = getStringParamFromTemplateRow(this._row, "variant", "")
+      .split(",")
+      .join(" ") as IDisplayGroupParams["variant"];
     this.type = this.getTypeFromStyles(this.params.style || "");
   }
 
