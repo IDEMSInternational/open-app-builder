847c980b2dd382125d4900fa5863d270e36ef1ea
Chris <chris.m.clarke@live.co.uk>
2024-05-21 11:19:00 +0100

    fix: protected field name retrieval

--- packages/data-models/fields.ts
+++ packages/data-models/fields.ts
@@ -2,7 +2,7 @@
  * Protected fields can be set by field service and will be prefixed with an underscore
  * They are used to store store computed or exported variables and are not user overridable
  */
-export enum PROTECTED_FIELDS {
+enum PROTECTED_FIELDS {
   APP_AUTH_USER = "app_auth_user",
   APP_FIRST_LAUNCH = "app_first_launch",
   APP_LANGUAGE = "app_language",
@@ -20,5 +20,8 @@ export enum PROTECTED_FIELDS {
   FEEDBACK_SIDEBAR_OPEN = "feedback_sidebar_open",
   SERVER_SYNC_LATEST = "server_sync_latest",
 }
+
+/** Whenever retrieving a protected field make sure to include underscore prefix */
+export const getProtectedFieldName = (key: IProtectedFieldName) => `_${PROTECTED_FIELDS[key]}`;
 
 export type IProtectedFieldName = keyof typeof PROTECTED_FIELDS;
