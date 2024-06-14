fe6593d675e411c0217973055d10f447a888b1e0
Johnny McQuade <jfmcquade@hotmail.com>
2024-06-14 16:41:20 +0100

    chore: code tidy; added test for remote asset service

--- src/app/shared/services/remote-asset/remote-asset.service.spec.ts
+++ src/app/shared/services/remote-asset/remote-asset.service.spec.ts
@@ -1,13 +1,47 @@
 import { TestBed } from "@angular/core/testing";
 
 import { RemoteAssetService } from "./remote-asset.service";
-import { AppDataService } from "../data/app-data.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { IAssetContents } from "src/app/data";
+import { FlowTypes } from "../../model";
 
+const MOCK_ASSETS_CONTENTS_LIST: IAssetContents = {
+  "audio/test_audio.mp3": {
+    md5Checksum: "5ddddf934d2187d084c75b7e27797fae",
+    size_kb: 43.4,
+  },
+};
+
+const MOCK_CORE_ASSET_PACK: FlowTypes.AssetPack = {
+  flow_type: "asset_pack",
+  flow_name: "core_assets",
+  rows: [
+    {
+      id: "audio/test_audio.mp3",
+      md5Checksum: "5ddddf934d2187d084c75b7e27797fae",
+      size_kb: 43.4,
+    },
+  ],
+  rowsHashmap: {
+    "audio/test_audio.mp3": {
+      id: "audio/test_audio.mp3",
+      md5Checksum: "5ddddf934d2187d084c75b7e27797fae",
+      size_kb: 43.4,
+    },
+  },
+};
+
+/**
+ * Call standalone tests via:
+ * yarn ng test --include src/app/shared/services/remote-asset/remote-asset.service.spec.ts
+ */
 describe("RemoteAssetsService", () => {
   let service: RemoteAssetService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(RemoteAssetService);
   });
 
@@ -15,7 +49,8 @@ describe("RemoteAssetsService", () => {
     expect(service).toBeTruthy();
   });
 
-  it("generates core asset pack from asset contents", () => {
-    // TODO
+  it("generates a core asset pack from asset contents", () => {
+    const coreAssetPack = service["generateCoreAssetPack"](MOCK_ASSETS_CONTENTS_LIST);
+    expect(coreAssetPack).toEqual(MOCK_CORE_ASSET_PACK);
   });
 });
