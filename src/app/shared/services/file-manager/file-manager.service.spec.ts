7acbb7801690183a03681c091504fa00ec8c1959
Johnny McQuade <jfmcquade@hotmail.com>
2024-06-20 12:37:30 +0100

    chore: code tidy

--- src/app/shared/services/file-manager/file-manager.service.spec.ts
+++ src/app/shared/services/file-manager/file-manager.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from "@angular/core/testing";
 
 import { FileManagerService } from "./file-manager.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { Filesystem } from "@capacitor/filesystem";
 
+/**
+ * Call standalone tests via:
+ * yarn ng test --include src/app/shared/services/file-manager/file-manager.service.spec.ts
+ */
 describe("FileManagerService", () => {
   let service: FileManagerService;
+  let filesystemDeleteFileSpy: jasmine.Spy<jasmine.Func>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    filesystemDeleteFileSpy = jasmine.createSpy();
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: Filesystem,
+          useValue: {
+            deleteFile: filesystemDeleteFileSpy,
+          },
+        },
+      ],
+    });
     service = TestBed.inject(FileManagerService);
   });
 
   it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  // TODO: Mock Filesystem not working
+  it("can delete a file", async () => {
+    const MOCK_LOCAL_FILEPATH =
+      "file:///data/user/0/international.idems.debug_app/cache/debug/images/example/jasper_1.jpg";
+    await service.deleteFile(MOCK_LOCAL_FILEPATH);
+    expect(filesystemDeleteFileSpy).toHaveBeenCalledWith({ path: MOCK_LOCAL_FILEPATH });
   });
 });
