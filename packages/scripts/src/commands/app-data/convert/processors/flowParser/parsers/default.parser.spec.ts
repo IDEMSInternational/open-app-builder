cd1b4c5ce7d6eceb4cb2729b26cdb94544287d99
Jody Gyekye <jodyagyekye@gmail.com>
2024-07-10 11:01:35 +0100

    feat: Created test data

--- packages/scripts/src/commands/app-data/convert/processors/flowParser/parsers/default.parser.spec.ts
+++ packages/scripts/src/commands/app-data/convert/processors/flowParser/parsers/default.parser.spec.ts
@@ -1,3 +1,41 @@
+import chalk from "chalk";
+import { FlowTypes } from "data-models";
+import { ActiveDeployment } from "../../../../../deployment/get";
+import { FlowParserProcessor } from "../flowParser";
+import { DefaultParser } from "./default.parser";
+
+// class MockFlowParserProcessor extends FlowParserProcessor {
+//   constructor() {
+//     super({} as ActiveDeployment);
+//   }
+// }
+
 describe("default Parser", () => {
-  it("should be able to remove whitespace", () => expect(true).toEqual(true));
+ 
+});
+
+describe("RowProcessor", () => {
+  let flow: FlowTypes.FlowTypeWithData;
+  let parser: DefaultParser;
+
+  beforeEach(() => {
+    parser = new DefaultParser(new MockFlowParserProcessor());
+    const mockRow = {
+      type: 'test', 
+      field1: '  value with spaces  ', // leading and trailing whitespace
+      field2: '\n\nvalue with line breaks\n\n', // whitespace and newline
+      field3: '   \n\n   ', // whitespace and newline
+      field4: '\n\r', // newline and carriage return
+      field5: '\r', // carriage return
+      field6: '\t', // tab
+      field7: '\r\rvalue with carriage returns' // carriage returns
+    };
+
+
+  });
+
+  // it should define first check if the row is just whitespace
+  it("should remove any whitespace within a row", () => {
+
+  });
 });
