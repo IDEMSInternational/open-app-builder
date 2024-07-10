1ff3cc759195ee1483c90147d7971f16eda91942
Jody Gyekye <jodyagyekye@gmail.com>
2024-07-10 12:43:08 +0100

    feat: restart of writing the test script

--- packages/scripts/src/commands/app-data/convert/processors/flowParser/parsers/default.parser.spec.ts
+++ packages/scripts/src/commands/app-data/convert/processors/flowParser/parsers/default.parser.spec.ts
@@ -4,39 +4,7 @@ import { ActiveDeployment } from "../../../../../deployment/get";
 import { FlowParserProcessor } from "../flowParser";
 import { DefaultParser } from "./default.parser";
 
-// class MockFlowParserProcessor extends FlowParserProcessor {
-//   constructor() {
-//     super({} as ActiveDeployment);
-//   }
-// }
-
 describe("default Parser", () => {
  
 });
 
-describe("RowProcessor", () => {
-  let flow: FlowTypes.FlowTypeWithData;
-  let parser: DefaultParser;
-
-  beforeEach(() => {
-    parser = new DefaultParser(new FlowParserProcessor());
-    const mockRow = {
-      type: 'test', 
-      field1: '  value with spaces  ', // leading and trailing whitespace
-      field2: '\n\nvalue with line breaks\n\n', // whitespace and newline
-      field3: '   \n\n   ', // whitespace and newline
-      field4: '\n\r', // newline and carriage return
-      field5: '\r', // carriage return
-      field6: '\t', // tab
-      field7: '\r\rvalue with carriage returns' // carriage returns
-    };
-
-    const rowProcessor = new RowProcessor(mockRow, this, undefined);
-    
-});
-
-  // it should define first check if the row is just whitespace
-  it("should remove any whitespace within a row", () => {
-
-  });
-});
