bb83ba46c833f98d25e9b5f6f055fa4251df17f2
Johnny McQuade <jfmcquade@hotmail.com>
2024-07-17 15:42:45 +0100

    feat: new display-group variant, 'speech_bubble'

--- src/app/shared/components/template/components/layout/display-group/display-group.component.ts
+++ src/app/shared/components/template/components/layout/display-group/display-group.component.ts
@@ -4,11 +4,15 @@ import { getNumberParamFromTemplateRow, getStringParamFromTemplateRow } from "..
 
 interface IDisplayGroupParams {
   /** TEMPLATE PARAMETER: "variant" */
-  variant: "box_gray" | "box_primary" | "box_secondary";
+  variant: "box_gray" | "box_primary" | "box_secondary" | "dashed_box" | "speech_bubble";
   /** TEMPLATE PARAMETER: "style". TODO: Various additional legacy styles, review and convert some to variants */
-  style: "form" | "dashed_box" | "default" | string | null;
+  style: "form" | "default" | string | null;
   /** TEMPLATE PARAMETER: "offset". Add a custom bottom margin */
   offset: number;
+  /** TEMPLATE PARAMETER: "speaker_image_asset". The path to an image to be used as the speaker for the 'speech_bubble' variant */
+  speakerImageAsset: string;
+  /** TEMPLATE PARAMETER: "speaker_position". The position of the speaker image and speech bubble tail for the 'speech_bubble' variant. Default 'left' */
+  speakerPosition: "left" | "right";
 }
 
 @Component({
@@ -34,15 +38,25 @@ export class TmplDisplayGroupComponent extends TemplateBaseComponent implements 
     this.params.offset = getNumberParamFromTemplateRow(this._row, "offset", 0);
     this.params.variant = getStringParamFromTemplateRow(this._row, "variant", "")
       .split(",")
-      .join(" ") as IDisplayGroupParams["variant"];
-    this.type = this.getTypeFromStyles(this.params.style || "");
+      .join(" ")
+      .concat(" " + this.params.style) as IDisplayGroupParams["variant"];
+    this.params.speakerImageAsset = getStringParamFromTemplateRow(
+      this._row,
+      "speaker_image_asset",
+      ""
+    );
+    this.params.speakerPosition = getStringParamFromTemplateRow(
+      this._row,
+      "speaker_position",
+      "left"
+    ) as "left" | "right";
+    this.type = this.getTypeFromStyles();
   }
 
-  private getTypeFromStyles(styles: string) {
-    if (styles) {
-      if (styles.includes("form")) return "form";
-      if (styles.includes("dashed_box")) return "dashed_box";
-    }
+  private getTypeFromStyles() {
+    if (this.params.style?.includes("form") || this.params.variant?.includes("form")) return "form";
+    if (this.params.style?.includes("dashed_box") || this.params.variant?.includes("dashed_box"))
+      return "dashed_box";
     return "default";
   }
 }
