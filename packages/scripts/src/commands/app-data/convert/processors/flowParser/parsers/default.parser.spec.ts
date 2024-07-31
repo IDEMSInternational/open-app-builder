31070e79967c35da094b17cdbdef2733ba604ca0
Jody Gyekye <jodyagyekye@gmail.com>
2024-07-31 16:55:59 +0100

    chore: Completed test script for default parser.

--- packages/scripts/src/commands/app-data/convert/processors/flowParser/parsers/default.parser.spec.ts
+++ packages/scripts/src/commands/app-data/convert/processors/flowParser/parsers/default.parser.spec.ts
@@ -5,9 +5,8 @@ const testInputSources = {
   data_list: { test_data_list: [{ id: 1 }, { id: 2 }, { id: 3 }] },
 };
 
-
 describe("Default Parser", () => {
-  it('Cleans field values - removes whitespace within empty strings', () => {
+  it("Cleans field values - removes whitespace within empty strings", () => {
     const parser = new DefaultParser({ processedFlowHashmap: testInputSources } as any);
     const output = parser.run({
       flow_name: "test_flow_input_empty",
@@ -40,38 +39,38 @@ describe("Default Parser", () => {
           value: "\r\n\n\r",
         },
       ],
-    }) as FlowTypes.FlowTypeWithData; 
+    }) as FlowTypes.FlowTypeWithData;
     // not possible to use the actual objects as the function used is not exported
     expect(output.rows).toEqual([
-        {
-          type: "text",
-          name: "empty",
-          value: "",
-        },
-        {
-          type: "text",
-          name: "tab_empty",
-          value: "",
-        },
-        {
-          type: "text",
-          name: "newline_empty",
-          value: "",
-        },
-        {
-          type: "text",
-          name: "return_empty",
-          value: "",
-        },
-        {
-          type: "text",
-          name: "newline_return_empty",
-          value: "",
-        },
-      ]);
+      {
+        type: "text",
+        name: "empty",
+        value: "",
+      },
+      {
+        type: "text",
+        name: "tab_empty",
+        value: "",
+      },
+      {
+        type: "text",
+        name: "newline_empty",
+        value: "",
+      },
+      {
+        type: "text",
+        name: "return_empty",
+        value: "",
+      },
+      {
+        type: "text",
+        name: "newline_return_empty",
+        value: "",
+      },
+    ]);
   });
 
-  it('Cleans field values - Replaces string consisting of only line breaks and new lines', () => {
+  it("Cleans field values - Replaces string consisting of only line breaks and new lines", () => {
     const parser = new DefaultParser({ processedFlowHashmap: testInputSources } as any);
     const output = parser.run({
       flow_name: "test_flow_input_nonempty",
@@ -96,14 +95,14 @@ describe("Default Parser", () => {
         {
           type: "text",
           name: "newline_return_not_empty",
-          value: "Cats are cool\rDogs are great\nPersonally...\nI prefer hamsters\r",
+          value: "Hello,\rI like dogs...\but not cats at all!\r",
         },
         {
           type: "text",
           name: "return_not_empty",
           value: "\tThis is a test string with spaces and tabs\t",
         },
-      ]
+      ],
     }) as FlowTypes.FlowTypeWithData;
     expect(output.rows).toEqual([
       {
@@ -124,7 +123,7 @@ describe("Default Parser", () => {
       {
         type: "text",
         name: "newline_return_not_empty",
-        value: "Cats are cool\rDogs are great\nPersonally...\nI prefer hamsters\r",
+        value: "Hello,\rI like dogs...\but not cats at all!",
       },
       {
         type: "text",
