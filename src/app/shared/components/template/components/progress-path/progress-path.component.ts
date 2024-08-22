fa704a4ab643f1c8fc455f9e58761bf9c3fdb1ca
Johnny McQuade <jfmcquade@hotmail.com>
2024-08-22 15:23:06 +0100

    refactor: path is now drawn using inline svg, with colour and stroke wifth parameters exposed to themes

--- src/app/shared/components/template/components/progress-path/progress-path.component.ts
+++ src/app/shared/components/template/components/progress-path/progress-path.component.ts
@@ -1,28 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { TemplateBaseComponent } from "../base";
-import { getStringParamFromTemplateRow } from "src/app/shared/utils";
 
-interface IProgressPathParams {
-  /** TEMPLATE PARAMETER: path_segment_asset */
-  pathSegmentAsset: string;
-}
 @Component({
   selector: "plh-progress-path",
   templateUrl: "./progress-path.component.html",
   styleUrls: ["./progress-path.component.scss"],
 })
-export class TmplProgressPathComponent extends TemplateBaseComponent implements OnInit {
-  params: Partial<IProgressPathParams> = {};
-
-  ngOnInit() {
-    this.getParams();
-  }
-
-  getParams() {
-    this.params.pathSegmentAsset = getStringParamFromTemplateRow(
-      this._row,
-      "path_segment_asset",
-      ""
-    );
-  }
-}
+export class TmplProgressPathComponent extends TemplateBaseComponent {}
