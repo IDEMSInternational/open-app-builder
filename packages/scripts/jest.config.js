f2a9fe60a04b56062117845391eae223a03dbfeb
chrismclarke <chris.m.clarke@live.co.uk>
2024-08-24 15:17:25 -0700

    refactor: global test setup

--- packages/scripts/jest.config.js
+++ packages/scripts/jest.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   testEnvironment: "node",
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    "^.+.tsx?$": ["ts-jest", {}],
   },
+  globalSetup: "<rootDir>/test/setup.ts",
 };
+
