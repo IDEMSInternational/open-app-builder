486f8ab106be9546b9488ca27e8090cdadcb1f75
chrismclarke <chris.m.clarke@live.co.uk>
2024-08-25 13:53:16 -0700

    chore: fix broken tests

--- packages/scripts/src/commands/app-data/postProcess/sheets.spec.ts
+++ packages/scripts/src/commands/app-data/postProcess/sheets.spec.ts
@@ -1,9 +1,6 @@
 import { AssetsPostProcessor } from "./assets";
 import type { IDeploymentConfigJson } from "../../deployment/common";
 
-import fs from "fs-extra";
-import mockFs from "mock-fs";
-
 // Use default imports to allow spying on functions and replacing with mock methods
 import { ActiveDeployment } from "../../deployment/get";
 import path from "path";
@@ -17,3 +14,7 @@ const mockDirs = {
 };
 
 // TODO
+/** yarn workspace scripts test -t sheets.spec.ts */
+describe.skip("PostProcess Sheets", () => {
+  it.skip("TODO", () => {});
+});
