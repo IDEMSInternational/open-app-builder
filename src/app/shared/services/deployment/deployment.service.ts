63972aed1a65fe5c68b10f8f20bdccdc6507ba90
chrismclarke <chris.m.clarke@live.co.uk>
2024-09-12 23:45:18 -0700

    chore: code tidying

--- src/app/shared/services/deployment/deployment.service.ts
+++ src/app/shared/services/deployment/deployment.service.ts
@@ -6,23 +6,26 @@ import { SyncServiceBase } from "../syncService.base";
  * Token to inject deployment config value into any service.
  * This is populated from json file before platform load, as part of src\main.ts
  *
- * Can be used directly in the constructor via `@Inject(DEPLOYMENT_CONFIG)`,
- * or values accessed from the DeploymentService
+ * Can be used directly by any service or module initialised at any time
+ * (including app.module.ts).
+ *
+ * @example Inject into service
+ * ```ts
+ * constructor(@Inject(DEPLOYMENT_CONFIG))
+ * ```
+ * @example Inject into module
+ * ```
+ * {provide: MyModule, useFactory:(config)=>{...}, deps: [DEPLOYMENT_CONFIG]`}
+ * ```
  */
 export const DEPLOYMENT_CONFIG: InjectionToken<IDeploymentRuntimeConfig> =
   new InjectionToken<IDeploymentRuntimeConfig>("Application Configuration");
 
-@Injectable({ providedIn: "root" })
 /**
- * Deployment runtime config settings
- *
- * NOTE - this is intialized using an `APP_INITIALIZER` token within
- * the main app.module.ts and will block all other services from loading until
- * it is fully initialised
- *
- * Services that access the deployment config therefore do not need to await
- * DeploymentService init/ready methods.
+ * The deployment service provides access to values loaded from the deployment json file
+ * It is an alternative to injecting directly via `@Inject(DEPLOYMENT_CONFIG)`
  */
+@Injectable({ providedIn: "root" })
 export class DeploymentService extends SyncServiceBase {
   constructor(@Inject(DEPLOYMENT_CONFIG) public readonly config: IDeploymentRuntimeConfig) {
     super("Deployment Service");
