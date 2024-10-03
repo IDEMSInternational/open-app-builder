7ac4aa68424cd99519f8620c802585df937a2905
chrismclarke <chris.m.clarke@live.co.uk>
2024-10-02 21:40:55 -0700

    feat: add appStringEvaluator support for any input data types

--- packages/shared/src/models/appStringEvaluator/appStringEvaluator.ts
+++ packages/shared/src/models/appStringEvaluator/appStringEvaluator.ts
@@ -1,9 +1,10 @@
 import { JSEvaluator } from "../jsEvaluator/jsEvaluator";
 import { TemplatedData } from "..";
+import { isObjectLiteral } from "../../utils/object-utils";
 
 /**
  * Utility class to allow evaluation of strings that contain a mix of context expressions,
- * JavaScript and static strings
+ * JavaScript and static strings. It combines both TemplatedData and jsEvaluator models
  *
  * @example
  * ```
@@ -31,6 +32,37 @@ export class AppStringEvaluator {
   setExecutionContext(context: any) {
     this.parser = new TemplatedData({ context });
   }
+
+  /**
+   * Evaluate app expression in two stages
+   *  1) Replace any instances of context variables with values
+   * e.g. `@row.number_1 > @row.number_2 ? '1 is greater' : '2 is greater'
+   * ->   `1 > 2 ? '1 is greater' : '2 is greater'
+   *
+   * 2) Attempt evaluation in a JS context
+   * -> '2 is greater'
+   *
+   * @param data Input data to evaluated. This can be any data type, including
+   * nested objects or arrays. All string entries within will be evaulated
+   */
+  evaluate(data: any) {
+    if (typeof data === "string") {
+      return this.evaluateString(data);
+    }
+    if (Array.isArray(data)) {
+      return data.map((el) => this.evaluate(el));
+    }
+    if (isObjectLiteral(data)) {
+      // create a new object to avoid changing initial data
+      const evaluated = {};
+      for (const [key, value] of Object.entries(data)) {
+        evaluated[key] = this.evaluate(value);
+      }
+      return evaluated;
+    }
+    return data;
+  }
+
   /**
    * Evaluate app string expression in two stages
    * 1) Replace any instances of context variables with values
@@ -41,7 +73,7 @@ export class AppStringEvaluator {
    * -> '2 is greater'
    * @returns
    */
-  evaluate(expression: string) {
+  private evaluateString(expression: string) {
     const parsed = this.parser.parse(expression);
     try {
       const evaluated = this.evaluator.evaluate(parsed);
