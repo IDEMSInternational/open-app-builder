2f2fd3f403faf05b00dc35df6d013ff7863b0b34
Johnny McQuade <jfmcquade@hotmail.com>
2024-10-09 16:35:09 +0100

    chore: pop-up used for text only now renders text inside a text component instance

--- src/app/shared/components/template/components/text.ts
+++ src/app/shared/components/template/components/text.ts
@@ -1,37 +1,55 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { TemplateBaseComponent } from "./base";
 import { getStringParamFromTemplateRow } from "../../../utils";
 
-@Component({
-  selector: "plh-tmpl-text",
-  template: `<div
-    *ngIf="_row && _row.value"
-    class="large standard normal margin-t-large"
-    [class]="style"
-    [ngStyle]="isFalsy ? { display: 'none' } : { display: 'block' }"
-    [innerHTML]="type === 'numbered' ? (_row.value | number) : (_row.value?.toString() | markdown)"
-    [style]="_row.style_list | styleList"
-    [style.textAlign]="textAlign"
-  ></div> `,
-  styleUrls: ["./tmpl-components-common.scss"],
-})
-export class TmplTextComponent extends TemplateBaseComponent implements OnInit {
+interface ITextParams {
+  text: string;
   textAlign: string | null;
   style: string | null;
   type: string;
   isFalsy: boolean;
+}
+@Component({
+  selector: "plh-tmpl-text",
+  template: `<div
+    *ngIf="params.text"
+    class="large standard normal margin-t-large"
+    [class]="params.style"
+    [ngStyle]="params.isFalsy ? { display: 'none' } : { display: 'block' }"
+    [innerHTML]="
+      params.type === 'numbered' ? (params.text | number) : (params.text?.toString() | markdown)
+    "
+    [style]="_row?.style_list | styleList"
+    [style.textAlign]="params.textAlign"
+  ></div> `,
+  styleUrls: ["./tmpl-components-common.scss"],
+})
+export class TmplTextComponent extends TemplateBaseComponent implements OnInit {
+  @Input() textInputParams: Partial<ITextParams>;
+  params: Partial<ITextParams> = {};
+
   constructor() {
     super();
   }
 
   ngOnInit() {
-    this.getParams();
+    // If instantiated from a template, get params from template row
+    if (this._row) {
+      this.getParams();
+    }
+    // Else if instantiated from a parent component, get params from Input()
+    else if (this.textInputParams) {
+      this.params = this.textInputParams;
+    }
   }
 
   getParams() {
-    this.isFalsy = ["undefined", "NaN", "null", '""'].includes(this._row.value);
-    this.textAlign = getStringParamFromTemplateRow(this._row, "text_align", null);
-    this.type = this._row.parameter_list?.style?.includes("numbered") ? "numbered" : "marked";
-    this.style = getStringParamFromTemplateRow(this._row, "style", null);
+    this.params.text = this._row.value;
+    this.params.isFalsy = ["undefined", "NaN", "null", '""'].includes(this.params.text);
+    this.params.textAlign = getStringParamFromTemplateRow(this._row, "text_align", null);
+    this.params.type = this._row.parameter_list?.style?.includes("numbered")
+      ? "numbered"
+      : "marked";
+    this.params.style = getStringParamFromTemplateRow(this._row, "style", null);
   }
 }
