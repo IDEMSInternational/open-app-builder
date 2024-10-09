66587023bdae1b0ec3fe6c99bc46b79b8e1b2a39
Johnny McQuade <jfmcquade@hotmail.com>
2024-10-09 17:35:11 +0100

    chore: revert changes to text component, instantiate in popup using ad-hoc row values instead

--- src/app/shared/components/template/components/text.ts
+++ src/app/shared/components/template/components/text.ts
@@ -1,55 +1,37 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { TemplateBaseComponent } from "./base";
 import { getStringParamFromTemplateRow } from "../../../utils";
 
-interface ITextParams {
-  text: string;
-  textAlign: string | null;
-  style: string | null;
-  type: string;
-  isFalsy: boolean;
-}
 @Component({
   selector: "plh-tmpl-text",
   template: `<div
-    *ngIf="params.text"
+    *ngIf="_row && _row.value"
     class="large standard normal margin-t-large"
-    [class]="params.style"
-    [ngStyle]="params.isFalsy ? { display: 'none' } : { display: 'block' }"
-    [innerHTML]="
-      params.type === 'numbered' ? (params.text | number) : (params.text?.toString() | markdown)
-    "
-    [style]="_row?.style_list | styleList"
-    [style.textAlign]="params.textAlign"
+    [class]="style"
+    [ngStyle]="isFalsy ? { display: 'none' } : { display: 'block' }"
+    [innerHTML]="type === 'numbered' ? (_row.value | number) : (_row.value?.toString() | markdown)"
+    [style]="_row.style_list | styleList"
+    [style.textAlign]="textAlign"
   ></div> `,
   styleUrls: ["./tmpl-components-common.scss"],
 })
 export class TmplTextComponent extends TemplateBaseComponent implements OnInit {
-  @Input() textInputParams: Partial<ITextParams>;
-  params: Partial<ITextParams> = {};
-
+  textAlign: string | null;
+  style: string | null;
+  type: string;
+  isFalsy: boolean;
   constructor() {
     super();
   }
 
   ngOnInit() {
-    // If instantiated from a template, get params from template row
-    if (this._row) {
-      this.getParams();
-    }
-    // Else if instantiated from a parent component, get params from Input()
-    else if (this.textInputParams) {
-      this.params = this.textInputParams;
-    }
+    this.getParams();
   }
 
   getParams() {
-    this.params.text = this._row.value;
-    this.params.isFalsy = ["undefined", "NaN", "null", '""'].includes(this.params.text);
-    this.params.textAlign = getStringParamFromTemplateRow(this._row, "text_align", null);
-    this.params.type = this._row.parameter_list?.style?.includes("numbered")
-      ? "numbered"
-      : "marked";
-    this.params.style = getStringParamFromTemplateRow(this._row, "style", null);
+    this.isFalsy = ["undefined", "NaN", "null", '""'].includes(this._row.value);
+    this.textAlign = getStringParamFromTemplateRow(this._row, "text_align", null);
+    this.type = this._row.parameter_list?.style?.includes("numbered") ? "numbered" : "marked";
+    this.style = getStringParamFromTemplateRow(this._row, "style", null);
   }
 }
