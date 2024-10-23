b89a5cbf45f4a6116691bac82dcf077e50fc1b2b
Johnny McQuade <jfmcquade@hotmail.com>
2024-10-23 15:07:55 +0100

    chore: improve basic demo component

--- packages/components/demo/example/example.component.spec.ts
+++ packages/components/demo/example/example.component.spec.ts
@@ -3,6 +3,12 @@ import { IonicModule } from "@ionic/angular";
 
 import { TmplExampleComponent } from "./example.component";
 
+/**
+ * Call standalone tests via:
+ * yarn ng test --include packages/components/demo/example/example.component.spec.ts
+ *
+ * TODO: pass a mock value for `_row` so that we can test against how the component would normally be mounted
+ */
 describe("TmplExampleComponent", () => {
   let component: TmplExampleComponent;
   let fixture: ComponentFixture<TmplExampleComponent>;
