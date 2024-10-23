476cebc2160b34b0d0c2f592af5300021c2301a7
Johnny McQuade <jfmcquade@hotmail.com>
2024-10-23 15:07:55 +0100

    chore: improve basic demo component

--- packages/components/demo/example/example.component.ts
+++ packages/components/demo/example/example.component.ts
@@ -6,8 +6,8 @@ import {
 } from "src/app/shared/utils";
 
 interface IExampleComponentParams {
-  /** TEMPLATE_PARAMETER: "variant". Default "red_square" */
-  variant: "red_square" | "yellow_circle";
+  /** TEMPLATE_PARAMETER: "variant". Default "square_primary" */
+  variant: "square_primary" | "circle_secondary";
   /** TEMPLATE_PARAMETER: "my_boolean_param". A paramater that takes a boolean value. Default true */
   myBooleanParam: boolean;
   /** TEMPLATE_PARAMETER: "my_string_param". A string to be displayed */
@@ -28,7 +28,7 @@ export class TmplExampleComponent extends TemplateBaseComponent implements OnIni
 
   // Parse parameters into variables from the template row's `parameter_list`
   private getParams() {
-    this.params.variant = getStringParamFromTemplateRow(this._row, "variant", "red_square")
+    this.params.variant = getStringParamFromTemplateRow(this._row, "variant", "square_primary")
       .split(",")
       .join(" ") as IExampleComponentParams["variant"];
     this.params.myBooleanParam = getBooleanParamFromTemplateRow(
