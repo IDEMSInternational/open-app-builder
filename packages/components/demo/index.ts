117a84bae189ef613df3210e8b32ed78f4abd502
Johnny McQuade <jfmcquade@hotmail.com>
2024-10-23 15:15:24 +0100

    chore: rename component 'example' -> 'demo_basic'

--- packages/components/demo/index.ts
+++ packages/components/demo/index.ts
@@ -1,17 +1,17 @@
 import { NgModule, Type } from "@angular/core";
 
-import { TmplExampleComponent } from "./example/example.component";
+import { DemoBasicComponent } from "./basic/basic.component";
 import type { ITemplateRowProps } from "src/app/shared/components/template/models";
 import { CommonModule } from "@angular/common";
 import { IonicModule } from "@ionic/angular";
 
 @NgModule({
   imports: [CommonModule, IonicModule],
-  exports: [TmplExampleComponent],
-  declarations: [TmplExampleComponent],
+  exports: [DemoBasicComponent],
+  declarations: [DemoBasicComponent],
 })
 export class DemoComponentsModule {}
 
 export const DEMO_COMPONENT_MAPPING: Record<string, Type<ITemplateRowProps>> = {
-  example: TmplExampleComponent,
+  demo_basic: DemoBasicComponent,
 };
