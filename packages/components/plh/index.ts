7efa4dcdcd2faad45b8788c68e7a05c3d4e7bd77
Johnny McQuade <64838927+jfmcquade@users.noreply.github.com>
2024-10-28 12:50:55 +0000

    Revert "chore: move plh-specific components to plh components folder"

--- packages/components/plh/index.ts
+++ packages/components/plh/index.ts
@@ -3,20 +3,12 @@ import { NgModule, Type } from "@angular/core";
 import type { ITemplateRowProps } from "src/app/shared/components/template/models";
 import { CommonModule } from "@angular/common";
 import { IonicModule } from "@ionic/angular";
-import { TemplatePipesModule } from "src/app/shared/components/template/pipes";
-import { LottieModule } from "ngx-lottie";
-import { PlhParentPointCounterComponent } from "./parent-point-counter/parent-point-counter.component";
-import { PlhParentPointBoxComponent } from "./parent-point-box/parent-point-box.component";
 
 @NgModule({
-  imports: [CommonModule, IonicModule, TemplatePipesModule, LottieModule],
-  exports: [PlhParentPointCounterComponent, PlhParentPointBoxComponent],
-  declarations: [PlhParentPointCounterComponent, PlhParentPointBoxComponent],
-  providers: [],
+  imports: [CommonModule, IonicModule],
+  exports: [],
+  declarations: [],
 })
 export class PlhComponentsModule {}
 
-export const PLH_COMPONENT_MAPPING: Record<string, Type<ITemplateRowProps>> = {
-  parent_point_counter: PlhParentPointCounterComponent,
-  parent_point_box: PlhParentPointBoxComponent,
-};
+export const PLH_COMPONENT_MAPPING: Record<string, Type<ITemplateRowProps>> = {};
