5ba6f7459ba7683c22ddd22bc9808284fb5b8e60
Johnny McQuade <jfmcquade@hotmail.com>
2024-11-01 09:08:06 +0000

    feat: set screen orientation based on 'landscape' queryParam

--- src/app/shared/services/screen-orientation/screen-orientation.service.ts
+++ src/app/shared/services/screen-orientation/screen-orientation.service.ts
@@ -1,20 +1,41 @@
-import { Injectable } from "@angular/core";
+import { effect, Injectable, WritableSignal } from "@angular/core";
 import { SyncServiceBase } from "../syncService.base";
 import { ScreenOrientation, OrientationLockType } from "@capacitor/screen-orientation";
 import { TemplateActionRegistry } from "../../components/template/services/instance/template-action.registry";
+import { ActivatedRoute } from "@angular/router";
+import { Capacitor } from "@capacitor/core";
+import { distinctUntilChanged, filter, map } from "rxjs";
 
+// Supported orientation types
 const ORIENTATION_TYPES: OrientationLockType[] = ["portrait", "landscape"];
+
+type IOrientationType = (typeof ORIENTATION_TYPES)[number];
 
 @Injectable({
   providedIn: "root",
 })
 export class ScreenOrientationService extends SyncServiceBase {
-  constructor(private templateActionRegistry: TemplateActionRegistry) {
+  private orientation: WritableSignal<IOrientationType>;
+  constructor(
+    private templateActionRegistry: TemplateActionRegistry,
+    private route: ActivatedRoute
+  ) {
     super("Screen Orientation Service");
+    effect(() => {
+      console.log(`[SCREEN ORIENTATION] - Orientation: ${this.orientation()}`);
+      this.setOrientation(this.orientation());
+    });
     this.initialise();
   }
 
-  initialise() {
+  async initialise() {
+    // TODO: also check if any templates actually use screen orientation metadata?
+    // Or maybe have a toggle to enable "landscape_mode" at deployment config level
+    if (Capacitor.isNativePlatform()) {
+      const currentOrientation = await this.getOrientation();
+      this.orientation.set(currentOrientation);
+      this.watchOrientationParam();
+    }
     this.registerTemplateActionHandlers();
   }
 
@@ -33,5 +54,23 @@ export class ScreenOrientationService extends SyncServiceBase {
 
   private async setOrientation(orientation: OrientationLockType) {
     return await ScreenOrientation.lock({ orientation });
+  }
+
+  private async getOrientation() {
+    return (await ScreenOrientation.orientation()).type;
+  }
+
+  private watchOrientationParam() {
+    this.route.queryParamMap
+      .pipe(
+        map((params) =>
+          params.get("landscape") === "true" ? "landscape" : ("portrait" as IOrientationType)
+        ),
+        distinctUntilChanged(),
+        filter((targetOrientation) => targetOrientation !== this.orientation())
+      )
+      .subscribe((targetOrientation: OrientationType) => {
+        this.orientation.set(targetOrientation);
+      });
   }
 }
