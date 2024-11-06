32d8c0c609f5b0612fbaa78c9df5d0e6798dc0c0
FaithDaka <faithbusigu8@gmail.com>
2024-11-06 11:19:22 +0300

    feat: kuwait app layout component

---
+++ packages/components/plh/plh-kids-kw/components/layout/layout.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "plh-layout",
+  templateUrl: "./layout.component.html",
+  styleUrls: ["./layout.component.scss"],
+})
+export class PlhKidsKuwaitLayoutComponent {
+  constructor() {}
+}
