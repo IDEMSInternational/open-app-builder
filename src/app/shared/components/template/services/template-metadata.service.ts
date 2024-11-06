4fa97542c96607031dfc1fafe97351f23acfef4c
Johnny McQuade <jfmcquade@hotmail.com>
2024-11-06 11:50:29 +0000

    chore: code tidy

--- src/app/shared/components/template/services/template-metadata.service.ts
+++ src/app/shared/components/template/services/template-metadata.service.ts
@@ -1,22 +1,22 @@
-import { Injectable, signal, WritableSignal } from "@angular/core";
+import { Injectable, OnDestroy, signal } from "@angular/core";
 import { SyncServiceBase } from "src/app/shared/services/syncService.base";
 import { TemplateService } from "./template.service";
 import { FlowTypes } from "src/app/shared/model";
 import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
-import { distinctUntilChanged, filter, map } from "rxjs";
+import { distinctUntilChanged, filter, map, Subject, takeUntil } from "rxjs";
 import { Capacitor } from "@capacitor/core";
 
-/** Some authored template metadata values should be stored in the url via query params */
-export interface ITemplateMetadataQueryParams {
-  landscape?: boolean;
-}
-
+/**
+ * Service responsible for handling metadata of the current top-level template,
+ * i.e. parameters authored through the template's parameter_list in a contents list
+ */
 @Injectable({
   providedIn: "root",
 })
-export class TemplateMetadataService extends SyncServiceBase {
-  public parameterList: WritableSignal<FlowTypes.Template["parameter_list"]> = signal({});
+export class TemplateMetadataService extends SyncServiceBase implements OnDestroy {
+  public parameterList = signal<FlowTypes.Template["parameter_list"]>({});
   private enabled: boolean;
+  private destroy$ = new Subject();
 
   constructor(
     private templateService: TemplateService,
@@ -26,8 +26,7 @@ export class TemplateMetadataService extends SyncServiceBase {
 
     // Currently the only watched parameter is for screen orientation,
     // which is only supported on native platforms
-    this.enabled = Capacitor.isNativePlatform();
-
+    this.enabled = !Capacitor.isNativePlatform();
     this.initialise();
   }
 
@@ -40,28 +39,38 @@ export class TemplateMetadataService extends SyncServiceBase {
   private watchRouteForTopLevelTemplate() {
     this.router.events
       .pipe(
-        filter((event) => event instanceof NavigationEnd), // Listen for route changes
+        filter((event) => event instanceof NavigationEnd),
         map(() => this.router.routerState.root),
-        map((root) => {
-          let active = root;
-          while (active.firstChild) {
-            active = active.firstChild;
-          }
-          return active.snapshot.params["templateName"]; // Access the parameter here
-        }),
-        distinctUntilChanged()
+        map((root) => this.extractTemplateNameFromRoute(root)),
+        distinctUntilChanged(),
+        takeUntil(this.destroy$)
       )
       .subscribe(async (templateName: string | undefined) => {
-        console.log("*****templateName*****", templateName);
-        if (!templateName) return this.parameterList.set({});
-        try {
-          const parameterList = await this.templateService.getTemplateMetadata(templateName);
-          this.parameterList.set(parameterList || {});
-          console.log("this.parameterList", this.parameterList());
-        } catch (error) {
-          console.error("[TEMPLATE METADATA] Failed to fetch template parameter_list", error);
-          this.parameterList.set({});
-        }
+        await this.updateParameterList(templateName);
       });
+  }
+
+  private extractTemplateNameFromRoute(root: ActivatedRoute): string | undefined {
+    let active = root;
+    while (active.firstChild) {
+      active = active.firstChild;
+    }
+    return active.snapshot.params["templateName"];
+  }
+
+  private async updateParameterList(templateName: string | undefined) {
+    if (!templateName) return this.parameterList.set({});
+    try {
+      const parameterList = await this.templateService.getTemplateMetadata(templateName);
+      this.parameterList.set(parameterList || {});
+    } catch (error) {
+      console.error("[TEMPLATE METADATA] Failed to fetch template parameter_list", error);
+      this.parameterList.set({});
+    }
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.complete();
   }
 }
