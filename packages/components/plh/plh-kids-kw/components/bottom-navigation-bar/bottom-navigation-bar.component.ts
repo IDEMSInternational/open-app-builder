f140a00477c3f2f009e9ecf1638663924d8b79e5
FaithDaka <faithbusigu8@gmail.com>
2024-11-06 19:17:59 +0300

    feat: nav-item routing

--- packages/components/plh/plh-kids-kw/components/bottom-navigation-bar/bottom-navigation-bar.component.ts
+++ packages/components/plh/plh-kids-kw/components/bottom-navigation-bar/bottom-navigation-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { TemplateBaseComponent } from "src/app/shared/components/template/components/base";
 import { getParamFromTemplateRow } from "src/app/shared/utils";
 
@@ -6,12 +6,12 @@ interface IPlhBottomNavigationParams {
   /** TEMPLATE PARAMETER: button_list. A list of nav button items */
   buttonList: INavButton[];
 }
-
 interface INavButton {
   icon: string | null;
   label: string | null;
   target_template: string | null;
   name: string | null;
+  active_icon: string | null;
 }
 @Component({
   selector: "plh-bottom-navigation-bar",
@@ -20,6 +20,7 @@ interface INavButton {
 })
 export class PlhBottomNavigationBarComponent extends TemplateBaseComponent implements OnInit {
   params: Partial<IPlhBottomNavigationParams> = {};
+  isActive = false;
 
   ngOnInit() {
     this.getParams();
@@ -28,5 +29,9 @@ export class PlhBottomNavigationBarComponent extends TemplateBaseComponent imple
   //get template parameters
   getParams() {
     this.params.buttonList = getParamFromTemplateRow(this._row, "button_list", []);
+  }
+
+  toggleIcon() {
+    this.isActive = !this.isActive;
   }
 }
