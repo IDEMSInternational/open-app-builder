e65bc0de21149cd1d6e919655bbd808eb2d52318
FaithDaka <faithbusigu8@gmail.com>
2024-11-14 09:38:35 +0300

    refactor: update variables and html structure

--- packages/components/plh/plh-kids-kw/components/module-list-item/module-list-item.component.ts
+++ packages/components/plh/plh-kids-kw/components/module-list-item/module-list-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FlowTypes } from "packages/data-models";
 import { TemplateBaseComponent } from "src/app/shared/components/template/components/base";
+import { TemplateTranslateService } from "src/app/shared/components/template/services/template-translate.service";
 import {
   getBooleanParamFromTemplateRow,
   getStringParamFromTemplateRow,
@@ -9,12 +10,10 @@ import {
 interface IModuleListItemParams {
   /* TEMPLATE PARAMETER: "module_image_asset". The image attached to the module */
   moduleImageAsset: string | null;
-  /* TEMPLATE PARAMETER: "module_alignment". The alignment of elements within the module item. Default "left" */
-  moduleAlignment: "left" | "right";
   /* TEMPLATE PARAMETER: "text_transform". The format of the text on the module item. Default null */
   textTransform: "capitalise" | "uppercase" | null;
-  /* TEMPLATE PARAMETER: "target_template". Points toward the page to be navigated to */
-  targetTemplate: string | null;
+  /* TEMPLATE PARAMETER: "is_nav_item". Marks an item as navigable  in order to show the navigation image asset. Default "true"  */
+  navItem: boolean;
   /* TEMPLATE PARAMETER: "is_locked". The boolean that marks the module as locked or unlocked */
   isLocked: boolean;
   /* TEMPLATE PARAMETER: "nav_image_asset". The navigation icon*/
@@ -31,6 +30,10 @@ interface IModuleListItemParams {
 export class PlhModuleListItemComponent extends TemplateBaseComponent implements OnInit {
   params: Partial<IModuleListItemParams> = {};
 
+  constructor(public templateTranslateService: TemplateTranslateService) {
+    super();
+  }
+
   ngOnInit() {
     this.getParams();
   }
@@ -41,15 +44,10 @@ export class PlhModuleListItemComponent extends TemplateBaseComponent implements
       "module_image_asset",
       ""
     );
-    this.params.moduleAlignment = getStringParamFromTemplateRow(
-      this._row,
-      "module_alignment",
-      "left"
-    ) as "left" | "right";
     this.params.textTransform = getStringParamFromTemplateRow(this._row, "text_transform", null) as
       | "capitalise"
       | "uppercase";
-    this.params.targetTemplate = getStringParamFromTemplateRow(this._row, "target_template", null);
+    this.params.navItem = getBooleanParamFromTemplateRow(this._row, "is_nav_item", true);
     this.params.isLocked = getBooleanParamFromTemplateRow(this._row, "is_locked", false);
     this.params.navImageAsset = getStringParamFromTemplateRow(this._row, "nav_image_asset", null);
     this.params.lockedImageAsset = getStringParamFromTemplateRow(
