28fc2201a8b8f91f7d14dc60e2cd95d86755c309
chrismclarke <chris.m.clarke@live.co.uk>
2024-11-21 14:04:23 -0800

    chore: code tidying

--- packages/@idemsInternational/gdrive-tools/src/utils/auth.utils.ts
+++ packages/@idemsInternational/gdrive-tools/src/utils/auth.utils.ts
@@ -1,8 +1,11 @@
-import type { IAuthorizeOptions } from '../types';
-import {AUTH_CREDENTIALS_PATH,AUTH_TOKEN_PATH} from '../paths'
+import type { IAuthorizeOptions } from "../types";
+import { AUTH_CREDENTIALS_PATH, AUTH_TOKEN_PATH } from "../paths";
 
-/** Small utility to return default auth token and credential paths when not provided */
-export function getAuthPaths(options:IAuthorizeOptions){
-    const {authTokenPath=AUTH_TOKEN_PATH, credentialsPath=AUTH_CREDENTIALS_PATH} = options
-    return {authTokenPath,credentialsPath}
+/**
+ * Small utility to extract common auth options and return the token and credential
+ * paths, using fallback defaults if not provided
+ */
+export function getAuthPaths(options: IAuthorizeOptions) {
+  const { authTokenPath = AUTH_TOKEN_PATH, credentialsPath = AUTH_CREDENTIALS_PATH } = options;
+  return { authTokenPath, credentialsPath };
 }
