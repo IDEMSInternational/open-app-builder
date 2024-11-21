02496246a9c3518501dcda3c9bbcb5d5176fb0ef
chrismclarke <chris.m.clarke@live.co.uk>
2024-11-21 13:28:22 -0800

    chore: update paths

--- packages/@idemsInternational/gdrive-tools/src/paths.ts
+++ packages/@idemsInternational/gdrive-tools/src/paths.ts
@@ -2,11 +2,7 @@ import path from "path";
 
 const PACKAGE_ROOT_DIR = path.join(__dirname, "../");
 
-export const PATHS = {
-  PACKAGE_ROOT_DIR,
-  DEFAULT_OUTPUT_FOLDER: path.join(PACKAGE_ROOT_DIR, "output"),
-  DEFAULT_CACHE_FOLDER: path.join(PACKAGE_ROOT_DIR, "cache"),
-  DEFAULT_CREDENTIALS: path.join(PACKAGE_ROOT_DIR, "credentials.dev.json"),
-  DEFAULT_TOKEN: path.join(PACKAGE_ROOT_DIR, "token.json"),
-  LOGS_DIR: path.join(PACKAGE_ROOT_DIR, "logs"),
-};
+export const AUTH_CREDENTIALS_PATH = path.join(PACKAGE_ROOT_DIR, "credentials.dev.json");
+export const AUTH_TOKEN_PATH = path.join(PACKAGE_ROOT_DIR, "token.json");
+export const GDRIVE_LOGS_DIR = path.join(PACKAGE_ROOT_DIR, "logs");
+export const GDRIVE_OUTPUT_FOLDER = path.join(PACKAGE_ROOT_DIR, "output");
