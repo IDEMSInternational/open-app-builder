24d83a631ae0a37296d34b6be4d250806dd4494f
FaithDaka <faithbusigu8@gmail.com>
2024-11-22 08:06:57 +0300

    init: header component

--- packages/components/plh/index.ts
+++ packages/components/plh/index.ts
@@ -7,11 +7,20 @@ import { TemplatePipesModule } from "src/app/shared/components/template/pipes";
 import { LottieModule } from "ngx-lottie";
 import { PlhParentPointCounterComponent } from "./parent-point-counter/parent-point-counter.component";
 import { PlhParentPointBoxComponent } from "./parent-point-box/parent-point-box.component";
+import { PlhModuleDetailsHeaderComponent } from "./plh-kids-kw/components/module-details-header/module-details-header.component";
 
 @NgModule({
   imports: [CommonModule, IonicModule, TemplatePipesModule, LottieModule],
-  exports: [PlhParentPointCounterComponent, PlhParentPointBoxComponent],
-  declarations: [PlhParentPointCounterComponent, PlhParentPointBoxComponent],
+  exports: [
+    PlhParentPointCounterComponent,
+    PlhParentPointBoxComponent,
+    PlhModuleDetailsHeaderComponent,
+  ],
+  declarations: [
+    PlhParentPointCounterComponent,
+    PlhParentPointBoxComponent,
+    PlhModuleDetailsHeaderComponent,
+  ],
   providers: [],
 })
 export class PlhComponentsModule {}
@@ -19,4 +28,5 @@ export class PlhComponentsModule {}
 export const PLH_COMPONENT_MAPPING: Record<string, Type<ITemplateRowProps>> = {
   parent_point_counter: PlhParentPointCounterComponent,
   parent_point_box: PlhParentPointBoxComponent,
+  plh_module_details_header: PlhModuleDetailsHeaderComponent,
 };
