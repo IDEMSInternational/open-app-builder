5e4970903391d7cf5b48d7551925885e1290ba69
chrismclarke <chris.m.clarke@live.co.uk>
2024-11-21 16:57:26 -0800

    fix: metadata row slice

--- packages/scripts/src/commands/app-data/convert/processors/flowParser/flowParser.ts
+++ packages/scripts/src/commands/app-data/convert/processors/flowParser/flowParser.ts
@@ -4,7 +4,7 @@ import { IConverterPaths, IFlowHashmapByType, IParsedWorkbookData } from "../../
 import { arrayToHashmap, groupJsonByKey, IContentsEntry } from "../../utils";
 import BaseProcessor from "../base";
 
-const cacheVersion = 20241121.0;
+const cacheVersion = 20241121.1;
 
 export class FlowParserProcessor extends BaseProcessor<FlowTypes.FlowTypeWithData> {
   public parsers: { [flowType in FlowTypes.FlowType]: Parsers.DefaultParser } = {
