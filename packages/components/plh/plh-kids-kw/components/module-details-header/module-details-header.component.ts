6652e07225fa5d4e6584ce4ba4de3373284bc2cf
FaithDaka <faithbusigu8@gmail.com>
2024-11-22 18:32:27 +0300

    feat: header params

--- packages/components/plh/plh-kids-kw/components/module-details-header/module-details-header.component.ts
+++ packages/components/plh/plh-kids-kw/components/module-details-header/module-details-header.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from "@angular/core";
 import { TemplateBaseComponent } from "src/app/shared/components/template/components/base";
+import { TemplateTranslateService } from "src/app/shared/components/template/services/template-translate.service";
+import { getParamFromTemplateRow, getStringParamFromTemplateRow } from "src/app/shared/utils";
 
+interface IModuleHeaderParams {
+  bgImage?: string;
+  moduleImageAsset?: string;
+}
 @Component({
   selector: "plh-module-details-header",
   templateUrl: "./module-details-header.component.html",
   styleUrls: ["./module-details-header.component.scss"],
 })
 export class PlhModuleDetailsHeaderComponent extends TemplateBaseComponent implements OnInit {
-  ngOnInit() {}
+  params: Partial<IModuleHeaderParams> = {};
+
+  constructor(public templateTranslateService: TemplateTranslateService) {
+    super();
+  }
+
+  ngOnInit() {
+    this.getParams();
+  }
+
+  private getParams() {
+    this.params.bgImage = getStringParamFromTemplateRow(this._row, "background_illustration", null);
+    this.params.moduleImageAsset = getStringParamFromTemplateRow(
+      this._row,
+      "module_image_asset",
+      null
+    );
+  }
 }
