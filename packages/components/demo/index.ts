538ad90598c8c9e3d578326e02b3956fa4165222
Johnny McQuade <jfmcquade@hotmail.com>
2024-11-25 13:11:43 +0000

    hotfix: plh components loading from external package

--- packages/components/demo/index.ts
+++ packages/components/demo/index.ts
@@ -1,16 +1,11 @@
-import { NgModule, Type } from "@angular/core";
+import { Type } from "@angular/core";
 
 import { DemoBasicComponent } from "./basic/basic.component";
 import type { ITemplateRowProps } from "src/app/shared/components/template/models";
-import { CommonModule } from "@angular/common";
-import { IonicModule } from "@ionic/angular";
 
-@NgModule({
-  imports: [CommonModule, IonicModule],
-  exports: [DemoBasicComponent],
-  declarations: [DemoBasicComponent],
-})
-export class DemoComponentsModule {}
+export { DemoBasicComponent };
+
+export const DEMO_COMPONENTS = [DemoBasicComponent];
 
 export const DEMO_COMPONENT_MAPPING: Record<string, Type<ITemplateRowProps>> = {
   demo_basic: DemoBasicComponent,
