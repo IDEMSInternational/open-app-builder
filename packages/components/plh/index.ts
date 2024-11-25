7ceb24d7242cc2d085cb1f83c0c356ef6860b01d
FaithDaka <faithbusigu8@gmail.com>
2024-11-25 17:15:25 +0300

    init: progress wheel component

--- packages/components/plh/index.ts
+++ packages/components/plh/index.ts
@@ -8,14 +8,21 @@ import { PlhParentPointCounterComponent } from "./parent-point-counter/parent-po
 import { PlhParentPointBoxComponent } from "./parent-point-box/parent-point-box.component";
 import { PlhModuleListItemComponent } from "./plh-kids-kw/components/module-list-item/module-list-item.component";
 import { RouterModule } from "@angular/router";
+import { PlhProgressWheelComponent } from "./plh-kids-kw/components/progress-wheel/progress-wheel.component";
 
 @NgModule({
   imports: [CommonModule, IonicModule, TemplatePipesModule, LottieModule, RouterModule],
-  exports: [PlhParentPointCounterComponent, PlhParentPointBoxComponent, PlhModuleListItemComponent],
+  exports: [
+    PlhParentPointCounterComponent,
+    PlhParentPointBoxComponent,
+    PlhModuleListItemComponent,
+    PlhProgressWheelComponent,
+  ],
   declarations: [
     PlhParentPointCounterComponent,
     PlhParentPointBoxComponent,
     PlhModuleListItemComponent,
+    PlhProgressWheelComponent,
   ],
   providers: [],
 })
@@ -25,6 +32,7 @@ export const PLH_COMPONENT_MAPPING = {
   parent_point_counter: PlhParentPointCounterComponent,
   parent_point_box: PlhParentPointBoxComponent,
   plh_module_list_item: PlhModuleListItemComponent,
+  plh_progress_wheel: PlhProgressWheelComponent,
 };
 
 export type PLHComponentName = keyof typeof PLH_COMPONENT_MAPPING;
