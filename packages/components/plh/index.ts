159645ca1b964f20113f3271c63da136b7c20959
FaithDaka <faithbusigu8@gmail.com>
2024-11-26 15:01:03 +0300

    init: check-in component

--- packages/components/plh/index.ts
+++ packages/components/plh/index.ts
@@ -1,19 +1,27 @@
 import { PlhParentPointCounterComponent } from "./parent-point-counter/parent-point-counter.component";
 import { PlhParentPointBoxComponent } from "./parent-point-box/parent-point-box.component";
 import { PlhModuleListItemComponent } from "./plh-kids-kw/components/module-list-item/module-list-item.component";
+import { PlhActivityCheckInComponent } from "./plh-kids-kw/components/activity-check-in/activity-check-in.component";
 
-export { PlhParentPointCounterComponent, PlhParentPointBoxComponent, PlhModuleListItemComponent };
+export {
+  PlhParentPointCounterComponent,
+  PlhParentPointBoxComponent,
+  PlhModuleListItemComponent,
+  PlhActivityCheckInComponent,
+};
 
 export const PLH_COMPONENTS = [
   PlhParentPointCounterComponent,
   PlhParentPointBoxComponent,
   PlhModuleListItemComponent,
+  PlhActivityCheckInComponent,
 ];
 
 export const PLH_COMPONENT_MAPPING = {
   parent_point_counter: PlhParentPointCounterComponent,
   parent_point_box: PlhParentPointBoxComponent,
   plh_module_list_item: PlhModuleListItemComponent,
+  plh_activity_check_in: PlhActivityCheckInComponent,
 };
 
 export type PLHComponentName = keyof typeof PLH_COMPONENT_MAPPING;
