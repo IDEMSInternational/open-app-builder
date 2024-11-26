bfac1416d01b12e9c182afae42dea6311d3d420d
FaithDaka <faithbusigu8@gmail.com>
2024-11-26 19:36:33 +0300

    feat: functionality

--- packages/components/plh/plh-kids-kw/components/activity-check-in/activity-check-in.component.ts
+++ packages/components/plh/plh-kids-kw/components/activity-check-in/activity-check-in.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from "@angular/core";
 import { TemplateBaseComponent } from "src/app/shared/components/template/components/base";
+import { getStringParamFromTemplateRow } from "src/app/shared/utils";
+
+interface IActivityCheckInParams {
+  id: string;
+  /* TEMPLATE PARAMETER: "title". The title attached at the bottom of the component */
+  title?: string;
+  /* TEMPLATE PARAMETER: "locked_icon_asset". The icon that shows when the activity is locked */
+  lockedIconAsset?: string;
+  /* TEMPLATE PARAMETER: "locked_image_asset". The illustration that shows when the activity is locked */
+  lockedImageAsset?: string;
+  /* TEMPLATE PARAMETER: "unlocked_icon_asset". The icon that shows when the activity is unlocked */
+  unlockedIconAsset?: string;
+  /* TEMPLATE PARAMETER: "unlocked_image_asset". The illustration that shows when the activity is locked */
+  unlockedImageAsset?: string;
+}
 
 @Component({
   selector: "plh-activity-check-in",
@@ -7,6 +22,84 @@ import { TemplateBaseComponent } from "src/app/shared/components/template/compon
   styleUrls: ["./activity-check-in.component.scss"],
 })
 export class PlhActivityCheckInComponent extends TemplateBaseComponent implements OnInit {
-  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
-  ngOnInit() {}
+  params: Partial<IActivityCheckInParams> = {};
+
+  countDownDays: number = 6; // Number of days being counted down
+  daysLeft: number = 6; // Days until unlock
+  progressPercentage: number = 100; // Initial progress
+
+  private unlockDate: Date;
+
+  ngOnInit() {
+    this.getParams();
+    if (this._row.value) {
+      this.checkInTimer();
+    }
+  }
+
+  private getParams() {
+    this.params.id = getStringParamFromTemplateRow(this._row, "id", null);
+    this.params.title = getStringParamFromTemplateRow(this._row, "title", null);
+    this.params.lockedIconAsset = getStringParamFromTemplateRow(
+      this._row,
+      "locked_icon_asset",
+      null
+    );
+    this.params.lockedImageAsset = getStringParamFromTemplateRow(
+      this._row,
+      "locked_image_asset",
+      null
+    );
+    this.params.unlockedIconAsset = getStringParamFromTemplateRow(
+      this._row,
+      "unlocked_icon_asset",
+      null
+    );
+    this.params.unlockedImageAsset = getStringParamFromTemplateRow(
+      this._row,
+      "unlocked_image_asset",
+      null
+    );
+  }
+
+  private getLocalStorageKey(): string {
+    return `activity_${this.params.id}`;
+  }
+
+  // Calculates the days until check in
+  private checkInTimer() {
+    const localStorageKey = this.getLocalStorageKey();
+    const storedDate = localStorage.getItem(localStorageKey);
+    if (storedDate) {
+      this.unlockDate = new Date(storedDate);
+    } else {
+      this.unlockDate = new Date();
+      this.unlockDate.setDate(this.unlockDate.getDate() + this.countDownDays);
+      localStorage.setItem(localStorageKey, this.unlockDate.toISOString());
+    }
+    const dailyInterval = 24 * 60 * 60 * 1000; // 1 day
+    setInterval(() => this.updateProgress(), dailyInterval);
+    this.calculateUnlockDate();
+  }
+
+  // Calculates the exact unlock date
+  private calculateUnlockDate(): void {
+    const now = new Date();
+    this.unlockDate = new Date(now.getTime() + this.countDownDays * 24 * 60 * 60 * 1000);
+    console.log("Unlock Date->", this.unlockDate);
+  }
+
+  // Update the progress bar and unlock state
+  private updateProgress(): void {
+    const now = new Date();
+    this.daysLeft = this.unlockDate.getTime() - now.getTime();
+
+    if (this.daysLeft > 0) {
+      this.daysLeft = Math.ceil(this.daysLeft / (24 * 60 * 60 * 1000));
+      this.progressPercentage = ((7 - this.countDownDays) / 7) * 100;
+    } else {
+      this.progressPercentage = 0;
+      localStorage.removeItem(this.getLocalStorageKey());
+    }
+  }
 }
