2e3a0a3a8b9b47c0e6342f7d936ad3808bd2ff91
FaithDaka <faithbusigu8@gmail.com>
2024-11-26 15:01:03 +0300

    init: check-in component

---
+++ packages/components/plh/plh-kids-kw/components/activity-check-in/activity-check-in.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+import { TemplateBaseComponent } from "src/app/shared/components/template/components/base";
+
+@Component({
+  selector: "plh-activity-check-in",
+  templateUrl: "./activity-check-in.component.html",
+  styleUrls: ["./activity-check-in.component.scss"],
+})
+export class PlhActivityCheckInComponent extends TemplateBaseComponent implements OnInit {
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
+  ngOnInit() {}
+}
