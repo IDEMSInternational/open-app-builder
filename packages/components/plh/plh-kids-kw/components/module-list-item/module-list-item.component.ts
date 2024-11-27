ed08a1949a27ea3b34874541395c1a7322b8854a
FaithDaka <faithbusigu8@gmail.com>
2024-11-27 10:29:44 +0300

    fix: parse Boolean from cell or local variable

--- packages/components/plh/plh-kids-kw/components/module-list-item/module-list-item.component.ts
+++ packages/components/plh/plh-kids-kw/components/module-list-item/module-list-item.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { TemplateBaseComponent } from "src/app/shared/components/template/components/base";
 import { TemplateTranslateService } from "src/app/shared/components/template/services/template-translate.service";
-import { getParamFromTemplateRow, getStringParamFromTemplateRow } from "src/app/shared/utils";
+import {
+  getBooleanParamFromTemplateRow,
+  getStringParamFromTemplateRow,
+} from "src/app/shared/utils";
 
 interface IModuleListItemParams {
   /* TEMPLATE PARAMETER: "module_image_asset". The image attached to the module */
@@ -41,7 +44,7 @@ export class PlhModuleListItemComponent extends TemplateBaseComponent implements
     this.params.textTransform = getStringParamFromTemplateRow(this._row, "text_transform", null) as
       | "capitalise"
       | "uppercase";
-    this.params.isLocked = getParamFromTemplateRow(this._row, "is_locked", false);
+    this.params.isLocked = getBooleanParamFromTemplateRow(this._row, "is_locked", false);
     this.params.navImageAsset = getStringParamFromTemplateRow(this._row, "nav_image_asset", null);
     this.params.lockedImageAsset = getStringParamFromTemplateRow(
       this._row,
