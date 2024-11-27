2efc333261751551fa3800da2d85910f81d5c016
Johnny McQuade <jfmcquade@hotmail.com>
2024-11-27 16:43:44 +0000

    feat: 'sticky' display group param, for use as an inline header or footer

--- src/app/shared/components/template/components/layout/display-group/display-group.component.ts
+++ src/app/shared/components/template/components/layout/display-group/display-group.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  signal,
+  ViewChild,
+} from "@angular/core";
 import { TemplateBaseComponent } from "../../base";
 import { getNumberParamFromTemplateRow, getStringParamFromTemplateRow } from "../../../../../utils";
 
@@ -9,6 +17,8 @@ interface IDisplayGroupParams {
   style: "form" | "default" | string | null;
   /** TEMPLATE PARAMETER: "offset". Add a custom bottom margin */
   offset: number;
+  /** TEMPLATE PARAMETER: "sticky". Set to "top" or "bottom" to make the display group a sticky inline header/footer */
+  sticky: "top" | "bottom" | null;
 }
 
 @Component({
@@ -16,26 +26,44 @@ interface IDisplayGroupParams {
   templateUrl: "./display-group.component.html",
   styleUrls: ["../../tmpl-components-common.scss", "./display-group.component.scss"],
 })
-export class TmplDisplayGroupComponent extends TemplateBaseComponent implements OnInit {
+export class TmplDisplayGroupComponent
+  extends TemplateBaseComponent
+  implements OnInit, AfterViewInit, OnDestroy
+{
   params: Partial<IDisplayGroupParams> = {};
   bgColor: string;
   type: "form" | "dashed_box" | "default";
+
+  @ViewChild("displayGroupWrapper") displayGroupWrapper: ElementRef;
+  private resizeObserver: ResizeObserver;
+  stickyHeight = signal<number>(0);
 
   ngOnInit() {
     this.getParams();
   }
 
-  clickDisplayGroup() {
+  ngAfterViewInit() {
+    if (this.params.sticky) {
+      this.initResizeObserver();
+    }
+  }
+
+  public clickDisplayGroup() {
     this.triggerActions("click");
   }
 
-  getParams() {
+  private getParams() {
     this.params.style = getStringParamFromTemplateRow(this._row, "style", "row");
     this.params.offset = getNumberParamFromTemplateRow(this._row, "offset", 0);
     this.params.variant = getStringParamFromTemplateRow(this._row, "variant", "")
       .split(",")
       .join(" ")
       .concat(" " + this.params.style) as IDisplayGroupParams["variant"];
+    this.params.sticky = getStringParamFromTemplateRow(
+      this._row,
+      "sticky",
+      null
+    ) as IDisplayGroupParams["sticky"];
     this.type = this.getTypeFromStyles();
   }
 
@@ -44,5 +72,23 @@ export class TmplDisplayGroupComponent extends TemplateBaseComponent implements 
     if (this.params.style?.includes("dashed_box") || this.params.variant?.includes("dashed_box"))
       return "dashed_box";
     return "default";
+  }
+
+  /** Observe the height of the display group wrapper element and update the stickyHeight property accordingly */
+  private initResizeObserver() {
+    if (this.displayGroupWrapper) {
+      this.resizeObserver = new ResizeObserver((entries) => {
+        const entry = entries[0];
+        this.stickyHeight.set(entry.contentRect.height);
+      });
+
+      this.resizeObserver.observe(this.displayGroupWrapper.nativeElement);
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.resizeObserver) {
+      this.resizeObserver.disconnect();
+    }
   }
 }
