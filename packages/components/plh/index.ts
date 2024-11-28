911599c807566f1e880a7ba59f08fed66600bf13
FaithDaka <faithbusigu8@gmail.com>
2024-11-28 12:38:02 +0300

    remove plh progress wheel

--- packages/components/plh/index.ts
+++ packages/components/plh/index.ts
@@ -1,27 +1,19 @@
 import { PlhParentPointCounterComponent } from "./parent-point-counter/parent-point-counter.component";
 import { PlhParentPointBoxComponent } from "./parent-point-box/parent-point-box.component";
 import { PlhModuleListItemComponent } from "./plh-kids-kw/components/module-list-item/module-list-item.component";
-import { PlhProgressWheelComponent } from "./plh-kids-kw/components/progress-wheel/progress-wheel.component";
 
-export {
-  PlhParentPointCounterComponent,
-  PlhParentPointBoxComponent,
-  PlhModuleListItemComponent,
-  PlhProgressWheelComponent,
-};
+export { PlhParentPointCounterComponent, PlhParentPointBoxComponent, PlhModuleListItemComponent };
 
 export const PLH_COMPONENTS = [
   PlhParentPointCounterComponent,
   PlhParentPointBoxComponent,
   PlhModuleListItemComponent,
-  PlhProgressWheelComponent,
 ];
 
 export const PLH_COMPONENT_MAPPING = {
   parent_point_counter: PlhParentPointCounterComponent,
   parent_point_box: PlhParentPointBoxComponent,
   plh_module_list_item: PlhModuleListItemComponent,
-  plh_progress_wheel: PlhProgressWheelComponent,
 };
 
 export type PLHComponentName = keyof typeof PLH_COMPONENT_MAPPING;
