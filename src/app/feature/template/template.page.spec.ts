8b52751f1777b29b925089b0fdcc494e2b72a168
chrismclarke <chris.m.clarke@live.co.uk>
2025-01-21 19:14:18 -0800

    fix: template page test

--- src/app/feature/template/template.page.spec.ts
+++ src/app/feature/template/template.page.spec.ts
@@ -3,6 +3,17 @@ import { IonicModule } from "@ionic/angular";
 
 import { TemplatePage } from "./template.page";
 import { ActivatedRoute } from "@angular/router";
+import { AppDataService } from "src/app/shared/services/data/app-data.service";
+import { MockAppDataService } from "src/app/shared/services/data/app-data.service.mock.spec";
+import { AppConfigService } from "src/app/shared/services/app-config/app-config.service";
+import { MockAppConfigService } from "src/app/shared/services/app-config/app-config.service.mock.spec";
+import { Component, input } from "@angular/core";
+
+// HACK - mock child `<plh-template-container>` component to bypass imports
+@Component({ selector: "plh-template-container", template: "<div></div>" })
+class MockTemplateContainerComponent {
+  templatename = input<string>();
+}
 
 describe("TemplatePage", () => {
   let component: TemplatePage;
@@ -10,12 +21,20 @@ describe("TemplatePage", () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [TemplatePage],
+      declarations: [TemplatePage, MockTemplateContainerComponent],
       imports: [IonicModule.forRoot()],
       providers: [
         {
           provide: ActivatedRoute,
           useValue: { snapshot: { params: { templateName: "mock_template_name" } } },
+        },
+        {
+          provide: AppDataService,
+          useValue: new MockAppDataService(),
+        },
+        {
+          provide: AppConfigService,
+          useValue: new MockAppConfigService(),
         },
       ],
     }).compileComponents();
