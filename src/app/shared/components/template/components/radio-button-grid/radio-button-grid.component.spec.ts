dcdf2dbfc880dc1f01996ad27953ebf2043027f0
chrismclarke <chris.m.clarke@live.co.uk>
2025-01-22 10:14:00 -0800

    test: NullInjectorError fixes

--- src/app/shared/components/template/components/radio-button-grid/radio-button-grid.component.spec.ts
+++ src/app/shared/components/template/components/radio-button-grid/radio-button-grid.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { TmplRadioButtonGridComponent } from "./radio-button-grid.component";
+import { FlowTypes } from "packages/data-models";
+
+const MOCK_ROW: FlowTypes.TemplateRow = { _nested_name: "", name: "", type: "radio_button_grid" };
 
 describe("RadioButtonGridComponent", () => {
   let component: TmplRadioButtonGridComponent;
@@ -9,11 +12,13 @@ describe("RadioButtonGridComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TmplRadioButtonGridComponent],
+      providers: [],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TmplRadioButtonGridComponent);
+    fixture.componentRef.setInput("row", MOCK_ROW);
+    await fixture.whenStable();
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it("should create", () => {
--- src/app/shared/services/auth/auth.service.spec.ts
+++ src/app/shared/services/auth/auth.service.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from "@angular/core/testing";
 
 import { AuthService } from "./auth.service";
+import { DeploymentService } from "../deployment/deployment.service";
+import { MockDeploymentService } from "../deployment/deployment.service.spec";
+import { TemplateService } from "../../components/template/services/template.service";
+import { ServerService } from "../server/server.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe("AuthService", () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: DeploymentService, useValue: new MockDeploymentService({}) },
+        { provide: TemplateService, useValue: {} },
+        { provide: ServerService, useValue: {} },
+      ],
+    });
     service = TestBed.inject(AuthService);
   });
 
