df5b909d5555ccbb1deae3c5c9685d7e951279ce
chrismclarke <chris.m.clarke@live.co.uk>
2025-01-21 19:28:03 -0800

    fix: task service and apConfig specs

--- src/app/shared/services/app-config/app-config.service.mock.spec.ts
+++ src/app/shared/services/app-config/app-config.service.mock.spec.ts
@@ -1,28 +1,16 @@
 import { AppConfigService } from "./app-config.service";
-import { BehaviorSubject } from "rxjs/internal/BehaviorSubject";
 import { IAppConfig } from "../../model";
-import { signal } from "@angular/core";
-import { IAppConfigOverride } from "packages/data-models";
-import { deepMergeObjects } from "../../utils";
+
+import { MockDeploymentService } from "../deployment/deployment.service.spec";
+import { DeploymentService } from "../deployment/deployment.service";
+import { Router } from "@angular/router";
 
 /** Mock calls for field values from the template field service to return test data */
-export class MockAppConfigService implements Partial<AppConfigService> {
-  appConfig = signal<IAppConfig>(undefined as any);
-  appConfig$ = new BehaviorSubject<IAppConfig>(undefined as any);
-
-  // allow additional specs implementing service to provide their own partial appConfig
-  constructor(private mockAppConfig: Partial<IAppConfig> = {}) {
-    this.setAppConfig();
-  }
-
-  public ready(timeoutValue?: number) {
-    return true;
-  }
-
-  public setAppConfig(overrides: IAppConfigOverride = {}) {
-    // merge onto empty object to avoid shared references across tests
-    const mergedConfig = deepMergeObjects({}, this.mockAppConfig, overrides) as IAppConfig;
-    this.appConfig$.next(mergedConfig);
-    this.appConfig.set(mergedConfig);
+export class MockAppConfigService extends AppConfigService {
+  constructor(mockAppConfig: Partial<IAppConfig> = {}) {
+    super(
+      new MockDeploymentService({ app_config: mockAppConfig }) as any as DeploymentService,
+      { resetConfig: () => null } as any as Router
+    );
   }
 }
