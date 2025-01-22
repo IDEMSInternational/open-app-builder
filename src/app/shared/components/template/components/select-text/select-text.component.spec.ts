1ed68b29a8e5f663c923e2f12cfbb04ea1f155ba
chrismclarke <chris.m.clarke@live.co.uk>
2025-01-22 14:50:06 -0800

    test: component spec fixes

--- src/app/shared/components/template/components/select-text/select-text.component.spec.ts
+++ src/app/shared/components/template/components/select-text/select-text.component.spec.ts
@@ -4,12 +4,15 @@ import { IonicModule } from "@ionic/angular";
 import { SelectTextComponent } from "./select-text.component";
 import { TemplateTranslateService } from "../../services/template-translate.service";
 import { MockTemplateTranslateService } from "../../services/template-translate.service.spec";
+import { FlowTypes } from "packages/data-models";
+
+const MOCK_ROW: FlowTypes.TemplateRow = { _nested_name: "", name: "", type: "select_text" };
 
 describe("SelectTextComponent", () => {
   let component: SelectTextComponent;
   let fixture: ComponentFixture<SelectTextComponent>;
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(waitForAsync(async () => {
     TestBed.configureTestingModule({
       declarations: [SelectTextComponent],
       imports: [IonicModule.forRoot()],
@@ -19,8 +22,9 @@ describe("SelectTextComponent", () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(SelectTextComponent);
+    fixture.componentRef.setInput("row", MOCK_ROW);
+    await fixture.whenStable();
     component = fixture.componentInstance;
-    fixture.detectChanges();
   }));
 
   it("should create", () => {
