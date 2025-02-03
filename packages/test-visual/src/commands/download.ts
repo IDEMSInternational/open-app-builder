ad62460542195e08f34ca2eb44bc19306f8ea6c8
chrismclarke <chris.m.clarke@live.co.uk>
2025-02-03 14:27:38 -0800

    refactor: esm module

--- packages/test-visual/src/commands/download.ts
+++ packages/test-visual/src/commands/download.ts
@@ -49,8 +49,13 @@ export class ScreenshotDownload {
    * TODO - not filtered to specific branch run (assume fine)
    */
   private async getLatestScreenshotsArtifact() {
-    const artifacts = await getGHRepoArtifacts();
-    const latestArtifact = artifacts.find((a) => a.name === SCREENSHOT_ARTIFACT_NAME);
+    const artifacts = await getGHRepoArtifacts({
+      name: SCREENSHOT_ARTIFACT_NAME,
+      page: 1,
+      per_page: 1,
+    });
+    // artifacts return in reverse-chronological order so first entry should be latest
+    const [latestArtifact] = artifacts;
     if (!latestArtifact) {
       throw new Error(`No artifacts found with name: ${SCREENSHOT_ARTIFACT_NAME}`);
     }
