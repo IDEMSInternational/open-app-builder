69077100296d80b6cb502245bb56a105cb02d137
Johnny McQuade <jfmcquade@hotmail.com>
2025-02-12 11:15:54 +0000

    chore(map): increase filter precision to 2 decimal places

--- src/app/shared/components/template/components/map/map.component.ts
+++ src/app/shared/components/template/components/map/map.component.ts
@@ -258,8 +258,8 @@ export class TmplMapComponent extends TemplateBaseComponent implements AfterView
   }
 
   private filterLayerFeatures(vectorLayer: VectorLayer, upperValue: number, lowerValue: number) {
-    lowerValue = this.roundToXDecimalPlace(lowerValue, 1);
-    upperValue = this.roundToXDecimalPlace(upperValue, 1);
+    lowerValue = this.roundToXDecimalPlace(lowerValue, 2);
+    upperValue = this.roundToXDecimalPlace(upperValue, 2);
     const propertyName = vectorLayer.get("propertyToPlot");
     const filterFeatures = (feature: Feature) => {
       const value = feature.get(propertyName);
@@ -334,7 +334,7 @@ export class TmplMapComponent extends TemplateBaseComponent implements AfterView
       translate: (value: number) => {
         return new Intl.NumberFormat("en-GB", {
           minimumFractionDigits: 0,
-          maximumFractionDigits: 1,
+          maximumFractionDigits: 2,
         }).format(value);
       },
       vertical: true,
