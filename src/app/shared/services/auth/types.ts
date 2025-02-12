8426bc1da050b4f9b6149882553b6e773a581325
chrismclarke <chris.m.clarke@live.co.uk>
2025-02-12 14:33:51 -0800

    refactor: openid profile data

--- src/app/shared/services/auth/types.ts
+++ src/app/shared/services/auth/types.ts
@@ -1,12 +1,15 @@
 import type { IDeploymentConfig } from "packages/data-models";
-import type { FirebaseAuthUser } from "./providers/firebase.auth";
 
 export type IAuthProvider = IDeploymentConfig["auth"]["provider"];
 
-export type IAuthUser =
-  | {
-      uid: string;
-      given_name?: string;
-      family_name?: string;
-    }
-  | FirebaseAuthUser;
+/**
+ * Auth user profile. Should adhere to properties within openid spec
+ * https://openid.net/specs/openid-connect-basic-1_0.html#StandardClaims
+ */
+export type IAuthUser = {
+  uid: string;
+  name?: string;
+  given_name?: string;
+  family_name?: string;
+  picture?: string;
+};
--- src/app/shared/services/local-storage/local-storage.service.spec.ts
+++ src/app/shared/services/local-storage/local-storage.service.spec.ts
@@ -52,15 +52,13 @@ describe("LocalStorageService", () => {
   });
 
   it("sets and gets private entries", () => {
-    service.setProtected("AUTH_USER_DISPLAY_NAME", "private_user_name");
-    expect(service.getProtected("AUTH_USER_DISPLAY_NAME")).toEqual("private_user_name");
-    expect(localStorage.getItem("rp-contact-field._auth_user_display_name")).toEqual(
-      "private_user_name"
-    );
+    service.setProtected("AUTH_USER_NAME", "private_user_name");
+    expect(service.getProtected("AUTH_USER_NAME")).toEqual("private_user_name");
+    expect(localStorage.getItem("rp-contact-field._auth_user_name")).toEqual("private_user_name");
   });
 
   it("omits private entries from getAll method", () => {
-    service.setProtected("AUTH_USER_DISPLAY_NAME", "private_user_name");
+    service.setProtected("AUTH_USER_NAME", "private_user_name");
     const res = service.getAll();
     console.log("res", res);
     expect(res).toEqual({});
