3005be7ffb81eaafd4786246cdb4bba41ba5e77c
Johnny McQuade <jfmcquade@hotmail.com>
2025-02-21 15:36:42 +0000

    chore: refactor component logic to make params responsive

--- packages/components/plh/plh-kids-kw/components/module-list-item/module-list-item.component.ts
+++ packages/components/plh/plh-kids-kw/components/module-list-item/module-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, computed } from "@angular/core";
 import { FlowTypes } from "packages/data-models";
 import { TemplateBaseComponent } from "src/app/shared/components/template/components/base";
 import { TemplateTranslateService } from "src/app/shared/components/template/services/template-translate.service";
@@ -8,14 +8,16 @@ import {
 } from "src/app/shared/utils";
 
 interface IModuleListItemParams {
-  /* TEMPLATE PARAMETER: "module_image_asset". The image attached to the module */
-  moduleImageAsset?: string;
+  /* TEMPLATE PARAMETER: "highlighted" */
+  highlighted?: boolean;
   /* TEMPLATE PARAMETER: "is_locked". The boolean that marks the module as locked or unlocked */
   isLocked?: boolean;
-  /* TEMPLATE PARAMETER: "nav_image_asset". The navigation icon*/
-  navImageAsset?: string;
   /* TEMPLATE PARAMETER: "locked_image_asset". The locked icon*/
   lockedImageAsset?: string;
+  /* TEMPLATE PARAMETER: "module_image_asset". The image attached to the module */
+  moduleImageAsset?: string;
+  /* TEMPLATE PARAMETER: "nav_image_asset". The navigation icon*/
+  navImageAsset?: string;
 }
 
 @Component({
@@ -23,34 +25,25 @@ interface IModuleListItemParams {
   templateUrl: "./module-list-item.component.html",
   styleUrls: ["./module-list-item.component.scss"],
 })
-export class PlhModuleListItemComponent extends TemplateBaseComponent implements OnInit {
-  params: Partial<IModuleListItemParams> = {};
+export class PlhModuleListItemComponent extends TemplateBaseComponent {
+  params = computed(() => this.getParams(this.parameterList()));
 
   constructor(public templateTranslateService: TemplateTranslateService) {
     super();
   }
 
-  ngOnInit() {
-    this.getParams();
-  }
-
   public handleClick() {
-    if (this.params.isLocked) return;
+    if (this.params().isLocked) return;
     this.triggerActions("click");
   }
 
-  private getParams() {
-    this.params.moduleImageAsset = getStringParamFromTemplateRow(
-      this._row,
-      "module_image_asset",
-      ""
-    );
-    this.params.isLocked = getBooleanParamFromTemplateRow(this._row, "is_locked", false);
-    this.params.navImageAsset = getStringParamFromTemplateRow(this._row, "nav_image_asset", null);
-    this.params.lockedImageAsset = getStringParamFromTemplateRow(
-      this._row,
-      "locked_image_asset",
-      null
-    );
+  private getParams(authorParams: FlowTypes.TemplateRow["parameter_list"]): IModuleListItemParams {
+    return {
+      moduleImageAsset: getStringParamFromTemplateRow(this._row, "module_image_asset", ""),
+      isLocked: getBooleanParamFromTemplateRow(this._row, "is_locked", false),
+      navImageAsset: getStringParamFromTemplateRow(this._row, "nav_image_asset", null),
+      lockedImageAsset: getStringParamFromTemplateRow(this._row, "locked_image_asset", null),
+      highlighted: getBooleanParamFromTemplateRow(this._row, "highlighted", false),
+    };
   }
 }
