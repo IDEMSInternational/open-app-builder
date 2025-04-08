ef7e43f0b1be6641aa4d5ff8b60cf1cedd0729d5
chrismclarke <chris.m.clarke@live.co.uk>
2025-04-08 11:21:01 -0700

    refactor: shared data provider

--- packages/data-models/deployment.model.ts
+++ packages/data-models/deployment.model.ts
@@ -3,7 +3,7 @@ import type { IGdriveEntry } from "../@idemsInternational/gdrive-tools";
 import type { IAppConfig, IAppConfigOverride } from "./appConfig";
 
 /** Update version to force recompile next time deployment set (e.g. after default config update) */
-export const DEPLOYMENT_CONFIG_VERSION = 20250407.0;
+export const DEPLOYMENT_CONFIG_VERSION = 20250407.1;
 
 /** Configuration settings available to runtime application */
 export interface IDeploymentRuntimeConfig {
@@ -75,9 +75,9 @@ export interface IDeploymentRuntimeConfig {
   /** Friendly name used to identify the deployment name */
   name: string;
 
-  /** 3rd party integration for shared data management */
-  shared_data: {
-    provider?: "firebase";
+  /** 3rd party integration for shared data management. Default enabled with firebase provider */
+  shared_data?: {
+    provider: "firebase";
   };
   /** 3rd party integration for remote asset storage and sync */
   supabase: {
@@ -222,7 +222,9 @@ export const DEPLOYMENT_RUNTIME_CONFIG_DEFAULTS: IDeploymentRuntimeConfig = {
   campaigns: {
     enabled: true,
   },
-  shared_data: {},
+  shared_data: {
+    provider: "firebase",
+  },
   supabase: {
     enabled: false,
   },
