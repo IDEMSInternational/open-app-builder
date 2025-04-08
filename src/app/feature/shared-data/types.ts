6f48423d8c276b4c10968386df202a3492b32523
chrismclarke <chris.m.clarke@live.co.uk>
2025-04-08 16:53:23 -0700

    chore: code tidying

--- src/app/feature/shared-data/types.ts
+++ src/app/feature/shared-data/types.ts
@@ -1,3 +1,14 @@
 import type { IDeploymentConfig } from "packages/data-models";
 
 export type ISharedDataProvider = IDeploymentConfig["shared_data"]["provider"];
+
+export interface ISharedDataItem {
+  id: string;
+  public?: boolean;
+  data: any;
+  // metadata
+  _created_by: string;
+  _created_at: string;
+  _updated_at: string;
+  _source: "server" | "cache";
+}
