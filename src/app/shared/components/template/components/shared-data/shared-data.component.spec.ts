f6178682e2947f541ccf788681656bec309a9974
Chris <chris.m.clarke@live.co.uk>
2025-04-21 10:53:38 +0100

    fix: tests

--- src/app/shared/components/template/components/shared-data/shared-data.component.spec.ts
+++ src/app/shared/components/template/components/shared-data/shared-data.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { IonicModule } from "@ionic/angular";
 
 import { TmplSharedDataComponent } from "./shared-data.component";
+import { DeploymentService } from "src/app/shared/services/deployment/deployment.service";
+import { MockDeploymentService } from "src/app/shared/services/deployment/deployment.service.mock.spec";
 
 describe("SharedDataComponent", () => {
   let component: TmplSharedDataComponent;
@@ -11,6 +13,12 @@ describe("SharedDataComponent", () => {
     TestBed.configureTestingModule({
       declarations: [TmplSharedDataComponent],
       imports: [IonicModule.forRoot()],
+      providers: [
+        {
+          provide: DeploymentService,
+          useValue: new MockDeploymentService(),
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TmplSharedDataComponent);
