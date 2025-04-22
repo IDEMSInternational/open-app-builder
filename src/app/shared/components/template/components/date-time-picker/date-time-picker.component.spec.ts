5b0a39f2cd92a2ddca04cf5b0ae7e44bc533c05c
Mark Harmon <mark.harmon@outlook.com>
2025-04-22 18:35:17 +0100

    Add a mock row to fix failing test

--- src/app/shared/components/template/components/date-time-picker/date-time-picker.component.spec.ts
+++ src/app/shared/components/template/components/date-time-picker/date-time-picker.component.spec.ts
@@ -2,20 +2,29 @@ import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { IonicModule } from "@ionic/angular";
 
 import { TmplDateTimePickerComponent } from "./date-time-picker.component";
+import { FlowTypes } from "packages/data-models";
+
+const MOCK_ROW: FlowTypes.TemplateRow = {
+  _nested_name: "",
+  name: "test_date_picker1",
+  type: "date-time-picker",
+};
 
 describe("DateTimePickerComponent", () => {
   let component: TmplDateTimePickerComponent;
   let fixture: ComponentFixture<TmplDateTimePickerComponent>;
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(waitForAsync(async () => {
     TestBed.configureTestingModule({
       declarations: [TmplDateTimePickerComponent],
       imports: [IonicModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TmplDateTimePickerComponent);
+    fixture.componentRef.setInput("row", MOCK_ROW);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    await fixture.whenStable();
+    component = fixture.componentInstance;
   }));
 
   it("should create", () => {
