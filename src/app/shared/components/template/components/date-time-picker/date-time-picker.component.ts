f43e4921876144990fc5392a00ba8938c242189b
Mark Harmon <mark.harmon@outlook.com>
2025-04-23 10:08:31 +0100

    Implement PR suggestions

--- src/app/shared/components/template/components/date-time-picker/date-time-picker.component.ts
+++ src/app/shared/components/template/components/date-time-picker/date-time-picker.component.ts
@@ -1,12 +1,12 @@
 import { Component, computed, OnInit, signal, WritableSignal } from "@angular/core";
 import { TemplateBaseComponent } from "../base";
-import { getStringParamFromTemplateRow, parseBoolean } from "src/app/shared/utils";
+import { generateUUID, getStringParamFromTemplateRow, parseBoolean } from "src/app/shared/utils";
 
 interface IDateTimePickerParams {
   /** TEMPLATE PARAMETER: "disabled". If true, date time picker is disabled and greyed out */
-  disabled: boolean;
+  disabled?: boolean;
   /** TEMPLATE PARAMETER: "type". Determines the type of picker date & time ('datetime'), date only ('date') or time only ('time') picker*/
-  type: string;
+  type?: string;
 }
 
 @Component({
@@ -14,16 +14,12 @@ interface IDateTimePickerParams {
   templateUrl: "./date-time-picker.component.html",
   styleUrls: ["./date-time-picker.component.scss"],
 })
-export class TmplDateTimePickerComponent extends TemplateBaseComponent implements OnInit {
-  public dateTimeValue: WritableSignal<string | undefined>;
+export class TmplDateTimePickerComponent extends TemplateBaseComponent {
   public params = computed(() => this.getParams());
+  public uuid = generateUUID();
 
   constructor() {
     super();
-  }
-
-  public ngOnInit(): void {
-    this.dateTimeValue = signal(this.value() || undefined);
   }
 
   public handleChange(value: string | string[]) {
@@ -31,11 +27,11 @@ export class TmplDateTimePickerComponent extends TemplateBaseComponent implement
   }
 
   private getParams(): IDateTimePickerParams {
-    let parameterList = this.parameterList();
+    let parameterList = this.parameterList() as IDateTimePickerParams;
 
     return {
       disabled: parseBoolean(parameterList.disabled),
-      type: getStringParamFromTemplateRow(this._row, "type", "date-time"),
+      type: parameterList.type || "date-time",
     };
   }
 }
