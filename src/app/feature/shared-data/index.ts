dd0eb95477d993503a74250665f86bc56ea6b731
chrismclarke <chris.m.clarke@live.co.uk>
2025-05-02 18:14:01 +0100

    refactor: shared data environment initializer

--- src/app/feature/shared-data/index.ts
+++ src/app/feature/shared-data/index.ts
@@ -1,9 +1,9 @@
-import { inject } from "@angular/core";
+import { inject, provideEnvironmentInitializer } from "@angular/core";
 import { Routes } from "@angular/router";
 import { SharedDataService } from "./shared-data.service";
 import { TemplateActionRegistry } from "src/app/shared/components/template/services/instance/template-action.registry";
 import { SharedDataActionFactory } from "./shared-data.actions";
-import { ENVIRONMENT_INITIALIZER, makeEnvironmentProviders } from "@angular/core";
+import { makeEnvironmentProviders } from "@angular/core";
 
 // Export the routes to allow debug route to be included in main app-routing.module
 export const SHARED_DATA_ROUTES: Routes = [
@@ -14,21 +14,20 @@ export const SHARED_DATA_ROUTES: Routes = [
   },
 ];
 
-// Use initializer to register actions used within global templates
-export function initializeShareData() {
-  return () => {
-    const service = inject(SharedDataService);
-    const templateActionRegistry = inject(TemplateActionRegistry);
-    const { shared_data } = new SharedDataActionFactory(service);
-    templateActionRegistry.register({ shared_data });
-  };
+function registerActions() {
+  const service = inject(SharedDataService);
+  const templateActionRegistry = inject(TemplateActionRegistry);
+  const { shared_data } = new SharedDataActionFactory(service);
+  templateActionRegistry.register({ shared_data });
 }
 
 // use environment initializer to ensure init only called once (sync/non-blocking)
-// if needing async logic an APP_INITIALIZER can be used instead
+// if needing async logic an `provideAppInitializer` can be used instead
 export function provideSharedData() {
   return makeEnvironmentProviders([
     SharedDataService,
-    { provide: ENVIRONMENT_INITIALIZER, useFactory: initializeShareData, multi: true },
+    provideEnvironmentInitializer(() => {
+      registerActions();
+    }),
   ]);
 }
