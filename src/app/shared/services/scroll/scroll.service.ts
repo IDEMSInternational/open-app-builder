9becbc67629ee4d181f18d3d3e91b7ef1f26124d
Johnny McQuade <jfmcquade@hotmail.com>
2025-05-21 16:08:08 +0100

    chore: refactor action syntax

--- src/app/shared/services/scroll/scroll.service.ts
+++ src/app/shared/services/scroll/scroll.service.ts
@@ -2,6 +2,14 @@ import { Injectable } from "@angular/core";
 import { TemplateActionRegistry } from "../../components/template/services/instance/template-action.registry";
 import { SyncServiceBase } from "../syncService.base";
 
+type ScrollPosition = "top" | "bottom";
+interface IScrollActionParams {
+  /**
+   * Duration of the scroll animation in milliseconds
+   */
+  duration?: number;
+}
+
 @Injectable({
   providedIn: "root",
 })
@@ -13,22 +21,44 @@ export class ScrollService extends SyncServiceBase {
 
   private registerTemplateActionHandlers() {
     this.templateActionRegistry.register({
-      scroll_top: async () => {
-        await this.handleScrollTop();
+      scroll: async ({ args, params }) => {
+        const [actionId] = args as [ScrollPosition];
+        const { duration } = params as IScrollActionParams;
+        const childActions = {
+          top: async () => await this.handleScroll("top", duration),
+          bottom: async () => await this.handleScroll("bottom", duration),
+        };
+        if (!(actionId in childActions)) {
+          console.error(`[SCROLL] - No action, "${actionId}"`);
+          return;
+        }
+        return childActions[actionId]();
       },
     });
   }
 
-  private async handleScrollTop() {
+  /**
+   * Scrolls the visible ion-content to the top or bottom
+   * @param position - "top" or "bottom"
+   * @param duration - Duration of the scroll animation in milliseconds
+   */
+  private async handleScroll(position: ScrollPosition, duration: number = 0) {
     const contents = Array.from(
       document.querySelectorAll("ion-content")
     ) as HTMLIonContentElement[];
     const visible = contents.find((c) => c.offsetParent !== null);
     if (visible) {
-      await visible.scrollToTop(0);
-      console.log("[SCROLL] Scrolled visible ion-content to top");
+      if (position === "top") {
+        await visible.scrollToTop(duration);
+      } else if (position === "bottom") {
+        await visible.scrollToBottom(duration);
+      } else {
+        console.error(`[SCROLL] - Invalid scroll position, "${position}"`);
+        return;
+      }
+      console.log(`[SCROLL] - Scrolled visible ion-content to ${position}`);
     } else {
-      console.warn("[SCROLL] No visible ion-content found");
+      console.warn("[SCROLL] - No visible ion-content found");
     }
   }
 }
