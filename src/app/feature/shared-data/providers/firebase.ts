229d449da561be8032a55616b4a972eb29345a08
Johnny McQuade <jfmcquade@hotmail.com>
2025-06-04 19:15:28 +0100

    chore: add ios emulator workaround

--- src/app/feature/shared-data/providers/firebase.ts
+++ src/app/feature/shared-data/providers/firebase.ts
@@ -1,5 +1,6 @@
 import { Injector } from "@angular/core";
 import {
+  CACHE_SIZE_UNLIMITED,
   collection,
   CollectionReference,
   deleteDoc,
@@ -10,7 +11,9 @@ import {
   Firestore,
   FirestoreDataConverter,
   getFirestore,
+  initializeFirestore,
   onSnapshot,
+  persistentLocalCache,
   Query,
   query,
   serverTimestamp,
@@ -79,6 +82,12 @@ export class FirebaseDataProvider implements SharedDataProviderBase {
       throw new Error(`Shared data provider required firebase to be configured`);
     }
     this.db = getFirestore(app);
+
+    // Possible fix for iOS emulator issues (replace getFirestore)
+    // this.db = initializeFirestore(app, {
+    //   experimentalForceLongPolling: true,
+    //   localCache: persistentLocalCache({ cacheSizeBytes: CACHE_SIZE_UNLIMITED })
+    // });
   }
 
   public querySingle$(params: SharedDataQueryParams) {
