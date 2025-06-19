e65420b1bf17263f77fe65f067a63b2b8cd68595
Johnny McQuade <jfmcquade@hotmail.com>
2025-06-19 14:35:47 +0100

    chore: revert changes to text component

--- src/app/shared/components/template/components/text/text.component.ts
+++ src/app/shared/components/template/components/text/text.component.ts
@@ -1,18 +1,11 @@
-import { Component, computed, effect } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { TemplateBaseComponent } from "../base";
 import { getStringParamFromTemplateRow } from "../../../../utils";
-import { FlowTypes } from "packages/data-models";
-import { TemplateTranslateService } from "../../services/template-translate.service";
 
 interface ITextParams {
-  style?: string | null;
-  textAlign?: string | null;
-  type?: string;
-  /**
-   * TEMPLATE PARAMETER: "language_code". The target language code to translate the value to.
-   * Used for displaying text in a different language to the global app language.
-   * */
-  languageCode?: string;
+  style: string | null;
+  textAlign: string | null;
+  type: string;
 }
 
 @Component({
@@ -20,36 +13,20 @@ interface ITextParams {
   templateUrl: "./text.component.html",
   styleUrls: ["../tmpl-components-common.scss", "./text.component.scss"],
 })
-export class TmplTextComponent extends TemplateBaseComponent {
-  params = computed(() => this.getParams(this.parameterList()));
+export class TmplTextComponent extends TemplateBaseComponent implements OnInit {
+  params: Partial<ITextParams> = {};
   hasTextValue: boolean;
-  currentLanguageCode: string = this.templateTranslateService.app_language;
 
-  constructor(public templateTranslateService: TemplateTranslateService) {
-    super();
-    effect(async () => {
-      const targetLanguageCode = this.params().languageCode;
-      if (targetLanguageCode) {
-        const translatedValue = await this.templateTranslateService.translateValueToLanguage(
-          this.value(),
-          targetLanguageCode,
-          this.currentLanguageCode
-        );
-        if (translatedValue) {
-          this.setValue(translatedValue);
-        }
-        this.currentLanguageCode = targetLanguageCode;
-      }
-    });
+  ngOnInit() {
+    this.getParams();
   }
 
-  private getParams(authorParams?: FlowTypes.TemplateRow["parameter_list"]): ITextParams {
+  getParams() {
     this.hasTextValue = !["undefined", "NaN", "null", '""'].includes(this._row.value as string);
-    return {
-      textAlign: getStringParamFromTemplateRow(this._row, "text_align", null),
-      type: this._row.parameter_list?.style?.includes("numbered") ? "numbered" : "marked",
-      style: getStringParamFromTemplateRow(this._row, "style", null),
-      languageCode: getStringParamFromTemplateRow(this._row, "language_code", null),
-    };
+    this.params.textAlign = getStringParamFromTemplateRow(this._row, "text_align", null);
+    this.params.type = this._row.parameter_list?.style?.includes("numbered")
+      ? "numbered"
+      : "marked";
+    this.params.style = getStringParamFromTemplateRow(this._row, "style", null);
   }
 }
