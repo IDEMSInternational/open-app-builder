269038c4c604bb3ba6961b2234984822d24599bd
chrismclarke <chris.m.clarke@live.co.uk>
2025-06-19 07:53:44 -0700

    refactor: blob data storage

--- src/app/shared/services/http/cache/adapters/types.ts
+++ src/app/shared/services/http/cache/adapters/types.ts
@@ -1,7 +1,7 @@
 export interface IHttpCacheAdapter {
   list: () => Promise<string[]>;
-  get: (key: string) => Promise<any>;
-  set: (key: string, data: any) => Promise<any>;
+  get: (key: string) => Promise<Blob>;
+  set: (key: string, data: Blob) => Promise<any>;
   clear: () => Promise<void>;
   delete: (key: string) => Promise<boolean>;
 }
