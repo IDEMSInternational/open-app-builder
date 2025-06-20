207a55840f84ec325d98111ea833e0eb86a5a378
chrismclarke <chris.m.clarke@live.co.uk>
2025-06-20 16:59:27 -0700

    feat: wip notification triggered actions

--- src/app/feature/notification/notification.actions.ts
+++ src/app/feature/notification/notification.actions.ts
@@ -1,10 +1,15 @@
 import type { IActionHandler } from "src/app/shared/components/template/services/instance/template-action.registry";
 import { NotificationService } from "./notification.service";
 import { INotification } from "./notification.types";
+import { FlowTypes } from "packages/data-models";
 
 interface INotificationRemoveParams {
   id: string;
 }
+type INotificationSetParams = Omit<INotification, "action"> & {
+  /** authored action param from data_list row that includes `action_list` column */
+  action: { id: string; action_list: FlowTypes.TemplateRowAction[] };
+};
 
 export class NotificationActionFactory {
   constructor(private service: NotificationService) {}
@@ -13,7 +18,8 @@ export class NotificationActionFactory {
     const [actionId] = args;
     const childActions = {
       create: async () => {
-        return this.service.scheduleNotification(params as INotification);
+        const notification = this.parseNotificationCreateParams(params as INotificationSetParams);
+        return this.service.scheduleNotification(notification);
       },
       cancel: async () => {
         const { id } = params as INotificationRemoveParams;
@@ -32,4 +38,14 @@ export class NotificationActionFactory {
     }
     return childActions[actionId]();
   };
+
+  private parseNotificationCreateParams(params: INotificationSetParams) {
+    const { action, ...rest } = params;
+    const notification: INotification = rest;
+    if (action) {
+      const { action_list } = action;
+      notification.action = action_list;
+    }
+    return notification;
+  }
 }
