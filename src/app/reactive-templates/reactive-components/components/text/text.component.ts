ac3b403e00dcac09641b8f900adddfd1779a5970
Mark Harmon <mark.harmon@outlook.com>
2025-07-16 14:40:40 +0100

    Add initial parameter implementation

--- src/app/reactive-templates/reactive-components/components/text/text.component.ts
+++ src/app/reactive-templates/reactive-components/components/text/text.component.ts
@@ -1,33 +1,38 @@
-import { Component, OnInit } from "@angular/core";
-// import { getStringParamFromTemplateRow } from "../../../../utils";
-import { ReactiveBaseComponent } from "../../reactive-base.component";
+import { Component, OnInit, signal, Signal } from "@angular/core";
+import { Parameters, ReactiveBaseComponent } from "../../reactive-base.component";
+import { VariableStore } from "src/app/reactive-templates/stores/variable-store";
+import { CommonModule } from "@angular/common";
+import { TemplatePipesModule } from "src/app/shared/components/template/pipes";
 
-interface ITextParams {
-  style: string | null;
-  textAlign: string | null;
-  type: string;
-}
+const parameters: Parameters = {
+  style: { name: "style", value: "" },
+  textAlign: { name: "text_align", value: null as string },
+  type: { name: "type", value: "marked" },
+};
 
 @Component({
-  selector: "oab-tmpl-text",
+  selector: "oab-text",
   templateUrl: "./text.component.html",
   // styleUrls: ["../tmpl-components-common.scss", "./text.component.scss"],
   styleUrls: ["./text.component.scss"],
+  standalone: true,
+  imports: [
+    CommonModule,
+    TemplatePipesModule, // todo: make all pipes standalone
+  ],
 })
 export class TextComponent extends ReactiveBaseComponent implements OnInit {
-  // params: Partial<ITextParams> = {};
-  // hasTextValue: boolean;
+  public hasTextValue: Signal<boolean>;
 
-  ngOnInit() {
-    // this.getParams();
+  constructor(variableStore: VariableStore) {
+    super(variableStore, parameters);
   }
 
-  // getParams() {
-  //   this.hasTextValue = !["undefined", "NaN", "null", '""'].includes(this._row.value as string);
-  //   this.params.textAlign = getStringParamFromTemplateRow(this._row, "text_align", null);
-  //   this.params.type = this._row.parameter_list?.style?.includes("numbered")
-  //     ? "numbered"
-  //     : "marked";
-  //   this.params.style = getStringParamFromTemplateRow(this._row, "style", null);
-  // }
+  ngOnInit(): void {
+    super.ngOnInit();
+
+    this.hasTextValue = signal(
+      !["undefined", "NaN", "null", '""'].includes(this.row().value as string)
+    );
+  }
 }
--- src/app/reactive-templates/reactive-components/reactive-base.component.ts
+++ src/app/reactive-templates/reactive-components/reactive-base.component.ts
@@ -1,38 +1,65 @@
-import { Component, input, OnInit, Signal } from "@angular/core";
+import { Component, input, OnInit, Optional, signal, Signal } from "@angular/core";
 import { FlowTypes } from "src/app/shared/model";
 import { VariableStore } from "../stores/variable-store";
 import { AppDataEvaluator } from "packages/shared/src/models/appDataEvaluator/appDataEvaluator";
+
+export class Parameter<T> {
+  name: string;
+  value: T;
+}
+
+export class Parameters {
+  [key: string]: Parameter<any>;
+}
 
 @Component({
   selector: "oab-base",
   template: ``,
 })
-export class ReactiveBaseComponent implements OnInit {
+export abstract class ReactiveBaseComponent implements OnInit {
   public row = input.required<FlowTypes.TemplateRow>();
 
   public name: string;
   public value: Signal<any>;
   public condition: Signal<boolean>;
-  public parameters;
+  public parameters: any = {};
   public actions;
 
   private evaluator = new AppDataEvaluator();
   private dependantVariables: string[];
 
-  constructor(private variableStore: VariableStore) {}
+  constructor(
+    @Optional() private variableStore: VariableStore,
+    @Optional() private params: Parameters
+  ) {}
 
   ngOnInit(): void {
     this.name = this.row()._nested_name;
     this.value = this.variableStore.asSignal(this.row()._nested_name);
 
     // Initialize the evaluator with a context
+    this.setParameters(this.params);
     this.setDependencies();
     this.watchDependencies();
+
+    // set default value
 
     // todo: Add listeners for condition dependencies
     //       reevaluate
     // todo: Add listeners for parameter dependencies
     //       reevaluate
+  }
+
+  private setParameters(parameters: Parameters) {
+    const rowParams = this.row().parameter_list;
+
+    if (!rowParams || !rowParams.length) return;
+
+    Object.keys(parameters).forEach((key) => {
+      const param = parameters[key];
+
+      this.parameters[key] = signal(rowParams[param.name] || param.value); // todo: cast to correct type
+    });
   }
 
   private setDependencies() {
@@ -51,10 +78,10 @@ export class ReactiveBaseComponent implements OnInit {
     this.dependantVariables.forEach((fieldName) => {
       this.variableStore.watch(fieldName).subscribe((value) => {
         // Update the value based on the fieldName
-        this.evaluator.setExecutionContext(this.getExecutionContext());
-        const rowValue = this.evaluator.evaluate(value);
+        //this.evaluator.setExecutionContext(this.getExecutionContext());
+        //const rowValue = this.evaluator.evaluate(value);
         // todo: dynamically evaluate row value;
-        this.variableStore.set(this.name, rowValue);
+        //this.variableStore.set(this.name, rowValue);
       });
     });
   }
