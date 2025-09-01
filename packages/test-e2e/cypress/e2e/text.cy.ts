596d4c60dcb5a87ef72284114e544cffd33cb45b
Johnny McQuade <jfmcquade@hotmail.com>
2025-09-01 14:59:54 +0100

    chore: test labeling

--- packages/test-e2e/cypress/e2e/text.cy.ts
+++ packages/test-e2e/cypress/e2e/text.cy.ts
@@ -3,11 +3,11 @@ describe("text component", () => {
     cy.visit("/template/test_text");
   });
 
-  it("displays values correctly", () => {
+  it("displays plain text values correctly", () => {
     cy.getDataTest("plainText").contains("p", "Plain text value");
   });
 
-  it("interprets markdown", () => {
+  it("interprets markdown correctly", () => {
     cy.getDataTest("titleText").contains("h1", "h1");
     cy.getDataTest("titleText").contains("h2", "h2");
     cy.getDataTest("titleText").contains("h3", "h3");
