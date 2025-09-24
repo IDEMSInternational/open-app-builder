3fea22eb24220418744a94307f99d9c2908328bf
chrismclarke <chris.m.clarke@live.co.uk>
2025-09-24 09:53:27 -0700

    refactor: gdrive models and caching
    Remove previous usage of md5Checksums (not compatible with google-native filetypes like sheets and docs), add validation for api metadata, and reduce api data retrieved to generate contents

--- packages/@idemsInternational/gdrive-tools/src/models/index.ts
+++ packages/@idemsInternational/gdrive-tools/src/models/index.ts
@@ -1,13 +1,28 @@
+import * as z from "zod";
+import {  getRelativeLocalPath } from "../utils";
+
+/** Zod schema used to validate and transform api file entry metadata */
+export const GDRIVE_FILE_ENTRY_SCHEMA = z
+  .object({
+    id: z.string(),
+    name: z.string(),
+    mimeType: z.string(),
+    modifiedTime: z.string(),
+    folderPath: z.string(),
+    lastModifyingUser: z.object({ displayName: z.string() }),
+    fileExtension: z.string().optional()
+  })
+  .transform((data) => ({
+    ...data,
+    // Assign additional relativePath to entires
+    relativePath: getRelativeLocalPath(data),
+  }));
+
+/** Zod schema used to validate array of api file entry metadata */
+export const GDRIVE_FILE_ENTRY_ARRAY_SCHEMA = z.array(GDRIVE_FILE_ENTRY_SCHEMA);
+
 /** File entry format from gdrive-downloader */
-export interface IGdriveEntry {
-  id: string;
-  name: string;
-  mimeType: string;
-  modifiedTime: string;
-  folderPath: string;
-  // only appears on xlsx uploaded files (not gsheet)
-  fullFileExtension?: string;
-  fileExtension?: string;
-  md5Checksum?: string;
-  size?: string;
-}
+export type IGdriveEntry = z.infer<typeof GDRIVE_FILE_ENTRY_SCHEMA>;
+
+
+
