ae411da239b2cf1627a3f86d5695ba2d0850728f
Johnny McQuade <jfmcquade@hotmail.com>
2025-09-25 12:31:10 +0100

    chore(wip): expose 'native_google' variant for google sign in button

--- src/app/shared/components/template/components/button-google-sign-in/button-google-sign-in.component.ts
+++ src/app/shared/components/template/components/button-google-sign-in/button-google-sign-in.component.ts
@@ -1,12 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, computed } from "@angular/core";
 import { TemplateBaseComponent } from "../base";
 import { AuthService } from "src/app/shared/services/auth/auth.service";
+import { FlowTypes } from "packages/data-models";
+import { getStringParamFromTemplateRow } from "src/app/shared/utils";
+
+interface IButtonGoogleSignInComponentParams {
+  variant: null | "native_google";
+}
+
 @Component({
   selector: "tmpl-button-google-sign-in",
   templateUrl: "./button-google-sign-in.component.html",
   styleUrls: ["./button-google-sign-in.component.scss"],
 })
 export class TmplButtonGoogleSignInComponent extends TemplateBaseComponent {
+  params = computed(() => this.getParams(this.parameterList()));
   // The button text is set as row value directly in the HTML template
 
   constructor(private authService: AuthService) {
@@ -16,5 +24,17 @@ export class TmplButtonGoogleSignInComponent extends TemplateBaseComponent {
   public async handleClick() {
     await this.authService.provider.signIn("google.com");
     this.triggerActions("click");
+  }
+
+  private getParams(
+    authorParams: FlowTypes.TemplateRow["parameter_list"]
+  ): IButtonGoogleSignInComponentParams {
+    return {
+      variant: getStringParamFromTemplateRow(
+        this._row,
+        "variant",
+        null
+      ) as IButtonGoogleSignInComponentParams["variant"],
+    };
   }
 }
