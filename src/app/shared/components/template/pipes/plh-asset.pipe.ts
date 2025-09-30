b222bd4c62d9103ca96ef589cf0140c1b47b6f8e
chrismclarke <chris.m.clarke@live.co.uk>
2025-09-30 12:17:25 -0700

    refactor: translated assets signal

--- src/app/shared/components/template/pipes/plh-asset.pipe.ts
+++ src/app/shared/components/template/pipes/plh-asset.pipe.ts
@@ -1,6 +1,14 @@
 import { Pipe, PipeTransform } from "@angular/core";
 import { TemplateAssetService } from "../services/template-asset.service";
 /**
+ * @deprecated 2025-09
+ *
+ * Prefer to use service directly which will return a signal, e.g.
+ * ```ts
+ * private templateAssetService = inject(TemplateAssetService)
+ * public imgSrc = templateAssetService.translatedAssetSignal(this.row.value)
+ * ```
+ *
  * Retrieve an asset for the current language from nested data asset folder,
  * returning original if not available
  * Usage @example <img src="images/my_icon.svg | plhAsset" />
