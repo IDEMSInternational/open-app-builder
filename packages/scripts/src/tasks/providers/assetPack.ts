52648f922555e8d90e54cdcc845989250a9adad3
Johnny McQuade <jfmcquade@hotmail.com>
2025-10-27 13:16:55 +0000

    wip: asset pack generate workflow

--- packages/scripts/src/tasks/providers/assetPack.ts
+++ packages/scripts/src/tasks/providers/assetPack.ts
@@ -1,18 +1,34 @@
-import { Logger } from "../../utils";
-
-interface IAssetPackOptions {
-  // TODO: Add options as needed
-}
+import { generateFolderFlatMap, Logger, logOutput } from "../../utils";
+import { WorkflowRunner } from "../../commands/workflow/run";
+import path from "path";
+import { existsSync } from "fs-extra";
 
 /**
- * Generate asset pack configuration
+ * Generate asset pack
  */
-const generate = async (options: IAssetPackOptions) => {
-  Logger.warning({
+const generate = async (options: { folderPath: string }) => {
+  // Convert relative path to absolute path
+  const absolutePath = path.resolve(
+    WorkflowRunner.config.app_data.output_path,
+    "assets",
+    options.folderPath
+  );
+
+  logOutput({
     msg1: "Asset pack generation",
-    msg2: "TODO: Implement generation logic",
+    msg2: `Folder path: ${absolutePath}`,
   });
 
+  console.log("Asset pack generation");
+  console.log(`Folder path: ${absolutePath}`);
+
+  if (!existsSync(absolutePath)) {
+    Logger.error({ msg1: `Folder path does not exist: ${absolutePath}` });
+    return Promise.reject(new Error(`Folder path does not exist: ${absolutePath}`));
+  }
+
+  const assetPack = generateFolderFlatMap(absolutePath);
+  console.log(assetPack);
   // TODO: Implement asset pack generation logic
   return Promise.resolve();
 };
