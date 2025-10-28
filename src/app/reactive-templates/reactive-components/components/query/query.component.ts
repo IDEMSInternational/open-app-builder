8cfc90ac11bc21d5c67e7d2b801e12e0f839ffb2
Mark Harmon <mark.harmon@outlook.com>
2025-10-28 14:26:40 +0000

    Add reactive query components

---
+++ src/app/reactive-templates/reactive-components/components/query/query.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject } from "@angular/core";
+import { DynamicDataService } from "src/app/shared/services/dynamic-data/dynamic-data.service";
+import { defineParameters, Parameter } from "../../parameters";
+import { ROW_PARAMETERS, RowBaseComponent } from "../../row-base.component";
+import json5 from "json5";
+import { firstValueFrom } from "rxjs";
+
+const parameters = () =>
+  defineParameters({
+    dataList: new Parameter("data_list", ""),
+  });
+
+@Component({
+  selector: "oab-query",
+  templateUrl: "./query.component.html",
+  styleUrls: ["./query.component.scss"],
+  providers: [{ provide: ROW_PARAMETERS, useFactory: parameters }],
+})
+export class QueryComponent extends RowBaseComponent<ReturnType<typeof parameters>> {
+  private dynamicDataService = inject(DynamicDataService);
+
+  protected async computeStoredValue(value) {
+    const queryString = `{${value as string}}`;
+    const mangoQuery = value ? json5.parse(queryString) : {};
+    const query = this.dynamicDataService.query$<any>(
+      "data_list",
+      this.params.dataList.value(),
+      mangoQuery
+    );
+    const result = await firstValueFrom(query);
+
+    return result;
+  }
+}
