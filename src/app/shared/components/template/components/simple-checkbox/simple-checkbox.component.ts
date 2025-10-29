bf57a7fc7f574e72557ad4f0aee3c564cba50239
Johnny McQuade <jfmcquade@hotmail.com>
2025-10-29 17:05:09 +0000

    chore: refactor checkbox component to use dyanmic authored params

--- src/app/shared/components/template/components/simple-checkbox/simple-checkbox.component.ts
+++ src/app/shared/components/template/components/simple-checkbox/simple-checkbox.component.ts
@@ -1,32 +1,41 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, computed } from "@angular/core";
+import { FlowTypes } from "packages/data-models";
 import { TemplateBaseComponent } from "../base";
 import { getBooleanParamFromTemplateRow, getStringParamFromTemplateRow } from "../../../../utils";
+
+interface ISimpleCheckboxParams {
+  /* TEMPLATE PARAMETER: "reverse". Reverses the checkbox and label positions */
+  reverse?: boolean;
+  /* TEMPLATE PARAMETER: "align". Sets the alignment of the checkbox */
+  position?: string;
+  /* TEMPLATE PARAMETER: "label_text". The text label for the checkbox */
+  label_text?: string | null;
+  /* TEMPLATE PARAMETER: "style". Additional style classes */
+  style?: string;
+}
 
 @Component({
   selector: "plh-simple-checkbox",
   templateUrl: "./simple-checkbox.component.html",
   styleUrls: ["./simple-checkbox.component.scss"],
 })
-export class TmplSimpleCheckboxComponent extends TemplateBaseComponent implements OnInit {
-  position: string;
-  reverse: boolean;
-  label_text: string | null;
-  style: string;
+export class TmplSimpleCheckboxComponent extends TemplateBaseComponent {
+  params = computed(() => this.getParams(this.parameterList()));
+
   constructor() {
     super();
   }
 
-  ngOnInit() {
-    this.getParams();
-  }
   public async handleChange(isChecked: boolean) {
     await this.setValue(isChecked);
   }
 
-  getParams() {
-    this.reverse = getBooleanParamFromTemplateRow(this._row, "reverse", false);
-    this.position = getStringParamFromTemplateRow(this._row, "align", "center");
-    this.label_text = getStringParamFromTemplateRow(this._row, "label_text", null);
-    this.style = getStringParamFromTemplateRow(this._row, "style", "");
+  private getParams(authorParams: FlowTypes.TemplateRow["parameter_list"]): ISimpleCheckboxParams {
+    return {
+      reverse: getBooleanParamFromTemplateRow(this._row, "reverse", false),
+      position: getStringParamFromTemplateRow(this._row, "align", "center"),
+      label_text: getStringParamFromTemplateRow(this._row, "label_text", null),
+      style: getStringParamFromTemplateRow(this._row, "style", ""),
+    };
   }
 }
