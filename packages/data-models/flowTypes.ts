1fa42851ba406e852c006513885d03ee174b7b09
chrismclarke <chris.m.clarke@live.co.uk>
2025-11-03 13:36:41 -0800

    feat: update _source name

--- packages/data-models/flowTypes.ts
+++ packages/data-models/flowTypes.ts
@@ -54,7 +54,11 @@ export namespace FlowTypes {
     };
     /** flow source metdata */
     _source?: {
+      /** Name of input source, e.g. folder name */
+      name?: string;
+      /** Nested path to flow data, e.g. sheet path */
       path?: string;
+      /** Remote url of source */
       url?: string;
     };
     process_on_start?: number; // priority order to process template variable setters on startup
