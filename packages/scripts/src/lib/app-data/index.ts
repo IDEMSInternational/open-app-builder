e4c5e398b5ec95486f8cc5e5356ec958c770d782
chrismclarke <chris.m.clarke@live.co.uk>
2025-11-04 10:20:34 -0800

    refactor: app-data commands

--- packages/scripts/src/lib/app-data/index.ts
+++ packages/scripts/src/lib/app-data/index.ts
@@ -1,13 +1,4 @@
-#!/usr/bin/env node
-import { Command } from "commander";
-
-import convertCmd from "./convert";
-import postProcessCmd from "./postProcess";
-
-const program = new Command("app-data").description("Manage app data");
-
-/** add sub-commands from child folders */
-program.addCommand(convertCmd);
-program.addCommand(postProcessCmd);
-
-export default program;
+export * from "./convert";
+export * from "./optimise";
+export * from "./postProcess";
+export * from "./report/report";
