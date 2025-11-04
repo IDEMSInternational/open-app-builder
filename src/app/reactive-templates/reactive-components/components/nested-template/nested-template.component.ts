b076824ea9d5f101dccbf5172b0ad58b6d73c232
Mark Harmon <mark.harmon@outlook.com>
2025-11-04 12:49:59 +0000

    Rename SetTemplateVariable method

--- src/app/reactive-templates/reactive-components/components/nested-template/nested-template.component.ts
+++ src/app/reactive-templates/reactive-components/components/nested-template/nested-template.component.ts
@@ -3,14 +3,13 @@ import { ROW_PARAMETERS, RowBaseComponent } from "../../row-base.component";
 import { ReactiveTemplateComponent } from "src/app/reactive-templates/reactive-template/reactive-template.component";
 import { FlowTypes } from "packages/data-models";
 import { Subscription } from "rxjs";
-import { RowListComponent } from "../../row-list.component";
 
 @Component({
   selector: "oab-nested-template",
   templateUrl: "./nested-template.component.html",
   styleUrls: ["./nested-template.component.scss"],
   standalone: true,
-  imports: [forwardRef(() => ReactiveTemplateComponent), RowListComponent],
+  imports: [forwardRef(() => ReactiveTemplateComponent)],
   providers: [{ provide: ROW_PARAMETERS, useValue: null }],
 })
 export class NestedTemplateComponent extends RowBaseComponent<null> implements OnInit, OnDestroy {
@@ -40,7 +39,7 @@ export class NestedTemplateComponent extends RowBaseComponent<null> implements O
 
   private onTemplateInitialised() {
     for (const row of this.row().rows) {
-      this.setTemplateVariable(row);
+      this.setChildVariable(row);
     }
   }
 
@@ -52,13 +51,13 @@ export class NestedTemplateComponent extends RowBaseComponent<null> implements O
       let sub = this.variableStore
         .watchMultiple(this.evaluationService.getDependencies(row.value, this.namespace()))
         .subscribe(() => {
-          this.setTemplateVariable(row);
+          this.setChildVariable(row);
         });
       this.childDependencySubscriptions.push(sub);
     }
   }
 
-  private setTemplateVariable(row: FlowTypes.TemplateRow) {
+  private setChildVariable(row: FlowTypes.TemplateRow) {
     const dependencies = this.evaluationService.getDependencies(row.value, this.namespace());
     const isStaticValue = dependencies && dependencies.length === 0;
 
