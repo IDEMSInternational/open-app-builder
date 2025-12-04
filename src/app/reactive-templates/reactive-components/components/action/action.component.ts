a2fba545c052ff8e73733d600e93131f0ae8673b
Mark Harmon <mark.harmon@outlook.com>
2025-12-04 11:02:29 +0000

    Add action component

---
+++ src/app/reactive-templates/reactive-components/components/action/action.component.ts
@@ -0,0 +1,76 @@
+import {
+  Component,
+  ComponentRef,
+  createComponent,
+  EnvironmentInjector,
+  inject,
+  OnDestroy,
+  OnInit,
+} from "@angular/core";
+import { defineParameters, Parameter } from "../../parameters";
+import { ROW_PARAMETERS, RowBaseComponent } from "../../row-base.component";
+import { ActionRegistry, IAction } from "src/app/reactive-templates/services/action.registry";
+import { REACTIVE_COMPONENT_MAP } from "..";
+
+const parameters = () =>
+  defineParameters({
+    index: new Parameter<string>("index", null),
+  });
+
+@Component({
+  selector: "oab-action",
+  templateUrl: "./action.component.html",
+  styleUrls: ["./action.component.scss"],
+  providers: [{ provide: ROW_PARAMETERS, useFactory: parameters }],
+})
+export class ActionComponent
+  extends RowBaseComponent<ReturnType<typeof parameters>>
+  implements OnInit, OnDestroy, IAction
+{
+  private actionRegistry = inject(ActionRegistry);
+  private injector = inject(EnvironmentInjector);
+
+  private readonly actions = new Map<string, IAction>();
+  private readonly componentRefs: ComponentRef<any>[] = [];
+
+  public execute(): void {
+    for (const action of this.actions.values()) {
+      action.execute();
+    }
+  }
+
+  public ngOnInit(): void {
+    super.ngOnInit();
+
+    for (const row of this.row().rows) {
+      const componentType = (REACTIVE_COMPONENT_MAP as any)[row.type];
+
+      if (componentType) {
+        const componentRef = createComponent(componentType, {
+          environmentInjector: this.injector,
+        });
+        componentRef.setInput("row", row);
+        componentRef.setInput("namespace", this.namespace());
+        componentRef.changeDetectorRef.detectChanges();
+
+        this.componentRefs.push(componentRef);
+
+        const instance = componentRef.instance;
+        if (this.isAction(instance)) {
+          this.actions.set(instance.name(), instance);
+        }
+      }
+    }
+  }
+
+  private isAction(instance: any): instance is IAction {
+    return typeof instance.execute === "function";
+  }
+
+  public ngOnDestroy(): void {
+    super.ngOnDestroy();
+
+    this.actionRegistry.unregister(this.name());
+    this.componentRefs.forEach((ref) => ref.destroy());
+  }
+}
---
+++ src/app/reactive-templates/services/action.registry.ts
@@ -0,0 +1,45 @@
+import { Injectable } from "@angular/core";
+
+export interface IAction {
+  name(): string; // name: Signal<string>;
+  execute(): void;
+}
+
+@Injectable({
+  providedIn: "root",
+})
+export class ActionRegistry {
+  private readonly actions = new Map<string, IAction>();
+
+  register(action: IAction) {
+    const name = action.name();
+
+    if (!name) {
+      throw new Error("ActionRegistry.register: action.name() is empty");
+    }
+
+    // Last write wins; warn if replacing a different instance
+    const existing = this.actions.get(name);
+    if (existing && existing !== action) {
+      console.warn(`ActionRegistry.register: replacing existing action '${name}'`);
+    }
+
+    this.actions.set(name, action);
+  }
+
+  get(name: string): IAction {
+    const action = this.actions.get(name);
+    if (!action) {
+      throw new Error(`RowRegistry.get: no action registered with name '${name}'`);
+    }
+    return action;
+  }
+
+  has(name: string): boolean {
+    return this.actions.has(name);
+  }
+
+  unregister(name: string) {
+    this.actions.delete(name);
+  }
+}
