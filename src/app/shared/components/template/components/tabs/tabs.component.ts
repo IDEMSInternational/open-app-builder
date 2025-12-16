01c94acf35682b6032a6dd5105f863304bdc5594
Johnny McQuade <jfmcquade@hotmail.com>
2025-12-16 12:52:28 +0000

    chore: implement and document simpler workaround

--- src/app/shared/components/template/components/tabs/tabs.component.ts
+++ src/app/shared/components/template/components/tabs/tabs.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  computed,
-  signal,
-  afterNextRender,
-  ChangeDetectionStrategy,
-} from "@angular/core";
+import { ChangeDetectionStrategy, Component, computed } from "@angular/core";
 import { defineAuthorParameterSchema, TemplateBaseComponentWithParams } from "../base";
 
 const AuthorSchema = defineAuthorParameterSchema((coerce) => ({
@@ -42,36 +36,4 @@ export class TmplTabsComponent extends TemplateBaseComponentWithParams(AuthorSch
         name: tabRow.name,
       }));
   });
-
-  // ============================================================================
-  // WORKAROUND: MatTabGroup initialization timing issue
-  // ============================================================================
-  // TODO: Workaround added in https://github.com/IDEMSInternational/open-app-builder/pull/3248
-  //       Consider removing this workaround when core Angular and/or Angular Material/related
-  //       libraries are updated. See https://github.com/IDEMSInternational/open-app-builder/issues/3234
-  //       Issue: MatTabGroup can fail to initialize properly if created before DOM elements
-  //       are available, attempting to add handlers to non-existent elements.
-  //       See: https://github.com/IDEMSInternational/open-app-builder/issues/3256
-  //
-  // To remove:
-  // 1. Delete the viewReady signal and shouldRenderTabs computed below
-  // 2. Remove the afterNextRender call from constructor
-  // 3. Remove the @if (shouldRenderTabs()) wrapper in tabs.component.html (replace with @if (tabRows().length > 0))
-  // 4. Remove [disableRipple]="true" from mat-tab-group in tabs.component.html
-  // 5. Test according to instructions in https://github.com/IDEMSInternational/open-app-builder/issues/3256
-  // ============================================================================
-  private viewReady = signal(false);
-
-  public shouldRenderTabs = computed(() => {
-    return this.viewReady() && this.tabRows().length > 0;
-  });
-
-  constructor() {
-    super();
-    // Defer rendering until after Angular's first render cycle completes
-    afterNextRender(() => this.viewReady.set(true));
-  }
-  // ============================================================================
-  // END WORKAROUND
-  // ============================================================================
 }
