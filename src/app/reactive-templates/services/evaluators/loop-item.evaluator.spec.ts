efbee1d496676d2aa6d2d286c05382a960ea23db
Mark Harmon <mark.harmon@outlook.com>
2026-01-08 18:43:22 +0000

    Fix issue with loop item evaluator mock

--- src/app/reactive-templates/services/evaluators/loop-item.evaluator.spec.ts
+++ src/app/reactive-templates/services/evaluators/loop-item.evaluator.spec.ts
@@ -126,6 +126,13 @@ describe("LoopItemEvaluator", () => {
         value: signal(() => undefined),
         setExpression: () => {},
         params: { index: new Parameter("index", "name") },
+        row: () => ({
+          name: "mockRow",
+          value: "",
+          type: "loop",
+          rows: [],
+          _nested_name: "mockRow",
+        }),
       });
       mockVariableStore.get.and.returnValue([
         { name: "Alpha", value: 10 },
@@ -219,6 +226,13 @@ describe("LoopItemEvaluator", () => {
         value: signal(() => undefined),
         setExpression: () => {},
         params: { index: new Parameter("index", null) },
+        row: () => ({
+          name: "mockRow",
+          value: "",
+          type: "loop",
+          rows: [],
+          _nested_name: "mockRow",
+        }),
       });
       mockVariableStore.get.and.returnValue([10, 20, 30]);
     });
