a84ac129c72c1411fd7c47551e9b03c223410d33
Johnny McQuade <jfmcquade@hotmail.com>
2026-01-14 15:13:59 +0000

    feat: add allow_fullscreen param to pdf component

--- src/app/shared/components/template/components/pdf/pdf.component.ts
+++ src/app/shared/components/template/components/pdf/pdf.component.ts
@@ -3,6 +3,7 @@ import { TemplateBaseComponent } from "../base";
 import { pdfDefaultOptions } from "ngx-extended-pdf-viewer";
 import { PDFViewerService } from "./pdf.service";
 import { toSignal } from "@angular/core/rxjs-interop";
+import { booleanStringToBoolean } from "src/app/shared/utils";
 
 interface IAuthorParams {
   starting_page?: string;
@@ -10,12 +11,15 @@ interface IAuthorParams {
   compatibility_error_message?: string;
   /** Open external text. Default "Open with..." */
   open_external_text?: string;
+  /** Show full screen button. Default false. Can be boolean or string "true"/"false" */
+  allow_fullscreen?: boolean | string;
 }
 
 const DEFAULT_PARAMS: IAuthorParams = {
   compatibility_error_message:
     "Embedded PDFs are not supported in this browser, please use an up-to-date version of Google Chrome to view or open in external app",
   open_external_text: "Open with...",
+  allow_fullscreen: true,
 };
 
 @Component({
@@ -30,12 +34,16 @@ export class TmplPdfComponent extends TemplateBaseComponent implements AfterView
   public pdfSrc = computed(() => this.value());
 
   public params = computed(() => {
-    const merged: IAuthorParams = { ...DEFAULT_PARAMS, ...this.params };
-    const { compatibility_error_message, starting_page, open_external_text } = merged;
+    const merged: IAuthorParams = { ...DEFAULT_PARAMS, ...this.parameterList() };
+    const { compatibility_error_message, starting_page, open_external_text, allow_fullscreen } =
+      merged;
+
     return {
-      startingPage: Number(starting_page || 1),
+      // Default is true, so only false or "false" should disable
+      allowFullScreen: booleanStringToBoolean(allow_fullscreen) !== false,
       compatErrorMessage: compatibility_error_message,
       openExternalText: open_external_text,
+      startingPage: Number(starting_page || 1),
     };
   });
 
