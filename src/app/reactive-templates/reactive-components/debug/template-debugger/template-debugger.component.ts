d099b83589422df0110af793f817347f5892f741
Mark Harmon <mark.harmon@outlook.com>
2026-02-12 16:08:03 +0000

    Add variable store debugger

--- src/app/reactive-templates/reactive-components/debug/template-debugger/template-debugger.component.ts
+++ src/app/reactive-templates/reactive-components/debug/template-debugger/template-debugger.component.ts
@@ -1,16 +1,27 @@
-import { Component, computed, inject, input } from "@angular/core";
+import { JsonPipe } from "@angular/common";
+import { Component, computed, inject, input, signal } from "@angular/core";
 import { DebugService } from "src/app/reactive-templates/services/debug.service";
+import { VariableStore } from "src/app/reactive-templates/stores/variable-store";
 import { FlowTypes } from "src/app/shared/components/template/models";
 
 @Component({
   selector: "oab-template-debugger",
   templateUrl: "./template-debugger.component.html",
   styleUrls: ["./template-debugger.component.scss"],
+  imports: [JsonPipe],
 })
 export class TemplateDebuggerComponent {
   public debug = inject(DebugService);
+  public variableStore = inject(VariableStore);
 
   public templateName = input.required<string>();
   public template = input<FlowTypes.Template | undefined>(undefined);
   public templateRows = computed(() => this.template()?.rows || []);
+
+  private _showVariableStore = signal(false);
+  public showVariableStore = this._showVariableStore.asReadonly();
+
+  public toggleVariableStore() {
+    this._showVariableStore.update((value) => !value);
+  }
 }
