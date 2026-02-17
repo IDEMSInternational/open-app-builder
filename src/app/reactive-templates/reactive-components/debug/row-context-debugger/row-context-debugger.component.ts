2368374e569bc2647b858900dadb197e953e870e
Mark Harmon <mark.harmon@outlook.com>
2026-02-17 14:18:01 +0000

    Add live context updates

--- src/app/reactive-templates/reactive-components/debug/row-context-debugger/row-context-debugger.component.ts
+++ src/app/reactive-templates/reactive-components/debug/row-context-debugger/row-context-debugger.component.ts
@@ -1,6 +1,7 @@
-import { Component, computed, inject, input } from "@angular/core";
+import { Component, computed, effect, inject, input, signal } from "@angular/core";
 import { ContextCreatorService } from "src/app/reactive-templates/services/context-creator.service";
 import { EvaluationService } from "src/app/reactive-templates/services/evaluation.service";
+import { VariableStore } from "src/app/reactive-templates/stores/variable-store";
 import { RowBaseComponent } from "../../row-base.component";
 import { JsonPipe } from "@angular/common";
 import { DebuggerBaseComponent } from "../debugger-base/debugger-base.component";
@@ -18,13 +19,28 @@ import {
 export class RowContextDebuggerComponent {
   private evaluationService = inject(EvaluationService);
   private contextCreator = inject(ContextCreatorService);
+  private variableStore = inject(VariableStore);
+  private dependencyChangeVersion = signal(0);
 
   public row = input.required<RowBaseComponent<any>>();
+
   public dependencies = computed(() => {
     const row = this.row();
     return row ? this.evaluationService.getDependencies(row.expression(), row.namespace()) : [];
   });
+
   public context = computed(() => {
+    this.dependencyChangeVersion();
     return this.contextCreator.createContext(this.dependencies());
   });
+
+  public constructor() {
+    effect((onCleanup) => {
+      const subscription = this.variableStore.watchMultiple(this.dependencies()).subscribe(() => {
+        this.dependencyChangeVersion.update((value) => value + 1);
+      });
+
+      onCleanup(() => subscription.unsubscribe());
+    });
+  }
 }
