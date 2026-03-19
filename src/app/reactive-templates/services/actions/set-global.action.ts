963be0c604bb4d7d170c4749151b876375523fb4
Mark Harmon <mark.harmon@outlook.com>
2026-03-19 15:01:20 +0000

    Update set-global.action.ts

--- src/app/reactive-templates/services/actions/set-global.action.ts
+++ src/app/reactive-templates/services/actions/set-global.action.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { TemplateActionRegistry } from "src/app/shared/components/template/services/instance/template-action.registry";
-import { EvaluationService } from "../evaluation.service";
 import { VariableStore } from "../../stores/variable-store";
 
 @Injectable({
@@ -9,7 +8,6 @@ import { VariableStore } from "../../stores/variable-store";
 export class SetGlobalActionService {
   constructor(
     private templateActionRegistry: TemplateActionRegistry,
-    private evaluationService: EvaluationService,
     private variableStore: VariableStore
   ) {
     this.templateActionRegistry.register({
