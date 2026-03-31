85fd983f11065d86a5421f6eeaf9f132e727ada5
Mark Harmon <mark.harmon@outlook.com>
2026-03-31 14:49:41 +0100

    Fix loop to update values

--- packages/test-e2e/cypress/e2e/components/update.cy.ts
+++ packages/test-e2e/cypress/e2e/components/update.cy.ts
@@ -7,7 +7,7 @@ describe("update component", () => {
     cy.clearIndexedDB();
   });
 
-  it("insert a record", () => {
+  it("inserts a record", () => {
     const guid = crypto.randomUUID();
 
     cy.getDataTest("recordId").find("input").clear().type(`id_${guid}`).blur();
@@ -22,6 +22,37 @@ describe("update component", () => {
       .should("exist")
       .should("contain", `ID: id_${guid}`)
       .and("contain", `Name: name ${guid}`)
+      .and("contain", `Value: value ${guid}`)
+      .and("contain", `Quantity: 5`);
+  });
+
+  it("updates a record", () => {
+    const guid = crypto.randomUUID();
+
+    cy.getDataTest("recordId").find("input").clear().type(`id_${guid}`).blur();
+    cy.getDataTest("recordName").find("input").clear().type(`name ${guid}`).blur();
+    cy.getDataTest("recordValue").find("input").clear().type(`value ${guid}`).blur();
+    cy.getDataTest("recordQuantity").find("input").clear().type("5").blur();
+
+    cy.getDataTest("insertButton").click();
+    cy.getDataTest("refreshButton").click();
+
+    cy.getDataTest(`itemLoop.id_${guid}.checkInsertedValue`)
+      .should("exist")
+      .should("contain", `ID: id_${guid}`)
+      .and("contain", `Name: name ${guid}`)
+      .and("contain", `Value: value ${guid}`)
+      .and("contain", `Quantity: 5`);
+
+    cy.getDataTest("recordName").find("input").clear().type(`name ${guid} updated`).blur();
+
+    cy.getDataTest("insertButton").click();
+    cy.getDataTest("refreshButton").click();
+
+    cy.getDataTest(`itemLoop.id_${guid}.checkInsertedValue`)
+      .should("exist")
+      .should("contain", `ID: id_${guid}`)
+      .and("contain", `Name: name ${guid} updated`)
       .and("contain", `Value: value ${guid}`)
       .and("contain", `Quantity: 5`);
   });
