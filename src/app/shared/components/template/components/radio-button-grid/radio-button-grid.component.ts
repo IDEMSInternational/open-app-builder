1c505e92620241f22189604cfeb6782bcf9892f6
Johnny McQuade <jfmcquade@hotmail.com>
2026-07-15 12:04:20 +0100

    chore: refactor radio-button-grid to use zod pased params pattern

--- src/app/shared/components/template/components/radio-button-grid/radio-button-grid.component.ts
+++ src/app/shared/components/template/components/radio-button-grid/radio-button-grid.component.ts
@@ -1,35 +1,21 @@
-import { Component, Input } from "@angular/core";
-import { TemplateBaseComponent } from "../base";
-import { FlowTypes } from "../../models";
-import {
-  getAnswerListParamFromTemplateRow,
-  getStringParamFromTemplateRow,
-  IAnswerListItem,
-} from "src/app/shared/utils";
+import { Component, computed } from "@angular/core";
+import { defineAuthorParameterSchema, TemplateBaseComponentWithParams } from "../base";
+import { IAnswerListItem } from "src/app/shared/utils";
 
-interface IRadioButtonGridParams {
+const AuthorSchema = defineAuthorParameterSchema((coerce) => ({
   /** List of options presented as radio items */
-  answer_list: IAnswerListItem[];
-  /**
-   * Minimum item width, will increase to fit grid.
-   * Default '200px'
-   **/
-  item_width: string;
-  /**
-   * Maximum grid width, if specified will center items in available space.
-   * Default '100%'
-   **/
-  grid_width: string;
-  /**
-   * Spacing between grid items.
-   * Default '16px'
-   **/
-  grid_gap: string;
-  /* TEMPLATE PARAMETER: "variant". The style variant of the button_grid */
-  variant?: "default" | "circle-icon" | "flex";
-  /* TEMPLATE PARAMETER: "style". The "secondary" style sets the colour of the buttons */
-  style?: "secondary" | undefined;
-}
+  answer_list: coerce.objectArray<IAnswerListItem>([]),
+  /** Minimum item width, will increase to fit grid. Default '200px'. */
+  item_width: coerce.string("200px"),
+  /** Maximum grid width, if specified will center items in available space. Default '100%'. */
+  grid_width: coerce.string("100%"),
+  /** Spacing between grid items. Default '16px'. */
+  grid_gap: coerce.string("16px"),
+  /** The style variant of the button grid. Default 'default'. */
+  variant: coerce.allowedValues(["default", "circle-icon", "flex"], "default"),
+  /** The 'secondary' style sets the colour of the buttons. Default 'default'. */
+  style: coerce.allowedValues(["default", "secondary"], "default"),
+}));
 
 @Component({
   selector: "plh-radio-button-grid",
@@ -37,71 +23,27 @@ interface IRadioButtonGridParams {
   styleUrls: ["./radio-button-grid.component.scss"],
   standalone: false,
 })
-export class TmplRadioButtonGridComponent extends TemplateBaseComponent {
-  /**
-   * Computed item array from input parameters
-   * @ignore
-   */
-  public radioItems: Array<IAnswerListItem>;
+export class TmplRadioButtonGridComponent extends TemplateBaseComponentWithParams(AuthorSchema) {
+  /** Computed item array from author parameters */
+  public radioItems = computed(() => this.params().answerList as IAnswerListItem[]);
 
-  /**
-   * Computed grid style from input parameters
-   * @ignore
-   */
-  public gridStyle: Partial<CSSStyleDeclaration>;
-
-  /**
-   * Authoring parameters
-   */
-  public parameter_list: IRadioButtonGridParams;
-
-  @Input() set row(row: FlowTypes.TemplateRow) {
-    this._row = row;
-    this.setParams();
-  }
-
-  /**
-   * S
-   * @ignore
-   */
-  public async handleItemClick(item: IAnswerListItem) {
-    await this.setValue(item.name);
-  }
-
-  private setParams() {
-    this.parameter_list = this._row.parameter_list || ({} as any);
-    this.radioItems = getAnswerListParamFromTemplateRow(this._row, "answer_list", []);
-    this.gridStyle = this.generateGridStyle();
-    this.parameter_list.style = getStringParamFromTemplateRow(
-      this._row,
-      "style",
-      undefined
-    ) as IRadioButtonGridParams["style"];
-    this.parameter_list.variant = getStringParamFromTemplateRow(this._row, "variant", "default")
-      .split(",")
-      .join(" ") as IRadioButtonGridParams["variant"];
-  }
-
-  /**
-   * Create a list of styles to be passed into ngStyle
-   * https://thesoftwayfarecoder.com/dynamically-creating-css-classes-in-angular/
-   */
-  private generateGridStyle() {
-    const minItemWidth = this.parameter_list.item_width || "200px";
-    const maxGridWidth = this.parameter_list.grid_width || "100%";
-    const gridGap = this.parameter_list.grid_gap || "16px";
-
-    const style: Partial<CSSStyleDeclaration> = {
+  /** Computed grid style passed into ngStyle */
+  public gridStyle = computed<Partial<CSSStyleDeclaration>>(() => {
+    const { itemWidth, gridWidth, gridGap } = this.params();
+    return {
       // center grid with maximum width
-      maxWidth: maxGridWidth,
+      maxWidth: gridWidth,
       margin: "auto",
       // apply fixed gap between grid items
       gap: gridGap,
       // fit columns with target item width
-      gridTemplateColumns: `repeat(auto-fit, minmax(${minItemWidth}, 1fr))`,
+      gridTemplateColumns: `repeat(auto-fit, minmax(${itemWidth}, 1fr))`,
       // make all rows same height
       gridAutoRows: "1fr",
     };
-    return style;
+  });
+
+  public async handleItemClick(item: IAnswerListItem) {
+    await this.setValue(item.name);
   }
 }
