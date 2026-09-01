f0f204f78b050a43513f937b8606e6f3fc31f7e2
Mark Harmon <mark.harmon@outlook.com>
2026-09-01 12:57:48 +0100

    Fix failing test

--- src/app/shared/components/template/components/map/map.component.spec.ts
+++ src/app/shared/components/template/components/map/map.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { IonicModule } from "@ionic/angular";
 
+import { DynamicDataService } from "src/app/shared/services/dynamic-data/dynamic-data.service";
 import { MapComponent } from "./map.component";
 
 describe("MapComponent", () => {
@@ -11,10 +12,26 @@ describe("MapComponent", () => {
     TestBed.configureTestingModule({
       declarations: [MapComponent],
       imports: [IonicModule.forRoot()],
+      providers: [
+        {
+          provide: DynamicDataService,
+          useValue: {
+            snapshot: jasmine.createSpy("snapshot").and.resolveTo([]),
+            remove: jasmine.createSpy("remove").and.resolveTo(),
+            bulkUpsert: jasmine.createSpy("bulkUpsert").and.resolveTo(),
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MapComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput("row", {
+      _nested_name: "map_test",
+      name: "map_test",
+      type: "map",
+      parameter_list: {},
+    } as any);
     fixture.detectChanges();
   }));
 
